895e4fdf6906fe42c821a7b33bad461b4dc2ea73
Cindee Madison <cindee@berkeley.edu>
2009-07-16 18:55:25 +0000

    added setup files
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@11 ead46cd0-7350-4e37-8683-fc4c6f79bf00

---
+++ nipype/setup.py
@@ -0,0 +1,15 @@
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('nipype', parent_package, top_path)
+
+    # List all packages to be loaded here
+    config.add_subpackage('interfaces')
+    config.add_subpackage('pipeline')
+    config.add_subpackage('utils')
+    
+    # List all data directories to be loaded here
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
