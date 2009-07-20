693f8d5d368ad4581831dd39a89d1fb97309ecdd
Cindee Madison <cindee@berkeley.edu>
2009-07-20 20:51:27 +0000

    updated install of nifti
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@34 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/externals/pynifti/setup.py
+++ nipype/externals/pynifti/setup.py
@@ -1,6 +1,7 @@
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
-    config = Configuration('externals', parent_package, top_path)
+    config = Configuration('nifti', parent_package, top_path)
+
 
     return config
 
