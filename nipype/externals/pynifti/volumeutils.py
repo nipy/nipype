e7f7e826e2ac814db0f5704a3cecdc3e8356b993
Cindee Madison <cindee@berkeley.edu>
2009-07-20 22:06:24 +0000

    changed pynifti brifti python2.5 dependency line 668 in volumeutils
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@44 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/externals/pynifti/volumeutils.py
+++ nipype/externals/pynifti/volumeutils.py
@@ -665,7 +665,7 @@ def finite_range(arr):
     return mn, mx
 
 
-class UnsupportedDataType():
+class UnsupportedDataType(object):
     ''' Class to indicated data type not supported '''
     pass
 
