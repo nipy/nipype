89440f40b766eaabbabedf8da571b041d0eac670
Satrajit Ghosh <satra@mit.edu>
2009-07-25 03:12:36 +0000

    added spm config extraction script and option list
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@131 ead46cd0-7350-4e37-8683-fc4c6f79bf00

---
+++ junk/parse_spm_config.m
@@ -0,0 +1,38 @@
+function parse_spm_config
+
+conf = spm_config;
+sub_parse_config(spm_config)
+
+function sub_parse_config(conf,level)
+if nargin<2,
+    level = 1;
+end
+if ~isfield(conf,'tag')
+    level = level - 1;
+else
+    if ~strcmp(conf.type,'entry')
+        fprintf('|%s-%s:%s\n',char(repmat(' |',1,level-1)),conf.tag,conf.type);
+    else
+        fprintf('|%s-%s:%s\n',char(repmat(' |',1,level-1)),conf.tag,conf.type);
+    end        
+end
+if isfield(conf,'values'),
+    for i0=1:numel(conf.values),
+        if isstruct(conf.values{i0}),
+            sub_parse_config(conf.values{i0},level+1)
+        else
+            if ischar(conf.values{i0})
+                fprintf('|%s-[%s:%s]\n',char(repmat(' |',1,level)),conf.labels{i0},conf.values{i0});
+            else
+                fprintf('|%s-[%s:%s]\n',char(repmat(' |',1,level)),conf.labels{i0},num2str(conf.values{i0}));
+            end
+        end
+    end
+    
+end
+if isfield(conf,'val'),
+    for i0=1:numel(conf.val),
+        sub_parse_config(conf.val{i0},level+1)
+    end
+    
+end
