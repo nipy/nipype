484b42edfb4715ca671ba88dd242ea76702b9d8b
Christopher Burns <cburns@berkeley.edu>
2009-08-03 23:10:39 +0000

    Use os.path.join instead of string join for paths.
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@154 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/interfaces/base.py
+++ nipype/interfaces/base.py
@@ -68,12 +68,14 @@ def load_template(name):
     Returns
     -------
       template : string.Template
+
     '''
-    full_fname = '/'.join([os.path.dirname(__file__), 'script_templates', name])
+
+    full_fname = os.path.join(os.path.dirname(__file__), 
+                              'script_templates', name)
     template_file = open(full_fname)
     template = Template(template_file.read())
     template_file.close()
-
     return template
     
 
