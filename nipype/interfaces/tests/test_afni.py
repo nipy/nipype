c00c9dc7dfd0b0870cdc20daaee3687f37f99a82
Christopher Burns <cburns@berkeley.edu>
2009-08-13 23:25:30 +0000

    Fix afni tests
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@231 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/interfaces/tests/test_afni.py
+++ nipype/interfaces/tests/test_afni.py
@@ -6,29 +6,41 @@ from nose.tools import assert_equal
 def test_To3d():
     cmd = afni.To3d()
     yield assert_equal, cmd.cmdline, 'to3d'
-    cmd = afni.To3d(anat=True)
+
+    cmd = afni.To3d(datatype='anat')
     yield assert_equal, cmd.cmdline, 'to3d -anat'
+    cmd = afni.To3d(datatype='epan')
+    yield assert_equal, cmd.cmdline, 'to3d -epan'
+
     cmd = afni.To3d()
     cmd.inputs.datum = 'float'
     yield assert_equal, cmd.cmdline, 'to3d -datum float'
+
     cmd = afni.To3d()
     cmd.inputs.session = '/home/bobama'
     yield assert_equal, cmd.cmdline, 'to3d -session /home/bobama'
+
     cmd = afni.To3d(prefix='foo.nii.gz')
     yield assert_equal, cmd.cmdline, 'to3d -prefix foo.nii.gz'
+
     cmd = afni.To3d(infiles='/data/*.dcm')
     yield assert_equal, cmd.cmdline, 'to3d /data/*.dcm'
-    cmd = afni.To3d(epan=True)
-    yield assert_equal, cmd.cmdline, 'to3d -epan'
+
+
     cmd = afni.To3d(skip_outliers=True)
     yield assert_equal, cmd.cmdline, 'to3d -skip_outliers'
+
     cmd = afni.To3d(assume_dicom_mosaic=True)
     yield assert_equal, cmd.cmdline, 'to3d -assume_dicom_mosaic'
+
     # Test slice time params
     cmd = afni.To3d()
-    cmd.inputs.time = ['zt', 12, 150, 2000, 'alt+z']
-    yield assert_equal, cmd.cmdline, 'to3d -time:zt 12 150 2000.000000 alt+z'
+    td = dict(slice_order='zt', nz=12, nt=150, TR=2000, tpattern='alt+z')
+    cmd.inputs.time_dependencies = td
+    yield assert_equal, cmd.cmdline, 'to3d -time:zt 12 150 2000 alt+z'
+
     cmd = afni.To3d()
-    cmd.inputs.time = ['tz', 150, 12, 2000, 'alt+z']
-    yield assert_equal, cmd.cmdline, 'to3d -time:tz 150 12 2000.000000 alt+z'
+    td = dict(slice_order='tz', nt=150, nz=12, TR=2000, tpattern='alt+z')
+    cmd.inputs.time_dependencies = td
+    yield assert_equal, cmd.cmdline, 'to3d -time:tz 150 12 2000 alt+z'
 
