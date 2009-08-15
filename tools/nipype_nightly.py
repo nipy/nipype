51a26ae2c29286360c919d63c00acdf8e60bd695
Christopher Burns <cburns@berkeley.edu>
2009-08-15 00:35:05 +0000

    Add script to update docs nightly.
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@236 ead46cd0-7350-4e37-8683-fc4c6f79bf00

---
+++ tools/nipype_nightly.py
@@ -0,0 +1,55 @@
+"""Simple script to update the trunk nightly, build the docs and push
+to sourceforge.
+
+Usage:
+    python nipype_nightly.py
+
+"""
+
+import os
+import subprocess
+
+dirname = '/home/cburns/src/nipy-sf/nipype/trunk/'
+
+color_green = '\033[0;32m'
+color_null = '\033[0m'
+
+def run_cmd(cmd):
+    print color_green + cmd + color_null
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE, shell=True)
+    output, error = proc.communicate()
+    returncode = proc.returncode
+    if returncode:
+        msg = 'Running cmd: %s\n Error: %s' % (cmd, error)
+        raise StandardError(msg)
+    print output
+
+def update_repos():
+    """Update svn repository."""
+    os.chdir(dirname)
+    cmd = 'svn update'
+    run_cmd(cmd)
+
+def build_docs():
+    """Build the sphinx documentation."""
+    os.chdir(dirname + 'doc')
+    cmd = 'make html'
+    run_cmd(cmd)
+
+def push_to_sf():
+    """Push documentation to sourceforge."""
+    os.chdir(dirname + 'doc')
+    cmd = 'make sf_cburns'
+    run_cmd(cmd)
+
+
+if __name__ == '__main__':
+    prev_dir = os.path.abspath(os.curdir)
+    update_repos()
+    build_docs()
+    push_to_sf()
+    os.chdir(prev_dir)
+
+
+
---
+++ tools/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Nipype Tools',
+      version='0.1',
+      description='Utilities used in nipype development',
+      author='Nipype Developers',
+      author_email='nipy-devel@neuroimaging.scipy.org',
+      url='http://nipy.sourceforge.net',
+      scripts=['./nipype_nightly.py']
+     )
+
