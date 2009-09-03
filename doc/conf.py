d3d9defa43763607bbc0d4fd67e291012c3bf465
Jarrod Millman <millman@berkeley.edu>
2009-09-03 02:26:18 +0000

    html title
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@315 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- doc/conf.py
+++ doc/conf.py
@@ -119,7 +119,7 @@ html_style = 'nipype.css'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = 'nipy pipeline and interfaces package'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
