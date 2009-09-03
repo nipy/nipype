6d35a4bd88687486596863749a291c0126787445
Christopher Burns <cburns@berkeley.edu>
2009-09-03 17:23:39 +0000

    Add nipype.testing
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@318 ead46cd0-7350-4e37-8683-fc4c6f79bf00

---
+++ nipype/testing/__init__.py
@@ -0,0 +1,7 @@
+"""Simple utility to pull in all the testing functions we're likely to use.
+"""
+
+from numpy.testing import *
+import decorators as dec
+from nose.tools import assert_true, assert_false
+
