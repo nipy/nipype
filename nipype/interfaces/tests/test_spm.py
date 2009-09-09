3348abc1a09c825c4f70b6f35872363a955636e7
Christopher Burns <cburns@berkeley.edu>
2009-09-09 21:58:26 +0000

    Some test cleanup.
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@352 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/interfaces/tests/test_spm.py
+++ nipype/interfaces/tests/test_spm.py
@@ -1,36 +1,39 @@
-import nipype.interfaces.spm as spm
 from nipype.testing import *
+
+import nipype.interfaces.spm as spm
 
 
 def test_spm_path():
     spm_path = spm.spm_info.spm_path
     if spm_path is not None:
-        yield assert_equal,type(spm_path),type('')
-        yield assert_equal,'spm' in spm_path,True
+        yield assert_equal, type(spm_path), type('')
+        yield assert_true, 'spm' in spm_path
 
 def test_reformat_dict_for_savemat():
     mlab = spm.SpmMatlabCommandLine()
     out = mlab._reformat_dict_for_savemat({'a':{'b':{'c':[]}}})
-    yield assert_equal,out,[{'a': [{'b': [{'c': []}]}]}]
+    yield assert_equal, out, [{'a': [{'b': [{'c': []}]}]}]
     
 def test_generate_job():
     mlab = spm.SpmMatlabCommandLine()
     out = mlab._generate_job()
-    yield assert_equal,out,''
+    yield assert_equal, out, ''
     contents = {'contents':[1,2,3,4]}
     out = mlab._generate_job(contents=contents)
-    yield assert_equal,out,'.contents(1) = 1;\n.contents(2) = 2;\n.contents(3) = 3;\n.contents(4) = 4;\n'
+    yield assert_equal, out, '.contents(1) = 1;\n.contents(2) = 2;\n.contents(3) = 3;\n.contents(4) = 4;\n'
     
 def test_make_matlab_command():
     mlab = spm.SpmMatlabCommandLine()
     contents = {'contents':[1,2,3,4]}
-    cmdline,script = mlab._make_matlab_command('jobtype','jobname',[contents])
-    yield assert_equal,cmdline,'matlab -nodesktop -nosplash -r "pyscript_jobname;exit" '
-    yield assert_equal,'jobs{1}.jobtype{1}.jobname{1}.contents(3) = 3;' in script, True
+    cmdline,script = mlab._make_matlab_command('jobtype', 'jobname', [contents])
+    yield assert_equal, cmdline, \
+        'matlab -nodesktop -nosplash -r "pyscript_jobname;exit" '
+    yield assert_true, 'jobs{1}.jobtype{1}.jobname{1}.contents(3) = 3;' \
+        in script
 
 def test_spm_realign():
     realign = spm.Realign(write=False)
     updatedopts = realign._parseinputs()
     yield assert_equal, updatedopts, {'data':[],'eoptions':{},'roptions':{}}
-    yield assert_equal, realign.inputs.write, False
+    yield assert_false, realign.inputs.write
 
