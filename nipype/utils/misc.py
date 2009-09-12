2f6bf13eb341510c29ce53e348bb42dd2c2644c5
Christopher Burns <cburns@berkeley.edu>
2009-09-12 00:00:51 +0000

    Add container_to_string utility function.
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@381 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/utils/misc.py
+++ nipype/utils/misc.py
@@ -18,3 +18,27 @@ def mktree(path):
       if not os.path.exists(outdir):
          os.mkdir(outdir)
       
+def container_to_string(cont):
+   """Convert a container to a command line string.
+   
+   Elements of the container are joined with a space between them,
+   suitable for a command line parameter.
+
+   If the container `cont` is only a sequence, like a string and not a
+   container, it is returned unmodified.
+
+   Parameters
+   ----------
+   cont : container
+      A container object like a list, tuple, dict, or a set.
+
+   Returns
+   -------
+   cont_str : string
+       Container elements joined into a string.
+
+   """
+   if hasattr(cont, '__iter__'):
+      return ' '.join(cont)
+   else:
+      return str(cont)
---
+++ nipype/utils/tests/test_misc.py
@@ -0,0 +1,24 @@
+from nipype.testing import assert_equal, assert_true
+
+from nipype.utils.misc import container_to_string
+
+def test_cont_to_str():
+    # list
+    x = ['a', 'b']
+    yield assert_equal, container_to_string(x), 'a b'
+    # tuple
+    x = tuple(x)
+    yield assert_equal, container_to_string(x), 'a b'
+    # set
+    x = set(x)
+    y = container_to_string(x)
+    yield assert_true, (y == 'a b') or (y == 'b a')
+    # dict
+    x = dict(a='a', b='b')
+    y = container_to_string(x)
+    yield assert_true, (y == 'a b') or (y == 'b a')
+    # string
+    yield assert_equal, container_to_string('foobar'), 'foobar'
+    # int.  Integers are not the main intent of this function, but see
+    # no reason why they shouldn't work.
+    yield assert_equal, container_to_string(123), '123'
