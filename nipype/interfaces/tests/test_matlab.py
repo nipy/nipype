24dfe3c6900c2f4ebc5f624ec7771fe59353b997
Satrajit Ghosh <satra@mit.edu>
2009-09-18 15:08:40 +0000

    added matlab tests
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@422 ead46cd0-7350-4e37-8683-fc4c6f79bf00

---
+++ nipype/interfaces/tests/test_matlab.py
@@ -0,0 +1,40 @@
+from nipype.testing import assert_raises, assert_equal, assert_true, assert_false
+import nipype.interfaces.matlab as mlab
+from nipype.interfaces.base import CommandLine, Bunch
+from tempfile import mkdtemp
+
+import os
+
+res = CommandLine('which matlab').run()
+matlab_path = res.runtime.stdout.strip()
+
+if matlab_path != '':
+    matlab_command = '%s -nodesktop -nosplash'%matlab_path.split(os.path.sep)[-1]
+    mlab.matlab_cmd = matlab_command
+    basedir = os.environ.get('TMPDIR', mkdtemp())
+        
+    def test_init():
+        mi = mlab.MatlabCommandLine()
+        yield assert_equal, mi._cmdline, None
+        yield assert_equal, mi._cmdline_inputs, None
+        yield assert_equal, mi.matlab_cmd, matlab_command
+        mi = mlab.MatlabCommandLine(matlab_cmd='foo')
+        yield assert_equal, mi.matlab_cmd, 'foo'
+        yield assert_equal, mlab.matlab_cmd, matlab_command
+
+    def test_cmdline():
+        mi = mlab.MatlabCommandLine(script_lines='whos',script_name='testscript',cwd=basedir)
+        yield assert_equal, mi.cmdline, 'matlab -nodesktop -nosplash -r "testscript;exit" '
+        yield assert_equal, mi._cmdline, 'matlab -nodesktop -nosplash -r "testscript;exit" '
+        inputs = str(Bunch(cwd=basedir, mfile=True, script_lines='whos', script_name='testscript', ))
+        yield assert_equal, inputs, str(mi._cmdline_inputs)
+        path_exists = os.path.exists(os.path.join(basedir,'testscript.m'))
+        yield assert_true, path_exists
+
+    def test_set_matlabcmd():
+        mi = mlab.MatlabCommandLine()
+        mi.set_matlabcmd('foo')
+        yield assert_equal, mi.matlab_cmd, 'foo'
+        yield assert_equal, mlab.matlab_cmd, matlab_command
+
+
