f589979a2099c6580770d89ee86f5564b373b3cb
Christopher Burns <cburns@berkeley.edu>
2009-09-23 04:35:52 +0000

    Remove debug statements and other cruft from spm_docs.py
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@433 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/utils/spm_docs.py
+++ nipype/utils/spm_docs.py
@@ -1,13 +1,8 @@
 """Grab documentation from spm."""
 
+import os
+
 from nipype.interfaces import matlab
-
-spm_config_doc = {'realign' : 'spm_config_realign',
-                  'coreg' : 'spm_config_coreg',
-                  'normalise' : 'spm_config_norm',
-                  }
-#                  'segment' : spm_config.values{2}.values{4}.help{1}, or spm_config_segment_old
-
 
 spm_doc_names = {'spm_realign' : 'Realign: Estimate & Reslice',
                  'coreg' : 'Coreg: Estimate & Reslice',
@@ -24,25 +19,22 @@ def grab_doc(funcname):
         Function for which we are grabbing documentation.
 
     """
-    cmd = matlab.MatlabCommandLine()
-    func_map = {'realign' : 'spm_realign_doc',
-                'coreg' : 'print_spm_docs',
-                }
 
-    #cmd.inputs.script_lines = 'print_spm_docs'
+    cmd = matlab.MatlabCommandLine()
+    # We need to tell Matlab where to find our spm_get_doc.m file.
+    cwd = os.path.dirname(__file__)
+    cmd.inputs.cwd = cwd
     try:
-        #cmd.inputs.script_lines = func_map[funcname]
         name = spm_doc_names[funcname]
         mcmd = "spm_get_doc('%s')" % name
-        print 'matlab command:\n', mcmd
+        #print 'matlab command:\n', mcmd
         cmd.inputs.script_lines = mcmd
     except KeyError:
         raise KeyError('funcname must match one of the options listed')
     
     out = cmd.run()
-    #return out.runtime.stdout
-    doc = out.runtime.stdout
-    return doc, _strip_header(doc)
+    #doc = out.runtime.stdout
+    return _strip_header(out.runtime.stdout)
 
 
 def _strip_header(doc):
