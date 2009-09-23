acfd83168c33bde8ebd16f81eecc3632822c32f9
Christopher Burns <cburns@berkeley.edu>
2009-09-23 23:37:42 +0000

    Split up functionality for spm_config parsing so I can easily print out the names when looking for docs.
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@438 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/utils/spm_get_doc.m
+++ nipype/utils/spm_get_doc.m
@@ -4,43 +4,17 @@ function doc = spm_get_doc(docname)
 %
 % This will search through the spm_config() object and grab the
 % documentation whose name matches docname.
-
-% XXX put in a try-catch?  I'm finding matlab's exception handling
-% a bit confusing.
-cfg = spm_config();
-cfgstruct = find_doc(cfg, docname);
-hdr = 'NIPYPE\n';
-doc = strcat(hdr, cfgstruct.help{:});
-end
-
-
-function docstruct = find_doc(astruct, docname)
-% Find the structure that contains the documentation `docname`.
-
-% Flatten the cfg structure
-cfg_list = spm_cfg_list(astruct, {});
-[rows, cols] = size(cfg_list);
+cfgstruct = spm_flat_config(0);
+[rows, cols] = size(cfgstruct);
 % Loop over cell array and search for the docname
 for i = 1:cols
-  if strcmp(cfg_list{i}.name, docname)
-    docstruct = cfg_list{i};
+  if strcmp(cfgstruct{i}.name, docname)
+    docstruct = cfgstruct{i};
     break
   end
 end
-end
-
-
-function objlist = spm_cfg_list(astruct, objlist)
-% Flatten the nested structure in 'astruct'.
-% Returns a cell array.
-% Usage:  objlist = spm_cfg_list(astruct, {})
-
-if isfield(astruct, 'values')
-  [rows, cols] = size(astruct.values);
-  for i = 1:cols
-    objlist = spm_cfg_list(astruct.values{i}, objlist);
-  end
-else
-  objlist = {objlist{:} astruct};
-end
+% Add a tag so we can strip off the Matlab header information and
+% only print out the SPM documentation.
+tag = 'NIPYPE\n';
+doc = strcat(tag, docstruct.help{:});
 end
