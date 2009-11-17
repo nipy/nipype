fb88a4ff1fbd6be9c9d32ea8d02c377b06bf317e
Christopher Burns <cburns@berkeley.edu>
2009-11-17 05:52:09 +0000

    Cleanup test_matlab.
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@701 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/interfaces/tests/test_matlab.py
+++ nipype/interfaces/tests/test_matlab.py
@@ -1,45 +1,60 @@
-from nipype.testing import assert_raises, assert_equal, assert_true, assert_false
+import os
+from tempfile import mkdtemp
+from shutil import rmtree
+
+from nipype.testing import assert_equal, assert_true
 import nipype.interfaces.matlab as mlab
 from nipype.interfaces.base import CommandLine, Bunch
-from tempfile import mkdtemp
-
-import os
 
 try:
     matlab_cmd = os.environ['MATLABCMD']
 except:
     matlab_cmd = 'matlab'
 
-res = CommandLine('which %s'%matlab_cmd).run()
+res = CommandLine('which %s' % matlab_cmd).run()
 matlab_path = res.runtime.stdout.strip()
 
+matlab_command = ''
+no_matlab = True
 if matlab_path != '':
-    matlab_command = '%s -nodesktop -nosplash'%matlab_path.split(os.path.sep)[-1]
-    mlab.matlab_cmd = matlab_command
-    basedir = os.environ.get('TMPDIR', mkdtemp())
-        
-    def test_init():
-        mi = mlab.MatlabCommandLine()
-        yield assert_equal, mi._cmdline, None
-        yield assert_equal, mi._cmdline_inputs, None
-        yield assert_equal, mi.matlab_cmd, matlab_command
-        mi = mlab.MatlabCommandLine(matlab_cmd='foo')
-        yield assert_equal, mi.matlab_cmd, 'foo'
-        yield assert_equal, mlab.matlab_cmd, matlab_command
+    matlab_command = '%s -nodesktop -nosplash' % \
+        matlab_path.split(os.path.sep)[-1]
+    no_matlab = False
+    
+# If a test requires matlab, prefix it with the skipif decorator like
+# below.  Must import skipif from nipype.testing
+#
+#@skipif(no_matlab)
+# def test_func():
+#     pass
 
-    def test_cmdline():
-        mi = mlab.MatlabCommandLine(script_lines='whos',script_name='testscript',cwd=basedir)
-        yield assert_equal, mi.cmdline, 'matlab -nodesktop -nosplash -r "testscript;exit" '
-        yield assert_equal, mi._cmdline, 'matlab -nodesktop -nosplash -r "testscript;exit" '
-        inputs = str(Bunch(cwd=basedir, mfile=True, script_lines='whos', script_name='testscript', ))
-        yield assert_equal, inputs, str(mi._cmdline_inputs)
-        path_exists = os.path.exists(os.path.join(basedir,'testscript.m'))
-        yield assert_true, path_exists
+def test_init():
+    mi = mlab.MatlabCommandLine()
+    yield assert_equal, mi._cmdline, None
+    yield assert_equal, mi._cmdline_inputs, None
+    mi = mlab.MatlabCommandLine(matlab_cmd='foo')
+    yield assert_equal, mi.matlab_cmd, 'foo'
 
-    def test_set_matlabcmd():
-        mi = mlab.MatlabCommandLine()
-        mi.set_matlabcmd('foo')
-        yield assert_equal, mi.matlab_cmd, 'foo'
-        yield assert_equal, mlab.matlab_cmd, matlab_command
+def test_cmdline():
+    basedir = mkdtemp()
+
+    mi = mlab.MatlabCommandLine(script_lines='whos',
+                                script_name='testscript',
+                                cwd=basedir)
+    yield assert_equal, mi.cmdline, \
+        'matlab -nodesktop -nosplash -r "testscript;exit" '
+    yield assert_equal, mi._cmdline, \
+        'matlab -nodesktop -nosplash -r "testscript;exit" '
+    inputs = str(Bunch(cwd=basedir, mfile=True, script_lines='whos', 
+                       script_name='testscript', ))
+    yield assert_equal, inputs, str(mi._cmdline_inputs)
+    path_exists = os.path.exists(os.path.join(basedir,'testscript.m'))
+    yield assert_true, path_exists
+    rmtree(basedir)
+
+def test_set_matlabcmd():
+    mi = mlab.MatlabCommandLine()
+    mi.set_matlabcmd('foo')
+    yield assert_equal, mi.matlab_cmd, 'foo'
 
 
