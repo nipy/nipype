b57fe16387c83bd32b6f38db2563e0bf4dffdd55
Yaroslav Halchenko <yoh@dartmouth.edu>
2010-02-04 00:32:20 +0000

    NF: skip_if_no_package -- throws SkipTest if no required dependency found
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@938 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/testing/__init__.py
+++ nipype/testing/__init__.py
@@ -4,4 +4,7 @@
 from numpy.testing import *
 from numpy.testing.decorators import *
 from nose.tools import assert_true, assert_false, assert_not_equal
+from nose import SkipTest
+
+from utils import *
 
---
+++ nipype/testing/utils.py
@@ -0,0 +1,21 @@
+#emacs: -*- mode: python-mode; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*- 
+#ex: set sts=4 ts=4 sw=4 noet:
+"""Additional handy utilities for testing
+"""
+__docformat__ = 'restructuredtext'
+
+from nipype.utils.misc import package_check
+from nose import SkipTest
+
+def skip_if_no_package(*args, **kwargs):
+    """Raise SkipTest if package_check fails
+
+    Parameters
+    ----------
+    *args Positional parameters passed to `package_check`
+    *kwargs Keyword parameters passed to `package_check`
+    """
+    package_check(exc_failed_import=SkipTest,
+                  exc_failed_check=SkipTest,
+                  *args, **kwargs)
+
