456f3cd61d45ac3463ce98212634b96c4ecbabd9
Christopher Burns <cburns@berkeley.edu>
2010-02-17 07:13:50 +0000

    Cleanup fsl and fsl_preprocess test modules.
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@998 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/interfaces/tests/test_fsl_preprocess.py
+++ nipype/interfaces/tests/test_fsl_preprocess.py
@@ -1,9 +1,6 @@
 import os
-import tempfile
-import shutil
 
-from nipype.testing import (assert_equal, assert_not_equal, assert_true,
-                            assert_raises)
+from nipype.testing import assert_equal, assert_not_equal, assert_raises
 
 import nipype.interfaces.fsl_preprocess as fsl
 from nipype.interfaces.base import InterfaceResult
