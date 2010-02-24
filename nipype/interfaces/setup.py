6df2a6e82a04c6cb19a789c55f758d0958a9b690
Christopher Burns <cburns@berkeley.edu>
2010-02-24 23:17:08 +0000

    Add fsl subpackage on install.
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@1050 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/interfaces/setup.py
+++ nipype/interfaces/setup.py
@@ -3,6 +3,8 @@ def configuration(parent_package='',top_path=None):
 
     config = Configuration('interfaces', parent_package, top_path)
 
+    config.add_subpackage('fsl')
+
     config.add_data_dir('tests')
     config.add_data_dir('script_templates')
 
