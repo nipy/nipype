8312dfffdc3cc4e07774483b24cb0d92950f7840
Satrajit Ghosh <satra@mit.edu>
2010-04-05 05:16:11 +0000

    fixed io test
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@1239 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/interfaces/tests/test_io.py
+++ nipype/interfaces/tests/test_io.py
@@ -7,11 +7,14 @@ import nipype.interfaces.io as nio
 def test_aggregate_outputs():
     basedir = mkdtemp()
     os.makedirs(os.path.join(basedir,'s1'))
+    filename = os.path.join(basedir,'s1','s1.nii')
+    fp = open(filename,'w')
+    fp.close()
     ds = nio.SubjectSource()
     ds.inputs.base_directory = basedir
-    ds.inputs.file_layout = '%s.nii'
+    ds.inputs.file_layout = 's%s.nii'
     ds.inputs.subject_info = dict(func=['1'])
     ds.inputs.subject_id = 's1'
     res = ds.run()
-    yield assert_equal, res.runtime.returncode, 0
+    yield assert_equal, res.outputs.func, filename
     shutil.rmtree(basedir)
