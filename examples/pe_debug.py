4228574868a2ab72214ed5d2f3fab55134d9c1bb
Satrajit Ghosh <satra@mit.edu>
2010-04-20 02:19:04 +0000

    add traited example to debug script
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@1303 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- examples/pe_debug.py
+++ examples/pe_debug.py
@@ -12,6 +12,9 @@ bet = pe.MapNode(fsl.Bet(), iterfield=['infile'], name='bet')
 w1 = pe.Workflow(name='spm')
 w1.connect([(realign, coreg, [('realigned_files', 'source')])])
 
+w1.inputs.spmrealign.fwhm = 0.5
+assert(realign.inputs.fwhm == 0.5)
+
 w2 = pe.Workflow(name='cplx')
 w2.connect(w1, 'coreg.coregistered_files', realign2, 'infile')
 
