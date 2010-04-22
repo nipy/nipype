adbc99b79e2f3a9c8b36f787123d89e5f4694e4d
Christopher Burns <cburns@berkeley.edu>
2010-04-22 23:04:31 +0000

    Pull updated version of nose monkey-patch for lightunit from nipy-trunk.
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@1343 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/testing/lightunit/nosepatch.py
+++ nipype/testing/lightunit/nosepatch.py
@@ -7,6 +7,7 @@ Note: merely importing this module causes the monkeypatch to be applied."""
 
 import unittest
 import nose.loader
+from inspect import ismethod, isfunction
 
 def getTestCaseNames(self, testCaseClass):
     """Override to select with selector, unless
@@ -19,11 +20,20 @@ def getTestCaseNames(self, testCaseClass):
         item = getattr(cls, attr, None)
         # MONKEYPATCH: replace this:
         #if not ismethod(item):
+        #    return False
+        # return sel.wantMethod(item)
         # With:
-        if not hasattr(item, '__call__'):
+        if ismethod(item):
+            return sel.wantMethod(item)
+        # static method or something. If this is a static method, we
+        # can't get the class information, and we have to treat it
+        # as a function.  Thus, we will miss things like class
+        # attributes for test selection
+        if isfunction(item):
+            return sel.wantFunction(item)
+        return False
         # END MONKEYPATCH
-            return False
-        return sel.wantMethod(item)
+
     cases = filter(wanted, dir(testCaseClass))
     for base in testCaseClass.__bases__:
         for case in self.getTestCaseNames(base):
