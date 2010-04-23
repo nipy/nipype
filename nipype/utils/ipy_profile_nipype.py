a472e164b668313b876ca4a009891269f850d63a
Christopher Burns <cburns@berkeley.edu>
2010-04-23 23:56:35 +0000

    Add nipype ipython profile.
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@1351 ead46cd0-7350-4e37-8683-fc4c6f79bf00

---
+++ nipype/utils/ipy_profile_nipype.py
@@ -0,0 +1,87 @@
+"""IPython 'nipype' profile.
+
+Updates the TAB completion of IPython to hide Traits attributes from
+results of TAB completion.  Also defines a magic command
+``nipype_mode`` to allow users to toggle the traits completer on and
+off.
+
+"""
+
+import IPython.ipapi
+
+def get_nipype_mode():
+    ip = IPython.ipapi.get()
+    try:
+        nipype_mode = ip.user_ns['nipype_mode']
+    except KeyError:
+        # Make sure nipype_mode exists in user namespace
+        vars = {'nipype_mode' : True}
+        ip.to_user_ns(vars, interactive=False)
+    return ip.user_ns['nipype_mode']
+
+def toggle_nipype_mode(self, args):
+    """Toggle nipype mode on and off.
+
+    When nipype mode is on, several attributes meant for internal use
+    only are hidden from view.  This effects the attributes that are
+    shown when using TAB-completion on nipype.interfaces objects.
+
+    Specifically this enables an ipython traits completer which
+    filters out the traits attributes.
+
+    Examples
+    --------
+    In [1]: %nipype_mode
+    nipype_mode is OFF
+
+    In [2]: %nipype_mode
+    nipype_mode is ON
+
+    """
+
+    ip = self.api
+    mode = get_nipype_mode()
+    if mode == True:
+        # switch mode off
+        nipype_mode_off()
+        ip.user_ns['nipype_mode'] = False
+    else:
+        # switch mode on
+        nipype_mode_on()
+        ip.user_ns['nipype_mode'] = True
+
+def nipype_mode_on():
+    """Turn on the traits completer.
+
+    This will hide all the traits attributes from the view of the user
+    when doing TAB completion on any object that is a descendant of
+    HasTraits.
+    """
+    from ipy_traits_completer import activate
+
+    ip = IPython.ipapi.get()
+    activate()
+    print 'nipype_mode is ON'
+
+def nipype_mode_off():
+    """Turn traits completer off"""
+    from IPython.strdispatch import StrDispatch
+
+    ip = IPython.ipapi.get()
+    sdisp = StrDispatch()
+    ip.IP.strdispatchers['complete_command'] = sdisp
+    ip.IP.Completer.custom_completers = sdisp
+    print 'nipype_mode is OFF'
+
+def main():
+    """When we use the nipype profile we turn nipype_mode on."""
+    ip = IPython.ipapi.get()
+
+    mode = get_nipype_mode() # initialize nipype_mode in user_ns
+    nipype_mode_on()
+
+    # enable magic function
+    ip.expose_magic('nipype_mode', toggle_nipype_mode)
+
+main()
+
