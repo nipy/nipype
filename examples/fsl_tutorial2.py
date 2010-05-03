4e3a78fe22d7beb44eef0edb8f3ead4cbcb89be7
Krzysztof Gorgolewski <krzysztof.gorgolewski@gmail.com>
2010-05-03 19:45:13 +0000

    FSL tutorial updated to new framework
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@1403 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- examples/fsl_tutorial2.py
+++ examples/fsl_tutorial2.py
@@ -11,7 +11,7 @@
 
 import nipype.interfaces.io as nio           # Data i/o 
 import nipype.interfaces.fsl as fsl          # fsl
-import nipype.pipeline.node_wrapper as nw    # nodes for pypelines
+import nipype.interfaces.utility as util     # utility 
 import nipype.pipeline.engine as pe          # pypeline engine
 import nipype.algorithms.modelgen as model   # model generation
 import os                                    # system functions
@@ -39,112 +39,111 @@ package_check('IPython', '0.10', 'tutorial1')
 
 # Tell fsl to generate all output in compressed nifti format
 print fsl.FSLInfo.version()
-fsl.FSLInfo.outputtype('NIFTI_GZ')
+fsl.NEW_FSLCommand.set_default_outputtype('NIFTI_GZ')
 
+
+"""The nipype tutorial contains data for two subjects.  Subject data
+is in two subdirectories, ``s1`` and ``s2``.  Each subject directory
+contains four functional volumes: f3.nii, f5.nii, f7.nii, f10.nii. And
+one anatomical volume named struct.nii.
+
+Below we set some variables to inform the ``datasource`` about the
+layout of our data.  We specify the location of the data, the subject
+sub-directories and a dictionary that maps each run to a mnemonic (or
+field) for the run type (``struct`` or ``func``).  These fields become
+the output fields of the ``datasource`` node in the pipeline.
+
+In the example below, run 'f3' is of type 'func' and gets mapped to a
+nifti filename through a template '%s.nii'. So 'f3' would become
+'f3.nii'.
 
 """
-3. The following lines of code sets up the necessary information
-   required by the datasource module. It provides a mapping between
-   run numbers (nifti files) and the mnemonic ('struct', 'func',
-   etc.,.)  that particular run should be called. These mnemonics or
-   fields become the output fields of the datasource module. In the
-   example below, run 'f3' is of type 'func'. The 'f3' gets mapped to
-   a nifti filename through a template '%s.nii'. So 'f3' would become
-   'f3.nii'.
-"""
 
-# The following lines create some information about location of your
-# data. 
+# Specify the location of the data.
 data_dir = os.path.abspath('data')
-subject_list = ['s1']#, 's3']
-# The following info structure helps the DataSource module organize
-# nifti files into fields/attributes of a data object. With DataSource
-# this object is of type Bunch.
-info = {}
-info['s1'] = ((['f3','f5','f7','f10'],'func'),(['struct'],'struct'))#,(['ref'],'func_ref'))
-info['s3'] = ((['f3','f5','f7','f10'],'func'),(['struct'],'struct'))#,(['ref'],'func_ref'))
+# Specify the subject directories
+subject_list = ['s1', 's3']
+# Map field names to individual subject runs.
+info = dict(func=[['subject_id', ['f3','f5','f7','f10']]],
+            struct=[['subject_id','struct']])
 
-######################################################################
-# Setup preprocessing pipeline nodes
+infosource = pe.Node(interface=util.IdentityInterface(fields=['subject_id']), name="infosource")
 
-"""
-4. Setup various nodes for preprocessing the data. 
+"""Here we set up iteration over all the subjects. The following line
+is a particular example of the flexibility of the system.  The
+``datasource`` attribute ``iterables`` tells the pipeline engine that
+it should repeat the analysis on each of the items in the
+``subject_list``. In the current example, the entire first level
+preprocessing and estimation will be repeated for each subject
+contained in subject_list.
 """
 
-"""
-   a. Setting up an instance of the interface
-   :class:`nipype.interfaces.io.DataSource`. This node looks into the
-   directory containing Nifti files and returns pointers to the files
-   in a structured format as determined by the field/attribute names
-   provided in the info structure above. The
-   :class:`nipype.pipeline.NodeWrapper` module wraps the interface
-   object and provides additional housekeeping and pipeline specific
-   functionality. 
-"""
-datasource = nw.NodeWrapper(interface=nio.DataSource())
-datasource.inputs.base_directory   = data_dir
-datasource.inputs.subject_template = '%s'
-datasource.inputs.file_template    = '%s.nii'
-datasource.inputs.subject_info     = info
-
+infosource.iterables = ('subject_id', subject_list)
 
 """
-   b. Setting up iteration over all subjects. The following line is a
-   particular example of the flexibility of the system.  The  variable
-   `iterables` for datasource tells the pipeline engine that it should
-   repeat any of the processes that are descendents of the datasource
-   process on each of the iterable items. In the current example, the
-   entire first level preprocessing and estimation will be repeated
-   for each subject contained in subject_list.
+Preprocessing pipeline nodes
+----------------------------
+
+Now we create a :class:`nipype.interfaces.io.DataSource` object and
+fill in the information from above about the layout of our data.  The
+:class:`nipype.pipeline.NodeWrapper` module wraps the interface object
+and provides additional housekeeping and pipeline specific
+functionality.
 """
-datasource.iterables = ('subject_id',subject_list)
+
+datasource = pe.Node(interface=nio.DataGrabber(infields=['subject_id'],
+                                                      outfields=['func', 'struct']),
+                            name = 'datasource')
+datasource.inputs.base_directory = data_dir
+datasource.inputs.template = '%s/%s.nii'
+datasource.inputs.template_args = info
 
 ## from the FLIRT web doc
 
-extract_ref = nw.NodeWrapper(interface=fsl.ExtractRoi(tmin=42,
+extract_ref = pe.Node(interface=fsl.ExtractRoi(tmin=42,
                                                       tsize=1),
                              name = 'middlevol.fsl',
                              diskbased=True)
 # run FSL's bet
 # bet my_structural my_betted_structural
-skullstrip = nw.NodeWrapper(interface=fsl.Bet(mask = True,
+skullstrip = pe.Node(interface=fsl.Bet(mask = True,
                                               frac = 0.34),
                             name = 'struct_bet.fsl',
                             diskbased=True)
 
 # Preprocess functionals
-motion_correct = nw.NodeWrapper(interface=fsl.McFlirt(saveplots = True),
+motion_correct = pe.MapNode(interface=fsl.McFlirt(saveplots = True),
                                 name='mcflirt.fsl',
-                                diskbased=True)
-motion_correct.iterfield = ['infile']
+                                diskbased=True,
+                                iterfield = ['infile'])
 
-func_skullstrip = nw.NodeWrapper(interface=fsl.Bet(functional = True),
+func_skullstrip = pe.MapNode(interface=fsl.Bet(functional = True),
                                  diskbased=True,
-                                 name='func_bet.fsl')
-func_skullstrip.iterfield = ['infile']
+                                 name='func_bet.fsl',
+                                 iterfield = ['infile'])
 
 
 # Finally do some smoothing!
 
-smoothing = nw.NodeWrapper(interface=fsl.Smooth(), diskbased=True)
-smoothing.iterfield = ['infile']
+smoothing = pe.MapNode(interface=fsl.Smooth(), diskbased=True, name="smooth.fsl",
+                       iterfield = ['infile'])
 smoothing.inputs.fwhm = 5
 
-inorm = nw.NodeWrapper(interface = fsl.ImageMaths(optstring = '-inm 10000',
+inorm = pe.MapNode(interface = fsl.ImageMaths(optstring = '-inm 10000',
                                                 suffix = '_inm',
                                                 outdatatype = 'float'),
                        name = 'inorm.fsl',
-                       diskbased            = True)
-inorm.iterfield = ['infile']
+                       diskbased            = True,
+                       iterfield = ['infile'])
 
-hpfilter = nw.NodeWrapper(interface=fsl.ImageMaths(),
+hpfilter = pe.MapNode(interface=fsl.ImageMaths(),
                           name='highpass.fsl',
-                          diskbased=True)
+                          diskbased=True,
+                          iterfield = ['infile'])
 hpcutoff = 120
 TR = 3.
 hpfilter.inputs.suffix = '_hpf'
 hpfilter.inputs.optstring = '-bptf %d -1'%(hpcutoff/TR)
-hpfilter.iterfield = ['infile']
 
 #######################################################################
 # setup analysis components
@@ -195,7 +194,7 @@ contrasts = [cont1,cont2]
    c. Use :class:`nipype.interfaces.spm.SpecifyModel` to generate
    SPM-specific design information. 
 """
-modelspec = nw.NodeWrapper(interface=model.SpecifyModel(), diskbased=True)
+modelspec = pe.Node(interface=model.SpecifyModel(), diskbased=True, name="modelspec.fsl")
 modelspec.inputs.concatenate_runs        = False
 modelspec.inputs.input_units             = 'secs'
 modelspec.inputs.output_units            = 'secs'
@@ -207,7 +206,7 @@ modelspec.inputs.high_pass_filter_cutoff = hpcutoff
    d. Use :class:`nipype.interfaces.fsl.Level1Design` to generate a
    run specific fsf file for analysis
 """
-level1design = nw.NodeWrapper(interface=fsl.Level1Design(),diskbased=True)
+level1design = pe.Node(interface=fsl.Level1Design(),diskbased=True, name="level1design.fsl")
 level1design.inputs.interscan_interval = modelspec.inputs.time_repetition
 level1design.inputs.bases = {'hrf':{'derivs': True}}
 level1design.inputs.contrasts = contrasts
@@ -218,12 +217,12 @@ level1design.inputs.reg_dof = 12
    e. Use :class:`nipype.interfaces.fsl.FeatModel` to generate a
    run specific mat file for use by FilmGLS
 """
-featmodel = nw.NodeWrapper(interface=fsl.FeatModel(),diskbased=True)
-featmodel.iterfield = ['fsf_file']
+featmodel = pe.MapNode(interface=fsl.FeatModel(),diskbased=True, name="featmodel.fsl",
+                       iterfield = ['fsf_file'])
 
-fedesign = nw.NodeWrapper(interface=fsl.FixedEffectsModel(num_copes=2),diskbased=True)
+fedesign = pe.Node(interface=fsl.FixedEffectsModel(num_copes=2),diskbased=True, name="featdesign")
 
-featfemodel = nw.NodeWrapper(interface=fsl.Feat(),
+featfemodel = pe.Node(interface=fsl.Feat(),
                              name='FixedEffects.feat',
                              diskbased=True)
 
@@ -233,7 +232,7 @@ featfemodel = nw.NodeWrapper(interface=fsl.Feat(),
 # Setup storage of results
 ##########################
 
-datasink = nw.NodeWrapper(interface=nio.DataSink())
+datasink = pe.Node(interface=nio.DataSink(), name="datasink")
 # I'd like this one to actually be preproc, but for now, I don't want to change
 # the pipeline around because of data already being on S3
 datasink.inputs.base_directory = os.path.abspath('./fsl/l1output')
@@ -242,9 +241,8 @@ datasink.inputs.base_directory = os.path.abspath('./fsl/l1output')
 # Set up l1pipeline pype
 #####################
 
-l1pipeline = pe.Pipeline()
-l1pipeline.config['workdir'] = os.path.abspath('./fsl/workingdir')
-l1pipeline.config['use_parameterized_dirs'] = True
+l1pipeline = pe.Workflow(name= "level1")
+l1pipeline.base_dir = os.path.abspath('./fsl/workingdir')
 
 def pickfirst(files):
     return files[0]
