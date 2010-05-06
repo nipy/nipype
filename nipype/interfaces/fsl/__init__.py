0378005f19899cbfe5ff1dda19c997577b4653cb
Satrajit Ghosh <satra@mit.edu>
2010-05-06 02:19:38 +0000

    added slicetimer to __init__ in fsl
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@1422 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/interfaces/fsl/__init__.py
+++ nipype/interfaces/fsl/__init__.py
@@ -8,7 +8,8 @@ Top-level namespace for fsl.  Perhaps should just make fsl a package!
 from nipype.interfaces.fsl.base import FSLCommand, FSLInfo
 from nipype.interfaces.fsl.base import NEW_FSLCommand, Info
 from nipype.interfaces.fsl.preprocess import (Fast, Flirt, ApplyXfm,
-                                              Bet, McFlirt, Fnirt, ApplyWarp)
+                                              Bet, McFlirt, Fnirt, ApplyWarp,
+                                              SliceTimer)
 from nipype.interfaces.fsl.model import (Level1Design, Feat, FeatModel,
                                          FilmGLS, FeatRegister, Flameo, ContrastMgr,
                                          L2Model, SMM)
