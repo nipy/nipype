30e31d845c5b45b6cbde749b8111c567f3719a76
Satrajit Ghosh <satra@mit.edu>
2010-05-13 12:03:21 +0000

    * traited a few more utility nodes * allowed merging in two different directions for merge node (hstack and vstack)
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@1465 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/interfaces/utility.py
+++ nipype/interfaces/utility.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from nipype.utils.filemanip import (filename_to_list, list_to_filename)
 from nipype.interfaces.base import (traits, TraitedSpec, DynamicTraitedSpec,
-                                    Undefined)
+                                    Undefined, isdefined, OutputMultiPath)
 from nipype.interfaces.io import IOBase, add_traits
 
     
@@ -53,6 +53,9 @@ class IdentityInterface(IOBase):
                 outputs[key] = val
         return outputs
 
+class MergeInputSpec(DynamicTraitedSpec):
+    axis = traits.Enum('vstack', 'hstack', usedefault=True,
+                desc='direction in which to merge, hstack requires same number of elements in each input')
 class MergeOutputSpec(TraitedSpec):
     out = traits.List(desc='Merged output')
 
@@ -72,25 +75,40 @@ class Merge(IOBase):
     [1, 2, 5, 3]
     
     """
-    input_spec = DynamicTraitedSpec
+    input_spec = MergeInputSpec
     output_spec = MergeOutputSpec
     
     def __init__(self, numinputs=0, **inputs):
         super(Merge, self).__init__(**inputs)
+        self.numinputs = numinputs
         add_traits(self.inputs, ['in%d'%(i+1) for i in range(numinputs)])
         
     def _list_outputs(self):
         outputs = self._outputs().get()
-        for value in self.inputs.get().values():
-            if value:
-                if isinstance(value, list):
-                    outputs['out'].extend(value)
-                else:
-                    outputs['out'].append(value)
+        out = []
+        if self.inputs.axis == 'vstack':
+            for value in self.inputs.get().values():
+                if isdefined(value):
+                    if isinstance(value, list):
+                        out.extend(value)
+                    else:
+                        out.append(value)
+        else:
+            for i in range(len(self.inputs.in1)):
+                out.insert(i,[])
+                for j in range(self.numinputs):
+                    out[i].append(getattr(self.inputs, 'in%d'%(j+1))[i])
+        if out:
+            outputs['out'] = out
         return outputs
 
-'''
-class Split(BasicInterface):
+class SplitInputSpec(TraitedSpec):
+    inlist = traits.List(traits.Any, mandatory=True,
+                  desc='list of values to split')
+    splits = traits.List(traits.Int, mandatory=True,
+                  desc='Number of outputs in each split - should add to number of inputs')
+
+class Split(IOBase):
     """Basic interface class to split lists into multiple outputs
 
     Examples
@@ -98,36 +116,48 @@ class Split(BasicInterface):
     
     >>> from nipype.interfaces.utility import Split
     >>> sp = Split()
-    >>> sp.inputs.update(inlist=[1,2,3],splits=[2,1])
+    >>> sp.inputs.set(inlist=[1,2,3],splits=[2,1])
     >>> out = sp.run()
     >>> out.outputs.out1
     [1, 2]
     
     """
-    def __init__(self):
-        self.inputs = Bunch(inlist=None,
-                            splits=None)
+
+    input_spec = SplitInputSpec
+    output_spec = DynamicTraitedSpec
         
-    def outputs(self):
-        outputs = Bunch()
-        if self.inputs.splits:
-            for i in range(len(self.inputs.splits)):
-                setattr(outputs, 'out%d'%(i+1), [])
-        return outputs
+    def _add_output_traits(self, base):
+        undefined_traits = {}
+        for i in range(len(self.inputs.splits)):
+            key = 'out%d'%(i+1)
+            base.add_trait(key, traits.Any)
+            undefined_traits[key] = Undefined
+        base.trait_set(trait_change_notify=False, **undefined_traits)
+        return base
     
-    def aggregate_outputs(self):
-        outputs = self.outputs()
-        if self.inputs.splits:
+    def _list_outputs(self):
+        outputs = self._outputs().get()
+        if isdefined(self.inputs.splits):
             if sum(self.inputs.splits) != len(self.inputs.inlist):
                 raise RuntimeError('sum of splits != num of list elements')
             splits = [0]
             splits.extend(self.inputs.splits)
             splits = np.cumsum(splits)
             for i in range(len(splits)-1):
-                setattr(outputs, 'out%d'%(i+1), np.array(self.inputs.inlist)[splits[i]:splits[i+1]].tolist())
+                outputs['out%d'%(i+1)] =  np.array(self.inputs.inlist)[splits[i]:splits[i+1]].tolist()
         return outputs
 
-class Select(BasicInterface):
+class SelectInputSpec(TraitedSpec):
+    inlist = traits.List(traits.Any, mandatory=True,
+                  desc='list of values to choose from')
+    index = traits.List(traits.Int, mandatory=True,
+                  desc='0-based indices of values to choose')
+    
+class SelectOutputSpec(TraitedSpec):
+    out = OutputMultiPath(traits.Any,
+                          desc='list of selected values')
+    
+class Select(IOBase):
     """Basic interface class to select specific elements from a list
 
     Examples
@@ -135,32 +165,27 @@ class Select(BasicInterface):
     
     >>> from nipype.interfaces.utility import Select
     >>> sl = Select()
-    >>> sl.inputs.update(inlist=[1,2,3,4,5],index=[3])
+    >>> sl.inputs.set(inlist=[1,2,3,4,5],index=[3])
     >>> out = sl.run()
     >>> out.outputs.out
     4
-    >>> sl.inputs.update(inlist=[1,2,3,4,5],index=[3,4])
+    >>> sl.inputs.set(inlist=[1,2,3,4,5],index=[3,4])
     >>> out = sl.run()
     >>> out.outputs.out
     [4, 5]
     
     """
-    def __init__(self):
-        self.inputs = Bunch(inlist=None,
-                            index=None)
-        
-    def outputs(self):
-        outputs = Bunch(out=None)
-        return outputs
+
+    input_spec = SelectInputSpec
+    output_spec = SelectOutputSpec
     
-    def aggregate_outputs(self):
-        outputs = self.outputs()
+    def _list_outputs(self):
+        outputs = self._outputs().get()
         out = np.array(self.inputs.inlist)[np.array(self.inputs.index)].tolist()
-        if len(out) == 1:
-            out = out[0]
-        outputs.out = out
+        outputs['out'] = out
         return outputs
 
+'''
 class SubstringMatch(BasicInterface):
     """Basic interface class to match list items containing specific substrings
 
