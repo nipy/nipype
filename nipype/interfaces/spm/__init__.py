81d3c7b1c4c434910d0565e101ce3c53accaea03
Satrajit Ghosh <satra@mit.edu>
2010-05-18 03:12:00 +0000

    added newsegment to spm init
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@1473 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/interfaces/spm/__init__.py
+++ nipype/interfaces/spm/__init__.py
@@ -3,7 +3,8 @@
 from nipype.interfaces.spm.base import (Info, SPMCommand, logger,
                                         scans_for_fname, scans_for_fnames)
 from nipype.interfaces.spm.preprocess import (SliceTiming, Realign, Coregister,
-                                              Normalize, Segment, Smooth)
+                                              Normalize, Segment, Smooth,
+                                              NewSegment)
 from nipype.interfaces.spm.model import (Level1Design, EstimateModel,
                                          EstimateContrast, OneSampleTTest,
                                          TwoSampleTTest, MultipleRegression,
