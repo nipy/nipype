2e47d47dfa72ecc96128c849472d25b60a36428c
Krzysztof Gorgolewski <krzysztof.gorgolewski@gmail.com>
2010-06-23 15:00:44 +0000

    added argstr and position support
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@1657 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/interfaces/slicer.py
+++ nipype/interfaces/slicer.py
@@ -34,6 +34,7 @@ class SlicerCommandLine(CommandLine):
     def __init__(self, name, **inputs):
         super(SlicerCommandLine, self).__init__(command= os.path.join(path, name), name= name, **inputs)
         dom = self._grab_xml()
+        self._outputs_filenames = {}
         
         inputs = []
         outputs = []
@@ -42,18 +43,44 @@ class SlicerCommandLine(CommandLine):
             for param in paramGroup.childNodes:
                 if param.nodeName in ['label', 'description', '#text']:
                     continue
-                type = {'integer': traits.Int(), 'double': traits.Float(), 'image': File(), 'transform': File()}[param.nodeName]
+                traitsParams = {}
+                
+                longFlagNode = param.getElementsByTagName('longflag')
+                if longFlagNode:
+                    traitsParams["argstr"] = "--" + longFlagNode[0].firstChild.nodeValue + " " + {'integer': "%d", 'double': "%f", 'image': "%s", 'transform': "%s"}[param.nodeName]
+                
+                index = param.getElementsByTagName('index')
+                if index:
+                    traitsParams["position"] = index[0].firstChild.nodeValue
+                    
+                desc = param.getElementsByTagName('description')
+                if index:
+                    traitsParams["desc"] = desc[0].firstChild.nodeValue
+                    
+                type = {'integer': traits.Int, 'double': traits.Float, 'image': File, 'transform': File}[param.nodeName](**traitsParams)
                 if param.nodeName in ['file', 'directory', 'image', 'transform'] and param.getElementsByTagName('channel')[0].firstChild.nodeValue == 'output':
+                    traitsParams["genfile"] = True
+                    self._outputs_filenames[param.getElementsByTagName('name')[0].firstChild.nodeValue] = self._gen_filename_from_param(param)
                     outputs.append(param)
-                else:
-                    self.inputs.add_trait(param.getElementsByTagName('name')[0].firstChild.nodeValue, type)
-                    print param.getElementsByTagName('name')[0].firstChild.nodeValue
-        
-        print inputs
+
+                self.inputs.add_trait(param.getElementsByTagName('name')[0].firstChild.nodeValue, type)
         print outputs
         
+    def _gen_filename(self, name):
+        if name in self._outputs_filenames:
+            return self._outputs_filenames[name]
+        return None
+    
+    def _gen_filename_from_param(self,param):
+        base = param.getElementsByTagName('name')[0].firstChild.nodeValue
+        fileExtensions = param.getAttribute("fileExtensions")
+        if fileExtensions:
+            ext = fileExtensions
+        else:
+            ext = {'image': '.nii', 'transform': '.txt'}[param.nodeName]
+        return os.path.abspath(base + ext)
         
 if __name__ == "__main__":
     test = SlicerCommandLine(name="AffineRegistration")
     test.inputs.Iterations = 2
-    test._grab_xml()
+    print test.cmdline
