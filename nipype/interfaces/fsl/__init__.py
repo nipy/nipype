3a56d349c3c820eb8c3a1a07079d386ed6467156
Satrajit Ghosh <satra@mit.edu>
2010-06-26 16:46:11 +0000

    changed init to reflect REGFILT name change
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@1685 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/interfaces/fsl/__init__.py
+++ nipype/interfaces/fsl/__init__.py
@@ -15,7 +15,7 @@ from nipype.interfaces.fsl.model import (Level1Design, FEAT, FEATModel,
                                          FILMGLS, FEATRegister, FLAMEO, ContrastMgr,
                                          L2Model, SMM, MELODIC)
 from nipype.interfaces.fsl.utils import (Smooth, Merge, ExtractROI, Split,
-                                         ImageMaths, ImageMeants, REGFILT)
+                                         ImageMaths, ImageMeants, FilterRegressor)
 from nipype.interfaces.fsl.dti import (EddyCorrect, BEDPOSTX, DTIFit, TBSS2Reg,
                                        TBSS1Preproc, TBSS3Postreg,
                                        TBSS4Prestats, Randomise,
