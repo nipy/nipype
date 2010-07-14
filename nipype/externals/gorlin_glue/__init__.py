784e882a885c0bc5d9f2572401af902055788b78
Satrajit Ghosh <satra@mit.edu>
2010-07-14 14:00:45 +0000

    removed unused externals
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@1845 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/externals/gorlin_glue/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-# emacs: -*- coding: utf-8; mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
-# vi: set fileencoding=utf-8 ft=python sts=4 ts=4 sw=4 et:
-"""Package w/ modules to help interface with various 3rd party software tools
-"""
-pass
