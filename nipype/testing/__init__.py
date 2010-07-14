6190fa1e8ac296148307107e4031b03230072a56
Cindee Madison <cindee@berkeley.edu>
2010-07-14 20:29:00 +0000

    added example_data function for use with doctests based on suggestion by Nate Smith
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@1848 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/testing/__init__.py
+++ nipype/testing/__init__.py
@@ -29,7 +29,20 @@ from lightunit import ParametricTestCase, parametric
 filepath = os.path.abspath(__file__)
 basedir = os.path.dirname(filepath)
 
+
 funcfile = os.path.join(basedir, 'data', 'functional.nii')
 anatfile = funcfile
 template = funcfile
 transfm = funcfile
+
+def example_data(infile='functional.nii'):
+    """returns path to empty example data files for doc tests
+    it will raise an exception if filename is not in the directory"""
+   
+    filepath = os.path.abspath(__file__)
+    basedir = os.path.dirname(filepath)
+    outfile = os.path.join(basedir, 'data', infile)
+    if not os.path.exists(outfile):
+        raise IOError('%s empty data file does NOT exist'%(outfile))
+    
+    return outfile 
