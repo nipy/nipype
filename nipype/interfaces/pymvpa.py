7598db1af008faec2bb9cd846aeb7c735ffd717f
Yaroslav Halchenko <yoh@dartmouth.edu>
2010-07-19 15:38:58 +0000

    removing duplicate modelines in few files (probably introduced by me :-/)
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@1871 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/interfaces/pymvpa.py
+++ nipype/interfaces/pymvpa.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-#emacs: -*- mode: python-mode; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
-#ex: set sts=4 ts=4 sw=4 noet:
 
 
 import os
