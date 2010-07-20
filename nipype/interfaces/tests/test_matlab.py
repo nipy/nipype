9e9eb727f9ae7fa7068273f6556fb5fee7016485
Satrajit Ghosh <satra@mit.edu>
2010-07-20 02:45:16 +0000

    fixed matlab tests for ubuntu systems with external MATLABCMD
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@1873 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/interfaces/tests/test_matlab.py
+++ nipype/interfaces/tests/test_matlab.py
@@ -17,51 +17,53 @@ except:
 res = CommandLine(command='which', args=matlab_cmd).run()
 matlab_path = res.runtime.stdout.strip()
 
-matlab_command = ''
 no_matlab = True
 if matlab_path != '':
-    matlab_command = '%s' % \
-        matlab_path.split(os.path.sep)[-1]
     no_matlab = False
+    mlab.MatlabCommand.set_default_matlab_cmd(matlab_cmd)
     
 # If a test requires matlab, prefix it with the skipif decorator like
 # below.  Must import skipif from nipype.testing
 #
 #@skipif(no_matlab)
-# def test_func():
-#     pass
+#def test_func():
+#    pass
 
+@parametric
+@skipif(no_matlab)
 def test_cmdline():
     basedir = mkdtemp()
-
     mi = mlab.MatlabCommand(script='whos',
                             script_file='testscript')
                                 
-    yield assert_equal, mi.cmdline, \
-        'matlab -nodesktop -nosplash -singleCompThread -r "fprintf(1,\'Executing code at %s:\\n\',datestr(now));ver,try,whos,catch ME,ME,ME.stack,fprintf(\'%s\\n\',ME.message);fprintf(2,\'<MatlabScriptException>\');fprintf(2,\'%s\\n\',ME.message);fprintf(2,\'File:%s\\nName:%s\\nLine:%d\\n\',ME.stack.file,ME.stack.name,ME.stack.line);fprintf(2,\'</MatlabScriptException>\');end;;exit"'
+    yield assert_equal(mi.cmdline, \
+        matlab_cmd + ' -nodesktop -nosplash -singleCompThread -r "fprintf(1,\'Executing code at %s:\\n\',datestr(now));ver,try,whos,catch ME,ME,ME.stack,fprintf(\'%s\\n\',ME.message);fprintf(2,\'<MatlabScriptException>\');fprintf(2,\'%s\\n\',ME.message);fprintf(2,\'File:%s\\nName:%s\\nLine:%d\\n\',ME.stack.file,ME.stack.name,ME.stack.line);fprintf(2,\'</MatlabScriptException>\');end;;exit"')
  
-    yield assert_equal, mi.inputs.script, 'whos'
-    yield assert_equal, mi.inputs.script_file, 'testscript'
+    yield assert_equal(mi.inputs.script, 'whos')
+    yield assert_equal(mi.inputs.script_file, 'testscript')
     path_exists = os.path.exists(os.path.join(basedir,'testscript.m'))
-    yield assert_false, path_exists
+    yield assert_false(path_exists)
     rmtree(basedir)
 
+@parametric
+@skipif(no_matlab)
 def test_mlab_inputspec():
     spec = mlab.MatlabInputSpec()
     for k in ['paths', 'script', 'nosplash', 'mfile', 'logfile', 'script_file',
               'nodesktop']:
-        yield assert_true, k in spec.copyable_trait_names()
-    yield assert_true, spec.nodesktop
-    yield assert_true, spec.nosplash
-    yield assert_false, spec.mfile
-    yield assert_equal, spec.script_file, 'pyscript.m'
+        yield assert_true(k in spec.copyable_trait_names())
+    yield assert_true(spec.nodesktop)
+    yield assert_true(spec.nosplash)
+    yield assert_false(spec.mfile)
+    yield assert_equal(spec.script_file, 'pyscript.m')
 
 @parametric
+@skipif(no_matlab)
 def test_mlab_init():
     yield assert_equal(mlab.MatlabCommand._cmd, 'matlab')
     yield assert_equal(mlab.MatlabCommand.input_spec, mlab.MatlabInputSpec)
 
-    yield assert_equal(mlab.MatlabCommand().cmd, mlab.MatlabCommand._cmd)
+    yield assert_equal(mlab.MatlabCommand().cmd, matlab_cmd)
     mc = mlab.MatlabCommand(matlab_cmd='foo_m')
     yield assert_equal(mc.cmd, 'foo_m')
 
@@ -82,9 +84,11 @@ def test_run_interface():
     os.chdir(cwd)
     rmtree(basedir)
     
+@parametric
+@skipif(no_matlab)
 def test_set_matlabcmd():
     mi = mlab.MatlabCommand()
     mi.set_default_matlab_cmd('foo')
-    yield assert_equal, mi._default_matlab_cmd, 'foo'
-    mi.set_default_matlab_cmd(matlab_command)
+    yield assert_equal(mi._default_matlab_cmd, 'foo')
+    mi.set_default_matlab_cmd(matlab_cmd)
     
