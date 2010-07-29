314bdd35beacbe120682f006b147b6c9de1dc71a
Satrajit Ghosh <satra@mit.edu>
2010-07-29 13:22:34 +0000

    updated CHANGES
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/nipype/trunk@1923 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- nipype/version.py
+++ nipype/version.py
@@ -1,6 +1,6 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-version = '0.3'
+version = '0.3.1'
 release  = True
 
 # Return the svn version as a string, raise a ValueError otherwise
