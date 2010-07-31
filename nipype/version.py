659ec6de73237f01a9b8973550566a3e71da9c15
Satrajit Ghosh <satra@mit.edu>
2010-07-30 23:56:53 -0400

    adding code from mvpa to get git version

--- nipype/version.py
+++ nipype/version.py
@@ -1,10 +1,49 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-version = '0.3.1'
-release  = True
+
+import time, sys, os, subprocess
+from tempfile import mkstemp
+
+import nipype
+
+version = '0.4'
+release = False
 
 # Return the svn version as a string, raise a ValueError otherwise
 # This code was copied from numpy trunk, revision 6873, and modified slightly
+
+__all__ = ['get_nipype_gitversion']
+
+
+def get_nipype_gitversion():
+    """PyMVPA version as reported by git.
+
+    Returns
+    -------
+    None or str
+      Version of PyMVPA according to git.
+    """
+    gitpath = os.path.realpath(os.path.join(os.path.dirname(nipype.__file__), os.path.pardir))
+    gitpathgit = os.path.join(gitpath, '.git')
+    if not os.path.exists(gitpathgit):
+        return None
+    ver = None
+    try:
+        (tmpd, tmpn) = mkstemp('nipype', 'git')
+        retcode = subprocess.call(['git',
+                                   '--git-dir=%s' % gitpathgit,
+                                   '--work-tree=%s' % gitpath,
+                                   'describe', '--abbrev=4', 'HEAD'
+                                   ],
+                                  stdout=tmpd,
+                                  stderr=subprocess.STDOUT)
+        outline = open(tmpn, 'r').readlines()[0].strip()
+        if outline.startswith('upstream/'):
+            ver = outline.replace('upstream/', '')
+    finally:
+        os.remove(tmpn)
+    return ver
+
 def svn_version():
     # Placed imports here (instead of top of module) so they're not
     # imported in released code
@@ -31,6 +70,6 @@ def svn_version():
 
 if not release:
     version += '.dev'
-    svnver = svn_version()
+    svnver = get_nipype_gitversion()
     version += svnver
 
