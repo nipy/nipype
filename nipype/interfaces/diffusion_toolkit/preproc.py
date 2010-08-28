cdbf67ab90fabb2b1e0d325917d6a9f9dd42d34a
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2010-08-28 17:48:51 -0400

    Added DTI tracker and spline filter

--- nipype/interfaces/diffusion_toolkit/preproc.py
+++ nipype/interfaces/diffusion_toolkit/preproc.py
@@ -10,7 +10,9 @@
 
 """
 import re
-from nipype.utils.filemanip import fname_presuffix
+from nipype.utils.filemanip import fname_presuffix, split_filename, copyfile
+import os
+from nipype.utils.misc import isdefined
 __docformat__ = 'restructuredtext'
 
 from nipype.interfaces.base import (TraitedSpec, File, traits, CommandLine,
@@ -78,7 +80,6 @@ class DTIRecon(CommandLine):
         bvecs_z = [val for val in  re.split('\s+', bvecs_f.readline().strip())]
         bvecs_f.close()
         gradient_matrix_f = open(self._gradient_matrix_file, 'w')
-        print len(bvals), len(bvecs_x), len(bvecs_y), len(bvecs_z)
         for i in range(len(bvals)):
             gradient_matrix_f.write("%s, %s, %s, %s\n"%(bvecs_x[i], bvecs_y[i], bvecs_z[i], bvals[i]))
         gradient_matrix_f.close()
@@ -89,17 +90,99 @@ class DTIRecon(CommandLine):
         output_type = self.inputs.output_type
 
         outputs = self.output_spec().get()
-        outputs['ADC'] = fname_presuffix("",  prefix=out_prefix, suffix='_adc.'+ output_type)
-        outputs['B0'] = fname_presuffix("",  prefix=out_prefix, suffix='_b0.'+ output_type)
-        outputs['L1'] = fname_presuffix("",  prefix=out_prefix, suffix='_e1.'+ output_type)
-        outputs['L2'] = fname_presuffix("",  prefix=out_prefix, suffix='_e2.'+ output_type)
-        outputs['L3'] = fname_presuffix("",  prefix=out_prefix, suffix='_e3.'+ output_type)
-        outputs['exp'] = fname_presuffix("",  prefix=out_prefix, suffix='_exp.'+ output_type)
-        outputs['FA'] = fname_presuffix("",  prefix=out_prefix, suffix='_fa.'+ output_type)
-        outputs['FA_color'] = fname_presuffix("",  prefix=out_prefix, suffix='_fa_color.'+ output_type)
-        outputs['tensor'] = fname_presuffix("",  prefix=out_prefix, suffix='_tensor.'+ output_type)
-        outputs['V1'] = fname_presuffix("",  prefix=out_prefix, suffix='_v1.'+ output_type)
-        outputs['V2'] = fname_presuffix("",  prefix=out_prefix, suffix='_v2.'+ output_type)
-        outputs['V3'] = fname_presuffix("",  prefix=out_prefix, suffix='_v3.'+ output_type)
+        outputs['ADC'] = os.path.abspath(fname_presuffix("",  prefix=out_prefix, suffix='_adc.'+ output_type))
+        outputs['B0'] = os.path.abspath(fname_presuffix("",  prefix=out_prefix, suffix='_b0.'+ output_type))
+        outputs['L1'] = os.path.abspath(fname_presuffix("",  prefix=out_prefix, suffix='_e1.'+ output_type))
+        outputs['L2'] = os.path.abspath(fname_presuffix("",  prefix=out_prefix, suffix='_e2.'+ output_type))
+        outputs['L3'] = os.path.abspath(fname_presuffix("",  prefix=out_prefix, suffix='_e3.'+ output_type))
+        outputs['exp'] = os.path.abspath(fname_presuffix("",  prefix=out_prefix, suffix='_exp.'+ output_type))
+        outputs['FA'] = os.path.abspath(fname_presuffix("",  prefix=out_prefix, suffix='_fa.'+ output_type))
+        outputs['FA_color'] = os.path.abspath(fname_presuffix("",  prefix=out_prefix, suffix='_fa_color.'+ output_type))
+        outputs['tensor'] = os.path.abspath(fname_presuffix("",  prefix=out_prefix, suffix='_tensor.'+ output_type))
+        outputs['V1'] = os.path.abspath(fname_presuffix("",  prefix=out_prefix, suffix='_v1.'+ output_type))
+        outputs['V2'] = os.path.abspath(fname_presuffix("",  prefix=out_prefix, suffix='_v2.'+ output_type))
+        outputs['V3'] = os.path.abspath(fname_presuffix("",  prefix=out_prefix, suffix='_v3.'+ output_type))
 
         return outputs
+    
+class DTITrackerInputSpec(CommandLineInputSpec):
+    tensor_file = File(exists=True, desc="reconstructed tensor file" )
+    input_type = traits.Enum('nii', 'analyze', 'ni1', 'nii.gz', desc="""input and output file type. accepted values are:
+analyze -> analyze format 7.5
+ni1     -> nifti format saved in seperate .hdr and .img file
+nii     -> nifti format with one .nii file
+nii.gz  -> nifti format with compression
+default type is 'nii'""", argstr = "-it %s")
+    tracking_method = traits.Enum('fact', 'rk2', 'tl', 'sl', desc="""fact -> use FACT method for tracking. this is the default method.
+rk2  -> use 2nd order runge-kutta method for tracking.
+tl   -> use tensorline method for tracking.
+sl   -> use interpolated streamline method with fixed step-length""", argstr="-%s")
+    step_length = traits.Float(desc="""set step length, in the unit of minimum voxel size.
+default value is 0.5 for interpolated streamline method
+and 0.1 for other methods""", argstr="-l %f")
+    angle_threshold = traits.Float(desc="set angle threshold. default value is 35 degree", argstr="-at %f")
+    angle_threshold_weight = traits.Float(desc="set angle threshold weighting factor. weighting will be be applied \
+on top of the angle_threshold", argstr = "-atw %f")
+    random_seed = traits.Int(desc = "use random location in a voxel instead of the center of the voxel \
+          to seed. can also define number of seed per voxel. default is 1", argstr="-rseed")
+    invert_x = traits.Bool(desc="invert x component of the vector", argstr = "-ix")
+    invert_y = traits.Bool(desc="invert y component of the vector", argstr = "-iy")
+    invert_z = traits.Bool(desc="invert z component of the vector", argstr = "-iz")
+    swap_xy = traits.Bool(desc="swap x & y vectors while tracking", argstr = "-sxy")
+    swap_yz = traits.Bool(desc="swap y & z vectors while tracking", argstr = "-syz")
+    swap_xz = traits.Bool(desc="swap x & z vectors while tracking", argstr = "-sxz")
+    mask1_file = File(desc="first mask image", mandatory=True, argstr="-m %s", position=2)
+    mask1_threshold = traits.Float(desc="threshold value for the first mask image, if not given, the program will \
+try automatically find the threshold", position=3)
+    mask2_file = File(desc="second mask image", argstr="-m2 %s", position=4)
+    mask2_threshold = traits.Float(desc="threshold value for the second mask image, if not given, the program will \
+try automatically find the threshold", position=5)
+    input_data_prefix = traits.Str("dti", desc="for internal naming use only", position=0, argstr="%s", usedefault=True)
+    output_file = File("tracks.trk", "file containing tracks", argstr="%s", position=1, usedefault=True)
+    output_mask = File(desc="output a binary mask file in analyze format", argstr="-om %s")
+    primary_vector = traits.Enum('v2', 'v3', desc = "which vector to use for fibre tracking: v2 or v3. If not set use v1", argstr="-%s")
+
+class DTITrackerOutputSpec(TraitedSpec):
+    track_file = File(exists=True)
+    mask_file = File(exists=True)
+    
+class DTITracker(CommandLine):
+    input_spec=DTITrackerInputSpec
+    output_spec=DTITrackerOutputSpec
+    
+    _cmd = 'dti_tracker'
+    
+    def _run_interface(self, runtime):
+        _, _, ext = split_filename(self.inputs.tensor_file)
+        copyfile(self.inputs.tensor_file, os.path.abspath(self.inputs.input_data_prefix + "_tensor" + ext), copy=False)
+        
+        return super(DTITracker, self)._run_interface(runtime)
+    
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        outputs['track_file'] = os.path.abspath(self.inputs.output_file)
+        if isdefined(self.inputs.output_mask) and self.inputs.output_mask:
+            outputs['mask_file'] = os.path.abspath(self.inputs.output_mask)
+        
+        return outputs
+            
+class SplineFilterInputSpec(CommandLineInputSpec):
+    track_file = File(exists=True, desc="file containing tracks to be filtered", position=0, argstr="%s", mandatory=True)
+    step_length = traits.Float(desc="in the unit of minimum voxel size", position=1, argstr="%f", mandatory=True)
+    output_file = File("spline_tracks.trk", desc="target file for smoothed tracks", position=2, argstr="%s", usedefault=True)
+    
+class SplineFilterOutputSpec(TraitedSpec):
+    smoothed_track_file = File(exists=True)
+    
+class SplineFilter(CommandLine):
+    input_spec=SplineFilterInputSpec
+    output_spec=SplineFilterOutputSpec
+    
+    _cmd = "spline_filter"
+    
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        outputs['smoothed_track_file'] = os.path.abspath(self.inputs.output_file)
+        return outputs
+    
+    
