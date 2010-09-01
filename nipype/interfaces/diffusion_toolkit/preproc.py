c25a13a341c9b6f3acb5ab8f8eb1343dd7de9311
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2010-09-01 16:17:43 -0400

    corrected gradient matrix generation

--- nipype/interfaces/diffusion_toolkit/preproc.py
+++ nipype/interfaces/diffusion_toolkit/preproc.py
@@ -18,20 +18,6 @@ __docformat__ = 'restructuredtext'
 from nipype.interfaces.base import (TraitedSpec, File, traits, CommandLine,
     CommandLineInputSpec)
 
-def _create_gradient_matrix(bvecs_file, bvals_file):
-    _gradient_matrix_file = 'gradient_matrix.txt'
-    bvals = [val for val in  re.split('\s+', open(bvals_file).readline().strip())]
-    bvecs_f = open(bvecs_file)
-    bvecs_x = [val for val in  re.split('\s+', bvecs_f.readline().strip())]
-    bvecs_y = [val for val in  re.split('\s+', bvecs_f.readline().strip())]
-    bvecs_z = [val for val in  re.split('\s+', bvecs_f.readline().strip())]
-    bvecs_f.close()
-    gradient_matrix_f = open(_gradient_matrix_file, 'w')
-    for i in range(len(bvals)):
-        gradient_matrix_f.write("%s, %s, %s, %s\n"%(bvecs_x[i], bvecs_y[i], bvecs_z[i], bvals[i]))
-    gradient_matrix_f.close()
-    return _gradient_matrix_file
-
 class DTIReconInputSpec(CommandLineInputSpec):
     dwi = File(desc='Input diffusion volume', argstr='%s',exists=True, mandatory=True,position=1)
     out_prefix = traits.Str("dti", desc='Output file prefix', argstr='%s', usedefault=True,position=2)
@@ -78,9 +64,23 @@ class DTIRecon(CommandLine):
     
     _cmd = 'dti_recon'
     
+    def _create_gradient_matrix(self, bvecs_file, bvals_file):
+        _gradient_matrix_file = 'gradient_matrix.txt'
+        bvals = [val for val in  re.split('\s+', open(bvals_file).readline().strip())]
+        bvecs_f = open(bvecs_file)
+        bvecs_x = [val for val in  re.split('\s+', bvecs_f.readline().strip())]
+        bvecs_y = [val for val in  re.split('\s+', bvecs_f.readline().strip())]
+        bvecs_z = [val for val in  re.split('\s+', bvecs_f.readline().strip())]
+        bvecs_f.close()
+        gradient_matrix_f = open(_gradient_matrix_file, 'w')
+        for i in range(len(bvals)):
+            gradient_matrix_f.write("%s, %s, %s, %s\n"%(bvecs_x[i], bvecs_y[i], bvecs_z[i], bvals[i]))
+        gradient_matrix_f.close()
+        return _gradient_matrix_file
+    
     def _format_arg(self, name, spec, value):
         if name == "bvecs":
-            new_val = _create_gradient_matrix(self.inputs.bvecs, self.inputs.bvals)
+            new_val = self._create_gradient_matrix(self.inputs.bvecs, self.inputs.bvals)
             return super(DTIRecon, self)._format_arg("bvecs", spec, new_val)
         return super(DTIRecon, self)._format_arg(name, spec, value)
 
@@ -226,9 +226,25 @@ class HARDIMat(CommandLine):
     
     _cmd = 'hardi_mat'
     
+    def _create_gradient_matrix(self, bvecs_file, bvals_file):
+        _gradient_matrix_file = 'gradient_matrix.txt'
+        bvals = [val for val in  re.split('\s+', open(bvals_file).readline().strip())]
+        bvecs_f = open(bvecs_file)
+        bvecs_x = [val for val in  re.split('\s+', bvecs_f.readline().strip())]
+        bvecs_y = [val for val in  re.split('\s+', bvecs_f.readline().strip())]
+        bvecs_z = [val for val in  re.split('\s+', bvecs_f.readline().strip())]
+        bvecs_f.close()
+        gradient_matrix_f = open(_gradient_matrix_file, 'w')
+        for i in range(len(bvals)):
+            if int(bvals[i]) == 0:
+                continue
+            gradient_matrix_f.write("%s %s %s\n"%(bvecs_x[i], bvecs_y[i], bvecs_z[i]))
+        gradient_matrix_f.close()
+        return _gradient_matrix_file
+    
     def _format_arg(self, name, spec, value):
         if name == "bvecs":
-            new_val = _create_gradient_matrix(self.inputs.bvecs, self.inputs.bvals)
+            new_val = self._create_gradient_matrix(self.inputs.bvecs, self.inputs.bvals)
             return super(HARDIMat, self)._format_arg("bvecs", spec, new_val)
         return super(HARDIMat, self)._format_arg(name, spec, value)
 
