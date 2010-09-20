7e7ff2a002e0e7e4ca94ec4b176fbaabe849b0a5
Michael Waskom <mwaskom@mit.edu>
2010-09-20 19:10:47 -0400

    Adding test for mri_robust_register

--- nipype/interfaces/freesurfer/tests/test_preprocess.py
+++ nipype/interfaces/freesurfer/tests/test_preprocess.py
@@ -1,9 +1,13 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
+import os
+from shutil import rmtree
+import nipype.externals.pynifti as nif
+import numpy as np
+from tempfile import mkdtemp
 from nipype.testing import (assert_equal, assert_false, assert_true, 
                             assert_raises, skipif)
 import nipype.interfaces.freesurfer as freesurfer
-
 
 def no_freesurfer():
     if freesurfer.Info().version is None:
@@ -244,3 +248,49 @@ def test_unpacksdicomdir():
     for key, metadata in input_map.items():
         for metakey, value in metadata.items():
             yield assert_equal, getattr(instance.inputs.traits()[key], metakey), value
+
+
+def create_files_in_directory():
+    outdir = mkdtemp()
+    cwd = os.getcwd()
+    os.chdir(outdir)
+    filelist = ['a.nii','b.nii']
+    for f in filelist:
+        hdr = nif.Nifti1Header()
+        shape = (3,3,3,4)
+        hdr.set_data_shape(shape)
+        img = np.random.random(shape)
+        nif.save(nif.Nifti1Image(img,np.eye(4),hdr),
+                 os.path.join(outdir,f))
+    return filelist, outdir, cwd
+    
+def clean_directory(outdir, old_wd):
+    if os.path.exists(outdir):
+        rmtree(outdir)
+    os.chdir(old_wd)
+
+@skipif(no_freesurfer)
+def test_robustregister():
+    filelist, outdir, cwd = create_files_in_directory()
+
+    reg = freesurfer.RobustRegister()
+
+    # make sure command gets called
+    yield assert_equal, reg.cmd, 'mri_robust_register'
+
+    # test raising error with mandatory args absent
+    yield assert_raises, ValueError, reg.run
+
+    # .inputs based parameters setting
+    reg.inputs.source_file = filelist[0]
+    reg.inputs.target_file = filelist[1]
+    reg.inputs.auto_sens = True
+    yield assert_equal, reg.cmdline, ('mri_robust_register '
+        '--satit --lta %s_robustreg.lta --mov %s --dst %s'%(filelist[0][:-4],filelist[0],filelist[1]))
+
+    # constructor based parameter setting
+    reg2 = freesurfer.RobustRegister(source_file=filelist[0],target_file=filelist[1],outlier_sens=3.0,
+                                     out_reg_file='foo.lta', half_targ=True)
+    yield assert_equal, reg2.cmdline, ('mri_robust_register --halfdst %s_halfway.nii --lta foo.lta '
+        '--sat 3.0000 --mov %s --dst %s'%(os.path.join(outdir,filelist[1][:-4]),filelist[0],filelist[1]))
+    clean_directory(outdir, cwd)
