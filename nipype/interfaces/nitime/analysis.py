4476584daab32c78cc64f213b64fed0d7ba2f440
arokem <arokem@berkeley.edu>
2010-09-22 13:27:24 -0700

    More work on the nitime coherence interface

--- nipype/interfaces/nitime/analysis.py
+++ nipype/interfaces/nitime/analysis.py
@@ -10,26 +10,86 @@ Interfaces to functionality from nitime for time-series analysis of fmri data
 
 """
 
+from nipype.utils.misc import package_check
+
 from nipype.interfaces.base import (TraitedSpec, File, InputMultiPath,
                                     OutputMultiPath, Undefined, traits,
                                     BaseInterface)
 
 from nitime.analysis import CoherenceAnalyzer
-from nitime import TimeSeries
+from nitime.timeseries import TimeSeries
+
+package_check('nitime')
+package_check('matplotlib')
 
 class CoherenceAnalyzerInputSpec(TraitedSpec):
 
     #Input either csv file, or time-series object and use _xor_inputs to
     #discriminate
+    _xor_inputs('in_file','in_TS')
+    in_file = File(desc=('csv file with ROIs on the columns and ',
+                   'time-points on the rows. ROI names at the top row'),
+                   exists=True,
+                   requires=('sampling_rate'))
+    
+    #If you gave just a file name, you need to specify the sampling_rate:
+    TR = traits.Float(desc=('The TR used to collect the data',
+                            'in your csv file <in_file>'))
 
+    in_TS = traits.Any(desc='a nitime TimeSeries object')
+
+    NFFT = traits.Range(low=32,value=64,
+                        desc=('This is the size of the window used for ',
+                        'the spectral estimation. Use values between ',
+                        '32 and the number of samples in your time-series.',
+                        '(Defaults to 64.)'))
+    n_overlap = traits.Range(low=0,value=0,
+                             desc=('The number of samples which overlap',
+                             'between subsequent windows.(Defaults to 0)'))
+    
+    frequency_range = traits.ListFloat(value=[0.02, 0.15],
+                                       minlen=2,
+                                       maxlen=2,
+                                       desc=('The range of frequencies over',
+                                             'which the analysis will average.',
+                                             '[low,high] (Default [0.02,0.15]'))
+
+    output_csv_file = traits.File(desc='File to write output')
+    output_figure_file = traits.File(desc='File to write output figures')
+    figure_type = traits.Enum('matrix','network',
+                              desc=("The type of plot to generate, where ",
+                                    "'matrix' denotes a matrix image and",
+                                    "'network' denotes a graph representation"))
     
 class CoherenceAnalyzerOutputSpec(TraitedSpec):
+    coherence_array = traits.Array(desc=('The pairwise coherence values between',
+                                   'the ROIs'))
+    timedelay_array = traits.Array(desc=('The pairwise time delays between the',
+                                         'ROIs (in seconds)'))
+
+    coherence_csv = traits.File(desc = ('A csv file containing the pairwise ',
+                                        'coherence values'))
+    timedelay_csv = traits.File(desc = ('A csv file containing the pairwise ',
+                                        'time delay values'))
+
+    coherence_fig = traits.File(desc = ('Figure representing coherence values'))
+    timedelay_fig = traits.File(desc = ('Figure representing coherence values'))
+
+    
 class CoherenceAnalyzer(BaseInterface):
 
-    # Use the get_attr to pull out the input values from the InputSpec
+    input_spec = CoherenceAnalyzerInputSpec
+    output_spec = CoherenceAnayzerOutputSpec
 
+    def _read_csv(self):
 
+        return roi_labels,TS
+        
     #Rewrite _run_interface, but not run
+    def _run_interface(self,runtime):
+        lb, ub = self.inputs.frequency_range
+        if
+    
     #Rewrite _list_outputs (look at BET)
     
     def _list_outputs(self):
