a5344d9f70e3952a59f750a813d0321e04b03970
arokem <arokem@berkeley.edu>
2010-09-29 12:16:02 -0700

    Implemented _read_csv, and several changes following testing

--- nipype/interfaces/nitime/tests/test_nitime.py
+++ nipype/interfaces/nitime/tests/test_nitime.py
@@ -3,17 +3,24 @@
 import os
 import tempfile
 import shutil
-from matplotlib import mlab.csv2rec as csv2rec
 
 from nipype.testing import (assert_equal, assert_not_equal, assert_raises,
                             with_setup, TraitError, parametric, skipif)
 
+from nipype.testing import example_data
+
+import nipype.interfaces.nitime as nitime 
 
 def test_read_csv():
     """Test that reading the data from csv file gives you back a reasonable
     time-series object """
 
-    #XXX need to finish this:
-    data_rec = csv2rec('data/fmri_timeseries.csv')
-    
-    
+    CA = nitime.CoherenceAnalyzer()
+    CA.inputs.TR = 1.89 # bogus value just to pass traits test
+    CA.inputs.in_file = example_data('fmri_timeseries_nolabels.csv')
+    yield assert_raises,ValueError,CA._read_csv 
+
+    CA.inputs.in_file = example_data('fmri_timeseries.csv')
+    rec_array = CA._read_csv()
+    yield assert_equal, rec_array['wm'][0],10125.9
+
