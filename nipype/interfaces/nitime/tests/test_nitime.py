a82183e29a53a9d14e811ca4b7e6f91c4cd04bdc
arokem <arokem@berkeley.edu>
2010-11-03 13:28:11 -0700

    Finished first pass of coherence analysis. Outputs now include arrays, csv files and matplotlib-generated figures.

--- nipype/interfaces/nitime/tests/test_nitime.py
+++ nipype/interfaces/nitime/tests/test_nitime.py
@@ -4,12 +4,19 @@ import os
 import tempfile
 import shutil
 
+import numpy as np
+
 from nipype.testing import (assert_equal, assert_not_equal, assert_raises,
                             with_setup, TraitError, parametric, skipif)
 
 from nipype.testing import example_data
 
 import nipype.interfaces.nitime as nitime 
+
+import nitime.analysis as nta
+import nitime.timeseries as ts
+
+from matplotlib.mlab import csv2rec
 
 def test_read_csv():
     """Test that reading the data from csv file gives you back a reasonable
@@ -24,3 +31,42 @@ def test_read_csv():
     data,roi_names = CA._read_csv()
     yield assert_equal, data[0][0],10125.9
     yield assert_equal, roi_names[0],'WM' 
+
+def test_coherence_analysis():
+    """Test that the coherence analyzer works """
+
+    #This is the nipype interface analysis:
+    CA = nitime.CoherenceAnalyzer()
+    CA.inputs.TR = 1.89
+    CA.inputs.in_file = example_data('fmri_timeseries.csv')
+    o = CA.run()
+    yield assert_equal,o.outputs.coherence_array.shape,(31,31)
+
+    #This is the nitime analysis:
+    TR=1.89
+    data_rec = csv2rec(example_data('fmri_timeseries.csv'))
+    roi_names= np.array(data_rec.dtype.names)
+    n_samples = data_rec.shape[0]
+    data = np.zeros((len(roi_names),n_samples))
+
+    for n_idx, roi in enumerate(roi_names):
+       data[n_idx] = data_rec[roi]
+
+    T = ts.TimeSeries(data,sampling_interval=TR)
+
+    yield assert_equal,CA._csv2ts().data,T.data
+
+    T.metadata['roi'] = roi_names 
+    C = nta.CoherenceAnalyzer(T,method=dict(this_method='welch',
+                                            NFFT=CA.inputs.NFFT,
+                                            n_overlap=CA.inputs.n_overlap))
+
+    freq_idx = np.where((C.frequencies>CA.inputs.frequency_range[0]) *
+                        (C.frequencies<CA.inputs.frequency_range[1]))[0]
+
+    #Extract the coherence and average across these frequency bands: 
+    coh = np.mean(C.coherence[:,:,freq_idx],-1) #Averaging on the last dimension 
+
+    yield assert_equal,o.outputs.coherence_array,coh
+
+    
