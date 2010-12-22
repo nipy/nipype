880504ad94b44f7a7ef5f1be519552e1e59e4c66
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2010-12-22 18:54:22 +0000

    Added static classes generation from slicer XML definitions.

---
+++ nipype/interfaces/slicer/BRAINSROIAuto.py
@@ -0,0 +1,52 @@
+from nipype.interfaces.base import CommandLine, CommandLineInputSpec, TraitedSpec
+import enthought.traits.api as traits
+import os
+from nipype.interfaces.traits import File
+from nipype.utils.misc import isdefined
+
+class BRAINSROIAutoInputSpec(CommandLineInputSpec):
+	inputVolume = File( exists = "True",argstr = "--inputVolume %s")
+	outputROIMaskVolume = traits.Either(traits.Bool, File, argstr = "--outputROIMaskVolume %s")
+	outputClippedVolumeROI = traits.Either(traits.Bool, File, argstr = "--outputClippedVolumeROI %s")
+	otsuPercentileThreshold = traits.Float( argstr = "--otsuPercentileThreshold %f")
+	thresholdCorrectionFactor = traits.Float( argstr = "--thresholdCorrectionFactor %f")
+	closingSize = traits.Float( argstr = "--closingSize %f")
+	ROIAutoDilateSize = traits.Float( argstr = "--ROIAutoDilateSize %f")
+	outputVolumePixelType = traits.Enum("float","short","ushort","int","uint","uchar", argstr = "--outputVolumePixelType %s")
+
+
+class BRAINSROIAutoOutputSpec(TraitedSpec):
+	outputROIMaskVolume = File(exists=True, argstr = "--outputROIMaskVolume %s")
+	outputClippedVolumeROI = File(exists=True, argstr = "--outputClippedVolumeROI %s")
+
+
+class BRAINSROIAuto(CommandLine):
+    
+    input_spec = BRAINSROIAutoInputSpec
+    output_spec = BRAINSROIAutoOutputSpec
+    _cmd = "Slicer3 --launch BRAINSROIAuto "
+    _outputs_filenames = {'outputROIMaskVolume':'outputROIMaskVolume.nii','outputClippedVolumeROI':'outputClippedVolumeROI.nii'}
+    
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        for name in outputs.keys():
+            coresponding_input = getattr(self.inputs, name)
+            if isdefined(coresponding_input):
+                if isinstance(coresponding_input, bool) and coresponding_input == True:
+                    outputs[name] = os.path.abspath(self._outputs_filenames[name])
+                else:
+                    outputs[name] = coresponding_input
+        return outputs
+    
+    def _format_arg(self, name, spec, value):
+        if name in self._outputs_filenames.keys():
+            if isinstance(value, bool):
+                if value == True:
+                    fname = os.path.abspath(self._outputs_filenames[name])
+                else:
+                    return ""
+            else:
+                fname = value
+            return spec.argstr % fname
+        return super(BRAINSROIAuto, self)._format_arg(name, spec, value)
+
---
+++ nipype/interfaces/slicer/BRAINSResample.py
@@ -0,0 +1,52 @@
+from nipype.interfaces.base import CommandLine, CommandLineInputSpec, TraitedSpec
+import enthought.traits.api as traits
+import os
+from nipype.interfaces.traits import File
+from nipype.utils.misc import isdefined
+
+class BRAINSResampleInputSpec(CommandLineInputSpec):
+	inputVolume = File( exists = "True",argstr = "--inputVolume %s")
+	referenceVolume = File( exists = "True",argstr = "--referenceVolume %s")
+	outputVolume = traits.Either(traits.Bool, File, argstr = "--outputVolume %s")
+	pixelType = traits.Enum("float","short","ushort","int","uint","uchar","binary", argstr = "--pixelType %s")
+	deformationVolume = File( exists = "True",argstr = "--deformationVolume %s")
+	warpTransform = File( exists = "True",argstr = "--warpTransform %s")
+	interpolationMode = traits.Enum("NearestNeighbor","Linear","BSpline","WindowedSinc", argstr = "--interpolationMode %s")
+	defaultValue = traits.Float( argstr = "--defaultValue %f")
+	gridSpacing = traits.List("traits.Int", sep = ",",argstr = "--gridSpacing %d")
+
+
+class BRAINSResampleOutputSpec(TraitedSpec):
+	outputVolume = File(exists=True, argstr = "--outputVolume %s")
+
+
+class BRAINSResample(CommandLine):
+    
+    input_spec = BRAINSResampleInputSpec
+    output_spec = BRAINSResampleOutputSpec
+    _cmd = "Slicer3 --launch BRAINSResample "
+    _outputs_filenames = {'outputVolume':'outputVolume.nii'}
+    
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        for name in outputs.keys():
+            coresponding_input = getattr(self.inputs, name)
+            if isdefined(coresponding_input):
+                if isinstance(coresponding_input, bool) and coresponding_input == True:
+                    outputs[name] = os.path.abspath(self._outputs_filenames[name])
+                else:
+                    outputs[name] = coresponding_input
+        return outputs
+    
+    def _format_arg(self, name, spec, value):
+        if name in self._outputs_filenames.keys():
+            if isinstance(value, bool):
+                if value == True:
+                    fname = os.path.abspath(self._outputs_filenames[name])
+                else:
+                    return ""
+            else:
+                fname = value
+            return spec.argstr % fname
+        return super(BRAINSResample, self)._format_arg(name, spec, value)
+
