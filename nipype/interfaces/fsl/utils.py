170210a7fb0a6fc0127a35009063e984d6d91e30
Michael Waskom <mwaskom@mit.edu>
2011-01-17 11:52:12 -0500

    Added SwapDimensions interface

--- nipype/interfaces/fsl/utils.py
+++ nipype/interfaces/fsl/utils.py
@@ -832,3 +832,45 @@ class ConvertXFM(FSLCommand):
         if name == "out_file":
             return self._list_outputs()["out_file"]
         return None
+
+
+class SwapDimensionsInputSpec(FSLCommandInputSpec):
+
+    in_file = File(exists=True, mandatory=True, argstr="%s", position="1",
+                   desc="input image")
+    _dims = ["x","-x","y","-y","z","-z","RL","LR","AP","PA","IS","SI"]
+    new_dims = traits.Tuple(traits.Enum(_dims), traits.Enum(_dims), traits.Enum(_dims),
+                            argstr="%s %s %s", mandatory=True,
+                            desc="3-tuple of new dimension order")
+    out_file = File(genfile=True, argstr="%s", desc="image to write")
+
+class SwapDimensionsOutputSpec(TraitedSpec):
+
+    out_file = File(exists=True, desc="image with new dimensions")
+
+class SwapDimensions(FSLCommand):
+    """Use fslswapdim to alter the orientation of an image.
+
+    This interface accepts a three-tuple corresponding to the new
+    orientation.  You may either provide dimension ids in the form of
+    (-)x, (-)y, or (-z), or nifti-syle dimension codes (RL, LR, AP, PA, IS, SI).
+
+    """
+    _cmd = "fslswapdim"
+    input_spec = SwapDimensionsInputSpec
+    output_spec = SwapDimensionsOutputSpec
+
+    def _list_outputs(self):
+        outputs = self._outputs().get()
+        outputs["out_file"] = self.inputs.out_file
+        if not isdefined(self.inputs.out_file):
+            outputs["out_file"] = fname_presuffix(self.inputs.in_file,
+                                                  suffix="_newdims",
+                                                  use_ext=True,
+                                                  newpath=os.getcwd())
+        return outputs
+
+    def _gen_filename(self, name):
+        if name == "out_file":
+            return self._list_outputs()["out_file"]
+        return None
