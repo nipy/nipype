4520e987525be0aa0e87fc28d8e50a5f6836351d
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2011-02-08 16:16:01 +0000

    added reorient and crop options, fixed collecting gzipped files, removed redundant code

--- nipype/interfaces/dcm2nii.py
+++ nipype/interfaces/dcm2nii.py
@@ -6,13 +6,15 @@ import os
 from copy import deepcopy
 
 class Dcm2niiInputSpec(CommandLineInputSpec):
-    source_names = InputMultiPath(File(exists=True), argstr="%s", position=6, mandatory=True)
+    source_names = InputMultiPath(File(exists=True), argstr="%s", position=8, mandatory=True)
     gzip_output = traits.Bool(False, argstr='-g', position=0, usedefault=True)
     nii_output = traits.Bool(True, argstr='-n', position=1, usedefault=True)
     anonymize = traits.Bool(argstr='-a', position=2)
     id_in_filename = traits.Bool(False, argstr='-i', usedefault=True, position=3)
-    output_dir = Directory(exists=True, argstr='-o %s', genfile=True, position=4)
-    config_file = File(exists=True, argstr="-b %s", genfile=True, position=5)
+    reorient = traits.Bool(argstr='-r', position=4)
+    reorient_and_crop = traits.Bool(argstr='-x', position=5)
+    output_dir = Directory(exists=True, argstr='-o %s', genfile=True, position=6)
+    config_file = File(exists=True, argstr="-b %s", genfile=True, position=7)
     
 class Dcm2niiOutputSpec(TraitedSpec):
     converted_files = OutputMultiPath(File(exists=True))
@@ -34,14 +36,7 @@ class Dcm2nii(CommandLine):
         return super(Dcm2nii, self)._format_arg(opt, spec, val)
     
     def _run_interface(self, runtime):
-        if self.inputs.config_file == "config.ini":
-            os.mknod(self.inputs.config_file)
-            f = open(self.inputs.config_file, "w")
-            # disable interactive mode
-            f.write("[BOOL]\nManualNIfTIConv=0\n")
-            f.close()
-            self.inputs.config_file = self.inputs.config_file
-            
+          
         new_runtime = super(Dcm2nii, self)._run_interface(runtime)
         self.output_files = self._parse_stdout(new_runtime.stdout)
         return new_runtime
@@ -49,10 +44,18 @@ class Dcm2nii(CommandLine):
     def _parse_stdout(self, stdout):
         files = []
         for line in stdout.split("\n"):
-            if line.endswith(".nii") and not (line.find("->") != -1 or line.startswith("Cropping") or line.startswith("Reorienting")):
-                file = line.split(" ")[-1]
-                if self.inputs.gzip_output:
-                    file +=".gz"
+            file = None
+            if line.startswith("Saving "):
+                file = line[len("Saving "):]
+            elif line.startswith("GZip..."):
+                #for gzipped outpus files are not absolute
+                if isdefined(self.inputs.output_dir):
+                    output_dir = self.inputs.output_dir
+                else:
+                    output_dir = self._gen_filename('output_dir')
+                file = os.path.abspath(os.path.join(output_dir, line[len("GZip..."):]))
+                                       
+            if file:
                 files.append(file)
         return files
     
