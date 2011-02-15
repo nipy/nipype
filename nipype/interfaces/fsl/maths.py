dbf737718fc9443c225f539d053fc21b39dc7057
Michael Waskom <mwaskom@mit.edu>
2011-02-15 17:50:50 -0500

    Finished adding fslmaths interfaces

--- nipype/interfaces/fsl/maths.py
+++ nipype/interfaces/fsl/maths.py
@@ -231,12 +231,23 @@ class BinaryMaths(MathsCommand):
 class MultiImageMathsInput(MathsInput):
 
     op_string = traits.String(position=3, argstr="%s", mandatory=True,
-                              desc="python string-formatting string")
+                              desc="python formatted string of operations to perform")
     operand_files = traits.List(File(exists=True), mandatory=True,
                                  desc="list of file names to plug into op string")
 
 class MultiImageMaths(MathsCommand):
     """Use fslmaths to perform a sequence of mathematical operations.
+
+    Examples
+    --------
+    from nipype.interfaces.fsl import MultiImageMaths
+    maths = MultiImageMaths()
+    maths.inputs.in_file = "functional.nii"
+    maths.inputs.op_string = "-add %s -mul -1 -div %s"
+    maths.inputs.operand_files = ["functional2.nii", "functional3.nii"]
+    maths.inputs.out_file = functional4.nii
+    maths.cmdline
+    fslmaths functional1.nii -add functional2.nii -mul -1 -div functional3.nii functional4.nii
 
     """
     input_spec = MultiImageMathsInput
