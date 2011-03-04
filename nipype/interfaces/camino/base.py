f57b78aa541931d42860b1448ede33bb1ec6acef
mankind <mankind@MacBook.local>
2011-03-04 12:10:20 +0000

    Changes to camino_dti_tutorial.py

--- nipype/interfaces/camino/base.py
+++ nipype/interfaces/camino/base.py
@@ -40,6 +40,49 @@ class CaminoCommand(CommandLine):
     """Base support for Camino commands.
     
     """
+
+    def _gen_fname(self, basename, cwd=None, suffix=None, change_ext=True, ext=None):
+        """Generate a filename based on the given parameters.
+
+        The filename will take the form: cwd/basename<suffix><ext>.
+        If change_ext is True, it will use the extentions specified in
+        <instance>intputs.output_type.
+
+        Parameters
+        ----------
+        basename : str
+            Filename to base the new filename on.
+        cwd : str
+            Path to prefix to the new filename. (default is os.getcwd())
+        suffix : str
+            Suffix to add to the `basename`.  (defaults is '' )
+        change_ext : bool
+            Flag to change the filename extension to the FSL output type.
+            (default True)
+
+        Returns
+        -------
+        fname : str
+            New filename based on given parameters.
+
+        """
+
+        if basename == '':
+            msg = 'Unable to generate filename for command %s. ' % self.cmd
+            msg += 'basename is not set!'
+            raise ValueError(msg)
+        if cwd is None:
+            cwd = os.getcwd()
+        if ext is None:
+            ext = ''
+        if change_ext:
+            if suffix:
+                suffix = ''.join((suffix, ext))
+            else:
+                suffix = ext
+        fname = fname_presuffix(basename, suffix = suffix,
+                                use_ext = False, newpath = cwd)
+        return fname
     
 input_spec = CaminoCommandInputSpec
     #_output_type = None
