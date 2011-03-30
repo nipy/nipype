778fed5f266ca38a668ae12202372b16830d3c52
Yaroslav Halchenko <debian@onerussian.com>
2011-03-30 17:44:19 -0400

    BF: allow sourced functions to be indented -- dedent before pickling

--- nipype/utils/tests/test_misc.py
+++ nipype/utils/tests/test_misc.py
@@ -2,7 +2,8 @@
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 from nipype.testing import assert_equal, assert_true
 
-from nipype.utils.misc import container_to_string
+from nipype.utils.misc import container_to_string, \
+     getsource, create_function_from_source
 
 def test_cont_to_str():
     # list
@@ -24,3 +25,17 @@ def test_cont_to_str():
     # int.  Integers are not the main intent of this function, but see
     # no reason why they shouldn't work.
     yield assert_equal, container_to_string(123), '123'
+
+def _func1(x):
+    return x**3
+
+def test_func_to_str():
+
+    def func1(x):
+        return x**2
+
+    # Should be ok with both functions!
+    for f in _func1, func1:
+        f_src = getsource(f)
+        f_recreated = create_function_from_source(f_src)
+        yield assert_equal, f(2.3), f_recreated(2.3)
