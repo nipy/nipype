e3e3839ee8c18f2fae750e7fae635e70b1d0d5c6
bnucon <bnucon@gmail.com>
2011-04-09 09:13:50 +0800

    Separated Michael's sharing code(for TBSS) git://gist.github.com/905015.git into 4 workflows. Added slicer node into tbss1. Created a tbss_all workflow by combining 4 workflows.
    'to find best target from all images in FA' has not yet been included.

---
+++ nipype/workflows/fsl/test_tbss.py
@@ -0,0 +1,62 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+A pipeline to perform TBSS.
+"""
+
+"""
+Tell python where to find the appropriate functions.
+"""
+import nipype.interfaces.io as nio           # Data i/o
+import nipype.interfaces.fsl as fsl          # fsl
+import nipype.interfaces.utility as util     # utility
+import nipype.pipeline.engine as pe          # pypeline engine
+import os                                    # system functions
+import tbss
+
+"""
+Confirm package dependencies installed.
+"""
+from nipype.utils.misc import package_check
+package_check('numpy', '1.3', 'tbss_test') 
+package_check('scipy', '0.7', 'tbss_test')
+package_check('networkx', '1.0', 'tbss_test')
+package_check('IPython', '0.10', 'tbss_test')
+
+"""
+Specify the related directories
+"""
+dataDir = '/nfs/j3/userhome/kongxiangzhen/mywork/tbssdata'
+workingdir = '/nfs/j3/userhome/kongxiangzhen/tbss_test_workingdir'
+subject_list = ['1260','1549']
+"""
+Here we get the FA list including all the subjects.
+"""
+def getFAList(subject_list):
+    fa_list = []
+    for subject_id in subject_list:
+        fa_list.append(subject_id)
+    return fa_list
+tbss_source = pe.Node(interface=nio.DataGrabber(outfiles=['fa_list']),name='tbss_source')
+tbss_source.inputs.base_directory = os.path.abspath(dataDir)
+tbss_source.inputs.template = '%s.nii.gz'
+tbss_source.inputs.template_args = dict(fa_list=[[getFAList(subject_list)]])
+
+'''
+TBSS analysis
+'''
+tbss_all = tbss.create_tbss_all(name='tbss_all')
+#tbss.base_dir = os.path.abspath(workingdir)
+tbss_all.inputs.inputnode.target = fsl.Info.standard_image("FMRIB58_FA_1mm.nii.gz")
+tbss_all.inputs.inputnode.skeleton_thresh = 0.2
+
+tbssproc = pe.Workflow(name="tbssproc")
+tbssproc.base_dir = os.path.abspath(workingdir)
+tbssproc.connect([
+                (tbss_source, tbss_all,[('fa_list','inputnode.fa_list')])
+                ])
+
+
+if __name__=='__main__':
+    tbssproc.run()
+    tbssproc.write_graph()
