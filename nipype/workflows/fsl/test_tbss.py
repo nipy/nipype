ad11c3cd5e20eb25736cb6ed839bd55c6310ed49
Xiangzhen Kong <bnucon@gmail.com>
2011-05-04 21:26:59 +0800

    merged the file into one before comparing the outputs from fsl with these from nipype tbss-worflow Now the test scripts works well and the results indicate that tbss.py works well.

--- nipype/workflows/fsl/test_tbss.py
+++ nipype/workflows/fsl/test_tbss.py
@@ -35,12 +35,14 @@ pluginName = 'IPython'
 @with_setup(setup_test_dir, remove_test_dir)
 
 def test_tbss_all_pipeline():
-    fsl_course_dir = os.getenv('FSL_COURSE_DATA')
-    data_dir = os.path.join(fsl_course_dir,'fsl_course_data/tbss')
-    subject_list = ['1260','1549','1636','1651','2078','2378']
+    data_dir = '/nfs/j3/userhome/kongxiangzhen/mywork/test_tbss/mydata'
+   # fsl_course_dir = os.getenv('FSL_COURSE_DATA')
+   # data_dir = os.path.join(fsl_course_dir,'fsl_course_data/tbss')
+   # subject_list = ['1260','1549','1636','1651','2078','2378']
+    subject_list = ['S0001','S0005','S0036','S0038','S0085','S0099','S0004','S0032','S0037','S0057','S0098']
     subject_list.sort()
-    fsl_tbss_dir = '/path/test_tbss/tbss_fsl/tbss_mydata/'# results fo fsl tbss
-    workingdir = '/path/mywork/test_tbss'
+    fsl_tbss_dir = '/nfs/j3/userhome/kongxiangzhen/mywork/test_tbss/tbss_fsl/tbss_mydata/'
+    workingdir = '/nfs/j3/userhome/kongxiangzhen/mywork/test_tbss'
     
     
     """
@@ -91,20 +93,31 @@ def test_tbss_all_pipeline():
     t4_mean_FA_skeleton_mask_dst = os.path.join(fsl_tbss_dir,'stats/mean_FA_skeleton_mask_dst.nii.gz')
     
     """
+    """
+    merge_fa_list = pe.Node(fsl.Merge(dimension="t", merged_file="all_fa.nii.gz"), name="merge_fa_list")
+    merge_mask_list = pe.Node(fsl.Merge(dimension="t", merged_file="all_mask.nii.gz"), name="merge_mask_list")
+    
+    """
     The Test Nodes
     
     Check outputs of tbss1
     """
-    FA_prep = pe.MapNode(util.AssertEqual(), name = "FA_prep", iterfield=['volume1','volume2'])
-    FA_prep.inputs.volume2 = getFA_prep_list(subject_list)
-    mask_prep = pe.MapNode(util.AssertEqual(), name = "mask_prep", iterfield=['volume1','volume2'])
-    mask_prep.inputs.volume2 = getmask_prep_list(subject_list)
+    FA_prep = pe.Node(util.AssertEqual(ignore_exception = False), name = "FA_prep")
+    merge_FA_prep = pe.Node(fsl.Merge(dimension="t", merged_file="all_FA_prep.nii.gz"), name="merge_FA_prep")
+    merge_FA_prep.inputs.in_files = getFA_prep_list(subject_list)
+    #FA_prep = pe.MapNode(util.AssertEqual(ignore_exception = True), name = "FA_prep", iterfield=['volume1','volume2'])
+    #FA_prep.inputs.volume2 = getFA_prep_list(subject_list)
+    mask_prep = pe.Node(util.AssertEqual(), name = "mask_prep")
+    merge_mask_prep = pe.Node(fsl.Merge(dimension="t", merged_file="all_mask_prep.nii.gz"), name="merge_mask_prep")
+    merge_mask_prep.inputs.in_files = getmask_prep_list(subject_list)
+    #mask_prep = pe.MapNode(util.AssertEqual(), name = "mask_prep", iterfield=['volume1','volume2'])
+    #mask_prep.inputs.volume2 = getmask_prep_list(subject_list)
     
     """
     Check outputs of tbss2
     """
-    field = pe.MapNode(util.AssertEqual(), name = "field", iterfield=['volume1','volume2'])
-    field.inputs.volume2 = getfield_list(subject_list)
+    #field = pe.MapNode(util.AssertEqual(), name = "field", iterfield=['volume1','volume2'])
+    #field.inputs.volume2 = getfield_list(subject_list)
     
     """
     Check outputs of tbss3
@@ -113,38 +126,43 @@ def test_tbss_all_pipeline():
     all_FA.inputs.volume2 = t3_all_FA
     mean_FA = pe.Node(util.AssertEqual(ignore_exception = False), name = "mean_FA") # right
     mean_FA.inputs.volume2 = t3_mean_FA
-    groupmask = pe.Node(util.AssertEqual(ignore_exception = True), name = "groupmask")
+    groupmask = pe.Node(util.AssertEqual(ignore_exception = False), name = "groupmask")
     groupmask.inputs.volume2 = t3_groupmask
-    skeleton_file = pe.Node(util.AssertEqual(ignore_exception = True), name = "skeleton_file")
+    skeleton_file = pe.Node(util.AssertEqual(ignore_exception = False), name = "skeleton_file")
     skeleton_file.inputs.volume2 = t3_skeleton_file
     
     """
     Check outputs of tbss4
-    """
-    all_FA_skeletonised = pe.Node(util.AssertEqual(ignore_exception = True), name = "all_FA_skeletonised")
+    """    
+    all_FA_skeletonised = pe.Node(util.AssertEqual(ignore_exception = False), name = "all_FA_skeletonised")
     all_FA_skeletonised.inputs.volume2 = t4_all_FA_skeletonised
-    mean_FA_skeleton_mask = pe.Node(util.AssertEqual(ignore_exception = True), name = "mean_FA_skeleton_mask")
+    mean_FA_skeleton_mask = pe.Node(util.AssertEqual(ignore_exception = False), name = "mean_FA_skeleton_mask")
     mean_FA_skeleton_mask.inputs.volume2 = t4_mean_FA_skeleton_mask
-    mean_FA_skeleton_mask_dst = pe.Node(util.AssertEqual(ignore_exception = True), name = "mean_FA_skeleton_mask_dst")
+    mean_FA_skeleton_mask_dst = pe.Node(util.AssertEqual(ignore_exception = False), name = "mean_FA_skeleton_mask_dst")
     mean_FA_skeleton_mask_dst.inputs.volume2 = t4_mean_FA_skeleton_mask_dst
     
     cmp_nipy2fsl = pe.Workflow(name="cmp_nipy2fsl")
     cmp_nipy2fsl.base_dir = workingdir
     cmp_nipy2fsl.connect([
-                        (tbss_all,FA_prep,[('outputall_node.fa_list1','volume1')]),
-                        (tbss_all,mask_prep,[('outputall_node.mask_list1','volume1')]),
+                        (tbss_all,merge_fa_list,[('outputall_node.fa_list1','in_files')]),#OK
+                        (merge_fa_list,FA_prep,[('merged_file','volume1')]),
+                        (merge_FA_prep,FA_prep,[('merged_file','volume2')]),
+                    #    (tbss_all,FA_prep,[('outputall_node.fa_list1','volume1')]),
+                        (tbss_all,merge_mask_list,[('outputall_node.mask_list1','in_files')]),#OK
+                        (merge_mask_list,mask_prep,[('merged_file','volume1')]),
+                        (merge_mask_prep,mask_prep,[('merged_file','volume2')]),
+                    #    (tbss_all,mask_prep,[('outputall_node.mask_list1','volume1')]),
                        
-                        (tbss_all,field,[('outputall_node.field_list2','volume1')]),
+                    #    (tbss_all,field,[('outputall_node.field_list2','volume1')]),
                         
-                        (tbss_all, all_FA,[('outputall_node.mergefa_file3','volume1')]),
-                        (tbss_all, mean_FA,[('outputall_node.meanfa_file3','volume1')]),
-                        (tbss_all, groupmask,[('outputall_node.groupmask3','volume1')]),
-                        (tbss_all, skeleton_file,[('outputall_node.skeleton_file3','volume1')]),
+                        (tbss_all, all_FA,[('outputall_node.mergefa_file3','volume1')]),#OK
+                        (tbss_all, mean_FA,[('outputall_node.meanfa_file3','volume1')]),#OK
+                        (tbss_all, groupmask,[('outputall_node.groupmask3','volume1')]),#OK
+                        (tbss_all, skeleton_file,[('outputall_node.skeleton_file3','volume1')]),#OK
                         
-                        (tbss_all, all_FA_skeletonised,[('outputall_node.projectedfa_file4','volume1')]),
-                        (tbss_all, mean_FA_skeleton_mask,[('outputall_node.skeleton_mask4','volume1')]),
-                        (tbss_all, mean_FA_skeleton_mask_dst,[('outputall_node.distance_map4','volume1')]),                        
+                        (tbss_all, all_FA_skeletonised,[('outputall_node.projectedfa_file4','volume1')]),#OK
+                        (tbss_all, mean_FA_skeleton_mask,[('outputall_node.skeleton_mask4','volume1')]),#OK
+                        (tbss_all, mean_FA_skeleton_mask_dst,[('outputall_node.distance_map4','volume1')]),#OK
                         ])
-#    cmp_nipy2fsl.run()
 
     cmp_nipy2fsl.run(plugin=pluginName)
