201174575c1677ee979feb82492b256310c7f644
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2011-05-15 20:12:56 +0100

    added concurency note

--- nipype/interfaces/io.py
+++ nipype/interfaces/io.py
@@ -1050,7 +1050,15 @@ class SQLiteSinkInputSpec(DynamicTraitedSpec, BaseInterfaceInputSpec):
 
 class SQLiteSink(IOBase):
     """Very simple frontend for storing values into SQLite database. input_names
-    correspond to input_names. 
+    correspond to input_names.
+    
+        Notes
+        -----
+
+            Unlike most nipype-nodes this is not a thread-safe node because it can
+            write to a common shared location. When run in parallel it will 
+            occasionally crash.
+            
     
         Examples
         --------
