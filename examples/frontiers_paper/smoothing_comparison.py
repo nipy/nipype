b6671d285412b5939f5576a86a43a96d5379de16
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2011-06-17 18:38:42 +0100

    added comments

--- examples/frontiers_paper/smoothing_comparison.py
+++ examples/frontiers_paper/smoothing_comparison.py
@@ -1,3 +1,18 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+=============================
+Smoothing comparison workflow
+=============================
+Uniform semantics for interfacing with a wide range of processing methods not only opens the possibility for richer Workflows, but also allows comparing algorithms that are designed to solve the same problem across and within such diverse Workflows. Typically, such an exhaustive comparison can be time-consuming, because of the need to deal with interfacing different software packages. NiPyPe simplifies this process by standardizing the access to the software. Additionally, the iterables mechanism allows users to easily extend such comparisons by providing a simple mechanism to test different parameter sets. 
+
+Accuracy or efficiency of algorithms can be determined in an isolated manner by comparing their outputs or execution time or memory consumption on a given set of data. However, researchers typically want to know how different algorithms used at earlier stages of processing might influence the final output or statistics they are interested in. As an example of such use, we have compared voxelwise isotropic, voxelwise anisotropic and surface based smoothing all for two levels of FWHM - 4 and 8mm. First one is the standard convolution with Gaussian kernel as implemented in SPM. Second one involves smoothing only voxels of similar intensity in attempt to retain structure. This was implemented in SUSAN from FSL (S.M. Smith, 1992). Third method involves reconstructing surface of the cortex and smoothing along it (Hagler Jr., Saygin, & Martin I. Sereno, 2006). This avoids bleeding of signal over sulci.
+
+Establishing parameters from data and smoothing using SUSAN is already build into NiPyPe as a Workflow. It can be created using create_susan_smooth() function. It has similar inputs and outputs as SPM Smooth Interface. Smoothing on a surface involves doing a full cortical reconstruction from T1 volume using FreeSurfer (Fischl, M I Sereno, & Dale, 1999) followed by coregistering functional images to the reconstructed surface using BBRegister. Finally surface smoothing algorithm from FreeSurfer is called.
+
+"""
+
+
 import nipype.interfaces.io as nio           # Data i/o
 import nipype.interfaces.spm as spm          # spm
 import nipype.interfaces.freesurfer as fs    # freesurfer
@@ -8,26 +23,6 @@ import nipype.algorithms.modelgen as model   # model specification
 import nipype.workflows.fsl as fsl_wf
 from nipype.interfaces.base import Bunch
 import os                                    # system functions
-
-
-"""In the following section, to showcase NiPyPe, we will describe how to create 
-and extend a typical fMRI processing pipeline. We will begin with a basic 
-processing layout and follow with extending it by adding/exchanging different 
-components.
-
-Most fMRI pipeline can be divided into two sections - preprocessing and 
-modelling. First one deals with cleaning data from confounds and noise and the 
-second one fits a model based on the experimental design. Preprocessing stage 
-in our first iteration of a pipeline will consist of only two steps: 
-realignment and smoothing. In NiPyPe Every processing step consist of an 
-Interface (which defines how to execute corresponding software) encapsulated 
-in a Node (which defines for example a unique name). For realignment (motion 
-correction achieved by coregistering all volumes to the mean) and smoothing 
-(convolution with 3D Gaussian kernel) we will use SPM implementation. 
-Definition of appropriate nodes can be found in Listing 1 (TODO). Inputs 
-(such as register_to_mean from listing 1) of nodes are accessible through the 
-inputs property. Upon setting any input its type is verified to avoid errors 
-during the execution."""
 
 preprocessing = pe.Workflow(name="preprocessing")
 
@@ -97,18 +92,6 @@ rename.inputs.parse_string = "(?P<orig>.*)"
 
 preprocessing.connect(select_smoothed_files, 'out', rename, 'in_file')
 
-
-"""Creating a modelling workflow which will define the design, estimate model and 
-contrasts follows the same suite. We will again use SPM implementations. 
-NiPyPe, however, adds extra abstraction layer to model definition which allows 
-using the same definition for many model estimation implemantations (for 
-example one from FSL or nippy). Therefore we will need four nodes: 
-SpecifyModel (NiPyPe specific abstraction layer), Level1Design (SPM design 
-definition), ModelEstimate, and ContrastEstimate. The connected modelling 
-Workflow can be seen on Figure TODO. Model specification supports block, event 
-and sparse designs. Contrasts provided to ContrastEstimate are defined using 
-the same names of regressors as defined in the SpecifyModel."""
-
 specify_model = pe.Node(interface=model.SpecifyModel(), name="specify_model")
 specify_model.inputs.input_units             = 'secs'
 specify_model.inputs.time_repetition         = 3.
@@ -135,13 +118,6 @@ modelling.connect(level1estimate,'spm_mat_file', contrastestimate,'spm_mat_file'
 modelling.connect(level1estimate,'beta_images', contrastestimate,'beta_images')
 modelling.connect(level1estimate,'residual_image', contrastestimate,'residual_image')
 
-"""Having preprocessing and modelling workflows we need to connect them together, 
-add data grabbing facility and save the results. For this we will create a 
-master Workflow which will host preprocessing and model Workflows as well as 
-DataGrabber and DataSink Nodes. NiPyPe allows connecting Nodes between 
-Workflows. We will use this feature to connect realignment_parameters and 
-smoothed_files to modelling workflow."""
-
 main_workflow = pe.Workflow(name="main_workflow")
 main_workflow.base_dir = "smoothing_comparison_workflow"
 main_workflow.connect(preprocessing, "realign.realignment_parameters", 
@@ -150,15 +126,6 @@ main_workflow.connect(preprocessing, "select_smoothed_files.out",
                       modelling, "specify_model.functional_runs")
 main_workflow.connect(preprocessing, "compute_mask.brain_mask", 
                       modelling, "level1design.mask_image")
-
-
-"""DataGrabber allows to define flexible search patterns which can be 
-parameterized by user defined inputs (such as subject ID, session etc.). 
-This allows to adapt to a wide range of file layouts. In our case we will 
-parameterize it with subject ID. In this way we will be able to run it for 
-different subjects. We can automate this by iterating over a list of subject 
-Ids, by setting an iterables property on the subject_id input of DataGrabber. 
-Its output will be connected to realignment node from preprocessing workflow."""
 
 datasource = pe.Node(interface=nio.DataGrabber(infields=['subject_id'],
                                                outfields=['func', 'struct']),
@@ -171,10 +138,6 @@ datasource.inputs.subject_id = 's1'
 
 main_workflow.connect(datasource, 'func', preprocessing, 'realign.in_files')
 main_workflow.connect(datasource, 'struct', preprocessing, 'recon_all.T1_files')
-
-"""DataSink on the other side provides means to storing selected results to a 
-specified location. It supports automatic creation of folder stricter and 
-regular expression based substitutions. In this example we will store T maps."""
 
 datasink = pe.Node(interface=nio.DataSink(), name="datasink")
 datasink.inputs.base_directory = os.path.abspath('smoothing_comparison_workflow/output')
--- examples/frontiers_paper/workflow_from_scratch.py
+++ examples/frontiers_paper/workflow_from_scratch.py
@@ -1,3 +1,12 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+=====================
+Workflow from scratch
+=====================
+
+"""
+
 import nipype.interfaces.io as nio           # Data i/o
 import nipype.interfaces.spm as spm          # spm
 import nipype.pipeline.engine as pe          # pypeline engine
