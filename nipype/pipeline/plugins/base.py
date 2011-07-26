deb86133d7251649467781867b56eeedf654d553
Christian Haselgrove <christian.haselgrove@umassmed.edu>
2011-07-26 16:25:56 -0400

    using sys.executable in SGE job script

--- nipype/pipeline/plugins/base.py
+++ nipype/pipeline/plugins/base.py
@@ -373,7 +373,7 @@ except:
         fp = open(pyscript, 'wt')
         fp.writelines(cmdstr)
         fp.close()
-        batchscript = '\n'.join((self._template, 'python %s'%pyscript))
+        batchscript = '\n'.join((self._template, '%s %s'%(sys.executable, pyscript)))
         batchscriptfile = os.path.join(batch_dir, 'batchscript_%s.sh'%suffix)
         fp = open(batchscriptfile, 'wt')
         fp.writelines(batchscript)
