08031b2ea13cd141ed97b81b88fccb9ff98d8e4f
Satrajit Ghosh <satra@mit.edu>
2011-08-21 00:57:23 -0400

    enh: initial tutorial with fsl modelfitting workflow

--- nipype/workflows/fsl/__init__.py
+++ nipype/workflows/fsl/__init__.py
@@ -1,4 +1,6 @@
-from dti import create_bedpostx_pipeline, create_eddy_correct_pipeline
-from preprocess import (create_susan_smooth, create_fsl_fs_preproc,
+from .dti import create_bedpostx_pipeline, create_eddy_correct_pipeline
+from .preprocess import (create_susan_smooth, create_fsl_fs_preproc,
                         create_parallelfeat_preproc, create_featreg_preproc)
-from resting import create_resting_preproc
+from .estimate import create_modelfit_workflow
+from .resting import create_resting_preproc
+
