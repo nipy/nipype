09cc2ee8df257e0eb2e249a25c76a89dc0586d32
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2011-09-09 11:00:16 -0400

    ENH : making nipy optional

--- nipype/interfaces/nipy/__init__.py
+++ nipype/interfaces/nipy/__init__.py
@@ -1,2 +1,9 @@
-from model import FitGLM, EstimateContrast
-from preprocess import ComputeMask
+import warnings
+from nipype.utils.misc import package_check
+
+try:
+    package_check('nipy')
+    from model import FitGLM, EstimateContrast
+    from preprocess import ComputeMask
+except Exception, e:
+    warnings.warn('nipy not installed')
