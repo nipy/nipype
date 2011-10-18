c76941b08a6d884be77de3497cf36ffd34081464
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2011-10-18 15:27:32 +0100

    explicit stat to p-value conversion

--- nipype/interfaces/spm/model.py
+++ nipype/interfaces/spm/model.py
@@ -389,9 +389,10 @@ class ThresholdInputSpec(SPMCommandInputSpec):
     spm_mat_file = File(exists=True, desc='absolute path to SPM.mat', copyfile=True, mandatory=True)
     stat_image = File(exists=True, desc='stat image', copyfile=False, mandatory=True)
     contrast_index = traits.Int(mandatory=True, desc='which contrast in the SPM.mat to use')
-    use_fwe_correction = traits.Bool(True, usedefault=True, desc="whether to use FWE (Bonferroni) correction for initial threshold")
+    use_fwe_correction = traits.Bool(True, usedefault=True, desc="whether to use FWE (Bonferroni) correction for initial threshold (height_threshold_type has to be set to p-value)")
     use_topo_fdr = traits.Bool(True, usedefault=True, desc="whether to use FDR over cluster extent probabilities")
-    height_threshold = traits.Float(0.05, usedefault=True, desc="p-value for initial thresholding (defining clusters)")
+    height_threshold = traits.Float(0.05, usedefault=True, desc="value for initial thresholding (defining clusters)")
+    height_threshold_type = traits.Enum('p-value','stat', usedefault=True, desc="Is the cluster forming threshold a stat value or p-value?")
     extent_fdr_p_threshold = traits.Float(0.05, usedefault=True, desc='p threshold on FDR corrected cluster size probabilities')
     extent_threshold = traits.Int(0, usedefault=True, desc="Minimum cluster size in voxels")
 
@@ -440,6 +441,7 @@ class Threshold(SPMCommand):
             script += "use_topo_fdr  = 0;\n"
         script += "cluster_extent_p_fdr_thr = %f;\n"% self.inputs.extent_fdr_p_threshold
         script += "stat_filename = '%s';\n"% self.inputs.stat_image
+        script += "height_threshold_type = '%s';\n"% self.inputs.height_threshold_type
         script += "extent_threshold = %d;\n" % self.inputs.extent_threshold
 
         script += "load %s;\n" % self.inputs.spm_mat_file
@@ -456,7 +458,7 @@ switch thresDesc
         cluster_forming_thr = spm_uc(cluster_forming_thr,df,STAT,R,n,S);
 
     case 'none'
-        if cluster_forming_thr <= 1
+        if height_threshold_type == 'p-value'
             cluster_forming_thr = spm_u(cluster_forming_thr^(1/n),df,STAT);
         end
 end
