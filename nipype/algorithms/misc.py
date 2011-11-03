34c8fd0182ab721519440c3cc8fa0f5d8f1ed157
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2011-11-03 13:44:16 +0000

    Added Gunzip converter

--- nipype/algorithms/misc.py
+++ nipype/algorithms/misc.py
@@ -456,3 +456,36 @@ class TSNR(BaseInterface):
         if isdefined(self.inputs.regress_poly):
             outputs['detrended_file'] = self._gen_output_file_name('detrended')
         return outputs
+
+class GunzipInputSpec(BaseInterfaceInputSpec):
+    in_file = File(exists=True, mandatory=True)
+
+class GunzipOutputSpec(TraitedSpec):
+    out_file = File(exists=True)
+
+class Gunzip(BaseInterface):
+    """
+
+    """
+    input_spec = GunzipInputSpec
+    output_spec = GunzipOutputSpec
+
+    def _gen_output_file_name(self):
+        _, base, ext = split_filename(self.inputs.in_file)
+        if ext[-2:].lower() == ".gz":
+            ext = ext[:-3]
+        return os.path.abspath(base + ext[:-3])
+
+    def _run_interface(self, runtime):
+        import gzip
+        in_file = gzip.open(self.inputs.in_file, 'rb')
+        out_file = open(self._gen_output_file_name(), 'wb')
+        out_file.write(in_file.read())
+        out_file.close()
+        in_file.close()
+        return runtime
+
+    def _list_outputs(self):
+        outputs = self._outputs().get()
+        outputs['out_file'] = self._gen_output_file_name()
+        return outputs
