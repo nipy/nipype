d72cba109ad9ac09c5f81e48fb7703298ebeffa3
Satrajit Ghosh <satra@mit.edu>
2012-01-09 18:03:53 -0700

    enh: added debug plugin

--- nipype/pipeline/plugins/__init__.py
+++ nipype/pipeline/plugins/__init__.py
@@ -1,6 +1,7 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 
+from .debug import DebugPlugin
 from .linear import LinearPlugin
 from .ipython import IPythonPlugin
 from .pbs import PBSPlugin
