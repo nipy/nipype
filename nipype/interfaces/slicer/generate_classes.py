cf8c8fbd9dde5b969c3b0bf1f086fdd64199719c
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2012-01-11 15:05:39 +0000

    Refactored Slicer support.

--- nipype/interfaces/slicer/generate_classes.py
+++ nipype/interfaces/slicer/generate_classes.py
@@ -11,18 +11,23 @@ def generate_all_classes(modules_list = [], launcher=[]):
         a proper environment for each of the modules.
     """
     init_imports = ""
+    f = open("cli_modules.py", 'w')
+    imports = """from nipype.interfaces.base import CommandLine, CommandLineInputSpec, TraitedSpec, File, Directory, traits, isdefined, InputMultiPath, OutputMultiPath
+import os\n\n\n"""
+    f.write(imports)
+    class_defs = []
     for module in modules_list:
         module_python_filename="%s.py"%module
         print("="*80)
         print("Generating Definition for module {0} in {1}".format(module,module_python_filename))
         print("^"*80)
         code = generate_class(module,launcher)
-        f = open(module_python_filename, "w")
-        f.write(code)
-        f.close()
-        init_imports += "from %s import %s\n"%(module,module)
+        class_defs.append(code)
+    f.write("\n\n\n".join(class_defs))
+    f.close()
+    
     f = open("__init__.py", "w")
-    f.write(init_imports)
+    f.write("from cli_modules import %s\n"%", ".join(modules_list))
     f.close()
 
 
@@ -33,15 +38,22 @@ def generate_class(module,launcher):
     outputs_filenames = {}
 
     #self._outputs_nodes = []
+    
+    class_string = "\"\"\""
+    
+    for desc_str in ['title','category','description','version',
+                     'documentation-url','license','contributor',
+                     'acknowledgements']:
+        el = dom.getElementsByTagName(desc_str)
+        if el and el[0].firstChild:
+            class_string += desc_str + ": " + el[0].firstChild.nodeValue + "\n\n"
+    class_string += "\"\"\""
 
     for paramGroup in dom.getElementsByTagName("parameters"):
         for param in paramGroup.childNodes:
             if param.nodeName in ['label', 'description', '#text', '#comment']:
                 continue
             traitsParams = {}
-
-            name = param.getElementsByTagName('name')[0].firstChild.nodeValue
-            name = name.lstrip().rstrip()
 
             longFlagNode = param.getElementsByTagName('longflag')
             if longFlagNode:
@@ -53,11 +65,23 @@ def generate_class(module,launcher):
                 ##     <longflag>--test</longflag>
                 ##     <longflag>test</longflag>
                 longFlagName = longFlagName.lstrip(" -").rstrip(" ")
+                name = longFlagName
                 traitsParams["argstr"] = "--" + longFlagName + " "
             else:
+                name = param.getElementsByTagName('name')[0].firstChild.nodeValue
+                name = name.lstrip().rstrip()
                 traitsParams["argstr"] = "--" + name + " "
+                
+            if param.getElementsByTagName('description'):
+                traitsParams["desc"] = param.getElementsByTagName('description')[0].firstChild.nodeValue.replace('"', "\\\"").replace("\n", ", ")
+            
 
-            argsDict = {'directory': '%s', 'file': '%s', 'integer': "%d", 'double': "%f", 'float': "%f", 'image': "%s", 'transform': "%s", 'boolean': '', 'string-enumeration': '%s', 'string': "%s", 'integer-enumeration' : '%s'}
+            argsDict = {'directory': '%s', 'file': '%s', 'integer': "%d", 
+                        'double': "%f", 'float': "%f", 'image': "%s", 
+                        'transform': "%s", 'boolean': '', 
+                        'string-enumeration': '%s', 'string': "%s", 
+                        'integer-enumeration' : '%s',
+                        'table' : '%s', 'point': '%s', 'region': '%s'}
 
             if param.nodeName.endswith('-vector'):
                 traitsParams["argstr"] += "%s"
@@ -72,28 +96,29 @@ def generate_class(module,launcher):
             if index:
                 traitsParams["desc"] = desc[0].firstChild.nodeValue
 
-            name = param.getElementsByTagName('name')[0].firstChild.nodeValue
-
             typesDict = {'integer': "traits.Int", 'double': "traits.Float",
                          'float': "traits.Float", 'image': "File",
                          'transform': "File", 'boolean': "traits.Bool",
-                         'string': "traits.Str", 'file':"File",
-                         'directory': "Directory"}
+                         'string': "traits.Str", 'file': "File",
+                         'directory': "Directory", 'table': "File",
+                         'point':"traits.List", 'region':"traits.List"}
 
             if param.nodeName.endswith('-enumeration'):
                 type = "traits.Enum"
                 values = ['"%s"'%el.firstChild.nodeValue for el in param.getElementsByTagName('element')]
             elif param.nodeName.endswith('-vector'):
                 type = "InputMultiPath"
-                if param.nodeName in ['file', 'directory', 'image', 'transform']:
+                if param.nodeName in ['file', 'directory', 'image', 'transform', 'table']:
                     values = ["%s(exists=True)"%typesDict[param.nodeName.replace('-vector','')]]
                 else:
                     values = [typesDict[param.nodeName.replace('-vector','')]]
                 traitsParams["sep"] = ','
             elif param.getAttribute('multiple') == "true":
                 type = "InputMultiPath"
-                if param.nodeName in ['file', 'directory', 'image', 'transform']:
+                if param.nodeName in ['file', 'directory', 'image', 'transform', 'table']:
                     values = ["%s(exists=True)"%typesDict[param.nodeName]]
+                elif param.nodeName in ['point', 'region']:
+                    values = ["%s(traits.Float(), minlen=3, maxlen=3)"%typesDict[param.nodeName]]
                 else:
                     values = [typesDict[param.nodeName]]
                 traitsParams["argstr"] += "..."
@@ -101,7 +126,7 @@ def generate_class(module,launcher):
                 values = []
                 type = typesDict[param.nodeName]
 
-            if param.nodeName in ['file', 'directory', 'image', 'transform'] and param.getElementsByTagName('channel')[0].firstChild.nodeValue == 'output':
+            if param.nodeName in ['file', 'directory', 'image', 'transform', 'table'] and param.getElementsByTagName('channel')[0].firstChild.nodeValue == 'output':
                 traitsParams["hash_files"] = False
                 inputTraits.append("%s = traits.Either(traits.Bool, %s(%s), %s)"%(name, 
                                                                                  type, 
@@ -110,14 +135,14 @@ def generate_class(module,launcher):
                 traitsParams["exists"] = True
                 traitsParams.pop("argstr")
                 traitsParams.pop("hash_files")
-                outputTraits.append("%s = %s(%s %s)"%(name, type.replace("Input", "Output"), parse_values(values), parse_params(traitsParams)))
+                outputTraits.append("%s = %s(%s%s)"%(name, type.replace("Input", "Output"), parse_values(values), parse_params(traitsParams)))
 
-                outputs_filenames[name] = gen_filename_from_param(param)
+                outputs_filenames[name] = gen_filename_from_param(param, name)
             else:
-                if param.nodeName in ['file', 'directory', 'image', 'transform'] and type not in ["InputMultiPath", "traits.List"]:
+                if param.nodeName in ['file', 'directory', 'image', 'transform', 'table'] and type not in ["InputMultiPath", "traits.List"]:
                     traitsParams["exists"] = True
 
-                inputTraits.append("%s = %s(%s %s)"%(name, type, parse_values(values), parse_params(traitsParams)))
+                inputTraits.append("%s = %s(%s%s)"%(name, type, parse_values(values), parse_params(traitsParams)))
 
     input_spec_code = "class " + module + "InputSpec(CommandLineInputSpec):\n"
     for trait in inputTraits:
@@ -135,10 +160,8 @@ def generate_class(module,launcher):
     input_spec_code += "\n\n"
     output_spec_code += "\n\n"
 
-    imports = """from nipype.interfaces.base import CommandLine, CommandLineInputSpec, TraitedSpec, File, Directory, traits, isdefined, InputMultiPath, OutputMultiPath
-import os\n\n"""
-
     template = """class %name%(CommandLine):
+    %class_str%
 
     input_spec = %name%InputSpec
     output_spec = %name%OutputSpec
@@ -166,12 +189,12 @@ import os\n\n"""
                     value = os.path.abspath(self._outputs_filenames[name])
                 else:
                     return ""
-        return super(%name%, self)._format_arg(name, spec, value)\n\n"""
+        return super(%name%, self)._format_arg(name, spec, value)\n"""
         
 
-    main_class = template.replace("%name%", module).replace("%output_filenames_code%", output_filenames_code).replace("%launcher%"," ".join(launcher))
+    main_class = template.replace('%class_str%', class_string).replace("%name%", module).replace("%output_filenames_code%", output_filenames_code).replace("%launcher%"," ".join(launcher))
 
-    return imports + input_spec_code + output_spec_code + main_class
+    return input_spec_code + output_spec_code + main_class
 
 def grab_xml(module,launcher):
 #        cmd = CommandLine(command = "Slicer3", args="--launch %s --xml"%module)
@@ -189,22 +212,21 @@ def parse_params(params):
     list = []
     for key, value in params.iteritems():
         if isinstance(value, str) or isinstance(value, unicode):
-            list.append('%s = "%s"'%(key, value))
+            list.append('%s="%s"'%(key, value))
         else:
-            list.append('%s = %s'%(key, value))
+            list.append('%s=%s'%(key, value))
 
-    return ",".join(list)
+    return ", ".join(list)
 
 def parse_values(values):
     values = ['%s'%value for value in values]
     if len(values) > 0:
-        retstr = ",".join(values) + ","
+        retstr = ", ".join(values) + ", "
     else:
         retstr = ""
     return retstr
 
-def gen_filename_from_param(param):
-    base = param.getElementsByTagName('name')[0].firstChild.nodeValue
+def gen_filename_from_param(param, base):
     fileExtensions = param.getAttribute("fileExtensions")
     if fileExtensions:
         ## It is possible that multiple file extensions can be specified in a
@@ -219,84 +241,63 @@ if __name__ == "__main__":
     ## NOTE:  For now either the launcher needs to be found on the default path, or
     ##        every tool in the modules list must be found on the default path
     ##        AND calling the module with --xml must be supported and compliant.
-    modules_list = ['Add', 'AffineRegistration', 
+    modules_list = ['Add',
+                    'AffineRegistration',
                     'BSplineDeformableRegistration', 
-                    'BSplineToDeformationField', 
-                    'DiffusionTensorEstimationBatchMakeModule', 
-                    'EMSegmentBatchMakeModule', 
-                    'GaussianBlurBatchMakeModule', 
-                    'RegisterImagesBatchMakeModule', 
-                    'ResampleVolumeBatchMake', 'Cast', 'CheckerBoard', 
+                    #'BSplineToDeformationField', http://na-mic.org/Mantis/view.php?id=1647
+                    'Cast', 'CheckerBoard', 
                     'ComputeSUVBodyWeight', 'ConfidenceConnected', 
                     'CurvatureAnisotropicDiffusion', 'DicomToNrrdConverter', 
-                    'DWICompare', 'ResampleDTI', 'dwiNoiseFilter', 'dwiUNLM', 
+                    'ResampleDTI', 'dwiNoiseFilter', 'dwiUNLM', 
                     'jointLMMSE', 'DiffusionTensorEstimation', 
                     'DiffusionTensorMathematics', 'DiffusionTensorTest', 
-                    'DiffusionWeightedMasking', 'DiffusionWeightedTest', 
-                    'ExecutionModelTour', 'ExtractSkeleton', 
-                    'FreesurferSurfaceSectionExtraction', 
-                    'GaussianBlurImageFilter', 'GradientAnisotropicDiffusion', 
-                    'GrayscaleFillHoleImageFilter', 
-                    'GrayscaleGrindPeakImageFilter', 'GrayscaleModelMaker', 
-                    'HistogramMatching', 'ImageLabelCombine', 
-                    'ImageReadDicomWrite', 'LabelMapSmoothing', 
-                    'LinearRegistration', 'DownloadSlicerTutorials', 
-                    'MRIBiasFieldCorrection', 'Mask', 'MedianImageFilter', 
-                    'MergeModels', 'ModelMaker', 'MultipleModelsExample', 
-                    'Multiply', 'N4ITKBiasFieldCorrection', 'OrientImage', 
-                    'OtsuThresholdImageFilter', 'OtsuThresholdSegmentation', 
-                    'PolyDataToLabelmap', 'ProbeVolumeWithModel', 'Realign', 
-                    'RegisterImagesSlicer', 'TransformFromFiducials', 
-                    'RegisterImagesMultiRes', 'ResampleVolume', 
-                    'ResampleVolume2', 'RigidRegistration', 
-                    'RobustStatSegmenter', 'SFLSRobustStat3DTest', 'Seeding', 
-                    'SparseFieldLevelSetContour', 'Subtract', 
-                    'TestGridTransformRegistration', 'Threshold', 
-                    'VotingBinaryHoleFillingImageFilter', 
-                    'ZeroCrossingBasedEdgeDetectionImageFilter', 'simpleEM', 
-                    'BRAINSABC', 'BRAINSCleanMask', 'ESLR', 
-                    'GenerateLabelMapFromProbabilityMap', 'normalA-30', 
-                    'brains.kws', 'FindCenterOfBrain', 'LandmarksCompare', 
-                    'BRAINSConstellationDetectorGUI', 'BRAINSAlignMSP', 
-                    'BRAINSClipInferior', 'BRAINSConstellationDetector', 
-                    'BRAINSConstellationModeler', 'BRAINSEyeDetector', 
-                    'BRAINSLinearModelerEPCA', 'BRAINSLmkTransform', 
-                    'BRAINSTrimForegroundInDirection', 'TransformFromFiducials', 
-                    'fcsv_to_matlab_new', 'BRAINSCut', 'CannyEdge', 
-                    'CoOccurrenceMatrix', 'ImageLabelCombine', 'DilateImage', 
-                    'DistanceMaps', 'ErodeImage', 'FlippedDifference', 
-                    'GradientAnisotropicDiffusionImageFilter', 
-                    'HammerAttributeCreator', 'HistogramMatchingFilter', 
-                    'ImageRegionPlotter', 'JointHistogram', 'NeighborhoodMean', 
-                    'NeighborhoodMedian', 'GenerateTestImage', 
-                    'GenerateBrainClippedImage', 'GenerateSummedGradientImage', 
-                    'Template', 'CannySegmentationLevelSetImageFilter', 
-                    'CombineLabelToMask', 'DilateMask', 
-                    'ExampleConfigurationFIle', 
-                    'ExampleConfigurationFIle_For_t1t2clsg_AllInOne_model', 
-                    'BRAINSDemonWarp', 'VBRAINSDemonWarp', 'BRAINSFit', 
-                    'BRAINSInitializedControlPoints', 'BRAINSMultiModeSegment', 
-                    'BRAINSMush', 'BRAINSROIAuto', 'BRAINSResample', 
-                    'compareTractInclusion', 'extractNrrdVectorIndex', 
-                    'gtractCoRegAnatomyRigid', 'gtractFreeTracking', 
-                    'gtractGraphSearchTracking', 'gtractGuidedTracking', 
-                    'gtractStreamlineTracking', 'gtractAnisotropyMap', 
-                    'gtractAverageBvalues', 'gtractClipAnisotropy', 
-                    'gtractCoRegAnatomy', 'gtractCoRegAnatomyBspline', 
-                    'gtractConcatDwi', 'gtractCopyImageOrientation', 
-                    'gtractCoregBvalues', 'gtractCostFastMarching', 
-                    'gtractCreateGuideFiber', 'gtractFastMarchingTracking', 
-                    'gtractFiberTracking', 'gtractImageConformity', 
-                    'gtractInvertBSplineTransform', 
-                    'gtractInvertDeformationField', 
-                    'gtractInvertRigidTransform', 'gtractResampleAnisotropy', 
-                    'gtractResampleB0', 'gtractResampleCodeImage', 
-                    'gtractResampleDWIInPlace', 'gtractResampleFibers', 
-                    'gtractTensor', 'gtractTransformToDeformationField']
+                    'DiffusionWeightedMasking', 
+                    'BRAINSFit',
+#'BRAINSABC',
+#'BRAINSCut',
+#'BRAINSAlignMSP',
+#'BRAINSClipInferior',
+#'BRAINSConstellationDetector',
+#'BRAINSConstellationModeler',
+#'BRAINSLandmarkInitializer',
+'BRAINSDemonWarp',
+#'BRAINSMush',
+#'BRAINSInitializedControlPoints',
+#'BRAINSLinearModelerEPCA',
+#'BRAINSLmkTransform',
+#'BRAINSMultiModeSegment',
+'BRAINSROIAuto',
+'BRAINSResample',
+#'BRAINSTrimForegroundInDirection',
+#'ESLR',
+#'GenerateLabelMapFromProbabilityMap',
+'VBRAINSDemonWarp',
+'extractNrrdVectorIndex',
+'gtractAnisotropyMap',
+'gtractAverageBvalues',
+'gtractClipAnisotropy',
+'gtractCoRegAnatomy',
+'gtractConcatDwi',
+'gtractCopyImageOrientation',
+'gtractCoregBvalues',
+'gtractCostFastMarching',
+'gtractImageConformity',
+'gtractInvertBSplineTransform',
+'gtractInvertDeformationField',
+'gtractInvertRigidTransform',
+'gtractResampleAnisotropy',
+'gtractResampleB0',
+'gtractResampleCodeImage',
+'gtractResampleDWIInPlace',
+'gtractTensor',
+'gtractTransformToDeformationField',
+#'GradientAnisotropicDiffusionImageFilter',
+#'GenerateSummedGradientImage'
+]
 
     ## SlicerExecutionModel compliant tools that are usually statically built, and don't need the Slicer3 --launcher
     #generate_all_classes(modules_list=modules_list,launcher=[])
     ## Tools compliant with SlicerExecutionModel called from the Slicer environment (for shared lib compatibility)
     launcher=['/home/filo/workspace/Slicer4-SuperBuild/Slicer-build/Slicer','--launch']
-    generate_all_classes(modules_list=modules_list, launcher=launcher )
+    generate_all_classes(modules_list=modules_list, launcher=[])
     #generate_all_classes(modules_list=['BRAINSABC'], launcher=[] )
