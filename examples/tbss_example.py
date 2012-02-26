830ef7e7e201d7db0e26ffb80d44c105299cc58d
Satrajit Ghosh <satra@mit.edu>
2012-02-26 10:12:45 -0500

    doc+sty: title + pep8 fixes

--- examples/tbss_example.py
+++ examples/tbss_example.py
@@ -1,7 +1,12 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
 """
-==========================================
-A pipeline to perform TBSS on NKI RS data.
-==========================================
+=========================
+dmri: TBSS on NKI RS data
+=========================
+
+A pipeline to do a TBSS analysis on the NKI rockland sample data
+
 """
 from nipype.workflows.dmri.fsl.dti import create_eddy_correct_pipeline
 from nipype.workflows.dmri.fsl.tbss import create_tbss_non_FA, create_tbss_all
@@ -9,37 +14,37 @@ from nipype.workflows.dmri.fsl.tbss import create_tbss_non_FA, create_tbss_all
 """
 Tell python where to find the appropriate functions.
 """
+
 import nipype.interfaces.io as nio           # Data i/o
 import nipype.interfaces.fsl as fsl          # fsl
 import nipype.interfaces.utility as util     # utility
 import nipype.pipeline.engine as pe          # pypeline engine
 import os                                    # system functions
 
-"""
-Confirm package dependencies installed.
-"""
-from nipype.utils.misc import package_check
-package_check('numpy', '1.3', 'tbss_test')
-package_check('scipy', '0.7', 'tbss_test')
-package_check('networkx', '1.0', 'tbss_test')
-package_check('IPython', '0.10', 'tbss_test')
-
 fsl.FSLCommand.set_default_output_type('NIFTI')
 
 """
-You can get the data from http://fcon_1000.projects.nitrc.org/indi/pro/eNKI_RS_TRT/FrontPage.html
+You can get the data from:
+
+http://fcon_1000.projects.nitrc.org/indi/pro/eNKI_RS_TRT/FrontPage.html
 """
+
 dataDir = os.path.abspath('nki_rs_data')
 workingdir = './tbss_example'
-subjects_list = ['2475376', '3313349', '3808535', '3893245', '8735778', '9630905']
+subjects_list = ['2475376', '3313349', '3808535', '3893245', '8735778',
+                 '9630905']
 
 gen_fa = pe.Workflow(name="gen_fa")
 gen_fa.base_dir = os.path.join(os.path.abspath(workingdir), 'l1')
 
-subject_id_infosource = pe.Node(interface=util.IdentityInterface(fields=['subject_id']), name='subject_id_infosource')
+subject_id_infosource = pe.Node(util.IdentityInterface(fields=['subject_id']),
+                                name='subject_id_infosource')
 subject_id_infosource.iterables = ('subject_id', subjects_list)
 
-datasource = pe.Node(interface=nio.DataGrabber(infields=['subject_id'], outfields=['dwi', 'bvec', 'bval']), name='datasource')
+datasource = pe.Node(interface=nio.DataGrabber(infields=['subject_id'],
+                                               outfields=['dwi', 'bvec',
+                                                          'bval']),
+                     name='datasource')
 datasource.inputs.base_directory = os.path.abspath(dataDir)
 datasource.inputs.template = '%s/session2/DTI_mx_137/dti.%s'
 datasource.inputs.template_args = dict(dwi=[['subject_id', 'nii.gz']],
@@ -64,7 +69,8 @@ gen_fa.connect(datasource, 'bvec', dtifit, 'bvecs')
 gen_fa.connect(datasource, 'bval', dtifit, 'bvals')
 
 datasink = pe.Node(interface=nio.DataSink(), name="datasink")
-datasink.inputs.base_directory = os.path.join(os.path.abspath(workingdir), 'l1_results')
+datasink.inputs.base_directory = os.path.join(os.path.abspath(workingdir),
+                                              'l1_results')
 datasink.inputs.parameterization = False
 gen_fa.connect(dtifit, 'FA', datasink, 'FA')
 gen_fa.connect(dtifit, 'MD', datasink, 'MD')
@@ -78,7 +84,9 @@ if __name__ == '__main__':
 Here we get the FA list including all the subjects.
 """
 
-tbss_source = pe.Node(interface=nio.DataGrabber(outfiles=['fa_list', 'md_list']), name='tbss_source')
+tbss_source = pe.Node(interface=nio.DataGrabber(outfiles=['fa_list',
+                                                          'md_list']),
+                      name='tbss_source')
 tbss_source.inputs.base_directory = datasink.inputs.base_directory
 tbss_source.inputs.template = '%s/%s_%s.nii'
 tbss_source.inputs.template_args = dict(fa_list=[['FA', subjects_list, 'FA']],
@@ -98,11 +106,16 @@ tbssproc.connect(tbss_source, 'fa_list', tbss_all, 'inputnode.fa_list')
 tbss_MD = create_tbss_non_FA(name='tbss_MD')
 tbss_MD.inputs.inputnode.skeleton_thresh = tbss_all.inputs.inputnode.skeleton_thresh
 
-tbssproc.connect([(tbss_all, tbss_MD, [('tbss2.outputnode.field_list', 'inputnode.field_list'),
-                                       ('tbss3.outputnode.groupmask', 'inputnode.groupmask'),
-                                       ('tbss3.outputnode.meanfa_file', 'inputnode.meanfa_file'),
-                                       ('tbss4.outputnode.distance_map', 'inputnode.distance_map')]),
-                  (tbss_source, tbss_MD, [('md_list', 'inputnode.file_list')]),
+tbssproc.connect([(tbss_all, tbss_MD, [('tbss2.outputnode.field_list',
+                                        'inputnode.field_list'),
+                                       ('tbss3.outputnode.groupmask',
+                                        'inputnode.groupmask'),
+                                       ('tbss3.outputnode.meanfa_file',
+                                        'inputnode.meanfa_file'),
+                                       ('tbss4.outputnode.distance_map',
+                                        'inputnode.distance_map')]),
+                  (tbss_source, tbss_MD, [('md_list',
+                                           'inputnode.file_list')]),
             ])
 
 if __name__ == '__main__':
