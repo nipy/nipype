c92d431216413cc1801bf01d7e79156e0002a9ce
Satrajit Ghosh <satrajit.ghosh@gmail.com>
2012-02-27 18:29:22 -0500

    fix: copy report generator template

--- nipype/pipeline/setup.py
+++ nipype/pipeline/setup.py
@@ -7,6 +7,7 @@ def configuration(parent_package='',top_path=None):
 
     config.add_subpackage('plugins')
     config.add_data_dir('tests')
+    config.add_data_files('report_template.html')
 
     return config
 
