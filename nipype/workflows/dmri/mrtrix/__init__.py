5fe6940bc3594820e15e7ae19e4f329a45a8fe10
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2012-02-27 12:02:06 +0000

    dmri subpackages follow the import pattern of other modalities now

--- nipype/workflows/dmri/mrtrix/__init__.py
+++ nipype/workflows/dmri/mrtrix/__init__.py
@@ -1,2 +1,4 @@
-from . import (diffusion, connectivity_mapping, group_connectivity)
+from diffusion import create_mrtrix_dti_pipeline
+from connectivity_mapping import create_connectivity_pipeline, 
+from group_connectivity import create_mrtrix_group_cff_pipeline_part1
 
