0ab99f583e64d2875cd985febd8550e45f67861c
Erik Ziegler <erik.ziegler@ulg.ac.be>
2012-04-04 17:39:43 +0200

    Replaced CFF option with multiple network output

--- nipype/interfaces/cmtk/nbs.py
+++ nipype/interfaces/cmtk/nbs.py
@@ -7,7 +7,6 @@ from nipype.utils.filemanip import split_filename
 import os, os.path as op
 import numpy as np
 import networkx as nx
-from ..cmtk import CFFConverter
 from nipype.utils.misc import package_check
 import warnings
 
@@ -47,108 +46,99 @@ class NetworkBrainStatisticInputSpec(BaseInterfaceInputSpec):
     t_tail = traits.Enum('left', 'right', 'both', usedefault=True, desc='Can be one of "left", "right", or "both"')
     edge_key = traits.Str('number_of_fibers', usedefault=True, desc='Usually "number_of_fibers, "fiber_length_mean", "fiber_length_std" for matrices made with CMTK' \
      'Sometimes "weight" or "value" for functional networks.')
-    output_as_cff = traits.Bool(True, usedefault=True, desc='Option to save the output networks in a Connectome File Format (CFF) file')
-    out_gpickled_groupavg = File(desc='Average network saved as a NetworkX .pck')
-    out_gexf_groupavg = File(desc='Average network saved as a .gexf file')
+    out_nbs_network = File(desc='Output network with edges identified by the NBS')
+    out_nbs_pval_network = File(desc='Output network with p-values to weight the edges identified by the NBS')
 
 class NetworkBrainStatisticOutputSpec(TraitedSpec):
-    connectome_file = File(desc='Output of both networks as a CFF file')
     nbs_network = File(exists=True, desc='Output network with edges identified by the NBS')
     nbs_pval_network = File(exists=True, desc='Output network with p-values to weight the edges identified by the NBS')
+    network_files = OutputMultiPath(File(exists=True), desc='Output network with edges identified by the NBS')
 
 class NetworkBrainStatistic(BaseInterface):
-    """
-    Calculates and outputs the average network given a set of input NetworkX gpickle files
+	"""
+	Calculates and outputs the average network given a set of input NetworkX gpickle files
 
-    For documentation of Network-based statistic parameters:
-        
-        https://github.com/LTS5/connectomeviewer/blob/master/cviewer/libs/pyconto/groupstatistics/nbs/_nbs.py
+	For documentation of Network-based statistic parameters:
+		
+		https://github.com/LTS5/connectomeviewer/blob/master/cviewer/libs/pyconto/groupstatistics/nbs/_nbs.py
 
-    Example
-    -------
+	Example
+	-------
 
-    >>> import nipype.interfaces.cmtk as cmtk
-    >>> nbs = cmtk.NetworkBrainStatistic()
-    >>> nbs.inputs.in_group1 = ['subj1.pck', 'subj2.pck'] # doctest: +SKIP
-    >>> nbs.inputs.in_group2 = ['pat1.pck', 'pat2.pck'] # doctest: +SKIP
-    >>> nbs.run()                 # doctest: +SKIP
-    """
-    input_spec = NetworkBrainStatisticInputSpec
-    output_spec = NetworkBrainStatisticOutputSpec
+	>>> import nipype.interfaces.cmtk as cmtk
+	>>> nbs = cmtk.NetworkBrainStatistic()
+	>>> nbs.inputs.in_group1 = ['subj1.pck', 'subj2.pck'] # doctest: +SKIP
+	>>> nbs.inputs.in_group2 = ['pat1.pck', 'pat2.pck'] # doctest: +SKIP
+	>>> nbs.run()                 # doctest: +SKIP
+	"""
+	input_spec = NetworkBrainStatisticInputSpec
+	output_spec = NetworkBrainStatisticOutputSpec
 
-    def _run_interface(self, runtime):
-        THRESH = self.inputs.threshold
-        K = self.inputs.number_of_permutations
-        TAIL = self.inputs.t_tail
-        edge_key = self.inputs.edge_key
-        
-        # Fill in the data from the networks
-        X = ntwks_to_matrices(self.inputs.in_group1, edge_key)
-        Y = ntwks_to_matrices(self.inputs.in_group2, edge_key)
+	def _run_interface(self, runtime):
+		THRESH = self.inputs.threshold
+		K = self.inputs.number_of_permutations
+		TAIL = self.inputs.t_tail
+		edge_key = self.inputs.edge_key
+		
+		# Fill in the data from the networks
+		X = ntwks_to_matrices(self.inputs.in_group1, edge_key)
+		Y = ntwks_to_matrices(self.inputs.in_group2, edge_key)
 
-        PVAL, ADJ, NULL = nbs.compute_nbs(X,Y,THRESH,K,TAIL)
-        
-        pADJ = ADJ.copy()
-        for idx, p in enumerate(PVAL):
-            x, y = np.where(ADJ == idx+1)
-            pADJ[x,y] = PVAL[idx]
+		PVAL, ADJ, NULL = nbs.compute_nbs(X,Y,THRESH,K,TAIL)
+		
+		iflogger.info('p-values:')
+		iflogger.info(PVAL)
+		
+		pADJ = ADJ.copy()
+		for idx, p in enumerate(PVAL):
+			x, y = np.where(ADJ == idx+1)
+			pADJ[x,y] = PVAL[idx]
 
-        # Create networkx graphs from the adjacency matrix
-        nbsgraph = nx.from_numpy_matrix(ADJ)
-        nbs_pval_graph = nx.from_numpy_matrix(pADJ)
-        
-        # Relabel nodes because they should not start at zero for our convention
-        nbsgraph=nx.relabel_nodes(nbsgraph, lambda x: x + 1)
-        nbs_pval_graph = nx.relabel_nodes(nbs_pval_graph, lambda x: x + 1)
-        
-        if isdefined(self.inputs.node_position_network):
+		# Create networkx graphs from the adjacency matrix
+		nbsgraph = nx.from_numpy_matrix(ADJ)
+		nbs_pval_graph = nx.from_numpy_matrix(pADJ)
+		
+		# Relabel nodes because they should not start at zero for our convention
+		nbsgraph=nx.relabel_nodes(nbsgraph, lambda x: x + 1)
+		nbs_pval_graph = nx.relabel_nodes(nbs_pval_graph, lambda x: x + 1)
+		
+		if isdefined(self.inputs.node_position_network):
 			node_ntwk_name = self.inputs.node_position_network
 		else:
 			node_ntwk_name = self.inputs.in_group1[0]
 
-        node_network = nx.read_gpickle(node_ntwk_name)
-        iflogger.info('Populate node dictionaries with attributes from {node}'.format(node=node_ntwk_name))
-        
-        for nid, ndata in node_network.nodes_iter(data=True):
-            nbsgraph.node[nid] = ndata
-            nbs_pval_graph.node[nid] = ndata
+		node_network = nx.read_gpickle(node_ntwk_name)
+		iflogger.info('Populate node dictionaries with attributes from {node}'.format(node=node_ntwk_name))
+		
+		for nid, ndata in node_network.nodes_iter(data=True):
+			nbsgraph.node[nid] = ndata
+			nbs_pval_graph.node[nid] = ndata
 
-        path = op.join(op.curdir,'NBS_Result_' + '_' + edge_key + '-thresh-' + str(THRESH) + '-k-' + str(K) + '-tail-' + TAIL + '.pck')
-        iflogger.info(path)
-        nx.write_gpickle(nbsgraph, path)
-        iflogger.info('Saving output NBS edge network as {out}'.format(out=path))
+		path = op.abspath('NBS_Result_' + '_' + edge_key + '-thresh-' + str(THRESH) + '-k-' + str(K) + '-tail-' + TAIL + '.pck')
+		iflogger.info(path)
+		nx.write_gpickle(nbsgraph, path)
+		iflogger.info('Saving output NBS edge network as {out}'.format(out=path))
 
-        pval_path = op.join(op.curdir,'NBS_P_vals_' + '_' + edge_key + '-thresh-' + str(THRESH) + '-k-' + str(K) + '-tail-' + TAIL + '.pck')
-        iflogger.info(pval_path)
-        nx.write_gpickle(nbs_pval_graph, pval_path)
-        iflogger.info('Saving output p-value network as {out}'.format(out=pval_path))
-        
-        if self.inputs.output_as_cff:
-            output_cff_name = 'NBS_' + edge_key + '_' + '-thresh-' + str(THRESH) + '-k-' + str(K) + '-tail-' + TAIL + '.cff'
-            cff = CFFConverter()
-            cff.inputs.gpickled_networks = [path, pval_path]
-            cff.inputs.out_file = output_cff_name
-            iflogger.info('Saving output cff as {out}'.format(out=output_cff_name))
-            cff.run()
+		pval_path = op.abspath('NBS_P_vals_' + '_' + edge_key + '-thresh-' + str(THRESH) + '-k-' + str(K) + '-tail-' + TAIL + '.pck')
+		iflogger.info(pval_path)
+		nx.write_gpickle(nbs_pval_graph, pval_path)
+		iflogger.info('Saving output p-value network as {out}'.format(out=pval_path))
+		return runtime
 
-        return runtime
+	def _list_outputs(self):
+		outputs = self.output_spec().get()
 
-    def _list_outputs(self):
-        outputs = self.output_spec().get()
+		THRESH = self.inputs.threshold
+		K = self.inputs.number_of_permutations
+		TAIL = self.inputs.t_tail
+		edge_key = self.inputs.edge_key
+		path = op.abspath('NBS_Result_' + '_' + edge_key + '-thresh-' + str(THRESH) + '-k-' + str(K) + '-tail-' + TAIL + '.pck')
+		pval_path = op.abspath('NBS_P_vals_' + '_' + edge_key + '-thresh-' + str(THRESH) + '-k-' + str(K) + '-tail-' + TAIL + '.pck')
+	
+		outputs['nbs_network'] = path
+		outputs['nbs_pval_network'] = pval_path
+		outputs['network_files'] = [path, pval_path]
+		return outputs
 
-        THRESH = self.inputs.threshold
-        K = self.inputs.number_of_permutations
-        TAIL = self.inputs.t_tail
-        edge_key = self.inputs.edge_key
-        path = op.join(op.curdir,'NBS_Result_' + '_' + edge_key + '-thresh-' + str(THRESH) + '-k-' + str(K) + '-tail-' + TAIL + '.pck')
-        pval_path = op.join(op.curdir,'NBS_P_vals_' + '_' + edge_key + '-thresh-' + str(THRESH) + '-k-' + str(K) + '-tail-' + TAIL + '.pck')
-        output_cff_name = 'NBS_' + edge_key + '_' + '-thresh-' + str(THRESH) + '-k-' + str(K) + '-tail-' + TAIL + '.cff'
-        
-        outputs['nbs_network'] = path
-        outputs['nbs_pval_network'] = pval_path
-        if self.inputs.output_as_cff:
-            outputs['connectome_file'] = output_cff_name
-        return outputs
-
-    def _gen_outfilename(self, name, ext):
-        return name + '.' + ext
+	def _gen_outfilename(self, name, ext):
+		return name + '.' + ext
