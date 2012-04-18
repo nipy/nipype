ab8ef4283348ed04ca6bbb61a5d7d553db6875ef
Erik Ziegler <erik.ziegler@ulg.ac.be>
2012-04-18 21:12:36 +0200

    Threshold to a float

--- nipype/interfaces/cmtk/nbs.py
+++ nipype/interfaces/cmtk/nbs.py
@@ -40,7 +40,7 @@ class NetworkBrainStatisticInputSpec(BaseInterfaceInputSpec):
     in_group2 = InputMultiPath(File(exists=True), mandatory=True, desc='Networks for the second group of subjects')
     node_position_network = File(desc='An optional network used to position the nodes for the output networks')
     number_of_permutations = traits.Int(1000, usedefault=True, desc='Number of permutations to perform')
-    threshold = traits.Int(2, usedefault=True, desc='Threshold for the number of components')
+    threshold = traits.Float(3, usedefault=True, desc='T-statistic threshold')
     t_tail = traits.Enum('left', 'right', 'both', usedefault=True, desc='Can be one of "left", "right", or "both"')
     edge_key = traits.Str('number_of_fibers', usedefault=True, desc='Usually "number_of_fibers, "fiber_length_mean", "fiber_length_std" for matrices made with CMTK' \
      'Sometimes "weight" or "value" for functional networks.')
