e869474517f7e4db84bc5d9a086b0b2003c74349
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2012-05-14 17:44:37 +0100

    simple FS example

---
+++ examples/amri_freesurfer.py
@@ -0,0 +1,50 @@
+import nipype.pipeline.engine as pe
+import nipype.interfaces.io as nio
+import nipype.interfaces.utility as util
+import os
+from nipype.interfaces.freesurfer.preprocess import ReconAll
+
+subject_list = ['s1', 's3']
+data_dir = os.path.abspath('data')
+subjects_dir = os.path.abspath('amri_freesurfer_tutorial/subjects_dir')
+
+wf = pe.Workflow(name="l1workflow")
+wf.base_dir = os.path.abspath('amri_freesurfer_tutorial/workdir')
+
+datasource = pe.MapNode(interface=nio.DataGrabber(infields=['subject_id'],
+                                                  outfields=['struct']),
+                        name='datasource',
+                        iterfield=['subject_id'])
+datasource.inputs.base_directory = data_dir
+datasource.inputs.template = '%s/%s.nii'
+datasource.inputs.template_args = dict(struct=[['subject_id', 'struct']])
+datasource.inputs.subject_id = subject_list
+
+recon_all = pe.MapNode(interface=ReconAll(), name='recon_all',
+                       iterfield=['subject_id'])
+recon_all.inputs.subject_id = subject_list
+
+wf.connect(datasource, 'struct', recon_all, 'T1_files')
+
+
+def MakeAverageSubject(subjects_dir, subjects_list, out_name):
+    from nipype.interfaces.base import CommandLine
+    mas = CommandLine(command='make_average_subject')
+    mas.inputs.args = "--sdir %s --subjects %s --out %s"%(subjects_dir, " "%subject_list, out_name)
+    mas.run()
+    return subjects_dir, out_name
+
+average = pe.Node(interface=util.Function(input_names=['subjects_dir',
+                                                       'subjects_list',
+                                                       'out_name'],
+                                          output_names=['subjects_dir',
+                                                        'out_name'],
+                                          function=MakeAverageSubject),
+                  name="average")
+
+average.inputs.out_name = "average"
+
+wf.connect(recon_all, 'subjects_dir', average, 'subjects_dir')
+wf.connect(recon_all, 'subject_id', average, 'subjects_list')
+
+wf.run("MultiProc", plugin_args={'n_procs': 4})
