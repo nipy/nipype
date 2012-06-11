7772438986e62f8c3b352b47ada3e4e3c097bc5e
Satrajit Ghosh <satra@mit.edu>
2012-06-11 08:30:34 -0400

    fix: allow for multiple levels of loops for estimation

--- nipype/interfaces/nipy/preprocess.py
+++ nipype/interfaces/nipy/preprocess.py
@@ -69,34 +69,31 @@ class ComputeMask(BaseInterface):
 
 class FmriRealign4dInputSpec(BaseInterfaceInputSpec):
 
+
     in_file = InputMultiPath(exists=True,
                              mandatory=True,
                              desc="File to realign")
     tr = traits.Float(desc="TR in seconds",
                       mandatory=True)
     slice_order = traits.List(traits.Int(),
-                              desc='0 based slice order', 
+                              desc='0 based slice order',
                               requires=["time_interp"])
     tr_slices = traits.Float(desc="TR slices")
     start = traits.Float(0.0, usedefault=True,
                          desc="time offset into TR to align slices to")
     time_interp = traits.Enum(True, requires=["slice_order"],
-                    desc="Assume smooth changes across time e.g.,\
+                              desc="Assume smooth changes across time e.g.,\
                      fmri series. If you don't want slice timing \
                      correction set this to undefined")
-    loops = traits.Either(traits.Int(5,usedefault=True), 
-                          traits.List(traits.Int), 
-                          usedefault=True, 
-                          desc="loops within each run")
-    between_loops = traits.Either(traits.Int(5),
-                                  traits.List(traits.Int), 
-                                  usedefault=True, desc="loops used to \
+    loops = InputMultiPath(traits.Int(5), usedefault=True,
+                           desc="loops within each run")
+    between_loops = InputMultiPath(traits.Int(5),
+                                   usedefault=True, desc="loops used to \
                                                           realign different \
                                                           runs")
-    speedup = traits.Either(traits.Int(5),
-                            traits.List(traits.Int(5)),
-                            usedefault=True, 
-                            desc="successive image \
+    speedup = InputMultiPath(traits.Int(5),
+                             usedefault=True,
+                             desc="successive image \
                                   sub-sampling factors \
                                   for acceleration")
 
