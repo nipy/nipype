e51dc983bfb419e1119e266bff4f421ba850f755
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2012-09-15 14:06:09 +0200

    Cleanup of ApplyTransforms

--- nipype/interfaces/ants/antsApplyTransforms.py
+++ nipype/interfaces/ants/antsApplyTransforms.py
@@ -12,7 +12,7 @@ import os
 
 from .base import ANTSCommand, ANTSCommandInputSpec
 from ..base import (TraitedSpec, File, traits,
-                    InputMultiPath, OutputMultiPath, isdefined)
+                    isdefined)
 from ...utils.filemanip import split_filename
 
 
@@ -58,13 +58,17 @@ class antsApplyTransforms(ANTSCommand):
     Examples
     --------
 
-    >>> from nipype.interfaces.ants import antsApplyTransforms
-    >>> atms = antsApplyTransforms()
-    >>> atms.inputs.input_image = 'structural.nii'
-    >>> atms.inputs.reference_image = 'ants_deformed.nii.gz'
-    >>> atms.inputs.transforms = ['ants_Warp.nii.gz','ants_Affine.txt']
-    >>> atms.cmdline
-    'antsApplyTransforms --dimensionality 3 --input structural.nii --output structural_trans.nii --reference-image ants_deformed.nii.gz --transform ants_Warp.nii.gz --transform ants_Affine.txt'
+    >>> from nipype.interfaces.ants.antsApplyTransforms import antsApplyTransforms
+    >>> at = antsApplyTransforms()
+    >>> at.inputs.dimension = 3
+    >>> at.inputs.input_image = 'moving1.nii'
+    >>> at.inputs.reference_image = 'fixed1.nii'
+    >>> at.inputs.interpolation = 'Linear'
+    >>> at.inputs.default_value = 0
+    >>> at.inputs.transforms = ['trans.mat', 'ants_Warp.nii.gz']
+    >>> at.cmdline
+    'antsApplyTransforms --default-value 0 --dimensionality 3 --input moving1.nii --interpolation Linear --output moving1_trans.nii --reference-image fixed1.nii --transform trans.mat --transform ants_Warp.nii.gz'
+
 
     """
     _cmd = 'antsApplyTransforms'
@@ -94,9 +98,9 @@ class antsApplyTransforms(ANTSCommand):
 
     def _getOutputWarpedFileName(self):
         if isdefined(self.inputs.print_out_composite_warp_file):
-            return "--output [%s,%s]"%(self.inputs.output_image, self.inputs.print_out_composite_warp_file)
+            return "--output [%s,%s]"%(self._gen_filename("output_image"), self.inputs.print_out_composite_warp_file)
         else:
-            return "--output %s"%(self.inputs.output_image)
+            return "--output %s"%(self._gen_filename("output_image"))
 
     def _format_arg(self, opt, spec, val):
         if opt == "output_image":
@@ -112,101 +116,3 @@ class antsApplyTransforms(ANTSCommand):
         outputs = self._outputs().get()
         outputs['output_image'] = os.path.abspath(self._gen_filename('output_image'))
         return outputs
-
-"""
-COMMAND:
-     antsApplyTransforms
-          antsApplyTransforms, applied to an input image, transforms it according to a
-          reference image and a transform (or a set of transforms).
-
-OPTIONS:
-     -d, --dimensionality 2/3
-          This option forces the image to be treated as a specified-dimensional image. If
-          not specified, antsWarp tries to infer the dimensionality from the input image.
-
-     -i, --input inputFileName
-          Currently, the only input objects supported are image objects. However, the
-          current framework allows for warping of other objects such as meshes and point
-          sets.
-
-     -r, --reference-image imageFileName
-          For warping input images, the reference image defines the spacing, origin, size,
-          and direction of the output warped image.
-
-     -o, --output warpedOutputFileName
-                  [compositeDisplacementField,<printOutCompositeWarpFile=0>]
-          One can either output the warped image or, if the boolean is set, one can print
-          out the displacement field based on thecomposite transform and the reference
-          image.
-
-     -n, --interpolation Linear
-                         NearestNeighbor
-                         MultiLabel[<sigma=imageSpacing>,<alpha=4.0>]
-                         Gaussian[<sigma=imageSpacing>,<alpha=1.0>]
-                         BSpline[<order=3>]
-                         CosineWindowedSinc
-                         WelchWindowedSinc
-                         HammingWindowedSinc
-                         LanczosWindowedSinc
-          Several interpolation options are available in ITK. These have all been made
-          available.
-
-     -t, --transform transformFileName
-                     [transformFileName,useInverse]
-          Several transform options are supported including all those defined in the ITK
-          library in addition to a deformation field transform. The ordering of the
-          transformations follows the ordering specified on the command line. An identity
-          transform is pushed onto the transformation stack. Each new transform
-          encountered on the command line is also pushed onto the transformation stack.
-          Then, to warp the input object, each point comprising the input object is warped
-          first according to the last transform pushed onto the stack followed by the
-          second to last transform, etc. until the last transform encountered which is the
-          identity transform. Also, it should be noted that the inverse transform can be
-          accommodated with the usual caveat that such an inverse must be defined by the
-          specified transform class
-
-     -v, --default-value value
-          Default voxel value to be used with input images only. Specifies the voxel value
-          when the input point maps outside the output domain
-
-     -h
-          Print the help menu (short version).
-          <VALUES>: 0
-
-     --help
-          Print the help menu.
-          <VALUES>: 0
-======================================================
-
-## Forward direction
-##   BtoA( X_A ) ~= B( AffineTfm(WarpTfm( X_A ) ) ),        Where X_A represents physical points from the space of image A
-{BINARIES_DIRECTORY}/bin/antsApplyTransforms \
-   --dimensionality 3 \
-   --input SUBJ_B_T1_resampled.nii.gz \
-   --reference-image SUBJ_A_T1_resampled.nii.gz \
-   --output antsResampleBtoA.nii.gz \
-   --interpolation Linear \
-   --default-value 0 \
-   --transforms [20120430_1348_ANTS6_1Warp.nii.gz, 20120430_1348_txfmv2fv_affine.mat] \
-   --invert_transforms_flags [True, True]
-
-## Reverse direction
-##   AtoB( X_B ) ~= A( InvWarpTfm( InvAffineTfm( X_B ) ) )  Where X_B represents physical points from the space of image B
-{BINARIES_DIRECTORY}/bin/antsApplyTransforms \
-   --dimensionality 3 \
-   --input SUBJ_B_T1_resampled.nii.gz \
-   --reference-image SUBJ_A_T1_resampled.nii.gz \
-   --output antsResampleBtoA.nii.gz \
-   --interpolation Linear \
-   --default-value 0 \
-   --transforms [20120430_1348_txfmv2fv_affine.mat, 20120430_1348_ANTS6_1InverseWarp.nii.gz] \
-   --invert_transforms_flags [True, False]
-
-======================================================
-    antsApplyTransforms.inputs  most of these are self explanatory.
-
-    ## For the transform list definition, there needs to be a vector of input images, and a corresponding vector of inversion commands.
-    antsApplyTransforms.inputs.transforms=[20120430_1348_ANTS6_1InverseWarp.nii.gz,20120430_1348_txfmv2fv_affine.mat]
-    antsApplyTransforms.inputs.invertTransformsList=[0,1]
-
-"""
