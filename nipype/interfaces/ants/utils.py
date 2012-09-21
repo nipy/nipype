bd6ed83fa1c17d64fa4c24f672fe139f2727d3f2
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2012-09-21 11:56:55 +0200

    We can provide a default value for an output - no need to have it mandatory.

--- nipype/interfaces/ants/utils.py
+++ nipype/interfaces/ants/utils.py
@@ -50,8 +50,8 @@ class AverageAffineTransform(ANTSCommand):
 
 
 class AverageImagesInputSpec(ANTSCommandInputSpec):
-    dimension = traits.Enum(3, 2, argstr='%d', usedefault=False, mandatory=True, position=0, desc='image dimension (2 or 3)')
-    output_average_image = File(argstr='%s', mandatory=True, position=1, desc='Outputfname.nii.gz: the name of the resulting image.')
+    dimension = traits.Enum(3, 2, argstr='%d', mandatory=True, position=0, desc='image dimension (2 or 3)')
+    output_average_image = File("average.nii", argstr='%s', position=1, desc='the name of the resulting image.', usedefault=True, hash_files=False)
     normalize = traits.Bool(argstr="%d", mandatory=True, position=2, desc='Normalize: if true, the 2nd image' +
                             'is divided by its mean. This will select the largest image to average into.')
     images = InputMultiPath(File(exists=True), argstr='%s', mandatory=True, position=3, desc=('image to apply transformation to (generally a coregistered functional)') )
