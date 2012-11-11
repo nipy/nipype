fd747e2c5f6913aa316cd24024e5bc6b8aa5eb0c
Hans Johnson <hans-johnson@uiowa.edu>
2012-11-11 08:11:08 -0600

    BUG: Incorrect indentation caused missing inputs
    Many inputs were failing to be processed due to incorrect indentation
    logic that caused only outputs to be fully processed.
    
    Include docstring and pep8 compliant formatting.
    Ran autopep8 tool to force compliant formatting.

--- nipype/interfaces/slicer/generate_classes.py
+++ nipype/interfaces/slicer/generate_classes.py
@@ -9,14 +9,24 @@ import os
 from shutil import rmtree
 
 import keyword
-python_keywords = keyword.kwlist ## If c++ SEM module uses one of these key words as a command line parameter, we need to modify variable
+python_keywords = keyword.kwlist  # If c++ SEM module uses one of these key words as a command line parameter, we need to modify variable
+
+
 def force_to_valid_python_variable_name(old_name):
-  new_name=old_name
-  new_name = new_name.lstrip().rstrip()
-  if old_name in python_keywords:
-      new_name="xx"+old_name
-  return new_name
-        
+    """  Valid c++ names are not always valid in python, so
+    provide alternate naming
+
+    >>> force_to_valid_python_variable_name('lambda')
+    'opt_lambda'
+    >>> force_to_valid_python_variable_name('inputVolume')
+    'inputVolume'
+    """
+    new_name = old_name
+    new_name = new_name.lstrip().rstrip()
+    if old_name in python_keywords:
+        new_name = 'opt_' + old_name
+    return new_name
+
 
 def add_class_to_package(class_codes, class_names, module_name, package_dir):
     module_python_filename = os.path.join(package_dir, "%s.py" % module_name)
@@ -28,7 +38,7 @@ If you spot a bug, please report it on the mailing list and/or change the genera
 import os
 from nipype.interfaces.slicer.base import SlicerCommandLine\n\n\n"""
     f_m.write(imports)
-    f_m.write("\n\n".join(class_codes).encode('utf8') )
+    f_m.write("\n\n".join(class_codes).encode('utf8'))
     f_i.write("from %s import %s\n" % (module_name, ", ".join(class_names)))
     f_m.close()
     f_i.close()
@@ -41,7 +51,8 @@ def crawl_code_struct(code_struct, package_dir):
             module_name = k.lower()
             class_name = k
             class_code = v
-            add_class_to_package([class_code], [class_name], module_name, package_dir)
+            add_class_to_package(
+                [class_code], [class_name], module_name, package_dir)
         else:
             l1 = {}
             l2 = {}
@@ -63,11 +74,12 @@ def crawl_code_struct(code_struct, package_dir):
                 crawl_code_struct(v, new_pkg_dir)
                 if l1:
                     for ik, iv in l1.iteritems():
-                        crawl_code_struct({ik:{ik:iv}}, new_pkg_dir)
+                        crawl_code_struct({ik: {ik: iv}}, new_pkg_dir)
             elif l1:
                 v = l1
                 module_name = k.lower()
-                add_class_to_package(v.values(), v.keys(), module_name, package_dir)
+                add_class_to_package(
+                    v.values(), v.keys(), module_name, package_dir)
         if subpackages:
             f = open(os.path.join(package_dir, "setup.py"), 'w')
             f.write("""# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
@@ -84,7 +96,7 @@ def configuration(parent_package='',top_path=None):
 if __name__ == '__main__':
     from numpy.distutils.core import setup
     setup(**configuration(top_path='').todict())
-""".format(pkg_name=package_dir.split("/")[-1], sub_pks="\n    ".join(["config.add_data_dir('%s')"%sub_pkg for sub_pkg in subpackages])))
+""".format(pkg_name=package_dir.split("/")[-1], sub_pks="\n    ".join(["config.add_data_dir('%s')" % sub_pkg for sub_pkg in subpackages])))
             f.close()
 
 
@@ -113,9 +125,8 @@ def generate_all_classes(modules_list=[], launcher=[]):
     crawl_code_struct(all_code, os.getcwd())
 
 
-
-def generate_class(module,launcher):
-    dom = grab_xml(module,launcher)
+def generate_class(module, launcher):
+    dom = grab_xml(module, launcher)
     inputTraits = []
     outputTraits = []
     outputs_filenames = {}
@@ -124,14 +135,13 @@ def generate_class(module,launcher):
 
     class_string = "\"\"\""
 
-
-
     for desc_str in ['title', 'category', 'description', 'version',
                      'documentation-url', 'license', 'contributor',
                      'acknowledgements']:
         el = dom.getElementsByTagName(desc_str)
         if el and el[0].firstChild:
-            class_string += desc_str + ": " + el[0].firstChild.nodeValue + "\n\n"
+            class_string += desc_str + ": " + el[
+                0].firstChild.nodeValue + "\n\n"
         if desc_str == 'category':
             category = el[0].firstChild.nodeValue
     class_string += "\"\"\""
@@ -158,11 +168,12 @@ def generate_class(module,launcher):
                 ##     <longflag>test</longflag>
                 longFlagName = longFlagName.lstrip(" -").rstrip(" ")
                 name = longFlagName
-                name=force_to_valid_python_variable_name(name)
+                name = force_to_valid_python_variable_name(name)
                 traitsParams["argstr"] = "--" + longFlagName + " "
             else:
-                name = param.getElementsByTagName('name')[0].firstChild.nodeValue
-                name=force_to_valid_python_variable_name(name)
+                name = param.getElementsByTagName(
+                    'name')[0].firstChild.nodeValue
+                name = force_to_valid_python_variable_name(name)
                 if param.getElementsByTagName('index'):
                     traitsParams["argstr"] = ""
                 else:
@@ -185,7 +196,8 @@ def generate_class(module,launcher):
 
             index = param.getElementsByTagName('index')
             if index:
-                traitsParams["position"] = int(index[0].firstChild.nodeValue)-(max_index+1)
+                traitsParams["position"] = int(
+                    index[0].firstChild.nodeValue) - (max_index + 1)
 
             desc = param.getElementsByTagName('description')
             if index:
@@ -204,7 +216,8 @@ def generate_class(module,launcher):
             elif param.nodeName.endswith('-vector'):
                 type = "InputMultiPath"
                 if param.nodeName in ['file', 'directory', 'image', 'geometry', 'transform', 'table']:
-                    values = ["%s(exists=True)" % typesDict[param.nodeName.replace('-vector', '')]]
+                    values = ["%s(exists=True)" % typesDict[
+                              param.nodeName.replace('-vector', '')]]
                 else:
                     values = [typesDict[param.nodeName.replace('-vector', '')]]
                 traitsParams["sep"] = ','
@@ -213,7 +226,8 @@ def generate_class(module,launcher):
                 if param.nodeName in ['file', 'directory', 'image', 'geometry', 'transform', 'table']:
                     values = ["%s(exists=True)" % typesDict[param.nodeName]]
                 elif param.nodeName in ['point', 'region']:
-                    values = ["%s(traits.Float(), minlen=3, maxlen=3)" % typesDict[param.nodeName]]
+                    values = ["%s(traits.Float(), minlen=3, maxlen=3)" %
+                              typesDict[param.nodeName]]
                 else:
                     values = [typesDict[param.nodeName]]
                 traitsParams["argstr"] += "..."
@@ -223,24 +237,31 @@ def generate_class(module,launcher):
 
             if param.nodeName in ['file', 'directory', 'image', 'geometry', 'transform', 'table']:
                 if not param.getElementsByTagName('channel'):
-                    raise RuntimeError("Insufficient XML specification: each element of type 'file', 'directory', 'image', 'geometry', 'transform',  or 'table' requires 'channel' field.")
+                    raise RuntimeError("Insufficient XML specification: each element of type 'file', 'directory', 'image', 'geometry', 'transform',  or 'table' requires 'channel' field.\n{0}".format(traitsParams))
                 elif param.getElementsByTagName('channel')[0].firstChild.nodeValue == 'output':
                     traitsParams["hash_files"] = False
-                    inputTraits.append("%s = traits.Either(traits.Bool, %s(%s), %s)" % (name,
-                                                                                     type,
-                                                                                     parse_values(values).replace("exists=True", ""),
-                                                                                     parse_params(traitsParams)))
+                    inputTraits.append(
+                        "%s = traits.Either(traits.Bool, %s(%s), %s)" % (name,
+                                                                         type,
+                                                                         parse_values(
+                                                                         values).replace("exists=True", ""),
+                                                                         parse_params(traitsParams)))
                     traitsParams["exists"] = True
                     traitsParams.pop("argstr")
                     traitsParams.pop("hash_files")
                     outputTraits.append("%s = %s(%s%s)" % (name, type.replace("Input", "Output"), parse_values(values), parse_params(traitsParams)))
-    
-                    outputs_filenames[name] = gen_filename_from_param(param, name)
-            else:
-                if param.nodeName in ['file', 'directory', 'image', 'geometry', 'transform', 'table'] and type not in ["InputMultiPath", "traits.List"]:
-                    traitsParams["exists"] = True
 
-                inputTraits.append("%s = %s(%s%s)" % (name, type, parse_values(values), parse_params(traitsParams)))
+                    outputs_filenames[
+                        name] = gen_filename_from_param(param, name)
+                elif param.getElementsByTagName('channel')[0].firstChild.nodeValue == 'input':
+                    if param.nodeName in ['file', 'directory', 'image', 'geometry', 'transform', 'table'] and type not in ["InputMultiPath", "traits.List"]:
+                        traitsParams["exists"] = True
+                    inputTraits.append("%s = %s(%s%s)" % (name, type, parse_values(values), parse_params(traitsParams)))
+                else:
+                    raise RuntimeError("Insufficient XML specification: each element of type 'file', 'directory', 'image', 'geometry', 'transform',  or 'table' requires 'channel' field to be in ['input','output'].\n{0}".format(traitsParams))
+            else:  # For all other parameter types, they are implicitly only input types
+                inputTraits.append("%s = %s(%s%s)" % (name, type, parse_values(
+                    values), parse_params(traitsParams)))
 
     input_spec_code = "class " + module + "InputSpec(CommandLineInputSpec):\n"
     for trait in inputTraits:
@@ -254,7 +275,8 @@ def generate_class(module,launcher):
             output_spec_code += "    " + trait + "\n"
 
     output_filenames_code = "_outputs_filenames = {"
-    output_filenames_code += ",".join(["'%s':'%s'" % (key, value) for key, value in outputs_filenames.iteritems()])
+    output_filenames_code += ",".join(["'%s':'%s'" % (
+        key, value) for key, value in outputs_filenames.iteritems()])
     output_filenames_code += "}"
 
     input_spec_code += "\n\n"
@@ -276,11 +298,12 @@ def generate_class(module,launcher):
 def grab_xml(module, launcher):
 #        cmd = CommandLine(command = "Slicer3", args="--launch %s --xml"%module)
 #        ret = cmd.run()
-        command_list = launcher[:] ## force copy to preserve original
-        command_list.extend([module, "--xml"])
-        final_command = " ".join(command_list)
-        xmlReturnValue = subprocess.Popen(final_command, stdout=subprocess.PIPE, shell=True).communicate()[0]
-        return xml.dom.minidom.parseString(xmlReturnValue)
+    command_list = launcher[:]  # force copy to preserve original
+    command_list.extend([module, "--xml"])
+    final_command = " ".join(command_list)
+    xmlReturnValue = subprocess.Popen(
+        final_command, stdout=subprocess.PIPE, shell=True).communicate()[0]
+    return xml.dom.minidom.parseString(xmlReturnValue)
 #        if ret.runtime.returncode == 0:
 #            return xml.dom.minidom.parseString(ret.runtime.stdout)
 #        else:
@@ -315,7 +338,8 @@ def gen_filename_from_param(param, base):
         firstFileExtension = fileExtensions.split(',')[0]
         ext = firstFileExtension
     else:
-        ext = {'image': '.nii', 'transform': '.mat', 'file': '', 'directory': '', 'geometry': '.vtk'}[param.nodeName]
+        ext = {'image': '.nii', 'transform': '.mat', 'file': '',
+               'directory': '', 'geometry': '.vtk'}[param.nodeName]
     return base + ext
 
 if __name__ == "__main__":
@@ -334,51 +358,52 @@ if __name__ == "__main__":
                     'DiffusionTensorMathematics', 'DiffusionTensorTest',
                     'DiffusionWeightedMasking',
                     'BRAINSFit',
-#'BRAINSABC',
-#'BRAINSCut',
-#'BRAINSAlignMSP',
-#'BRAINSClipInferior',
-#'BRAINSConstellationDetector',
-#'BRAINSConstellationModeler',
-#'BRAINSLandmarkInitializer',
-'BRAINSDemonWarp',
-#'BRAINSMush',
-#'BRAINSInitializedControlPoints',
-#'BRAINSLinearModelerEPCA',
-#'BRAINSLmkTransform',
-#'BRAINSMultiModeSegment',
-'BRAINSROIAuto',
-'BRAINSResample',
-#'BRAINSTrimForegroundInDirection',
-#'ESLR',
-#'GenerateLabelMapFromProbabilityMap',
-'VBRAINSDemonWarp',
-'extractNrrdVectorIndex',
-'gtractAnisotropyMap',
-'gtractAverageBvalues',
-'gtractClipAnisotropy',
-'gtractCoRegAnatomy',
-'gtractConcatDwi',
-'gtractCopyImageOrientation',
-'gtractCoregBvalues',
-'gtractCostFastMarching',
-'gtractImageConformity',
-'gtractInvertBSplineTransform',
-'gtractInvertDeformationField',
-'gtractInvertRigidTransform',
-'gtractResampleAnisotropy',
-'gtractResampleB0',
-'gtractResampleCodeImage',
-'gtractResampleDWIInPlace',
-'gtractTensor',
-'gtractTransformToDeformationField',
-#'GradientAnisotropicDiffusionImageFilter',
-#'GenerateSummedGradientImage'
-]
+                    #'BRAINSABC',
+                    #'BRAINSCut',
+                    #'BRAINSAlignMSP',
+                    #'BRAINSClipInferior',
+                    #'BRAINSConstellationDetector',
+                    #'BRAINSConstellationModeler',
+                    #'BRAINSLandmarkInitializer',
+                    'BRAINSDemonWarp',
+                    #'BRAINSMush',
+                    #'BRAINSInitializedControlPoints',
+                    #'BRAINSLinearModelerEPCA',
+                    #'BRAINSLmkTransform',
+                    #'BRAINSMultiModeSegment',
+                    'BRAINSROIAuto',
+                    'BRAINSResample',
+                    #'BRAINSTrimForegroundInDirection',
+                    #'ESLR',
+                    #'GenerateLabelMapFromProbabilityMap',
+                    'VBRAINSDemonWarp',
+                    'extractNrrdVectorIndex',
+                    'gtractAnisotropyMap',
+                    'gtractAverageBvalues',
+                    'gtractClipAnisotropy',
+                    'gtractCoRegAnatomy',
+                    'gtractConcatDwi',
+                    'gtractCopyImageOrientation',
+                    'gtractCoregBvalues',
+                    'gtractCostFastMarching',
+                    'gtractImageConformity',
+                    'gtractInvertBSplineTransform',
+                    'gtractInvertDeformationField',
+                    'gtractInvertRigidTransform',
+                    'gtractResampleAnisotropy',
+                    'gtractResampleB0',
+                    'gtractResampleCodeImage',
+                    'gtractResampleDWIInPlace',
+                    'gtractTensor',
+                    'gtractTransformToDeformationField',
+                    #'GradientAnisotropicDiffusionImageFilter',
+                    #'GenerateSummedGradientImage'
+                    ]
 
     ## SlicerExecutionModel compliant tools that are usually statically built, and don't need the Slicer3 --launcher
     #generate_all_classes(modules_list=modules_list,launcher=[])
     ## Tools compliant with SlicerExecutionModel called from the Slicer environment (for shared lib compatibility)
-    launcher = ['/home/filo/workspace/Slicer4-SuperBuild/Slicer-build/Slicer', '--launch']
+    launcher = ['/home/filo/workspace/Slicer4-SuperBuild/Slicer-build/Slicer',
+                '--launch']
     generate_all_classes(modules_list=modules_list, launcher=[])
     #generate_all_classes(modules_list=['BRAINSABC'], launcher=[] )
