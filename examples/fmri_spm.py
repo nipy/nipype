307c7408622cc4b8054d576582e5db6e4caa3d15
Satrajit Ghosh <satra@mit.edu>
2013-03-16 15:40:34 -0400

    fix: clean up spm example

--- examples/fmri_spm.py
+++ examples/fmri_spm.py
@@ -98,7 +98,7 @@ datasource = pe.Node(interface=nio.DataGrabber(infields=['subject_id'],
 datasource.inputs.base_directory = data_dir
 datasource.inputs.template = '%s/%s.nii'
 datasource.inputs.template_args = info
-
+datasource.inputs.sort_filelist = True
 
 """Use :class:`nipype.interfaces.spm.Realign` for motion correction
 and register all images to the mean image.
@@ -194,7 +194,7 @@ contrasts = [cont1,cont2]
 """
 
 modelspec = pe.Node(interface=model.SpecifySPMModel(), name= "modelspec")
-modelspec.inputs.concatenate_runs        = True
+modelspec.inputs.concatenate_runs        = False
 modelspec.inputs.input_units             = 'secs'
 modelspec.inputs.output_units            = 'secs'
 modelspec.inputs.time_repetition         = 3.
