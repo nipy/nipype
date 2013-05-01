b146eb56475d2b6d574ff445f865ea8ab6667d5f
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2013-05-01 19:16:50 +0200

    added desc into inputspec

--- nipype/interfaces/cmtk/parcellation.py
+++ nipype/interfaces/cmtk/parcellation.py
@@ -524,7 +524,8 @@ class ParcellateInputSpec(BaseInterfaceInputSpec):
     subjects_dir = Directory(exists=True, desc='Freesurfer subjects directory')
     out_roi_file = File(
         genfile=True, desc='Region of Interest file for connectivity mapping')
-    dilation = traits.Bool(False, usedefault=True)
+    dilation = traits.Bool(False, usedefault=True, 
+                           desc='Dilate cortical parcels? Useful for fMRI connectivity')
 
 
 class ParcellateOutputSpec(TraitedSpec):
