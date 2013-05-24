45f66cf1cfc5a3a17fda78bb9663db02086fe1c2
FredLoney <loneyf@ohsu.edu>
2013-05-23 18:56:31 -0700

    Disable the ants Registration interface sigma_units vox value until it works in an ANTS release.

--- nipype/interfaces/ants/registration.py
+++ nipype/interfaces/ants/registration.py
@@ -286,7 +286,7 @@ class RegistrationInputSpec(ANTSCommandInputSpec):
     smoothing_sigmas = traits.List(traits.List(traits.Float()), mandatory=True)
     sigma_units = traits.List(traits.Enum('mm', 'vox'),
                               requires=['smoothing_sigmas'],
-                              desc="units for smoothing sigmas", mandatory=True)
+                              desc="units for smoothing sigmas")
     shrink_factors = traits.List(traits.List(traits.Int()), mandatory=True)
     convergence_threshold = traits.List(trait=traits.Float(), value=[1e-6], minlen=1, requires=['number_of_iterations'], usedefault=True)
     convergence_window_size = traits.List(trait=traits.Int(), value=[10], minlen=1, requires=['convergence_threshold'], usedefault=True)
