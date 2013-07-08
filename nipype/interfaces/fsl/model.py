714d94dc07eff0a3668a3e166623c3115df069bc
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2013-07-08 21:07:19 +0200

    Merge and clean up FSLGLM into GLM.

--- nipype/interfaces/fsl/model.py
+++ nipype/interfaces/fsl/model.py
@@ -549,180 +549,6 @@ threshold=10, results_dir='stats')
                                              cwd=results_dir)
         return outputs
 
-class FSLGLMInputSpec(FSLCommandInputSpec):
-    """
-    Use FSL GLM
-    @author: sebastian urchs
-    @contact: sebastian.urchs@gmail.com
-
-    """
-
-    in_file = File(exists=True, argstr='-i %s', mandatory=True, position=1,
-                   desc='input file name (text matrix or 3D/4D image file)')
-
-    output_file = File(argstr='-o %s', mandatory=True, position=3,
-                       desc=('filename for GLM parameter estimates'
-                             + ' (GLM betas)'))
-
-    design_file = File(exists=True, argstr='-d %s', mandatory=True, position=2,
-                       desc=('file name of the GLM design matrix (text time'
-                             + ' courses for temporal regression or an image'
-                             + ' file for spatial regression)'))
-
-    contrasts = File(exists=True, argstr='-c %s', desc=('matrix of t-statics'
-                                                        + ' contrasts'))
-
-    mask = File(exists=True, argstr='-m %s', desc=('mask image file name if'
-                                                   + ' input is image'))
-
-    dof = traits.Int(argstr='--dof %d', desc=('set degrees of freedom'
-                                              + ' explicitly'))
-
-    des_norm = traits.Bool(argstr='--des_norm', desc=('switch on normalization'
-                                                     + ' of the design matrix'
-                                                     + ' columns to unit std'
-                                                     + ' deviation'))
-
-    dat_norm = traits.Bool(argstr='--dat_norm', desc=('switch on normalization'
-                                                      + ' of the data time'
-                                                      + ' series to unit std'
-                                                      + ' deviation'))
-
-    var_norm = traits.Bool(argstr='--vn', desc=('perform MELODIC variance-'
-                                                + 'normalisation on data'))
-
-    demean = traits.Bool(argstr='--demean', desc=('switch on demeaining of '
-                                                  + ' design and data'))
-
-    out_cope = File(argstr='--out_cope=%s',
-                    desc='output file name for COPE (either as txt or image')
-
-    out_z_name = File(argstr='--out_z=%s',
-                 desc='output file name for Z-stats (either as txt or image')
-
-    out_t_name = File(argstr='--out_t=%s',
-                 desc='output file name for t-stats (either as txt or image')
-
-    out_p_name = File(argstr='--out_p=%s',
-                 desc=('output file name for p-values of Z-stats (either as'
-                       + ' text file or image)'))
-
-    out_f_name = File(argstr='--out_f=%s',
-                 desc='output file name for F-value of full model fit')
-
-    out_pf_name = File(argstr='--out_pf=%s',
-                 desc='output file name for p-value for full model fit')
-
-    out_res_name = File(argstr='--out_res=%s',
-                 desc='output file name for residuals')
-
-    out_varcb_name = File(argstr='--out_varcb=%s',
-                 desc='output file name for variance of COPEs')
-
-    out_sigsq_name = File(argstr='--out_sigsq=%s',
-                 desc=('output file name for residual noise variance'
-                       + ' sigma-square'))
-
-    out_data_name = File(argstr='--out_data=%s',
-                 desc='output file name for pre-processed data')
-
-    out_vnscales_name = File(argstr='--out_vnscales=%s',
-                 desc=('output file name for scaling factors for variance'
-                       + ' normalisation'))
-
-
-class FSLGLMOutputSpec(TraitedSpec):
-    out_file = File(exists=True, desc=('file name of GLM parameters'
-                                       + ' (if generated)'))
-
-    out_cope = OutputMultiPath(exists=True, desc='output file name for COPEs' +
-                               ' (either as text file or image)')
-
-    out_z = OutputMultiPath(exists=True, desc='output file name for COPEs' +
-                            ' (either as text file or image)')
-
-    out_t = OutputMultiPath(exists=True, desc='output file name for t-stats' +
-                            ' (either as text file or image)')
-
-    out_p = OutputMultiPath(exists=True, desc='output file name for p-values' +
-                            '  of Z-stats (either as text file or image)')
-
-    out_f = OutputMultiPath(exists=True, desc='output file name for F-value' +
-                            ' of full model fit')
-
-    out_pf = OutputMultiPath(exists=True, desc='output file name for p-value' +
-                             ' for full model fit')
-
-    out_res = OutputMultiPath(exists=True, desc='output file name for' +
-                              ' residuals')
-
-    out_varcb = OutputMultiPath(exists=True, desc='output file name for' +
-                                ' variance of COPEs')
-
-    out_sigsq = OutputMultiPath(exists=True, desc='output file name for' +
-                                ' residual noise variance sigma-square')
-
-    out_data = OutputMultiPath(exists=True, desc='output file name for' +
-                               ' residual noise variance sigma-square')
-
-    out_vnscales = OutputMultiPath(exists=True, desc='output file name' +
-                                   ' for scaling factors for variance' +
-                                   ' normalisation')
-
-
-class FSLGLM(FSLCommand):
-    """
-    Use FSL GLM.
-    @author: sebastian urchs
-    @contact: sebastian.urchs@gmail.com
-    @status: testing
-
-    """
-    _cmd = 'fsl_glm'
-    input_spec = FSLGLMInputSpec
-    output_spec = FSLGLMOutputSpec
-
-    def _list_outputs(self):
-        import os
-        outputs = self.output_spec().get()
-
-        outputs['out_file'] = os.path.join(os.getcwd(), self.inputs.output_file)
-
-        if isdefined(self.inputs.out_cope):
-            outputs['out_cope'] = os.path.join(os.getcwd(), self.inputs.out_cope)
-
-        if isdefined(self.inputs.out_z_name):
-            outputs['out_z'] = os.path.join(os.getcwd(), self.inputs.out_z_name)
-
-        if isdefined(self.inputs.out_t_name):
-            outputs['out_t'] = os.path.join(os.getcwd(), self.inputs.out_t_name)
-
-        if isdefined(self.inputs.out_p_name):
-            outputs['out_p'] = os.path.join(os.getcwd(), self.inputs.out_p_name)
-
-        if isdefined(self.inputs.out_f_name):
-            outputs['out_f'] = os.path.join(os.getcwd(), self.inputs.out_f_name)
-
-        if isdefined(self.inputs.out_pf_name):
-            outputs['out_pf'] = os.path.join(os.getcwd(), self.inputs.out_pf_name)
-
-        if isdefined(self.inputs.out_res_name):
-            outputs['out_res'] = os.path.join(os.getcwd(), self.inputs.out_res_name)
-
-        if isdefined(self.inputs.out_varcb_name):
-            outputs['out_varcb'] = os.path.join(os.getcwd(), self.inputs.out_varcb_name)
-
-        if isdefined(self.inputs.out_sigsq_name):
-            outputs['out_sigsq'] = os.path.join(os.getcwd(), self.inputs.out_sigsq_name)
-
-        if isdefined(self.inputs.out_data_name):
-            outputs['out_data'] = os.path.join(os.getcwd(), self.inputs.out_data_name)
-
-        if isdefined(self.inputs.out_vnscales_name):
-            outputs['out_vnscales'] = os.path.join(os.getcwd(), self.inputs.out_vnscales_name)
-
-        return outputs
-
 
 
 class FEATRegisterInputSpec(BaseInterfaceInputSpec):
@@ -1686,34 +1512,88 @@ class Randomise(FSLCommand):
 
 
 class GLMInputSpec(FSLCommandInputSpec):
-    in_file = File(
-        exists=True,mandatory=True,
-        argstr='-i %s',
-        desc='input 3d+t file',)
-    design = File(
-        exists=True, mandatory=True,
-        argstr='-d %s',
-        desc='design file or image file')
-    out_file = File(
-        genfile=True, argstr="-o %s",
-        desc="file or image output")
-    mask = File(
-        exists=True,
-        argstr='-m %s',
-        desc='mask file')
-    contrasts = File(
-        exists=True,
-        argstr='-c %s',
-        desc='t-contrasts file')
-    options = traits.String(
-        argstr='%s',
-        desc = 'fsl_glm options')
+    in_file = File(exists=True, argstr='-i %s', mandatory=True, position=1,
+                   desc='input file name (text matrix or 3D/4D image file)')
+    out_file = File(argstr='-o %s', genfile=True, position=3,
+                       desc=('filename for GLM parameter estimates'
+                             + ' (GLM betas)'))
+    design = File(exists=True, argstr='-d %s', mandatory=True, position=2,
+                       desc=('file name of the GLM design matrix (text time'
+                             + ' courses for temporal regression or an image'
+                             + ' file for spatial regression)'))
+    contrasts = File(exists=True, argstr='-c %s', desc=('matrix of t-statics'
+                                                        + ' contrasts'))
+    mask = File(exists=True, argstr='-m %s', desc=('mask image file name if'
+                                                   + ' input is image'))
+    dof = traits.Int(argstr='--dof %d', desc=('set degrees of freedom'
+                                              + ' explicitly'))
+    des_norm = traits.Bool(argstr='--des_norm', desc=('switch on normalization'
+                                                     + ' of the design matrix'
+                                                     + ' columns to unit std'
+                                                     + ' deviation'))
+    dat_norm = traits.Bool(argstr='--dat_norm', desc=('switch on normalization'
+                                                      + ' of the data time'
+                                                      + ' series to unit std'
+                                                      + ' deviation'))
+    var_norm = traits.Bool(argstr='--vn', desc=('perform MELODIC variance-'
+                                                + 'normalisation on data'))
+    demean = traits.Bool(argstr='--demean', desc=('switch on demeaining of '
+                                                  + ' design and data'))
+    out_cope = File(argstr='--out_cope=%s',
+                    desc='output file name for COPE (either as txt or image')
+    out_z_name = File(argstr='--out_z=%s',
+                 desc='output file name for Z-stats (either as txt or image')
+    out_t_name = File(argstr='--out_t=%s',
+                 desc='output file name for t-stats (either as txt or image')
+
+    out_p_name = File(argstr='--out_p=%s',
+                 desc=('output file name for p-values of Z-stats (either as'
+                       + ' text file or image)'))
+    out_f_name = File(argstr='--out_f=%s',
+                 desc='output file name for F-value of full model fit')
+    out_pf_name = File(argstr='--out_pf=%s',
+                 desc='output file name for p-value for full model fit')
+    out_res_name = File(argstr='--out_res=%s',
+                 desc='output file name for residuals')
+    out_varcb_name = File(argstr='--out_varcb=%s',
+                 desc='output file name for variance of COPEs')
+
+    out_sigsq_name = File(argstr='--out_sigsq=%s',
+                 desc=('output file name for residual noise variance'
+                       + ' sigma-square'))
+    out_data_name = File(argstr='--out_data=%s',
+                 desc='output file name for pre-processed data')
+    out_vnscales_name = File(argstr='--out_vnscales=%s',
+                 desc=('output file name for scaling factors for variance'
+                       + ' normalisation'))
 
 
 class GLMOutputSpec(TraitedSpec):
-    out_file = File(
-        exists=True,
-        desc = 'file or image output')
+    out_file = File(exists=True, desc=('file name of GLM parameters'
+                                       + ' (if generated)'))
+    out_cope = OutputMultiPath(exists=True, desc='output file name for COPEs' +
+                               ' (either as text file or image)')
+    out_z = OutputMultiPath(exists=True, desc='output file name for COPEs' +
+                            ' (either as text file or image)')
+    out_t = OutputMultiPath(exists=True, desc='output file name for t-stats' +
+                            ' (either as text file or image)')
+    out_p = OutputMultiPath(exists=True, desc='output file name for p-values' +
+                            '  of Z-stats (either as text file or image)')
+    out_f = OutputMultiPath(exists=True, desc='output file name for F-value' +
+                            ' of full model fit')
+    out_pf = OutputMultiPath(exists=True, desc='output file name for p-value' +
+                             ' for full model fit')
+    out_res = OutputMultiPath(exists=True, desc='output file name for' +
+                              ' residuals')
+    out_varcb = OutputMultiPath(exists=True, desc='output file name for' +
+                                ' variance of COPEs')
+    out_sigsq = OutputMultiPath(exists=True, desc='output file name for' +
+                                ' residual noise variance sigma-square')
+    out_data = OutputMultiPath(exists=True, desc='output file name for' +
+                               ' residual noise variance sigma-square')
+    out_vnscales = OutputMultiPath(exists=True, desc='output file name' +
+                                   ' for scaling factors for variance' +
+                                   ' normalisation')
 
 
 class GLM(FSLCommand):
@@ -1731,13 +1611,49 @@ class GLM(FSLCommand):
     _cmd = 'fsl_glm'
     input_spec = GLMInputSpec
     output_spec = GLMOutputSpec
-
+    
     def _list_outputs(self):
         outputs = self.output_spec().get()
+
         outputs['out_file'] = self.inputs.out_file
         # Generate an out_file if one is not provided
         if not isdefined(outputs['out_file']) and isdefined(self.inputs.in_file):
-            outputs['out_file'] = os.path.abspath(self._gen_filename('out_file'))
+            outputs['out_file'] = self._gen_filename('out_file')
+        outputs['out_file'] = os.path.abspath(outputs['out_file'])
+
+        if isdefined(self.inputs.out_cope):
+            outputs['out_cope'] = os.path.abspath(self.inputs.out_cope)
+
+        if isdefined(self.inputs.out_z_name):
+            outputs['out_z'] = os.path.abspath(self.inputs.out_z_name)
+
+        if isdefined(self.inputs.out_t_name):
+            outputs['out_t'] = os.path.abspath(self.inputs.out_t_name)
+
+        if isdefined(self.inputs.out_p_name):
+            outputs['out_p'] = os.path.abspath(self.inputs.out_p_name)
+
+        if isdefined(self.inputs.out_f_name):
+            outputs['out_f'] = os.path.abspath(self.inputs.out_f_name)
+
+        if isdefined(self.inputs.out_pf_name):
+            outputs['out_pf'] = os.path.abspath(self.inputs.out_pf_name)
+
+        if isdefined(self.inputs.out_res_name):
+            outputs['out_res'] = os.path.abspath(self.inputs.out_res_name)
+
+        if isdefined(self.inputs.out_varcb_name):
+            outputs['out_varcb'] = os.path.abspath(self.inputs.out_varcb_name)
+
+        if isdefined(self.inputs.out_sigsq_name):
+            outputs['out_sigsq'] = os.path.abspath(self.inputs.out_sigsq_name)
+
+        if isdefined(self.inputs.out_data_name):
+            outputs['out_data'] = os.path.abspath(self.inputs.out_data_name)
+
+        if isdefined(self.inputs.out_vnscales_name):
+            outputs['out_vnscales'] = os.path.abspath(self.inputs.out_vnscales_name)
+
         return outputs
 
     def _gen_filename(self, name):
