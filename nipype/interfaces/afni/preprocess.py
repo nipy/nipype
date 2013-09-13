aa76f523c6e85df502f1b3342489708be590e099
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2013-09-13 10:56:55 +0200

    fixed inputs for Retroicor

--- nipype/interfaces/afni/preprocess.py
+++ nipype/interfaces/afni/preprocess.py
@@ -1765,30 +1765,34 @@ class Autobox(AFNICommand):
 
 class RetroicorInputSpec(AFNICommandInputSpec):
     in_file = File(desc='input file to 3dretroicor',
-                  argstr='%s',
+                   argstr='%s',
                    position=-1,
                    mandatory=True,
                    exists=True)
     out_file = File(desc='output image file name', argstr='-prefix %s', mandatory=True, position=1)
     card = File(desc='1D cardiac data file for cardiac correction',
-                    argstr='-card %s',
-                    position=-2)
+                argstr='-card %s',
+                position=-2,
+                exists=True)
     resp = File(desc='1D respiratory waveform data for correction',
-                    argstr='-resp %s',
-                    position=-3)
+                argstr='-resp %s',
+                position=-3,
+                exists=True)
     threshold = traits.Int(desc='Threshold for detection of R-wave peaks in input (Make sure it is above the background noise level, Try 3/4 or 4/5 times range plus minimum)',
-                      argstr='-threshold %d',
-                      position=-4)
+                           argstr='-threshold %d',
+                           position=-4)
     order = traits.Int(desc='The order of the correction (2 is typical)',
-                    argstr='-order %s',
-                    position=-5)
+                       argstr='-order %s',
+                       position=-5)
 
     cardphase = File(desc='Filename for 1D cardiac phase output',
-                    argstr='-cardphase %s',
-                    position=-6)
+                     argstr='-cardphase %s',
+                     position=-6,
+                     hash_files=False)
     respphase = File(desc='Filename for 1D resp phase output',
-                    argstr='-respphase %s',
-                    position=-7)
+                     argstr='-respphase %s',
+                     position=-7,
+                     hash_files=False)
 
 
 class Retroicor(AFNICommand):
