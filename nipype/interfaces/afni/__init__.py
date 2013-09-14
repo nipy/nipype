3127289a9ee7e6ad34ab57fdd33f6c4e3dd42305
Satrajit Ghosh <satrajit.ghosh@gmail.com>
2013-09-14 11:36:30 -0600

    fix: afni url in afni package
    closes #657

--- nipype/interfaces/afni/__init__.py
+++ nipype/interfaces/afni/__init__.py
@@ -1,10 +1,9 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """The afni module provides classes for interfacing with the `AFNI
-<http://www.fmrib.ox.ac.uk/afni/index.html>`_ command line tools.  This
-was written to work with AFNI version 4.1.4.
+<http://afni.nimh.nih.gov/afni>`_ command line tools.  
 
-Top-level namespace for afni.  Perhaps should just make afni a package!
+Top-level namespace for afni.
 """
 
 from .base import Info
