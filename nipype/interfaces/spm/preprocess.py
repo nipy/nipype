29895bd4d160e3cb9bff00c49e0695583a566bbb
Ben Acland <benacland@gmail.com>
2013-10-15 22:28:11 -0500

    fix indents - nothing real

--- nipype/interfaces/spm/preprocess.py
+++ nipype/interfaces/spm/preprocess.py
@@ -149,12 +149,13 @@ class RealignInputSpec(SPMCommandInputSpec):
 
 class RealignOutputSpec(TraitedSpec):
     mean_image = File(exists=True, desc='Mean image file from the realignment')
-    in_files = OutputMultiPath(traits.Either(
-        traits.List(File(exists=True)),
-        File(exists=True)),
-        desc='Copies of all files passed to in_files. Headers will have been modified\
-              to align all images with the first, or optionally to first do that,\
-              extract a mean image, and re-align to that.')
+    in_files = OutputMultiPath(traits.Either(traits.List(File(exists=True)),
+                                             File(exists=True)),
+                               desc='Copies of all files passed to in_files.\
+                                     Headers will have been modified to align all\
+                                     images with the first, or optionally to first\
+                                     do that, extract a mean image, and re-align to\
+                                     that mean image.')
     realigned_files = OutputMultiPath(traits.Either(traits.List(File(exists=True)),
                                                     File(exists=True)),
                                       desc='Realigned files')
