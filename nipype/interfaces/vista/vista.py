d63901f8fed6c79dff16b31d4e32203246f17e18
dmordom <moreno@cbs.mpg.de>
2013-12-06 17:05:46 +0100

    Created interface for vimage2nifti command from lipvia vista package

---
+++ nipype/interfaces/vista/vista.py
@@ -0,0 +1,41 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+    Change directory to provide relative paths for doctests
+    >>> import os
+    >>> filepath = os.path.dirname( os.path.realpath( __file__ ) )
+    >>> datadir = os.path.realpath(os.path.join(filepath, '../../testing/data'))
+    >>> os.chdir(datadir)
+
+"""
+
+from nipype.interfaces.base import CommandLineInputSpec, CommandLine, traits, TraitedSpec, File
+from nipype.utils.filemanip import split_filename
+import os, os.path as op
+from nipype.interfaces.traits_extension import isdefined
+
+class Vnifti2ImageInputSpec(CommandLineInputSpec):
+    in_file = File(exists=True, argstr='-in %s', mandatory=True, position=1, desc='in file')
+    attributes = File(exists=True, argstr='-attr %s', mandatory=False, position=2, desc='attribute file')
+    out_file = File(name_template="%s.v", keep_extension=False, argstr='-out %s', hash_files=False,
+                    position= -1, desc='output data file', name_source=["in_file"])
+
+class Vnifti2ImageOutputSpec(TraitedSpec):
+    out_file = File(exists=True, desc='Output vista file')
+
+class Vnifti2Image(CommandLine):
+    """
+    Convert a nifti file into a vista file.
+
+    Example
+    -------
+
+    >>> vimage = Vnifti2Image()
+    >>> vimage.inputs.in_file = 'image.nii'
+    >>> vimage.inputs.out_file = 'image.v'
+    >>> vimage.run()                                       # doctest: +SKIP
+    """
+
+    _cmd = 'vnifti2image'
+    input_spec=Vnifti2ImageInputSpec
+    output_spec=Vnifti2ImageOutputSpec
