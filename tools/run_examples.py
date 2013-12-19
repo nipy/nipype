aea57fef8b46503fbf99e54827884f8c40ed54ed
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2013-12-19 21:30:39 +0100

    fixed workflow name

--- tools/run_examples.py
+++ tools/run_examples.py
@@ -22,7 +22,7 @@ def run_examples(example, pipelines, plugin):
 if __name__ == '__main__':
     path, file = os.path.split(__file__)
     sys.path.insert(0, os.path.realpath(os.path.join(path, '..', 'examples')))
-    examples = {'fmri_fsl_reuse':['l1pipeline'],
+    examples = {'fmri_fsl_reuse':['level1_workflow'],
                 'fmri_spm_nested':['level1','l2pipeline'],
                 'fmri_spm_dartel':['level1','l2pipeline'],
                 'fmri_fsl_feeds':['l1pipeline']}
