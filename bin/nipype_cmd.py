a3c7bd86eaee9b5958abd25a1fbebf08f470b19d
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2014-02-10 19:43:48 +0100

    The command now runs.

--- bin/nipype_cmd.py
+++ bin/nipype_cmd.py
@@ -24,63 +24,36 @@ def add_options(parser=None, module=None, function=None):
         interface = getattr(sys.modules[module],function)()
 
         for k,v in interface.inputs.items():
-            parser.add_argument("--%s"%k, dest=k,
+            if hasattr(v, "mandatory") and v.mandatory:
+                parser.add_argument(k, help=v.desc)
+            else:
+                parser.add_argument("--%s"%k, dest=k,
                                 help=v.desc)
     return parser, interface
 
 def run_instance(interface, options):
     if interface:
         print "setting function inputs"
-        for k,v in interface.inputs.items():
-            optionskey = ''.join(('IXI',k))
-            if hasattr(options, optionskey):
+        for k,_ in interface.inputs.items():
+            if getattr(options, k) != None:
                 setattr(interface.inputs, k,
-                        getattr(options, optionskey))
+                        getattr(options, k))
         print interface.inputs
-        print "not really running anything"
+        res = interface.run()
+        print res.outputs    
 
-def get_modfunc(args):
-    module = None
-    function = None
-    posargs = []
-    skip = False
-    for a in args:
-        if skip:
-            skip = False
-            continue
-        if a.startswith('--'):
-            pass
-        elif a.startswith('-'):
-            skip = True
-        else:
-            posargs.append(a)
-    if posargs:
-        module = posargs[0]
-        if len(posargs)==2:
-            function = posargs[1]
-    return module, function
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Nipype interface runner')
-    parser.add_argument("--run", dest="run", help="Execute", default=False)
+    parser.add_argument("module", type=str, help="Module name")
+    parser.add_argument("interface", type=str, help="Interface name")
+    parsed = parser.parse_args(args=sys.argv[1:3])
     
-#     
-#     usage = "usage: %prog [options] module function"
-#     parser = OptionParser(usage=usage,version="%prog 1.0",
-#                           conflict_handler="resolve")
-    
-
-    module, function = get_modfunc(sys.argv[1:])
-    parser, interface  = add_options(parser, module, function)
-    args = parser.parse_args()
-    if args.run and interface:
-        #assign inputs
-        run_instance(interface, args)
-    else:
-        parser.print_help()
-        if module and not function:
-            listClasses(module)
-        parser.exit()
+    _, prog = os.path.split(sys.argv[0])
+    interface_parser = argparse.ArgumentParser(description="Run %s"%parsed.interface, prog=" ".join([prog] + sys.argv[1:3]))
+    interface_parser, interface  = add_options(interface_parser, parsed.module, parsed.interface)
+    args = interface_parser.parse_args(args=sys.argv[3:])
+    run_instance(interface, args)
 
 
 #*****************************************************************************
