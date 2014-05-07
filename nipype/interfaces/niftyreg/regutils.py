b8ce074b80f252bbd19d2022da074c4c301ca647
Pankaj Daga <p.daga@ucl.ac.uk>
2014-05-07 14:54:57 +0100

    Wrapped the reg_f3d command line app

--- nipype/interfaces/niftyreg/regutils.py
+++ nipype/interfaces/niftyreg/regutils.py
@@ -27,16 +27,29 @@ warnings.filterwarnings('always', category=UserWarning)
 class PositiveInt (traits.BaseInt):
 
     # Define the default value
-    default_value = 1
+    default_value = 0
     # Describe the trait type
     info_text = 'A positive integer'
 
     def validate ( self, object, name, value ):
-        value = super(OddInt, self).validate(object, name, value)
+        value = super(PositiveInt, self).validate(object, name, value)
         if (value >= 0) == 1:
             return value
         self.error( object, name, value )
 
+# A custom trait class for positive floats
+class PositiveFloat (traits.BaseFloat):
+
+    # Define the default value
+    default_value = 0
+    # Describe the trait type
+    info_text = 'A positive float'
+
+    def validate ( self, object, name, value ):
+        value = super(PositiveFloat, self).validate(object, name, value)
+        if (value >= 0) == 1:
+            return value
+        self.error( object, name, value )
 
 #-----------------------------------------------------------
 # reg_resample wrapper interface
@@ -306,3 +319,134 @@ class RegAladin(CommandLine):
 
         if isdefined(self.inputs.result_file) and self.inputs.result_file:
             outputs['result_file'] = os.path.abspath(self.inputs.result_file)
+
+
+#-----------------------------------------------------------
+# reg_f3d wrapper interface
+#-----------------------------------------------------------
+# Input spec
+class RegF3DInputSpec(NiftyRegCommandInputSpec):
+    # Input reference file
+    ref_file = File(exists=True, desc='The input reference/target image',
+                   argstr='-ref %s', mandatory=True)
+    # Input floating file
+    flo_file = File(exists=True, desc='The input floating/source image',
+                   argstr='-flo %s', mandatory=True)
+    # Output CPP file
+    cpp_file = File(desc='The output CPP file', argstr='-cpp %s')
+    # Output image file
+    res_file = File(desc='The output resampled image', argstr='-res %s')
+    
+    # Reference mask
+    rmask_file = File(exists=True, desc='Reference image mask', argstr='-rmask %s')
+    
+    # Smoothing kernel for reference
+    ref_smooth_val = PositiveFloat(desc='Smoothing kernel width for reference image',
+                    argstr='-smooR %f')
+    # Smoothing kernel for floating
+    flo_smooth_val = PositiveFloat(desc='Smoothing kernel width for floating image',
+                    argstr='-smooF %f')
+    
+    # Lower threshold for reference image
+    rlwth_thr_val = traits.Float(desc='Lower threshold for reference image',
+                    argstr='--rLwTh %f')
+    # Upper threshold for reference image
+    rupth_thr_val = traits.Float(desc='Upper threshold for reference image',
+                    argstr='--rUpTh %f')
+    # Lower threshold for reference image
+    flwth_thr_val = traits.Float(desc='Lower threshold for floating image',
+                    argstr='--fLwTh %f')
+    # Upper threshold for reference image
+    fupth_thr_val = traits.Float(desc='Upper threshold for floating image',
+                    argstr='--fUpTh %f')
+
+
+    # Lower threshold for reference image
+    rlwth2_thr_val = traits.Tuple(PositiveInt, traits.Float, 
+                        desc='Lower threshold for reference image at the specified time point',
+                        argstr='-rLwTh %d %f')
+    # Upper threshold for reference image
+    rupth2_thr_val = traits.Tuple(PositiveInt, traits.Float, 
+                        desc='Upper threshold for reference image at the specified time point',
+                        argstr='-rUpTh %d %f')
+    # Lower threshold for reference image
+    flwth2_thr_val = traits.Tuple(PositiveInt, traits.Float, 
+                        desc='Lower threshold for floating image at the specified time point',
+                        argstr='-fLwTh %d %f')
+    # Upper threshold for reference image
+    fupth2_thr_val = traits.Tuple(PositiveInt, traits.Float, 
+                        desc='Upper threshold for floating image at the specified time point',
+                        argstr='-fUpTh %d %f')
+
+    # Final grid spacing along the 3 axes
+    sx_val = PositiveFloat(desc='Final grid spacing along the x axes', argstr='-sx %f')
+    sy_val = PositiveFloat(desc='Final grid spacing along the y axes', argstr='-sy %f')
+    sz_val = PositiveFloat(desc='Final grid spacing along the z axes', argstr='-sz %f')
+
+    # Regularisation options
+    be_val = traits.Float(desc='Bending energy value', argstr='-be %f')
+    le_val = traits.Tuple(traits.Float, traits.Float, desc='Linear elasticity penalty term',
+                        argstr='-le %f %f')
+    l2_val = traits.Float(desc='L2 norm of displacement penalty value', argstr='-l2 %f')
+    jl_val = traits.Float(desc='Log of jacobian of deformation penalty value', argstr='-jl %f')
+    no_app_jl_flag = traits.Bool(argstr='-noAppJL', 
+                desc='Do not approximate the log of jacobian penalty at control points only')
+
+    # Similarity measure options
+    nmi_flag = traits.Bool(argstr='--nmi', desc='use NMI even when other options are specified')
+    rbn_val = PositiveInt(desc='Number of bins in the histogram for reference image',
+                    argstr='--rbn %d')
+    fbn_val = PositiveInt(desc='Number of bins in the histogram for reference image',
+                    argstr='--fbn %d')
+    rbn2_val = traits.Tuple(PositiveInt, PositiveInt,
+        desc='Number of bins in the histogram for reference image for given time point',
+        argstr='-rbn %d %d')
+
+    fbn2_val = traits.Tuple(PositiveInt, PositiveInt, 
+        desc='Number of bins in the histogram for reference image for given time point',
+        argstr='-fbn %d %d')
+
+    lncc_val = PositiveFloat(desc='SD of the Gaussian for computing LNCC', argstr='--lncc %f')
+    lncc2_val = traits.Tuple(PositiveInt, PositiveFloat, 
+        desc='SD of the Gaussian for computing LNCC for a given time point', argstr='-lncc %d %f')
+    
+    ssd_flag = traits.Bool(desc='Use SSD as the similarity measure', argstr='--ssd')
+    ssd2_flag = PositiveInt(desc='Use SSD as the similarity measure for a given time point', 
+        argstr='-ssd %d')
+    kld_flag = traits.Bool(desc='Use KL divergence as the similarity measure', argstr='--kld')
+    kld2_flag = PositiveInt(desc='Use KL divergence as the similarity measure for a given time point', 
+        argstr='-kld %d')
+    amc_flag = traits.Bool(desc='Use additive NMI', argstr='-amc')
+
+    # Optimization options
+    maxit_val = PositiveInt(desc='Maximum number of iterations per level', argstr='-maxit %d')
+    ln_val = PositiveInt(desc='Number of resolution levels to create', argstr='-ln %d')
+    lp_val = PositiveInt(desc='Number of resolution levels to perform', argstr='-lp %d')
+    nopy_flag = traits.Bool(desc='Do not use the multiresolution approach', argstr='-nopy')
+    noconj_flag = traits.Bool(desc='Use simple GD optimization', argstr='-noConj')
+    pert_val = PositiveInt(desc='Add perturbation steps after each optimization step', 
+                    argstr='-pert %d')
+
+    # F3d2 options
+    vel_flag = traits.Bool(desc='Use velocity field integration', argstr='-vel')
+    fmask_file = File(exists=True, desc='Floating image mask', argstr='-fmask %s')
+
+    # Other options
+    smooth_grad_val = PositiveFloat(desc='Kernel width for smoothing the metric gradient',
+                        argstr='-smoothGrad %f')
+    # Padding value
+    pad_val = traits.Float(desc = 'Padding value', argstr="-pad %f")
+    # verbosity off
+    verbosity_off_flag = traits.Bool(argstr='-voff', desc='Turn off verbose output')
+
+
+# Output spec
+class RegF3dOutputSpec(TraitedSpec):
+    cpp_file = File(desc='The output CPP file')
+    res_file = File(desc='The output resampled image')
+
+# Main interface class
+class RegF3D(CommandLine):
+    _cmd = 'reg_f3d'
+    input_spec = RegF3DInputSpec
+    output_spec = RegF3dOutputSpec
