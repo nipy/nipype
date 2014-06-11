af62d2c171bf701a75f2cbefc72084f8481330ce
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2014-06-11 14:29:12 +0100

    niftyseg-interface: GIF: coding style improved, output_dir exported, outputs better handled

--- nipype/interfaces/niftyseg/gif.py
+++ nipype/interfaces/niftyseg/gif.py
@@ -10,7 +10,8 @@ from nibabel import load
 import os.path as op
 import warnings
 
-from nipype.interfaces.niftyseg.base import NIFTYSEGCommand, NIFTYSEGCommandInputSpec
+from nipype.interfaces.niftyseg.base import NIFTYSEGCommandInputSpec
+from nipype.interfaces.fsl.base import FSLCommand as NIFTYSEGCommand
 from nipype.interfaces.base import (TraitedSpec, File, Directory, traits, InputMultiPath,
                                     isdefined)
 
@@ -18,48 +19,49 @@ from nipype.interfaces.base import (TraitedSpec, File, Directory, traits, InputM
 warn = warnings.warn
 warnings.filterwarnings('always', category=UserWarning)
 
-
 class GifInputSpec(NIFTYSEGCommandInputSpec):
     
-    in_file = File(argstr="-in %s", exists=True, mandatory=True,
-                desc="Input target image filename")
+    in_file = File(argstr='-in %s', exists=True, mandatory=True,
+                desc='Input target image filename')
     
-    database_file = File(argstr="-db %s", exists=True, mandatory=True,
-                desc="Path to database <XML> file")    
+    database_file = File(argstr='-db %s', exists=True, 
+                         mandatory=True,
+                         desc='Path to database <XML> file')    
 
-    cpp_dir = Directory(exists=True, mandatory=True, argstr="-cpp %s", 
-                        desc="Folder to read/store cpp files.")
+    cpp_dir = Directory(exists=True, mandatory=True, argstr='-cpp %s', 
+                        desc='Folder to read/store cpp files.')
 
-    mask_file = File(exists=True, mandatory=False, argstr="-mask %s", 
-                     desc="Mask over the input image [default: none]")
+    mask_file = File(exists=True, argstr='-mask %s', 
+                     desc='Mask over the input image [default: none]')
 
-    out_dir = Directory(exists=True, mandatory=False, argstr="-out %s", 
-                        desc="Output folder [default: ./]")
+    out_dir = Directory(exists=True, argstr='-out %s', 
+                        desc='Output folder [default: ./]')
     
 
 class GifOutputSpec(TraitedSpec):
-    out_dir   = Directory(exists=True, desc="Output folder [default: ./]")
-    out_parc  = File(exists=False, genfile=True, desc='Parcellation file')
-    out_geo   = File(exists=False, genfile=True, desc='Geodesic distance file')
-    out_prior = File(exists=False, genfile=True, desc='Prior file')
+
+    out_dir    = Directory(desc='Output folder [default: ./]')    
+    parc_file  = File(genfile=True, desc='Parcellation file')
+    geo_file   = File(genfile=True, desc='Geodesic distance file')
+    prior_file = File(genfile=True, desc='Prior file')
 
 class Gif(NIFTYSEGCommand):
 
 
     """
 
-  GIF Propagation :
-  Usage -> seg_GIF <mandatory> <options>
-
-
-  * * * * * * * * * * * Mandatory * * * * * * * * * * * * * * * * * * * * * * *
-
+    GIF Propagation :
+    Usage -> seg_GIF <mandatory> <options>
+    
+    
+    * * * * * * * * * * * Mandatory * * * * * * * * * * * * * * * * * * * * * * *
+    
     -in <filename>	| Input target image filename
     -db <XML>   	| Path to database <XML> file
     -cpp <cpp_path>	| Folder to read/store cpp files.
-
-  * * * * * * * * * * * General Options * * * * * * * * * * * * * * * * * * * *
-
+    
+    * * * * * * * * * * * General Options * * * * * * * * * * * * * * * * * * * *
+    
     -mask <filename>	| Mask over the input image
     -out <path> 	| Output folder [./]
 
@@ -76,40 +78,30 @@ class Gif(NIFTYSEGCommand):
 
     """
 
-    _cmd = "seg_GIF"
-    _suffix = "_seg_GIF"
+    _cmd = 'seg_GIF'
     input_spec = GifInputSpec  
     output_spec = GifOutputSpec
 
-    def _gen_outputbasename(self, outputdir):
-        bname = os.path.basename(self.inputs.in_file)
-        if bname.endswith('.nii.gz'):
-            bname = bname[:-7]
-        if bname.endswith('.nii'):
-            bname = bname[:-4]
-        return os.path.join(outputdir, bname)
+    def _get_basename_without_extension(self, in_file):
+        ret = os.path.basename(in_file)
+        if ret.endswith('.nii.gz'):
+            ret = ret[:-7]
+        if ret.endswith('.nii'):
+            ret = ret[:-4]
+        return ret
         
-    def _gen_filename(self, name, outputdir):
-        if name == 'out_parc':
-            return self._gen_outputbasename(outputdir) + '_labels_Parcellation.nii.gz'
-        if name == 'out_geo':
-            return self._gen_outputbasename(outputdir) + '_labels_geo.nii.gz'
-        if name == 'out_prior':
-            return self._gen_outputbasename(outputdir) + '_labels_prior.nii.gz'
-        return None
-
     def _list_outputs(self):
-        """Execute this module.
-        """
         outputs = self.output_spec().get()
+
         if isdefined(self.inputs.out_dir) and self.inputs.out_dir:
             outputs['out_dir'] = os.path.abspath(self.inputs.out_dir)
         else:
-            outputs['out_dir'] = os.getcwd()        
-        
-        outputs['out_parc']  = self._gen_filename('out_parc', outputs['out_dir'])
-        outputs['out_geo']   = self._gen_filename('out_geo', outputs['out_dir'])
-        outputs['out_prior'] = self._gen_filename('out_prior', outputs['out_dir'])
+            outputs['out_dir'] = os.getcwd()
+ 
+        basename = self._get_basename_without_extension(self.inputs.in_file)
+        outputs['parc_file']   = os.path.join (outputs['out_dir'], basename + '_labels_Parcellation.nii.gz')
+        outputs['geo_file']    = os.path.join (outputs['out_dir'], basename + '_labels_geo.nii.gz')
+        outputs['prior_file']  = os.path.join (outputs['out_dir'], basename + '_labels_prior.nii.gz')        
 
         return outputs
         
