4e333045f04ac0673aefeafc731cbfa71ce9fcf1
Oscar Esteban <code@oscaresteban.es>
2014-06-19 20:06:11 +0200

    Review FSL eddy interface
    Just fixed PEP8 errors in the interface and reviewed the solution
    for the bugs, that were already solved.
    
    Please, close #769.

--- nipype/interfaces/fsl/epi.py
+++ nipype/interfaces/fsl/epi.py
@@ -327,77 +327,81 @@ class ApplyTOPUP( FSLCommand ):
         return super(ApplyTOPUP, self)._format_arg(name, spec, value)
 
 
-class EddyInputSpec( FSLCommandInputSpec ):
-    in_file =  File(exists=True, mandatory=True,
-                    desc=('File containing all the images to estimate '
-                          'distortions for'), argstr='--imain=%s')
-    in_mask =  File(exists=True, mandatory=True,
-                    desc='Mask to indicate brain', argstr='--mask=%s')
-    in_index = File(exists=True, mandatory=True,
+class EddyInputSpec(FSLCommandInputSpec):
+    in_file = File(exists=True, mandatory=True, argstr='--imain=%s',
+                   desc=('File containing all the images to estimate '
+                         'distortions for'))
+    in_mask = File(exists=True, mandatory=True, argstr='--mask=%s',
+                   desc='Mask to indicate brain')
+    in_index = File(exists=True, mandatory=True, argstr='--index=%s',
                     desc=('File containing indices for all volumes in --imain '
-                          'into --acqp and --topup'), argstr='--index=%s')
-    in_acqp =  File(exists=True, mandatory=True,
-                    desc='File containing acquisition parameters',
-                    argstr='--acqp=%s' )
-    in_bvec =  File(exists=True, mandatory=True,
-                    desc=('File containing the b-vectors for all volumes in '
-                          '--imain'), argstr='--bvecs=%s')
-    in_bval =  File(exists=True, mandatory=True,
-                    desc=('File containing the b-values for all volumes in '
-                          '--imain'), argstr='--bvals=%s')
-    out_base = File( desc='basename for output (warped) image',
-                     argstr='--out=%s' )
-    session =  File(exists=True,
-                    desc=('File containing session indices for all volumes in '
-                          '--imain'), argstr='--session=%s')
-    in_topup_fieldcoef = File(exists=True, argstr="--topup=%s", copyfile=False,
+                          'into --acqp and --topup'))
+    in_acqp = File(exists=True, mandatory=True, argstr='--acqp=%s',
+                   desc='File containing acquisition parameters')
+    in_bvec = File(exists=True, mandatory=True, argstr='--bvecs=%s',
+                   desc=('File containing the b-vectors for all volumes in '
+                         '--imain'))
+    in_bval = File(exists=True, mandatory=True, argstr='--bvals=%s',
+                   desc=('File containing the b-values for all volumes in '
+                         '--imain'))
+    out_base = File(argstr='--out=%s',
+                    desc=('basename for output (warped) image'))
+
+    session = File(exists=True,  argstr='--session=%s',
+                   desc=('File containing session indices for all volumes in '
+                         '--imain'))
+    in_topup_fieldcoef = File(exists=True, argstr="--topup=%s",
                               requires=['in_topup_movpar'],
                               desc=('topup file containing the field '
                                     'coefficients'))
     in_topup_movpar = File(exists=True, requires=['in_topup_fieldcoef'],
-                           copyfile=False, desc='topup movpar.txt file')
-    flm =  traits.Enum(('linear','quadratic','cubic'),
-                        desc='First level EC model', argstr='--flm=%s' )
+                           desc='topup movpar.txt file')
+
+    flm = traits.Enum('linear', 'quadratic', 'cubic', argstr='--flm=%s',
+                      desc='First level EC model')
+
     fwhm = traits.Float(desc=('FWHM for conditioning filter when estimating '
                               'the parameters'), argstr='--fwhm=%s')
-    niter = traits.Int( 5, desc='Number of iterations', argstr='--niter=%s')
-    method = traits.Enum(('jac','lsr'), argstr='--resamp=%s',
+
+    niter = traits.Int(5, argstr='--niter=%s', desc='Number of iterations')
+
+    method = traits.Enum('jac', 'lsr', argstr='--resamp=%s',
                          desc=('Final resampling method (jacobian/least '
-                               'squeares)'))
-    repol = traits.Bool( False, desc='Detect and replace outlier slices',
-                         argstr='--repol' )
+                               'squares)'))
+    repol = traits.Bool(False, argstr='--repol',
+                        desc='Detect and replace outlier slices')
 
 
-class EddyOutputSpec( TraitedSpec ):
+class EddyOutputSpec(TraitedSpec):
     out_corrected = File(exists=True,
-                        desc=('4D image file containing all the corrected '
-                              'volumes'))
+                         desc=('4D image file containing all the corrected '
+                               'volumes'))
     out_parameter = File(exists=True,
-                         desc=('text file with parameters definining the field '
-                               'and movement for each scan'))
+                         desc=('text file with parameters definining the '
+                               'field and movement for each scan'))
 
-class Eddy( FSLCommand ):
-    """ Interface for FSL eddy, a tool for estimating and correcting eddy
-        currents induced distortions. `User guide
-        <http://fsl.fmrib.ox.ac.uk/fsl/fslwiki/Eddy/UsersGuide>`_ and
-        `more info regarding acqp file
-        <http://fsl.fmrib.ox.ac.uk/fsl/fslwiki/eddy/Faq#How_do_I_know_what_to_put_into_my_--acqp_file>`_.
 
-        Examples
-        --------
+class Eddy(FSLCommand):
+    """
+    Interface for FSL eddy, a tool for estimating and correcting eddy
+    currents induced distortions. `User guide
+    <http://fsl.fmrib.ox.ac.uk/fsl/fslwiki/Eddy/UsersGuide>`_ and
+    `more info regarding acqp file
+    <http://fsl.fmrib.ox.ac.uk/fsl/fslwiki/eddy/Faq#How_do_I_know_what_to_put_into_my_--acqp_file>`_.
 
-        >>> from nipype.interfaces.fsl import Eddy
-        >>> eddy = Eddy()
-        >>> eddy.inputs.in_file = 'epi.nii'
-        >>> eddy.inputs.in_mask  = 'epi_mask.nii'
-        >>> eddy.inputs.in_index = 'epi_index.txt'
-        >>> eddy.inputs.in_acqp  = 'epi_acqp.txt'
-        >>> eddy.inputs.in_bvec  = 'bvecs.scheme'
-        >>> eddy.inputs.in_bval  = 'bvals.scheme'
-        >>> eddy.cmdline #doctest: +ELLIPSIS
-        'eddy --acqp=epi_acqp.txt --bvals=bvals.scheme --bvecs=bvecs.scheme --imain=epi.nii --index=epi_index.txt --mask=epi_mask.nii --out=.../eddy_corrected'
-        >>> res = eddy.run() # doctest: +SKIP
-
+    Examples
+    --------
+    >>> from nipype.interfaces.fsl import Eddy
+    >>> eddy = Eddy()
+    >>> eddy.inputs.in_file = 'epi.nii'
+    >>> eddy.inputs.in_mask  = 'epi_mask.nii'
+    >>> eddy.inputs.in_index = 'epi_index.txt'
+    >>> eddy.inputs.in_acqp  = 'epi_acqp.txt'
+    >>> eddy.inputs.in_bvec  = 'bvecs.scheme'
+    >>> eddy.inputs.in_bval  = 'bvals.scheme'
+    >>> eddy.cmdline #doctest: +ELLIPSIS
+    'eddy --acqp=epi_acqp.txt --bvals=bvals.scheme --bvecs=bvecs.scheme --imain=epi.nii --index=epi_index.txt --mask=epi_mask.nii --out=.../eddy_corrected'
+    >>> res = eddy.run() # doctest: +SKIP
 
     """
     _cmd = 'eddy'
@@ -408,15 +412,14 @@ class Eddy( FSLCommand ):
         if name == 'in_topup_fieldcoef':
             return spec.argstr % value.split('_fieldcoef')[0]
         return super(Eddy, self)._format_arg(name, spec, value)
-    
-    def _parse_inputs( self, skip=None ):
+
+    def _parse_inputs(self, skip=None):
         if skip is None:
             skip = []
-        
-        if not isdefined(self.inputs.out_base ):
-            self.inputs.out_base = os.path.abspath( './eddy_corrected' )
-        return super(Eddy, self)._parse_inputs(skip=skip)
 
+        if not isdefined(self.inputs.out_base):
+            self.inputs.out_base = os.path.abspath('eddy_corrected')
+        return super(Eddy, self)._parse_inputs(skip=skip)
 
     def _list_outputs(self):
         outputs = self.output_spec().get()
@@ -590,9 +593,13 @@ class SigLoss(FSLCommand):
         return None
 
 class EddyCorrectInputSpec(FSLCommandInputSpec):
-    in_file = File(exists=True, desc='4D input file', argstr='%s', position=0, mandatory=True)
-    out_file = File(desc='4D output file', argstr='%s', position=1, genfile=True, hash_files=False)
-    ref_num = traits.Int(argstr='%d', position=2, desc='reference number', mandatory=True)
+    in_file = File(exists=True, desc='4D input file', argstr='%s', position=0,
+                   mandatory=True)
+    out_file = File(desc='4D output file', argstr='%s', position=1,
+                    name_source=['in_file'], name_template='%s_edc',
+                    output_name='eddy_corrected')
+    ref_num = traits.Int(0, argstr='%d', position=2, desc='reference number',
+                         mandatory=True, usedefault=True)
 
 
 class EddyCorrectOutputSpec(TraitedSpec):
@@ -620,26 +627,7 @@ class EddyCorrect(FSLCommand):
         return super(EddyCorrect, self).__init__(**inputs)
 
     def _run_interface(self, runtime):
-        if not isdefined(self.inputs.out_file):
-            self.inputs.out_file = self._gen_fname(self.inputs.in_file, suffix='_edc')
         runtime = super(EddyCorrect, self)._run_interface(runtime)
         if runtime.stderr:
             self.raise_exception(runtime)
         return runtime
-
-    def _list_outputs(self):
-        outputs = self.output_spec().get()
-        outputs['eddy_corrected'] = self.inputs.out_file
-        if not isdefined(outputs['eddy_corrected']):
-            outputs['eddy_corrected'] = self._gen_fname(self.inputs.in_file, suffix='_edc')
-        outputs['eddy_corrected'] = os.path.abspath(outputs['eddy_corrected'])
-        return outputs
-
-    def _gen_filename(self, name):
-        if name is 'out_file':
-            return self._list_outputs()['eddy_corrected']
-        else:
-            return None
-
-
-
