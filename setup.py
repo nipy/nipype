60d9a171825b832501d01e701275bfad26df2aab
Roman Valls Guimera <roman@incf.org>
2014-08-25 15:56:32 +0200

    import setuptools (supports install_requires) instead of distutils

--- setup.py
+++ setup.py
@@ -6,19 +6,13 @@
 Nipype intends to create python interfaces to other neuroimaging
 packages and create an API for specifying a full analysis pipeline in
 python.
-
-Much of the machinery at the beginning of this file has been copied over from
-nibabel denoted by ## START - COPIED FROM NIBABEL and a corresponding ## END
-
 """
 
 import sys
 from glob import glob
 import os
-
-## START - COPIED FROM NIBABEL
-from os.path import join as pjoin
-from functools import partial
+from pip.req import parse_requirements
+import versioneer
 
 PY3 = sys.version_info[0] >= 3
 if PY3:
@@ -34,125 +28,24 @@ except ImportError:
 # update it when the contents of directories change.
 if os.path.exists('MANIFEST'): os.remove('MANIFEST')
 
-# For some commands, use setuptools.
-if len(set(('develop', 'bdist_egg', 'bdist_rpm', 'bdist', 'bdist_dumb',
-            'install_egg_info', 'egg_info', 'easy_install', 'bdist_wheel',
-            'bdist_mpkg')).intersection(sys.argv)) > 0:
-    # setup_egg imports setuptools setup, thus monkeypatching distutils.
-    import setup_egg
-
-from distutils.core import setup
-
-from distutils.version import LooseVersion
+from setuptools import setup, find_packages
 from distutils.command.build_py import build_py
 
-from distutils import log
-
-def get_comrec_build(pkg_dir, build_cmd=build_py):
-    """ Return extended build command class for recording commit
-
-    The extended command tries to run git to find the current commit, getting
-    the empty string if it fails.  It then writes the commit hash into a file
-    in the `pkg_dir` path, named ``COMMIT_INFO.txt``.
-
-    In due course this information can be used by the package after it is
-    installed, to tell you what commit it was installed from if known.
-
-    To make use of this system, you need a package with a COMMIT_INFO.txt file -
-    e.g. ``myproject/COMMIT_INFO.txt`` - that might well look like this::
-
-        # This is an ini file that may contain information about the code state
-        [commit hash]
-        # The line below may contain a valid hash if it has been substituted
-        during 'git archive' archive_subst_hash=$Format:%h$
-        # This line may be modified by the install process
-        install_hash=
-
-    The COMMIT_INFO file above is also designed to be used with git substitution
-    - so you probably also want a ``.gitattributes`` file in the root directory
-    of your working tree that contains something like this::
-
-       myproject/COMMIT_INFO.txt export-subst
-
-    That will cause the ``COMMIT_INFO.txt`` file to get filled in by ``git
-    archive`` - useful in case someone makes such an archive - for example with
-    via the github 'download source' button.
-
-    Although all the above will work as is, you might consider having something
-    like a ``get_info()`` function in your package to display the commit
-    information at the terminal.  See the ``pkg_info.py`` module in the nipy
-    package for an example.
-    """
-    class MyBuildPy(build_cmd):
-        ''' Subclass to write commit data into installation tree '''
-        def run(self):
-            build_cmd.run(self)
-            import subprocess
-            proc = subprocess.Popen('git rev-parse HEAD',
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE,
-                                    shell=True)
-            repo_commit, _ = proc.communicate()
-            # Fix for python 3
-            repo_commit = str(repo_commit)
-            # We write the installation commit even if it's empty
-            cfg_parser = ConfigParser()
-            cfg_parser.read(pjoin(pkg_dir, 'COMMIT_INFO.txt'))
-            cfg_parser.set('commit hash', 'install_hash', repo_commit)
-            out_pth = pjoin(self.build_lib, pkg_dir, 'COMMIT_INFO.txt')
-            cfg_parser.write(open(out_pth, 'wt'))
-    return MyBuildPy
-
-def _add_append_key(in_dict, key, value):
-    """ Helper for appending dependencies to setuptools args """
-    # If in_dict[key] does not exist, create it
-    # If in_dict[key] is a string, make it len 1 list of strings
-    # Append value to in_dict[key] list
-    if key not in in_dict:
-        in_dict[key] = []
-    elif isinstance(in_dict[key], string_types):
-        in_dict[key] = [in_dict[key]]
-    in_dict[key].append(value)
-
-## END - COPIED FROM NIBABEL
+# XXX: write COMMIT_INFO.txt with versioneer
+versioneer.VCS = 'git'
+versioneer.versionfile_source = 'src/nipype/_version.py'
+versioneer.versionfile_build = 'nipype/_version.py'
+versioneer.tag_prefix = '' # tags are like 1.2.0
+versioneer.parentdir_prefix = 'nipype-' # dirname like 'myproject-1.2.0'
 
 from build_docs import cmdclass, INFO_VARS
 
-# Add custom commit-recording build command
-cmdclass['build_py'] = get_comrec_build('nipype')
+#XXX: Build docs too
 
-def configuration(parent_package='',top_path=None):
-    from numpy.distutils.misc_util import Configuration
-
-    config = Configuration(None, parent_package, top_path)
-    config.set_options(ignore_setup_xxx_py=True,
-                       assume_default_configuration=True,
-                       delegate_options_to_subpackages=True,
-                       quiet=True)
-    # The quiet=True option will silence all of the name setting warnings:
-    # Ignoring attempt to set 'name' (from 'nipy.core' to
-    #    'nipy.core.image')
-    # Robert Kern recommends setting quiet=True on the numpy list, stating
-    # these messages are probably only used in debugging numpy distutils.
-    config.get_version('nipype/__init__.py') # sets config.version
-    config.add_subpackage('nipype', 'nipype')
-    return config
-
-################################################################################
-# Import the documentation building classes.
-
-try:
-    from build_docs import cmdclass
-except ImportError:
-    """ Pass by the doc build gracefully if sphinx is not installed """
-    print "Sphinx is not installed, docs cannot be built"
-    cmdclass = {}
-
-
-################################################################################
+install_reqs = parse_requirements('requirements.txt')
+reqs = [str(ir.req) for ir in install_reqs]
 
 def main(**extra_args):
-    from numpy.distutils.core import setup
     setup(name=INFO_VARS['NAME'],
           maintainer=INFO_VARS['MAINTAINER'],
           maintainer_email=INFO_VARS['MAINTAINER_EMAIL'],
@@ -165,12 +58,10 @@ def main(**extra_args):
           author=INFO_VARS['AUTHOR'],
           author_email=INFO_VARS['AUTHOR_EMAIL'],
           platforms=INFO_VARS['PLATFORMS'],
-          version=INFO_VARS['VERSION'],
-          configuration=configuration,
-          cmdclass=cmdclass,
-          install_requires=['networkx', 'nibabel', 'numpy', 'scipy', 'traits',
-                            'nose', 'dateutils'],
+          version=versioneer.get_version(),
+          cmdclass=versioneer.get_cmdclass(),
           scripts=glob('bin/*'),
+          install_requires=reqs,
           **extra_args)
 
 if __name__ == "__main__":
