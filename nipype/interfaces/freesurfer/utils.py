1bb44168b70a411c88fbcde4abaf734726be6f1d
Julia Huntenburg <ju.huntenburg@gmail.com>
2014-09-02 23:01:36 +0200

    add to init and changes, rename some arguments and add descriptions, add example

--- nipype/interfaces/freesurfer/utils.py
+++ nipype/interfaces/freesurfer/utils.py
@@ -1189,22 +1189,39 @@ class ExtractMainComponent(CommandLine):
     input_spec=ExtractMainComponentInputSpec
     output_spec=ExtractMainComponentOutputSpec
 
+
 class Tkregister2InputSpec(FSTraitedSpec):
-
-    mov = File(exists=True, mandatory=True, argstr="--mov %s")
-    fsl = File(exists=True, argstr="--fsl %s")
-    subject_id = traits.String(argstr="--s %s", mandatory=True)
-    noedit = traits.Bool(argstr="--noedit")
-    out_reg_file = File(name_template='%s.dat', name_source='fsl', mandatory=True, argstr="--reg %s")
-
+    moving_image = File(exists=True, mandatory=True, argstr="--mov %s", 
+               desc='moving volume')
+    fsl_in_matrix = File(exists=True, argstr="--fsl %s", 
+               desc='fsl-style registration input matrix')
+    subject_id = traits.String(argstr="--s %s", mandatory=True, 
+                               desc='freesurfer subject ID')
+    subjects_dir = traits.Str(argstr='--sd %s',
+                              desc=('freesurfer subjects directory defaults to $SUBJECTS_DIR'))
+    noedit = traits.Bool(True, argstr="--noedit", desc='do not open edit window (exit)', usedefault=True)
+    reg_file = File(name_template='%s.dat', name_source='fsl', 
+                        mandatory=True, argstr="--reg %s", 
+                        desc='freesurfer-style registration file')
 
 class Tkregister2OutputSpec(TraitedSpec):
-
-    out_reg_file = File(exists=True)
-
+    reg_file = File(exists=True, desc='freesurfer-style registration file')
 
 class Tkregister2(FSCommand):
-    
+    """Use tkregister2 without the manual editing stage to convert 
+    FSL-style registration matrix (.mat) to FreeSurfer-style registration matrix (.dat)
+
+    Examples
+    --------
+
+    >>> from nipype.interfaces.freesurfer import Tkregister2
+    >>> tk2 = Tkregister2(reg_file='register.dat')
+    >>> tk2.inputs.moving_image = 'func.nii.gz'
+    >>> tk2.inputs.fsl_in_matrix = 'func2anat.mat'
+    >>> tk2.inputs.subject_id = 'test_subject'
+    >>> tk2.inputs.subjects_dir = '.'
+    >>> tk2.run()
+    """
     _cmd = "tkregister2"
     input_spec = Tkregister2InputSpec
     output_spec = Tkregister2OutputSpec
