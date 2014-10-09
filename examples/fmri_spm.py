7bf9d11bb9b74c968aaf1ca513ef055ff699c0ec
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2014-10-08 17:23:17 -0700

    revert MCR related change

--- examples/fmri_spm.py
+++ examples/fmri_spm.py
@@ -23,8 +23,9 @@ from nipype import config
 from nipype.interfaces import spm, fsl
 
 # In order to use this example with SPM's matlab common runtime
-matlab_cmd = ('/Applications/spm12/run_spm12.sh /Applications/MATLAB/MATLAB_Compiler_Runtime/v713/ script')
-spm.SPMCommand.set_mlab_paths(matlab_cmd=matlab_cmd, use_mcr=True)
+# matlab_cmd = ('/Users/satra/Downloads/spm8/run_spm8.sh '
+#               '/Applications/MATLAB/MATLAB_Compiler_Runtime/v713/ script')
+# spm.SPMCommand.set_mlab_paths(matlab_cmd=matlab_cmd, use_mcr=True)
 
 import nipype.interfaces.io as nio           # Data i/o
 import nipype.interfaces.utility as util     # utility
