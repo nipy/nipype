470901c37b759b7c6e4cb3d62223764ea7692c52
Satrajit Ghosh <satra@mit.edu>
2014-10-16 00:10:35 -0400

    eddy requires full path for out_base

--- nipype/interfaces/fsl/epi.py
+++ nipype/interfaces/fsl/epi.py
@@ -489,6 +489,8 @@ class Eddy(FSLCommand):
     def _format_arg(self, name, spec, value):
         if name == 'in_topup_fieldcoef':
             return spec.argstr % value.split('_fieldcoef')[0]
+        if name == 'out_base':
+            return spec.argstr % os.path.abspath(value)
         return super(Eddy, self)._format_arg(name, spec, value)
 
     def _list_outputs(self):
