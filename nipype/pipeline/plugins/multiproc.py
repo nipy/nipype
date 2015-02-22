03cf3c1b4966368b17801b0c9f452204b6dea72f
Oscar Esteban <code@oscaresteban.es>
2015-02-22 19:41:33 +0100

    new implementation, lock inside runner

--- nipype/pipeline/plugins/multiproc.py
+++ nipype/pipeline/plugins/multiproc.py
@@ -1,6 +1,7 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-"""Parallel workflow execution via multiprocessing
+"""
+Parallel workflow execution via multiprocessing
 
 Support for child processes running as non-daemons based on
 http://stackoverflow.com/a/8963618/1183453
@@ -11,18 +12,27 @@ from traceback import format_exception
 import sys
 import time
 import os.path as op
-
 from .base import (DistributedPluginBase, report_crash)
 
-def run_node(node, updatehash):
+
+def run_node(node, updatehash, lock=None):
     result = dict(result=None, traceback=None)
+
+    if lock is not None:
+        lock.acquire()
+
     try:
         result['result'] = node.run(updatehash=updatehash)
     except:
         etype, eval, etr = sys.exc_info()
-        result['traceback'] = format_exception(etype,eval,etr)
+        result['traceback'] = format_exception(etype, eval, etr)
         result['result'] = node.result
+
+    if lock is not None:
+        lock.release()
+
     return result
+
 
 class NonDaemonProcess(Process):
     """A non-daemon process to support internal multiprocessing.
@@ -35,13 +45,16 @@ class NonDaemonProcess(Process):
 
     daemon = property(_get_daemon, _set_daemon)
 
+
 class NonDaemonPool(pool.Pool):
     """A process pool with non-daemon processes.
     """
     Process = NonDaemonProcess
 
+
 class MultiProcPlugin(DistributedPluginBase):
-    """Execute workflow with multiprocessing
+    """
+    Execute workflow with multiprocessing
 
     The plugin_args input to run can be used to control the multiprocessing
     execution. Currently supported options are:
@@ -63,9 +76,6 @@ class MultiProcPlugin(DistributedPluginBase):
                 self._n_procs = plugin_args['n_procs']
             if 'non_daemon' in plugin_args:
                 self._non_daemon = plugin_args['non_daemon']
-        self._renew_pool()
-
-    def _renew_pool(self):
         if self._non_daemon:
             # run the execution using the non-daemon pool subclass
             self.pool = NonDaemonPool(processes=self._n_procs)
@@ -74,7 +84,7 @@ class MultiProcPlugin(DistributedPluginBase):
 
     def _get_result(self, taskid):
         if taskid not in self._taskresult:
-            raise RuntimeError('Multiproc task %d not found'%taskid)
+            raise RuntimeError('Multiproc task %d not found' % taskid)
         if not self._taskresult[taskid].ready():
             return None
         return self._taskresult[taskid].get()
@@ -93,18 +103,9 @@ class MultiProcPlugin(DistributedPluginBase):
         except:
             pass
 
-        if allworkers:
-            self._lock.acquire(True)
-            self.pool.close()
-            self.pool.join()
-            self._taskresult[self._taskid] = run_node(node, updatehash)
-            self._renew_pool()
-            self._lock.release()
-            return self._taskid
-
-        self._taskresult[self._taskid] = self.pool.apply_async(run_node,
-                                                               (node,
-                                                                updatehash,))
+        lock = self._lock if allworkers else None
+        self._taskresult[self._taskid] = self.pool.apply_async(
+            run_node, (node, updatehash, lock,))
         return self._taskid
 
     def _report_crash(self, node, result=None):
