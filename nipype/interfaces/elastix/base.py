d99ae10078512732bf88638edba65ee70c3c06e1
Oscar Esteban <code@oscaresteban.es>
2015-03-02 19:52:24 +0100

    update elastix interface to use num_threads

--- nipype/interfaces/elastix/base.py
+++ nipype/interfaces/elastix/base.py
@@ -15,9 +15,21 @@ from ..base import (CommandLine, CommandLineInputSpec, isdefined,
 from ... import logging
 logger = logging.getLogger('interface')
 
+from multiprocessing import cpu_count
+
 
 class ElastixBaseInputSpec(CommandLineInputSpec):
     output_path = Directory('./', exists=True, mandatory=True, usedefault=True,
                             argstr='-out %s', desc='output directory')
-    num_threads = traits.Int(1, argstr='-threads %01d',
+    num_threads = traits.Int(cpu_count(), argstr='-threads %01d', usedefault=True,
                              desc='set the maximum number of threads of elastix')
+
+
+class ElastixBaseInterface(CommandLine):
+    _singleworker = True
+
+    def _format_arg(self, name, trait_spec, value):
+        if name == 'num_threads' and value > 1:
+            self._singleworker = False
+
+        return super(ElastixBaseInterface, self)._format_arg(name, trait_spec, value)
