f971e0e9a275f319f40a90bbf3c79e1391208ad9
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2015-03-17 15:05:00 -0700

    Added more tests

--- nipype/utils/tests/test_cmd.py
+++ nipype/utils/tests/test_cmd.py
@@ -51,6 +51,100 @@ positional arguments:
 optional arguments:
   -h, --help  show this help message and exit
 """)
+    
+    def test_list_nipy_interfacesp(self):
+        with self.assertRaises(SystemExit) as cm:
+            with capture_sys_output() as (stdout, stderr):
+                nipype_cmd.main(['nipype_cmd', 'nipype.interfaces.nipy'])
+        
+        exit_exception = cm.exception
+        self.assertEqual(exit_exception.code, 0)
+        
+        self.assertEqual(stderr.getvalue(), '')
+        self.assertEqual(stdout.getvalue(),
+"""Available Interfaces:
+	SpaceTimeRealigner
+	Similarity
+	ComputeMask
+	FitGLM
+	EstimateContrast
+	FmriRealign4d
+""")
+
+    def test_run_4d_realign_without_arguments(self):
+        with self.assertRaises(SystemExit) as cm:
+            with capture_sys_output() as (stdout, stderr):
+                nipype_cmd.main(['nipype_cmd', 'nipype.interfaces.nipy', 'FmriRealign4d'])
+        
+        exit_exception = cm.exception
+        self.assertEqual(exit_exception.code, 2)
+        
+        self.assertEqual(stderr.getvalue(),
+"""usage: nipype_cmd nipype.interfaces.nipy FmriRealign4d [-h]
+                                                       [--between_loops BETWEEN_LOOPS]
+                                                       [--ignore_exception IGNORE_EXCEPTION]
+                                                       [--loops LOOPS]
+                                                       [--slice_order SLICE_ORDER]
+                                                       [--speedup SPEEDUP]
+                                                       [--start START]
+                                                       [--time_interp TIME_INTERP]
+                                                       [--tr_slices TR_SLICES]
+                                                       in_file tr
+nipype_cmd nipype.interfaces.nipy FmriRealign4d: error: too few arguments
+""")
+        self.assertEqual(stdout.getvalue(), '')
+        
+    def test_run_4d_realign_help(self):
+        with self.assertRaises(SystemExit) as cm:
+            with capture_sys_output() as (stdout, stderr):
+                nipype_cmd.main(['nipype_cmd', 'nipype.interfaces.nipy', 'FmriRealign4d', '-h'])
+        
+        exit_exception = cm.exception
+        self.assertEqual(exit_exception.code, 0)
+        
+        self.assertEqual(stderr.getvalue(), '')
+        self.assertEqual(stdout.getvalue(),
+"""usage: nipype_cmd nipype.interfaces.nipy FmriRealign4d [-h]
+                                                       [--between_loops BETWEEN_LOOPS]
+                                                       [--ignore_exception IGNORE_EXCEPTION]
+                                                       [--loops LOOPS]
+                                                       [--slice_order SLICE_ORDER]
+                                                       [--speedup SPEEDUP]
+                                                       [--start START]
+                                                       [--time_interp TIME_INTERP]
+                                                       [--tr_slices TR_SLICES]
+                                                       in_file tr
+
+Run FmriRealign4d
+
+positional arguments:
+  in_file               File to realign
+  tr                    TR in seconds
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --between_loops BETWEEN_LOOPS
+                        loops used to realign different runs
+  --ignore_exception IGNORE_EXCEPTION
+                        Print an error message instead of throwing an
+                        exception in case the interface fails to run
+  --loops LOOPS         loops within each run
+  --slice_order SLICE_ORDER
+                        0 based slice order. This would be equivalent to
+                        enteringnp.argsort(spm_slice_order) for this field.
+                        This effectsinterleaved acquisition. This field will
+                        be deprecated infuture Nipy releases and be replaced
+                        by actual sliceacquisition times.
+  --speedup SPEEDUP     successive image sub-sampling factors for acceleration
+  --start START         time offset into TR to align slices to
+  --time_interp TIME_INTERP
+                        Assume smooth changes across time e.g., fmri series.
+                        If you don't want slice timing correction set this to
+                        undefined
+  --tr_slices TR_SLICES
+                        TR slices
+""")
+
 
 if __name__ == '__main__':
     unittest.main()
