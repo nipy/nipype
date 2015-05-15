2192e5b8c97b5ed27135f451e385259e70cf1421
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2015-05-15 17:24:55 +0100

    Bug fix for the naming: priority to image file names

--- nipype/interfaces/niftyreg/regutils.py
+++ nipype/interfaces/niftyreg/regutils.py
@@ -553,9 +553,15 @@ class RegTransform(NIFTYREGCommand):
 
     def _find_input(self):
         inputs = [self.inputs.def_input, self.inputs.disp_input, self.inputs.flow_input, self.inputs.comp_input, self.inputs.comp_input2, self.inputs.upd_s_form_input2, self.inputs.inv_aff_input, self.inputs.inv_nrr_input, self.inputs.half_input, self.inputs.make_aff_input, self.inputs.aff_2_rig_input, self.inputs.flirt_2_nr_input]
+        entries = []
         for entry in inputs:
             if isdefined(entry):
-                return entry
+                entries.append(entry)
+                _, _, ext = split_filename(entry)
+                if ext == '.nii' or ext == '.nii.gz' or ext == '.hdr':
+                    return entry
+        if len(entries):
+            return entries[0]
         return None
 
     def _gen_filename(self, name):
