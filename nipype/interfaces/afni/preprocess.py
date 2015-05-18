b916e7bdc2e90470bc54ad995b2c440916f724c2
rafaceschin <rafaceschin@gmail.com>
2015-05-18 18:47:06 -0400

    fixed vim option

--- nipype/interfaces/afni/preprocess.py
+++ nipype/interfaces/afni/preprocess.py
@@ -1,5 +1,5 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
-#vi: set ft = python sts = 4 ts = 4 sw = 4 et:
+# vi: set ft = python sts = 4 ts = 4 sw = 4 et:
 """Afni preprocessing interfaces
 
     Change directory to provide relative paths for doctests
