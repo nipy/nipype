ad16a8d3c8c7f0d013d24ba509dfa31bbc59fdbb
Hans Johnson <hans-johnson@uiowa.edu>
2015-06-14 08:00:02 -1000

    ENH: A set of autogenerated interfaces
    This is a set of automatically generated interfaces
    to provide the nodes necessary to for running the
    BRAINSAutoWorkup pipeline.  The main source code
    for the binaries related to this interface is
    located at: https://github.com/BRAINSia/NAMICExternalProjects
    
    This is a parallel sytem to the Slicer GUI that provides many
    the consistently performing tools in a command line project
    without the need for complicated extension building and shared
    library dependancy manipulation.
    
    This set of interfaces was automatically generated with
    the following psuedocode set of instructions:
    
    cd ~/
    git clone https://github.com/BRAINSia/NAMICExternalProjects.git
    mkdir ~/NEP-bld
    cd ~/NEP-bld
    ccmake ~/NAMICExternalProjects.git
    make
    
    ~/NEP-bld/BRAINSTools/AutoWorkup/local_generate_classes.py

---
+++ nipype/interfaces/semtools/registration/__init__.py
@@ -0,0 +1,4 @@
+from specialized import VBRAINSDemonWarp, BRAINSDemonWarp, BRAINSTransformFromFiducials
+from brainsresample import BRAINSResample
+from brainsfit import BRAINSFit
+from brainsresize import BRAINSResize
