031f853b0b01cc03b60541fed7562af746508cd9
Michael <mdayan.research@gmail.com>
2015-07-21 18:06:22 -0400

    Update init file with new fslcpgeom interface

--- nipype/interfaces/fsl/__init__.py
+++ nipype/interfaces/fsl/__init__.py
@@ -17,7 +17,7 @@ from .utils import (Smooth, Merge, ExtractROI, Split, ImageMaths, ImageMeants,
                     PlotTimeSeries, PlotMotionParams, ConvertXFM,
                     SwapDimensions, PowerSpectrum, Reorient2Std,
                     Complex, InvWarp, WarpUtils, ConvertWarp, WarpPoints,
-                    WarpPointsToStd, RobustFOV)
+                    WarpPointsToStd, RobustFOV, CopyGeom)
 
 from .epi import (PrepareFieldmap, TOPUP, ApplyTOPUP, Eddy, EPIDeWarp,
                   SigLoss, EddyCorrect, EpiReg)
