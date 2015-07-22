814538a8819e2a8b5caa434805980a029d0f425f
Michael <mdayan.research@gmail.com>
2015-07-22 15:20:50 -0400

    New FSL interface for fslcpgeom

--- nipype/interfaces/fsl/utils.py
+++ nipype/interfaces/fsl/utils.py
@@ -26,10 +26,55 @@ from .base import FSLCommand, FSLCommandInputSpec, Info
 from ..base import (traits, TraitedSpec, OutputMultiPath, File,
                     CommandLine, CommandLineInputSpec, isdefined)
 from ...utils.filemanip import (load_json, save_json, split_filename,
-                                fname_presuffix)
+                                fname_presuffix, copyfile)
 
 warn = warnings.warn
 warnings.filterwarnings('always', category=UserWarning)
+
+class CopyGeomInputSpec(FSLCommandInputSpec):
+    in_file = File(exists=True, mandatory=True, argstr="%s", position=0,
+                   desc="source image")
+    dest_file = File(exists=True, mandatory=True, argstr="%s", position=1,
+                     desc="destination image", copyfile=True)
+    ignore_dims = traits.Bool(desc=('Do not copy image dimensions'),
+                              argstr='-d', position="-1")
+
+class CopyGeomOutputSpec(TraitedSpec):
+    out_file = File(exists=True, desc="image with new geometry header")
+
+class CopyGeom(FSLCommand):
+    """Use fslcpgeom to copy the header geometry information to another image.
+
+    Copy certain parts of the header information (image dimensions, voxel dimensions,
+    voxel dimensions units string, image orientation/origin or qform/sform info)
+    from one image to another. Note that only copies from Analyze to Analyze
+    or Nifti to Nifti will work properly. Copying from different files will result
+    in loss of information or potentially incorrect settings.
+
+    """
+    _cmd = "fslcpgeom"
+    input_spec = CopyGeomInputSpec
+    output_spec = CopyGeomOutputSpec
+
+    def _run_interface(self, runtime):
+        # Copy destination file to new local file to prevent overwriting
+        # and update destination file
+        out_file = self._gen_filename('out_file')
+        copyfile(self.inputs.dest_file, out_file, copy=True)
+        self.inputs.dest_file = out_file
+        return super(CopyGeom, self)._run_interface(runtime)
+
+    def _gen_filename(self, name):
+        if name == 'out_file':
+            return self._gen_fname(self.inputs.dest_file, suffix="_newhd")
+        return None
+
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        # Use the new destination file updated by _run_interface
+        outputs['out_file'] = self.inputs.dest_file
+        return outputs
+
 
 class RobustFOVInputSpec(FSLCommandInputSpec):
     in_file = File(exists=True,
