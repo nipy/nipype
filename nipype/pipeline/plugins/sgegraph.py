9cfb703dc962ae67d35a9b313961f08596bc3ab0
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2015-07-22 15:31:42 +0200

    extra condition ported from SLURMGraph

--- nipype/pipeline/plugins/sgegraph.py
+++ nipype/pipeline/plugins/sgegraph.py
@@ -122,7 +122,7 @@ class SGEGraphPlugin(GraphPluginBase):
                         values = ' '
                         for jobid in dependencies[idx]:
                             ## Avoid dependancies of done jobs
-                            if cache_doneness_per_node[jobid] == False:
+                            if not self._dont_resubmit_completed_jobs or cache_doneness_per_node[jobid] == False:
                                 values += "${{{0}}},".format(make_job_name(jobid, nodes))
                         if values != ' ': # i.e. if some jobs were added to dependency list
                             values = values.rstrip(',')
