c6e0258017d79e5c05554cf0ed12d27f6bc18a86
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2015-08-06 19:13:30 -0700

    more PATH fixes

--- tools/install_spm_mcr.sh
+++ tools/install_spm_mcr.sh
@@ -1,13 +1,13 @@
 if [ ! -d $HOME/mcr ]
 then
-  echo "destinationFolder=$HOME/mcr" > mcr_options.txt
-  echo "agreeToLicense=yes" >> mcr_options.txt
-  echo "outputFile=/tmp/matlabinstall_log" >> mcr_options.txt
-  echo "mode=silent" >> mcr_options.txt
+  echo "destinationFolder=$HOME/mcr" > $HOME/mcr_options.txt
+  echo "agreeToLicense=yes" >> $HOME/mcr_options.txt
+  echo "outputFile=/tmp/matlabinstall_log" >> $HOME/mcr_options.txt
+  echo "mode=silent" >> $HOME/mcr_options.txt
   mkdir -p $HOME/matlab_installer
   wget -nc --quiet http://www.mathworks.com/supportfiles/downloads/R2015a/deployment_files/R2015a/installers/glnxa64/MCR_R2015a_glnxa64_installer.zip -O $HOME/matlab_installer/installer.zip
   unzip $HOME/matlab_installer/installer.zip -d $HOME/matlab_installer/
-  $HOME/matlab_installer/install -inputFile mcr_options.txt
+  $HOME/matlab_installer/install -inputFile $HOME/mcr_options.txt
   rm -rf $HOME/matlab_installer $HOME/mcr_options.txt
 fi
 
