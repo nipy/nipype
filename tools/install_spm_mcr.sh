df749900a0ad6503a33c9406239cd3fd568c330b
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2015-08-06 19:12:23 -0700

    fixes

--- tools/install_spm_mcr.sh
+++ tools/install_spm_mcr.sh
@@ -4,16 +4,16 @@ then
   echo "agreeToLicense=yes" >> mcr_options.txt
   echo "outputFile=/tmp/matlabinstall_log" >> mcr_options.txt
   echo "mode=silent" >> mcr_options.txt
-  mkdir -p matlab_installer
+  mkdir -p $HOME/matlab_installer
   wget -nc --quiet http://www.mathworks.com/supportfiles/downloads/R2015a/deployment_files/R2015a/installers/glnxa64/MCR_R2015a_glnxa64_installer.zip -O $HOME/matlab_installer/installer.zip
   unzip $HOME/matlab_installer/installer.zip -d $HOME/matlab_installer/
-  ./matlab_installer/install -inputFile mcr_options.txt
-  rm -rf matlab_installer mcr_options.txt
+  $HOME/matlab_installer/install -inputFile mcr_options.txt
+  rm -rf $HOME/matlab_installer $HOME/mcr_options.txt
 fi
 
 if [ ! -d $HOME/spm12 ]
 then
-  wget --quiet http://www.fil.ion.ucl.ac.uk/spm/download/restricted/utopia/dev/spm12_r6472_Linux_R2015a.zip -O spm12.zip
-  unzip spm12.zip
-  rm -rf spm12.zip
+  wget --quiet http://www.fil.ion.ucl.ac.uk/spm/download/restricted/utopia/dev/spm12_r6472_Linux_R2015a.zip -O $HOME/spm12.zip
+  unzip $HOME/spm12.zip -d $HOME
+  rm -rf $HOME/spm12.zip
 fi
