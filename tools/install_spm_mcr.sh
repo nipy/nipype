7943a121fe69c3cfd3c761dbafc1d8267ab72379
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2015-08-06 19:26:57 -0700

    make verbose for CircleCI

--- tools/install_spm_mcr.sh
+++ tools/install_spm_mcr.sh
@@ -5,7 +5,7 @@ then
   echo "outputFile=/tmp/matlabinstall_log" >> $HOME/mcr_options.txt
   echo "mode=silent" >> $HOME/mcr_options.txt
   mkdir -p $HOME/matlab_installer
-  wget -nc --quiet http://www.mathworks.com/supportfiles/downloads/R2015a/deployment_files/R2015a/installers/glnxa64/MCR_R2015a_glnxa64_installer.zip -O $HOME/matlab_installer/installer.zip
+  wget -nc http://www.mathworks.com/supportfiles/downloads/R2015a/deployment_files/R2015a/installers/glnxa64/MCR_R2015a_glnxa64_installer.zip -O $HOME/matlab_installer/installer.zip
   unzip $HOME/matlab_installer/installer.zip -d $HOME/matlab_installer/
   $HOME/matlab_installer/install -inputFile $HOME/mcr_options.txt
   rm -rf $HOME/matlab_installer $HOME/mcr_options.txt
@@ -13,7 +13,7 @@ fi
 
 if [ ! -d $HOME/spm12 ]
 then
-  wget --quiet http://www.fil.ion.ucl.ac.uk/spm/download/restricted/utopia/dev/spm12_r6472_Linux_R2015a.zip -O $HOME/spm12.zip
+  wget http://www.fil.ion.ucl.ac.uk/spm/download/restricted/utopia/dev/spm12_r6472_Linux_R2015a.zip -O $HOME/spm12.zip
   unzip $HOME/spm12.zip -d $HOME
   rm -rf $HOME/spm12.zip
 fi
