0b308b5e72acb787b691331275d1e6949b209d16
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2015-08-18 10:50:32 +0100

    Adding an interface to the seg_gif binary, the interface is for the robust affine registration step and the program exits after that step.

--- nipype/interfaces/niftyseg/__init__.py
+++ nipype/interfaces/niftyseg/__init__.py
@@ -9,6 +9,6 @@ Top-level namespace for niftyseg.
 from .base import (NIFTYSEGCommand, Info, no_niftyseg)
 from .maths import (UnaryMaths, BinaryMaths, BinaryMathsInteger)
 from .stats import (UnaryStats, BinaryStats)
-from .gif import (Gif)
+from .gif import (Gif, GifRobustGroupwise)
 from .steps import (STEPS, CalcTopNCC)
 from .patchmatch import (PatchMatch)
