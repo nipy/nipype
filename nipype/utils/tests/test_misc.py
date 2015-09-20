41e9a0a1fc8964300ca53035742f0186c02414b3
Alexandre M. S <alexsavio@gmail.com>
2015-09-20 02:25:53 +0200

    fix: utils str fixes and nipype_cmd sort interfaces list

--- nipype/utils/tests/test_misc.py
+++ nipype/utils/tests/test_misc.py
@@ -1,9 +1,11 @@
-from __future__ import unicode_literals
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 from __future__ import unicode_literals
 from future import standard_library
 standard_library.install_aliases()
+
+from builtins import next
+from builtins import str
 
 from nipype.testing import assert_equal, assert_true, assert_false
 
@@ -31,7 +33,7 @@ def test_cont_to_str():
     yield assert_true, container_to_string('foobar') == 'foobar'
     # int.  Integers are not the main intent of this function, but see
     # no reason why they shouldn't work.
-    yield assert_true, (container_to_string(123) == '123')
+    yield assert_equal, container_to_string(123), '123'
 
 def _func1(x):
     return x**3
