6c23db17bd0187276d6976cd02651aa710ee1f84
Alexandre M. S <alexsavio@gmail.com>
2015-09-22 15:29:44 +0200

    enh: test_engine: put some spaces in the code

--- nipype/pipeline/tests/test_engine.py
+++ nipype/pipeline/tests/test_engine.py
@@ -231,34 +231,46 @@ def test_synchronize_expansion():
 def test_synchronize_tuples_expansion():
     import nipype.pipeline.engine as pe
     wf1 = pe.Workflow(name='test')
-    node1 = pe.Node(TestInterface(),name='node1')
-    node2 = pe.Node(TestInterface(),name='node2')
-    node1.iterables = [('input1','input2'), [(1,3), (2,4), (None,5)]]
+
+    node1 = pe.Node(TestInterface(), name='node1')
+    node2 = pe.Node(TestInterface(), name='node2')
+    node1.iterables = [('input1', 'input2'), [(1, 3), (2, 4), (None, 5)]]
+
     node1.synchronize = True
-    wf1.connect(node1,'output1', node2, 'input2')
+
+    wf1.connect(node1, 'output1', node2, 'input2')
+
     wf3 = pe.Workflow(name='group')
-    for i in [0,1,2]:
+    for i in [0, 1, 2]:
         wf3.add_nodes([wf1.clone(name='test%d'%i)])
+
     wf3._flatgraph = wf3._create_flat_graph()
     # Identical to test_synchronize_expansion
     yield assert_equal, len(pe.generate_expanded_graph(wf3._flatgraph).nodes()), 18
 
 def test_itersource_expansion():
     import nipype.pipeline.engine as pe
+
     wf1 = pe.Workflow(name='test')
-    node1 = pe.Node(TestInterface(),name='node1')
-    node1.iterables = ('input1',[1,2])
-    node2 = pe.Node(TestInterface(),name='node2')
-    wf1.connect(node1,'output1', node2, 'input1')
-    node3 = pe.Node(TestInterface(),name='node3')
+    node1 = pe.Node(TestInterface(), name='node1')
+    node1.iterables = ('input1', [1, 2])
+
+    node2 = pe.Node(TestInterface(), name='node2')
+    wf1.connect(node1, 'output1', node2, 'input1')
+
+    node3 = pe.Node(TestInterface(), name='node3')
     node3.itersource = ('node1', 'input1')
-    node3.iterables = [('input1', {1:[3,4], 2:[5,6,7]})]
-    wf1.connect(node2,'output1', node3, 'input1')
-    node4 = pe.Node(TestInterface(),name='node4')
-    wf1.connect(node3,'output1', node4, 'input1')
+    node3.iterables = [('input1', {1: [3, 4], 2: [5, 6, 7]})]
+
+    wf1.connect(node2, 'output1', node3, 'input1')
+    node4 = pe.Node(TestInterface(), name='node4')
+
+    wf1.connect(node3, 'output1', node4, 'input1')
+
     wf3 = pe.Workflow(name='group')
-    for i in [0,1,2]:
+    for i in [0, 1, 2]:
         wf3.add_nodes([wf1.clone(name='test%d'%i)])
+
     wf3._flatgraph = wf3._create_flat_graph()
 
     # each expanded graph clone has:
@@ -275,14 +287,14 @@ def test_itersource_synchronize1_expansion():
     import nipype.pipeline.engine as pe
     wf1 = pe.Workflow(name='test')
     node1 = pe.Node(TestInterface(),name='node1')
-    node1.iterables = [('input1',[1,2]), ('input2',[3,4])]
+    node1.iterables = [('input1', [1, 2]), ('input2', [3, 4])]
     node1.synchronize = True
     node2 = pe.Node(TestInterface(),name='node2')
-    wf1.connect(node1,'output1', node2, 'input1')
+    wf1.connect(node1, 'output1', node2, 'input1')
     node3 = pe.Node(TestInterface(),name='node3')
     node3.itersource = ('node1', ['input1', 'input2'])
-    node3.iterables = [('input1', {(1,3):[5,6]}),
-                       ('input2', {(1,3):[7,8], (2,4): [9]})]
+    node3.iterables = [('input1', {(1, 3): [5, 6]}),
+                       ('input2', {(1, 3): [7, 8], (2, 4): [9]})]
     wf1.connect(node2,'output1', node3, 'input1')
     node4 = pe.Node(TestInterface(),name='node4')
     wf1.connect(node3,'output1', node4, 'input1')
@@ -304,6 +316,7 @@ def test_itersource_synchronize1_expansion():
 def test_itersource_synchronize2_expansion():
     import nipype.pipeline.engine as pe
     wf1 = pe.Workflow(name='test')
+
     node1 = pe.Node(TestInterface(),name='node1')
     node1.iterables = [('input1',[1,2]), ('input2',[3,4])]
     node1.synchronize = True
