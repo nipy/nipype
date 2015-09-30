52da583c02d60e5272b036e7a64a9919300f8e50
carolFrohlich <carol.frohlich@gmail.com>
2015-09-30 14:00:22 -0400

    fix multiproc tests. create lot 2 json converter

--- nipype/pipeline/plugins/callback_log.py
+++ nipype/pipeline/plugins/callback_log.py
@@ -4,8 +4,47 @@ import logging
 def log_nodes_cb(node, status):
     logger = logging.getLogger('callback')
     if status == 'start':
-        message  = "name:",node.name, "id:", node._id, "start:", datetime.datetime.now(), "memory:", node._interface.memory, "num_threads:", node._interface.num_threads
+        message  = '{"name":' + '"' + node.name + '"' + ',"id":' + '"' + node._id + '"' + ',"start":' + '"' +str(datetime.datetime.now()) + '"' + ',"memory":' + str(node._interface.memory) + ',"num_threads":' + str(node._interface.num_threads) +  '}'
         logger.debug(message)
     else:
-        message  = "name:",node.name, "id:", node._id, "finish:", datetime.datetime.now(), "memory:", node._interface.memory, "num_threads:", node._interface.num_threads
+        message  = '{"name":' + '"' + node.name + '"' + ',"id":' + '"' + node._id + '"' + ',"finish":' + '"' + str(datetime.datetime.now()) + '"' + ',"memory":' + str(node._interface.memory) + ',"num_threads":' + str(node._interface.num_threads) +  '}'
         logger.debug(message)
+
+
+
+import json
+from dateutil import parser
+
+def convert_logcb_to_json(filename):
+    with open(filename, 'r') as content:
+        #read file separating each line
+        content = content.read()
+        lines = content.split('\n')
+
+        #separate lines of starting nodes and ending nodes
+        starts = [ json.loads(x) for x in lines if '"start":' in x ]
+        ends = [json.loads(x) for x in lines if '"finish":' in x ]
+
+
+
+        #foreach start, search its end. They have same name and id
+        #this line is O(n^2). refactor
+        for element in starts:
+            end = next((f for f in ends if (f['id'] == element['id'] and  f['name'] == element['name'])), None)
+
+            if end is not None:
+                element['finish'] = end['finish']
+            else:
+                element['finish'] = element['start']
+
+
+        first_node = starts[0]['start']
+        last_node = ends[-1]['finish']
+
+        duration = parser.parse(last_node) - parser.parse(first_node)
+
+        #sorted(starts, key=lambda e: parser.parse(e['start']))   # sort by age
+        result = {'start': first_node, 'finish': last_node, 'duration':duration.total_seconds(), 'nodes': starts}
+        #finally, save the json file
+        with open(filename + '.json', 'w') as outfile:
+            json.dump(result, outfile)
--- nipype/pipeline/plugins/tests/test_multiproc.py
+++ nipype/pipeline/plugins/tests/test_multiproc.py
@@ -3,7 +3,7 @@ import nipype.interfaces.base as nib
 from tempfile import mkdtemp
 from shutil import rmtree
 
-from nipype.testing import assert_equal
+from nipype.testing import assert_equal, assert_less_equal
 import nipype.pipeline.engine as pe
 
 class InputSpec(nib.TraitedSpec):
@@ -47,3 +47,161 @@ def test_run_multiproc():
     yield assert_equal, result, [1, 1]
     os.chdir(cur_dir)
     rmtree(temp_dir)
+
+
+
+#################################
+
+
+class InputSpecSingleNode(nib.TraitedSpec):
+    input1 = nib.traits.Int(desc='a random int')
+    input2 = nib.traits.Int(desc='a random int')
+
+class OutputSpecSingleNode(nib.TraitedSpec):
+    output1 = nib.traits.Int(desc='a random int')
+
+
+class TestInterfaceSingleNode(nib.BaseInterface):
+    input_spec = InputSpecSingleNode
+    output_spec = OutputSpecSingleNode
+
+    def _run_interface(self, runtime):
+        runtime.returncode = 0
+        return runtime
+
+    def _list_outputs(self):
+        outputs = self._outputs().get()
+        outputs['output1'] =  self.inputs.input1
+        return outputs
+
+
+def parse_log(filename, measure):
+    import json
+    from dateutil.parser import parse
+    from datetime import datetime
+    import datetime as d
+
+    json_data = open(filename).read()
+    data = json.loads(json_data)
+    total_duration = int(float(data['duration']) * 60) #total duration in seconds
+
+    total = []
+    for i in range(total_duration):
+        total.append(0)
+
+    now = parse(data['start'])
+    for i in range(total_duration):
+        start_index = 0
+        node_start = None
+        node_finish = None
+
+        x = now
+
+        for j in range(start_index, len(data['nodes'])):
+            node_start = parse(data['nodes'][j]['start'])
+            node_finish = parse(data['nodes'][j]['finish'])
+
+            if node_start < x and node_finish > x:
+                total[i] += data['nodes'][j][measure]
+                start_index = j
+
+            if node_start > x:
+                break
+
+        now += d.timedelta(seconds=1)
+
+    return total
+
+
+import os
+from nipype.pipeline.plugins.callback_log import log_nodes_cb, convert_logcb_to_json
+import logging
+import logging.handlers
+def test_do_not_use_more_memory_then_specified():
+    LOG_FILENAME = 'callback.log'
+    my_logger = logging.getLogger('callback')
+    my_logger.setLevel(logging.DEBUG)
+
+    # Add the log message handler to the logger
+    handler = logging.FileHandler(LOG_FILENAME)
+    my_logger.addHandler(handler)
+
+    max_memory = 10
+    pipe = pe.Workflow(name='pipe')
+    n1 = pe.Node(interface=TestInterfaceSingleNode(), name='n1')
+    n2 = pe.Node(interface=TestInterfaceSingleNode(), name='n2')
+    n3 = pe.Node(interface=TestInterfaceSingleNode(), name='n3')
+    n4 = pe.Node(interface=TestInterfaceSingleNode(), name='n4')
+
+    n1.interface.memory = 1
+    n2.interface.memory = 1
+    n3.interface.memory = 10
+    n4.interface.memory = 1
+
+    pipe.connect(n1, 'output1', n2, 'input1')
+    pipe.connect(n1, 'output1', n3, 'input1')
+    pipe.connect(n2, 'output1', n4, 'input1')
+    pipe.connect(n3, 'output1', n4, 'input2')
+    n1.inputs.input1 = 10
+    pipe.config['execution']['poll_sleep_duration'] = 1
+    pipe.run(plugin='ResourceMultiProc', plugin_args={'memory': max_memory, 'status_callback': log_nodes_cb})
+
+    convert_logcb_to_json(LOG_FILENAME)
+    #memory usage in every second
+    memory = parse_log(LOG_FILENAME + '.json' , 'memory')
+
+    result = True
+    for m in memory:
+        if m > max_memory:
+            result = False
+            break
+
+    yield assert_equal, result, True
+
+    os.remove(LOG_FILENAME)
+    os.remove(LOG_FILENAME + '.json') 
+
+
+def test_do_not_use_more_threads_then_specified():
+    LOG_FILENAME = 'callback.log'
+    my_logger = logging.getLogger('callback')
+    my_logger.setLevel(logging.DEBUG)
+
+    # Add the log message handler to the logger
+    handler = logging.FileHandler(LOG_FILENAME)
+    my_logger.addHandler(handler)
+
+    max_threads = 10
+    pipe = pe.Workflow(name='pipe')
+    n1 = pe.Node(interface=TestInterfaceSingleNode(), name='n1')
+    n2 = pe.Node(interface=TestInterfaceSingleNode(), name='n2')
+    n3 = pe.Node(interface=TestInterfaceSingleNode(), name='n3')
+    n4 = pe.Node(interface=TestInterfaceSingleNode(), name='n4')
+
+    n1.interface.num_threads = 1
+    n2.interface.num_threads = 1
+    n3.interface.num_threads = 10
+    n4.interface.num_threads = 1
+
+    pipe.connect(n1, 'output1', n2, 'input1')
+    pipe.connect(n1, 'output1', n3, 'input1')
+    pipe.connect(n2, 'output1', n4, 'input1')
+    pipe.connect(n3, 'output1', n4, 'input2')
+    n1.inputs.input1 = 10
+    pipe.config['execution']['poll_sleep_duration'] = 1
+    pipe.run(plugin='ResourceMultiProc', plugin_args={'n_procs': max_threads, 'status_callback': log_nodes_cb})
+
+    convert_logcb_to_json(LOG_FILENAME)
+    #memory usage in every second
+    threads = parse_log(LOG_FILENAME + '.json' , 'num_threads')
+
+    result = True
+    for t in threads:
+        if t > max_threads:
+            result = False
+            break
+
+    yield assert_equal, result, True
+
+    os.remove(LOG_FILENAME)
+    os.remove(LOG_FILENAME + '.json')
