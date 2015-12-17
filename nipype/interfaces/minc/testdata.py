420dcb6a02f8623d8bbfa1f0a8484e9af6643bb6
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2015-12-18 07:47:33 +0800

    Add interfaces for MINC tools.

---
+++ nipype/interfaces/minc/testdata.py
@@ -0,0 +1,8 @@
+import os
+from nipype.testing import example_data
+
+minc2Dfile = example_data('minc_test_2D_00.mnc')
+minc3Dfile = example_data('minc_test_3D_00.mnc')
+
+def nonempty_minc_data(i, shape='2D'):
+    return example_data('minc_test_%s_%.2d.mnc' % (shape, i,))
