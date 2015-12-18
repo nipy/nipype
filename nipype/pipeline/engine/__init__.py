7fe023f577da0fcd566c1c8cdb0448a688e5da20
Oscar Esteban <code@oscaresteban.es>
2015-12-18 20:46:34 +0100

    fix __init__

--- nipype/pipeline/engine/__init__.py
+++ nipype/pipeline/engine/__init__.py
@@ -3,5 +3,5 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 
-from workflows import *
-from nodes import *
+from .workflows import *
+from .nodes import *
