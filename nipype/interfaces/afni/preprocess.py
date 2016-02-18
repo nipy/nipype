cbd08e0246ad2783a4bc05a80e41eadb00807d4f
dclark87 <danieljclark87@gmail.com>
2016-02-18 12:32:47 -0500

    Added back in the automaskinputspec

--- nipype/interfaces/afni/preprocess.py
+++ nipype/interfaces/afni/preprocess.py
@@ -507,6 +507,33 @@ class Despike(AFNICommand):
     output_spec = AFNICommandOutputSpec
 
 
+class AutomaskInputSpec(AFNICommandInputSpec):
+    in_file = File(desc='input file to 3dAutomask',
+                   argstr='%s',
+                   position=-1,
+                   mandatory=True,
+                   exists=True,
+                   copyfile=False)
+
+    out_file = File(name_template="%s_mask", desc='output image file name',
+                    argstr='-prefix %s', name_source="in_file")
+
+    brain_file = File(name_template="%s_masked",
+                      desc="output file from 3dAutomask",
+                      argstr='-apply_prefix %s',
+                      name_source="in_file")
+
+    clfrac = traits.Float(desc='sets the clip level fraction' +
+                          ' (must be 0.1-0.9). ' +
+                          'A small value will tend to make the mask larger [default = 0.5].',
+                          argstr="-clfrac %s")
+
+    dilate = traits.Int(desc='dilate the mask outwards',
+                        argstr="-dilate %s")
+
+    erode = traits.Int(desc='erode the mask inwards',
+                       argstr="-erode %s")
+
 
 class AutomaskOutputSpec(TraitedSpec):
     out_file = File(desc='mask file',
