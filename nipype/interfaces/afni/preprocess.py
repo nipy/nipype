7ec5538626af6849fd6f80e7195be9d28f83f832
sgiavasis <sgiava77@gmail.com>
2016-02-23 18:27:27 -0500

    Added an interface for 3dSeg.

--- nipype/interfaces/afni/preprocess.py
+++ nipype/interfaces/afni/preprocess.py
@@ -1540,7 +1540,8 @@ class ClipLevelOutputSpec(TraitedSpec):
 
 
 class ClipLevel(AFNICommandBase):
-    """Compute maximum and/or minimum voxel values of an input dataset
+    """Estimates the value at which to clip the anatomical dataset so
+       that background regions are set to zero.
 
     For complete details, see the `3dClipLevel Documentation.
     <https://afni.nimh.nih.gov/pub/dist/doc/program_help/3dClipLevel.html>`_
@@ -1583,6 +1584,88 @@ class ClipLevel(AFNICommandBase):
                 clip_val = clip_val[0]
             save_json(outfile, dict(stat=clip_val))
         outputs.clip_val = clip_val
+
+        return outputs
+
+
+class SegInputSpec(CommandLineInputSpec):
+    in_file = File(desc='ANAT is the volume to segment',
+                   argstr='-anat %s',
+                   position=-1,
+                   mandatory=True,
+                   exists=True,
+                   copyfile=True)
+
+    mask = traits.Str(desc='only non-zero voxels in mask are analyzed. mask can either be a dataset or the string \'AUTO\' which would use AFNI\'s automask function to create the mask.',
+                   argstr='-mask %s',
+                   position=-2,
+                   mandatory=True,
+                   exists=True)
+
+    blur_meth = traits.Enum('BFT', 'BIM',
+                            argstr='-blur_meth %s',
+                            desc='set the blurring method for bias field estimation')
+
+    bias_fwhm = traits.Float(desc='The amount of blurring used when estimating the field bias with the Wells method',
+                             argstr='-bias_fwhm %f')
+
+    classes = traits.Str(desc='CLASS_STRING is a semicolon delimited string of class labels',
+                         argstr='-classes %s')
+
+    bmrf = traits.Float(desc='Weighting factor controlling spatial homogeneity of the classifications',
+                        argstr='-bmrf %f')
+
+    bias_classes = traits.Str(desc='A semcolon demlimited string of classes that contribute to the estimation of the bias field',
+                              argstr='-bias_classes %s')
+
+    prefix = traits.Str(desc='the prefix for the output folder containing all output volumes',
+                        argstr='-prefix %s')
+
+    mixfrac = traits.Str(desc='MIXFRAC sets up the volume-wide (within mask) tissue fractions while initializing the segmentation (see IGNORE for exception)',
+                              argstr='-mixfrac %s')
+
+    mixfloor = traits.Float(desc='Set the minimum value for any class\'s mixing fraction',
+                            argstr='-mixfloor %f')
+
+    main_N = traits.Int(desc='Number of iterations to perform.',
+                        argstr='-main_N %d')
+
+
+class Seg(AFNICommandBase):
+    """3dSeg segments brain volumes into tissue classes. The program allows
+       for adding a variety of global and voxelwise priors. However for the
+       moment, only mixing fractions and MRF are documented.
+
+    For complete details, see the `3dSeg Documentation.
+    <https://afni.nimh.nih.gov/pub/dist/doc/program_help/3dSeg.html>
+
+    Examples
+    ========
+
+    >>> from nipype.interfaces.afni import preprocess
+    >>> seg = preprocess.Seg()
+    >>> seg.inputs.in_file = 'structural.nii'
+    >>> seg.inputs.mask = 'AUTO'
+    >>> res = seg.run()
+    '3drefit -deoblique structural.nii'
+    >>> res = refit.run() # doctest: +SKIP
+
+    """
+
+    _cmd = '3dSeg'
+    input_spec = SegInputSpec
+    output_spec = AFNICommandOutputSpec
+
+    def aggregate_outputs(self, runtime=None, needed_outputs=None):
+
+        outputs = self._outputs()
+
+        if isdefined(self.inputs.prefix):
+            outfile = os.path.join(os.getcwd(), self.inputs.prefix, 'Classes+orig.BRIK')
+        else:
+            outfile = os.path.join(os.getcwd(), 'Segsy', 'Classes+orig.BRIK')
+
+        outputs.out_file = outfile
 
         return outputs
 
