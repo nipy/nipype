8e88e098fd3f5b418d530569d72cb2a1e4012440
dclark87 <danieljclark87@gmail.com>
2016-03-23 16:20:31 -0400

    Added 3dECM interface

--- nipype/interfaces/afni/preprocess.py
+++ nipype/interfaces/afni/preprocess.py
@@ -509,9 +509,9 @@ class Despike(AFNICommand):
 
 
 class CentralityInputSpec(AFNICommandInputSpec):
+    """Common input spec class for all centrality-related commmands
     """
-    inherits the out_file parameter from AFNICommandOutputSpec base class
-    """
+
 
     mask = File(desc='mask file to mask input data',
                    argstr="-mask %s",
@@ -530,8 +530,7 @@ class CentralityInputSpec(AFNICommandInputSpec):
 
 
 class DegreeCentralityInputSpec(CentralityInputSpec):
-    """
-    inherits the out_file parameter from AFNICommandOutputSpec base class
+    """DegreeCentrality inputspec
     """
 
     in_file = File(desc='input file to 3dDegreeCentrality',
@@ -549,8 +548,7 @@ class DegreeCentralityInputSpec(CentralityInputSpec):
 
 
 class DegreeCentralityOutputSpec(AFNICommandOutputSpec):
-    """
-    inherits the out_file parameter from AFNICommandOutputSpec base class
+    """DegreeCentrality outputspec
     """
 
     oned_file = File(desc='The text output of the similarity matrix computed'\
@@ -574,8 +572,9 @@ class DegreeCentrality(AFNICommand):
     >>> degree.inputs.in_file = 'func_preproc.nii'
     >>> degree.inputs.mask = 'mask.nii'
     >>> degree.inputs.sparsity = 1 # keep the top one percent of connections
+    >>> degree.inputs.out_file = 'out.nii'
     >>> degree.cmdline
-    '3dDegreeCentrality -sparsity 1 -mask mask.nii func_preproc.nii'
+    '3dDegreeCentrality -sparsity 1 -mask mask.nii -prefix out.nii func_preproc.nii'
     >>> res = degree.run() # doctest: +SKIP
     """
 
@@ -596,9 +595,84 @@ class DegreeCentrality(AFNICommand):
         return outputs
 
 
-class LFCDInputSpec(CentralityInputSpec):
+class ECMInputSpec(CentralityInputSpec):
+    """ECM inputspec
     """
-    inherits the out_file parameter from AFNICommandOutputSpec base class
+
+    in_file = File(desc='input file to 3dECM',
+                   argstr='%s',
+                   position=-1,
+                   mandatory=True,
+                   exists=True,
+                   copyfile=False)
+
+    sparsity = traits.Float(desc='only take the top percent of connections',
+                            argstr='-sparsity %f')
+
+    full = traits.Bool(desc='Full power method; enables thresholding; '\
+                            'automatically selected if -thresh or -sparsity '\
+                            'are set',
+                       argstr='-full')
+
+    fecm = traits.Bool(desc='Fast centrality method; substantial speed '\
+                            'increase but cannot accomodate thresholding; '\
+                            'automatically selected if -thresh or -sparsity '\
+                            'are not set',
+                       argstr='-fecm')
+
+    shift = traits.Float(desc='shift correlation coefficients in similarity '\
+                              'matrix to enforce non-negativity, s >= 0.0; '\
+                              'default = 0.0 for -full, 1.0 for -fecm',
+                            argstr='-shift %f')
+
+    scale = traits.Float(desc='scale correlation coefficients in similarity '\
+                              'matrix to after shifting, x >= 0.0; '\
+                              'default = 1.0 for -full, 0.5 for -fecm',
+                            argstr='-scale %f')
+
+    eps = traits.Float(desc='sets the stopping criterion for the power '\
+                            'iteration; l2|v_old - v_new| < eps*|v_old|; '\
+                            'default = 0.001',
+                            argstr='-eps %f')
+
+    max_iter = traits.Int(desc='sets the maximum number of iterations to use '\
+                               'in the power iteration; default = 1000',
+                          argstr='-max_iter %d')
+
+    memory = traits.Float(desc='Limit memory consumption on system by setting '\
+                               'the amount of GB to limit the algorithm to; '\
+                               'default = 2GB',
+                          argstr='-memory %f')
+
+
+class ECM(AFNICommand):
+    """Performs degree centrality on a dataset using a given maskfile
+    via the 3dLFCD command
+
+    For complete details, see the `3dECM Documentation.
+    <http://afni.nimh.nih.gov/pub/dist/doc/program_help/3dECM.html>
+
+    Examples
+    ========
+
+    >>> from nipype.interfaces import afni as afni
+    >>> ecm = afni.ECM()
+    >>> ecm.inputs.in_file = 'func_preproc.nii'
+    >>> ecm.inputs.mask = 'mask.nii'
+    >>> ecm.inputs.sparsity = 0.1 # keep top 0.1% of connections
+    >>> ecm.inputs.out_file = 'out.nii'
+    >>> ecm.cmdline
+    '3dECM -sparsity 0.1 -mask mask.nii -prefix out.nii func_preproc.nii'
+    >>> res = ecm.run() # doctest: +SKIP
+    """
+
+    _cmd = '3dECM'
+    input_spec = ECMInputSpec
+    output_spec = AFNICommandOutputSpec
+
+
+class LFCDInputSpec(CentralityInputSpec):
+    """LFCD inputspec
     """
 
     in_file = File(desc='input file to 3dLFCD',
@@ -623,9 +697,10 @@ class LFCD(AFNICommand):
     >>> lfcd = afni.LFCD()
     >>> lfcd.inputs.in_file = 'func_preproc.nii'
     >>> lfcd.inputs.mask = 'mask.nii'
-    >>> lfcd.inputs.threshold = .8 # keep all connections with corr >= 0.8
+    >>> lfcd.inputs.thresh = 0.8 # keep all connections with corr >= 0.8
+    >>> lfcd.inputs.out_file = 'out.nii'
     >>> lfcd.cmdline
-    '3dLFCD -threshold 0.8 -mask mask.nii func_preproc.nii'
+    '3dLFCD -thresh 0.8 -mask mask.nii -prefix out.nii func_preproc.nii'
     >>> res = lfcd.run() # doctest: +SKIP
     """
 
