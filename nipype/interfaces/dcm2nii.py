5aa402a8fd7a2585045bb32f3133a161b7c283d2
Dimitri Papadopoulos <dimitri.papadopoulos@cea.fr>
2016-04-20 10:45:27 +0200

    Minor typo in comment

--- nipype/interfaces/dcm2nii.py
+++ nipype/interfaces/dcm2nii.py
@@ -107,7 +107,7 @@ class Dcm2nii(CommandLine):
                 if line.startswith("Saving "):
                     out_file = line[len("Saving "):]
                 elif line.startswith("GZip..."):
-                    # for gzipped outpus files are not absolute
+                    # for gzipped output files are not absolute
                     fname = line[len("GZip..."):]
                     if len(files) and os.path.basename(files[-1]) == fname[:-3]:
                         # we are seeing a previously reported conversion
