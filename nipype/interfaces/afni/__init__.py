0a5bb0b36ad2a8744a30b61bb4e5967ae9276c4d
dclark87 <danieljclark87@gmail.com>
2016-04-27 14:33:37 -0400

    Added new interfaces to __init__ import

--- nipype/interfaces/afni/__init__.py
+++ nipype/interfaces/afni/__init__.py
@@ -8,8 +8,9 @@ Top-level namespace for afni.
 
 from .base import Info
 from .preprocess import (To3D, Refit, Resample, TStat, Automask, Volreg, Merge,
-                         ZCutUp, Calc, TShift, Warp, Detrend, Despike, DegreeCentrality,
-                         LFCD, Copy, Fourier, Allineate, Maskave, SkullStrip, TCat,
+                         ZCutUp, Calc, TShift, Warp, Detrend, Despike,
+                         DegreeCentrality, ECM, LFCD, Copy, Fourier, Allineate,
+                         Maskave, SkullStrip, TCat, ClipLevel, MaskTool, Seg,
                          Fim, BlurInMask, Autobox, TCorrMap, Bandpass, Retroicor,
                          TCorrelate, TCorr1D, BrickStat, ROIStats, AutoTcorrelate,
                          AFNItoNIFTI, Eval, Means, Hist, FWHMx, OutlierCount,
