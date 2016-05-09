f0a3889a0ba8b7e5b3fa8c3ab339eb4c582c60ce
dclark87 <danieljclark87@gmail.com>
2016-05-09 10:35:03 -0400

    Added one test back in

--- nipype/pipeline/plugins/tests/test_multiproc.py
+++ nipype/pipeline/plugins/tests/test_multiproc.py
@@ -34,27 +34,27 @@ class TestInterface(nib.BaseInterface):
         return outputs
 
 
-#def test_run_multiproc():
-#    cur_dir = os.getcwd()
-#    temp_dir = mkdtemp(prefix='test_engine_')
-#    os.chdir(temp_dir)
-#
-#    pipe = pe.Workflow(name='pipe')
-#    mod1 = pe.Node(interface=TestInterface(), name='mod1')
-#    mod2 = pe.MapNode(interface=TestInterface(),
-#                      iterfield=['input1'],
-#                      name='mod2')
-#    pipe.connect([(mod1, mod2, [('output1', 'input1')])])
-#    pipe.base_dir = os.getcwd()
-#    mod1.inputs.input1 = 1
-#    pipe.config['execution']['poll_sleep_duration'] = 2
-#    execgraph = pipe.run(plugin="MultiProc")
-#    names = ['.'.join((node._hierarchy, node.name)) for node in execgraph.nodes()]
-#    node = execgraph.nodes()[names.index('pipe.mod1')]
-#    result = node.get_output('output1')
-#    yield assert_equal, result, [1, 1]
-#    os.chdir(cur_dir)
-#    rmtree(temp_dir)
+def test_run_multiproc():
+    cur_dir = os.getcwd()
+    temp_dir = mkdtemp(prefix='test_engine_')
+    os.chdir(temp_dir)
+
+    pipe = pe.Workflow(name='pipe')
+    mod1 = pe.Node(interface=TestInterface(), name='mod1')
+    mod2 = pe.MapNode(interface=TestInterface(),
+                      iterfield=['input1'],
+                      name='mod2')
+    pipe.connect([(mod1, mod2, [('output1', 'input1')])])
+    pipe.base_dir = os.getcwd()
+    mod1.inputs.input1 = 1
+    pipe.config['execution']['poll_sleep_duration'] = 2
+    execgraph = pipe.run(plugin="MultiProc")
+    names = ['.'.join((node._hierarchy, node.name)) for node in execgraph.nodes()]
+    node = execgraph.nodes()[names.index('pipe.mod1')]
+    result = node.get_output('output1')
+    yield assert_equal, result, [1, 1]
+    os.chdir(cur_dir)
+    rmtree(temp_dir)
 
 
 class InputSpecSingleNode(nib.TraitedSpec):
