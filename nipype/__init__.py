4e1b437a2c1615d5d8350f2a476b768c39b6c65b
Alexandre Manhaes Savio <alexsavio@gmail.com>
2016-05-11 17:34:30 +0200

    wip: first duecredit implementation suggestion

--- nipype/__init__.py
+++ nipype/__init__.py
@@ -16,22 +16,14 @@ logging = Logging(config)
 from distutils.version import LooseVersion
 
 from .fixes.numpy.testing import nosetester
+from .refs import due
 
-# Use duecredit (duecredit.org) to provide a citation to relevant work to
-# be cited. This does nothing, unless the user has duecredit installed,
-# And calls this with duecredit (as in `python -m duecredit script.py`):
-from .due import due, Doi
-due.cite(Doi("10.3389/fninf.2011.00013"),
-         description="A flexible, lightweight and extensible neuroimaging data"
-                     " processing framework in Python",
-         tags=["nipype"],)
 
 try:
     import faulthandler
     faulthandler.enable()
 except (ImportError,IOError) as e:
     pass
-
 
 class _NoseTester(nosetester.NoseTester):
     """ Subclass numpy's NoseTester to add doctests by default
