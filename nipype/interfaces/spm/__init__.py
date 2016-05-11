5129557c96d5481bb93d64e157a0faf4caa729eb
Alexandre Manhaes Savio <alexsavio@gmail.com>
2016-05-11 17:34:30 +0200

    wip: first duecredit implementation suggestion

--- nipype/interfaces/spm/__init__.py
+++ nipype/interfaces/spm/__init__.py
@@ -12,3 +12,5 @@ from .model import (Level1Design, EstimateModel, EstimateContrast, Threshold,
                     PairedTTestDesign, MultipleRegressionDesign)
 from .utils import (Analyze2nii, CalcCoregAffine, ApplyTransform, Reslice,
                     ApplyInverseDeformation, ResliceToReference, DicomImport)
+
+from .refs import due
