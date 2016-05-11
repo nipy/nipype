55872db4cb49158de58eb0bdfdf00c00447d007d
Alexandre Manhaes Savio <alexsavio@gmail.com>
2016-05-11 17:36:00 +0200

    fix merge

--- nipype/__init__.py
+++ nipype/__init__.py
@@ -18,16 +18,6 @@ from distutils.version import LooseVersion
 from .fixes.numpy.testing import nosetester
 from .refs import due
 
-
-# Use duecredit (duecredit.org) to provide a citation to relevant work to
-# be cited. This does nothing, unless the user has duecredit installed,
-# And calls this with duecredit (as in `python -m duecredit script.py`):
-from .due import due, Doi
-due.cite(Doi("10.3389/fninf.2011.00013"),
-         description="A flexible, lightweight and extensible neuroimaging data"
-                     " processing framework in Python",
-         tags=["nipype"],)
-
 try:
     import faulthandler
     faulthandler.enable()
