5795b125bad651cbb4ee9348df1446204625c960
Alexandre Manhaes Savio <alexsavio@gmail.com>
2016-05-12 11:24:18 +0200

    fix spm citation

--- nipype/interfaces/spm/refs.py
+++ nipype/interfaces/spm/refs.py
@@ -2,10 +2,10 @@
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """References for this interface"""
 
-from .due import due, Doi, BibTeX
+from ...due import due, Doi, BibTeX
 
-# http://www.fil.ion.ucl.ac.uk/spm
-due.dcite(BibTeX("""book{FrackowiakFristonFrithDolanMazziotta1997,
+# # http://www.fil.ion.ucl.ac.uk/spm
+due.cite(BibTeX("""@book{FrackowiakFristonFrithDolanMazziotta1997,
         author={R.S.J. Frackowiak, K.J. Friston, C.D. Frith, R.J. Dolan, and J.C. Mazziotta},
         title={Human Brain Function},
         publisher={Academic Press USA}
