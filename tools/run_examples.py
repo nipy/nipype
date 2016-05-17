395b60ac3e3cd5ca67308563852bf0be9a5ff1a4
dclark87 <danieljclark87@gmail.com>
2016-05-17 17:45:55 -0400

    Added smoke test for draw gantt chart

--- tools/run_examples.py
+++ tools/run_examples.py
@@ -1,19 +1,30 @@
 from __future__ import print_function
 import os
 import sys
-from shutil import rmtree
+from shutil import rmtree, copyfile
 
 
 def run_examples(example, pipelines, plugin):
-    print('running example: %s with plugin: %s' % (example, plugin))
+    '''
+    Run example workflows
+    '''
+
+    # Import packages
     from nipype import config
+    from nipype.interfaces.base import CommandLine
+    from nipype.utils import draw_gantt_chart
+    from nipype.pipeline.plugins import log_nodes_cb
+
+    print('running example: %s with plugin: %s' % (example, plugin))
+
     config.enable_debug_mode()
     config.enable_provenance()
-    from nipype.interfaces.base import CommandLine
     CommandLine.set_default_terminal_output("stream")
 
     __import__(example)
+
     for pipeline in pipelines:
+        # Init and run workflow
         wf = getattr(sys.modules[example], pipeline)
         wf.base_dir = os.path.join(os.getcwd(), 'output', example, plugin)
         if os.path.exists(wf.base_dir):
@@ -21,9 +32,30 @@ def run_examples(example, pipelines, plugin):
         wf.config = {'execution': {'hash_method': 'timestamp',
                                    'stop_on_first_rerun': 'true',
                                    'write_provenance': 'true'}}
-        wf.run(plugin=plugin, plugin_args={'n_procs': 4})
+
+        # Callback log setup
+        if example == 'fmri_spm_nested' and plugin == 'MultiProc' and \
+           pipeline == 'l2pipeline':
+            # Init callback log
+            import logging
+            cb_log_path = os.path.join(wf.base_dir, 'callback.log')
+            cb_logger = logging.getLogger('callback')
+            cb_logger.setLevel(logging.DEBUG)
+            handler = logging.FileHandler(cb_log_path)
+            cb_logger.addHandler(handler)
+            plugin_args = {'n_procs' : 4, 'status_callback' : log_nodes_cb}
+        else:
+            plugin_args = {'n_procs' : 4}
+
+        wf.run(plugin=plugin, plugin_args=plugin_args)
         # run twice to check if nothing is rerunning
         wf.run(plugin=plugin)
+
+        # Draw gantt chart
+        if plugin_args.has_key('status_callback'):
+            draw_gantt_chart.generate_gantt_chart(cb_log_path, 4)
+            dst_log_html = os.path.join(os.path.expanduser('~'), 'callback.log.html')
+            copyfile(cb_log_path+'.html', dst_log_html)
 
 if __name__ == '__main__':
     path, file = os.path.split(__file__)
