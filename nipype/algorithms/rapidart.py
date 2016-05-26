d375c2a9918e48699c4184ee936025227e57bdc8
Shoshana Berleant <nope>
2016-05-25 18:22:44 -0700

    Revert "Revert "Revert "minor improvements to error reporting"""
    This reverts commit 5d22313d7f5e2a14c4e8339d51c9722f7ad74586.

--- nipype/algorithms/rapidart.py
+++ nipype/algorithms/rapidart.py
@@ -362,10 +362,7 @@ class ArtifactDetect(BaseInterface):
                 nim = funcs.concat_images(images)
 
         # compute global intensity signal
-        try:
-            (x, y, z, timepoints) = nim.shape
-        except ValueError as ve:
-            raise NipypeInterfaceError(ve, 'Check dimensions of input image; 4-D input required.')
+        (x, y, z, timepoints) = nim.shape
 
         data = nim.get_data()
         affine = nim.affine
