d57a0c16b5fda120762c87f891bba0a0bee200a8
Horea Christian <horea.christ@yandex.com>
2016-06-10 05:08:01 +0200

    added test

---
+++ nipype/interfaces/fsl/tests/test_Level1Design.py
@@ -0,0 +1,13 @@
+import os
+import test_Level1Design
+from ...base import Undefined
+from ..model import Level1Design
+
+def test_level1design():
+	l = Level1Design()
+	runinfo = dict(cond=[{'name': 'test_condition', 'onset': [0, 10], 'duration':[10, 10]}],regress=[])
+	runidx = 0
+	contrasts = Undefined
+	usetd = False
+	do_tempfilter = False
+	return Level1Design._create_ev_files(l,os.getcwd(),runinfo,runidx,usetd,contrasts,do_tempfilter,"hrf")
