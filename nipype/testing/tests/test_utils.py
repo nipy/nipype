68154f7ad4d2aea6d8046dc45d6834545f2b2316
Shoshana Berleant <berleant@stanford.edu>
2016-06-17 02:38:27 +0000

    This reverts commit 24d0935fc1dcee9d44c099c71f6a6f78291c3329 (didn't work in python 3)

--- nipype/testing/tests/test_utils.py
+++ nipype/testing/tests/test_utils.py
@@ -20,11 +20,11 @@ def test_tempfatfs():
         with fatfs as tmpdir:
             yield assert_true, os.path.exists(tmpdir)
 
+@patch('subprocess.check_call', MagicMock(
+    side_effect=subprocess.CalledProcessError('','')))
 def test_tempfatfs_calledprocesserror():
-    with patch('subprocess.check_call', MagicMock(side_effect=subprocess.CalledProcessError('',''))):
-        yield assert_raises, IOError, TempFATFS
+    yield assert_raises, IOError, TempFATFS
 
+@patch('subprocess.Popen', MagicMock(side_effect=OSError()))
 def test_tempfatfs_oserror():
-    with patch('subprocess.Popen', MagicMock()):
-        subprocess.Popen.return_value.side_effect = OSError()
-        yield assert_raises, IOError, TempFATFS
+    yield assert_raises, IOError, TempFATFS
