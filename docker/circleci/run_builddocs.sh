fed480844c72c89e188e5088eff7452b244a60d4
oesteban <code@oscaresteban.es>
2016-07-19 10:45:21 -0700

    fix test_spm example, change permissions on artifacts

--- docker/circleci/run_builddocs.sh
+++ docker/circleci/run_builddocs.sh
@@ -7,5 +7,6 @@ source activate nipypetests-2.7
 mkdir -p /scratch/docs
 set -o pipefail && cd /root/src/nipype/doc && make html 2>&1 | tee /scratch/builddoc.log
 cp -r /root/src/nipype/doc/_build/html/* /scratch/docs/
+chmod 755 -R /scratch/docs
+chmod 755 /scratch/builddoc.log
 cat /scratch/builddoc.log && if grep -q "ERROR" /scratch/builddoc.log; then false; else true; fi
-
