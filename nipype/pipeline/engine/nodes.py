8b3c8df5cdcaab4b9bb4bf1c77ca35b8a13f2119
ashgillman <ashley.gillman@csiro.au>
2016-07-27 12:39:48 +1000

    Use reference to err to avoid deletion in Py3.4

--- nipype/pipeline/engine/nodes.py
+++ nipype/pipeline/engine/nodes.py
@@ -1154,7 +1154,8 @@ class MapNode(Node):
             err = None
             try:
                 node.run(updatehash=updatehash)
-            except Exception as err:
+            except Exception as this_err:
+                err = this_err # save reference
                 if str2bool(self.config['execution']['stop_on_first_crash']):
                     self._result = node.result
                     raise
