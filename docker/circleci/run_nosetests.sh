b89c3120200c34ef7bffb182f41bd75839a88f5a
oesteban <code@oscaresteban.es>
2016-08-15 15:07:46 -0700

    save crashfiles to an artifacts dir

--- docker/circleci/run_nosetests.sh
+++ docker/circleci/run_nosetests.sh
@@ -3,13 +3,13 @@ set -e
 set -x
 set -u
 
-mkdir -p /scratch/nose
+mkdir -p /scratch/nose /scratch/crashfiles
 mkdir -p /root/.nipype
 echo '[logging]' > /root/.nipype/nipype.cfg
 echo 'log_to_file = true' >> /root/.nipype/nipype.cfg
 echo 'log_directory = /scratch/logs/' >> /root/.nipype/nipype.cfg
 
 nosetests -c /root/src/nipype/.noserc --xunit-file="/scratch/nosetests.xml" --cover-xml-file="/scratch/coverage.xml"
-chmod 777 /scratch/nosetests.xml
-chmod 777 /scratch/coverage.xml
-chmod 777 -R /scratch/nose
+
+cp /root/nipype/src/crash-* /scratch/crashfiles/
+chmod 777 -R /scratch/*
