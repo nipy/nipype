ac41ff2f88410164a7f2a500b61b0ab0f38383d4
oesteban <code@oscaresteban.es>
2016-08-16 15:59:46 -0700

    circle: fix copy crashfiles, update py27 image

--- docker/circleci/run_nosetests.sh
+++ docker/circleci/run_nosetests.sh
@@ -3,15 +3,21 @@ set -e
 set -x
 set -u
 
+# Create necessary directories
 mkdir -p /scratch/nose /scratch/crashfiles
+
+# Create a nipype config file
 mkdir -p /root/.nipype
 echo '[logging]' > /root/.nipype/nipype.cfg
 echo 'log_to_file = true' >> /root/.nipype/nipype.cfg
 echo 'log_directory = /scratch/logs/' >> /root/.nipype/nipype.cfg
 
+
+# Run tests
 cd /root/src/nipype/
 make clean
 nosetests -s nipype -c /root/src/nipype/.noserc --xunit-file="/scratch/nosetests.xml" --cover-xml-file="/scratch/coverage.xml"
 
-cp /root/src/nipype/crash-* /scratch/crashfiles/
+# Copy crashfiles to scratch
+for i in $(find /root/src/nipype/ -name "crash-*" ); do cp $i /scratch/crashfiles/; done
 chmod 777 -R /scratch/*
