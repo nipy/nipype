e7e4dcc027ff2303032d239c9c5e94303ff15fb7
oesteban <code@oscaresteban.es>
2016-09-08 10:04:29 -0700

    add +IGNORE_UNICODE to doctest, update specs

--- nipype/interfaces/fsl/utils.py
+++ nipype/interfaces/fsl/utils.py
@@ -2073,7 +2073,7 @@ class WarpPointsFromStd(CommandLine):
     >>> warppoints.inputs.std_file = 'mni.nii'
     >>> warppoints.inputs.warp_file = 'warpfield.nii'
     >>> warppoints.inputs.coord_mm = True
-    >>> warppoints.cmdline # doctest: +ELLIPSIS
+    >>> warppoints.cmdline # doctest: +ELLIPSIS +IGNORE_UNICODE
     'std2imgcoord -mm -img T1.nii -std mni.nii -warp warpfield.nii surf.txt'
     >>> res = warppoints.run() # doctest: +SKIP
 
