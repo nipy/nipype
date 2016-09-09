ce23a6d7bfedfadde091a9888e2832709c76be4d
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-09-08 18:59:59 -0700

    doh

--- docker/circleci/run_examples.sh
+++ docker/circleci/run_examples.sh
@@ -11,7 +11,7 @@ echo 'filemanip_level = DEBUG' >> /root/.nipype/nipype.cfg
 echo 'log_to_file = true' >> /root/.nipype/nipype.cfg
 echo 'log_directory = /scratch/logs/' >> /root/.nipype/nipype.cfg
 
-coverage run python /root/src/nipype/tools/run_examples.py $@
+coverage run /root/src/nipype/tools/run_examples.py $@
 coverage xml -o /scratch/smoketest_${@//[^A-Za-z0-9._-]/_}.xml
 
 chmod 777 -R /scratch/logs
