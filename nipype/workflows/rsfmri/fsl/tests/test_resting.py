f8cea6145666b64ef240924abdec362b014694e2
Shoshana Berleant <berleant@stanford.edu>
2016-09-10 03:31:46 +0000

    set up mocks

--- nipype/workflows/rsfmri/fsl/tests/test_resting.py
+++ nipype/workflows/rsfmri/fsl/tests/test_resting.py
@@ -1,15 +1,26 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 
-from nipype.testing import (assert_equal, assert_true, assert_almost_equal,
-                            skipif, utils)
-from nipype.workflows.rsfmri.fsl import resting
+from .....testing import (assert_equal, assert_true, assert_almost_equal,
+                          skipif, utils)
+from .....interfaces import fsl
+
+from ..resting import create_resting_preproc
 
 import unittest
 import mock
+from mock import MagicMock
 import nibabel as nb
 import numpy as np
 import os
+
+def mock_node_factory(*args, **kwargs):
+    mock = MagicMock()
+    mock.name = kwargs['name'] if 'name' in kwargs.keys() else ''
+    if 'interface' in kwargs.keys():
+        mock = mock.create_autospec(kwargs['interface'], instance=True)
+    mock.iterables = None
+    return mock
 
 class TestResting(unittest.TestCase):
 
@@ -27,11 +38,15 @@ class TestResting(unittest.TestCase):
         utils.save_toy_nii(self.fake_data, self.in_filenames['in_file'])
 
     @skipif(True)
-    def test_create_resting_preproc(self):
+    @mock.patch('nipype.pipeline.engine.Workflow._write_report_info')
+    @mock.patch('nipype.workflows.rsfmri.fsl.resting.create_realign_flow',
+                side_effect=mock_node_factory)
+    @mock.patch('nipype.pipeline.engine.Node', side_effect=mock_node_factory)
+    def test_create_resting_preproc(self, mock_Node, mock_realign_wf, nothing):
         # setup
         print(np.random.randint(0, 10, (2, 2, 2, 5)))
         # run
-        wf = resting.create_resting_preproc()
+        wf = create_resting_preproc()
         wf.run()
 
         # assert
