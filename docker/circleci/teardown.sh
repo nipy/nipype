2637541de9e1c134c60137e5238517ffaade23e5
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-09-11 13:21:19 -0700

    reverting

--- docker/circleci/teardown.sh
+++ docker/circleci/teardown.sh
@@ -7,9 +7,9 @@ set -u
 set -e
 
 mkdir -p ${CIRCLE_TEST_REPORTS}/nose
-sudo cp ~/scratch/*.xml ${CIRCLE_TEST_REPORTS}/nose
-# mkdir -p ~/docs
-# sudo mv ~/scratch/docs/* ~/docs/
+sudo mv ~/scratch/*.xml ${CIRCLE_TEST_REPORTS}/nose
+mkdir -p ~/docs
+sudo mv ~/scratch/docs/* ~/docs/
 mkdir -p ~/logs
 sudo mv ~/scratch/builddocs.log ~/logs/builddocs.log
 sudo mv ~/scratch/logs/* ~/logs/
