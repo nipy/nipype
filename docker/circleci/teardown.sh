c54de25ce3cf33d68978aec45eb1ce43faa60909
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-09-11 11:08:56 -0700

    strip

--- docker/circleci/teardown.sh
+++ docker/circleci/teardown.sh
@@ -8,8 +8,8 @@ set -e
 
 mkdir -p ${CIRCLE_TEST_REPORTS}/nose
 sudo cp ~/scratch/*.xml ${CIRCLE_TEST_REPORTS}/nose
-mkdir -p ~/docs
-sudo mv ~/scratch/docs/* ~/docs/
+# mkdir -p ~/docs
+# sudo mv ~/scratch/docs/* ~/docs/
 mkdir -p ~/logs
 sudo mv ~/scratch/builddocs.log ~/logs/builddocs.log
 sudo mv ~/scratch/logs/* ~/logs/
