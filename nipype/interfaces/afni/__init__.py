7bab56de23d42d5f5de9474377337332a657c4fa
Taylor Salo <tsalo006@fiu.edu>
2016-09-30 16:07:29 -0400

    Clean afni/preprocess and afni/base.
    Sort imports in afni base.
    
    Sort classes in afni preprocess, split several long strings, and
    replace traits.Str with base Str throughout all classes.

--- nipype/interfaces/afni/__init__.py
+++ nipype/interfaces/afni/__init__.py
@@ -8,12 +8,12 @@ Top-level namespace for afni.
 """
 
 from .base import Info
-from .preprocess import (To3D, Refit, Resample, TStat, Automask, Volreg, Merge,
-                         ZCutUp, Calc, TShift, Warp, Detrend, Despike,
-                         DegreeCentrality, ECM, LFCD, Copy, Fourier, Allineate,
-                         Maskave, SkullStrip, TCat, ClipLevel, MaskTool, Seg,
-                         Fim, BlurInMask, Autobox, TCorrMap, Bandpass, Retroicor,
-                         TCorrelate, TCorr1D, BrickStat, ROIStats, AutoTcorrelate,
-                         AFNItoNIFTI, Eval, Means, Hist, FWHMx, OutlierCount,
-                         QualityIndex, Notes)
+from .preprocess import (AFNItoNIFTI, Allineate, AutoTcorrelate, Autobox,
+                         Automask, Bandpass, BlurInMask, BlurToFWHM, BrickStat,
+                         Calc, ClipLevel, Copy, DegreeCentrality, Despike,
+                         Detrend, ECM, Eval, FWHMx, Fim, Fourier, Hist, LFCD,
+                         MaskTool, Maskave, Means, Merge, Notes, OutlierCount,
+                         QualityIndex, ROIStats, Refit, Resample, Retroicor,
+                         Seg, SkullStrip, TCat, TCorr1D, TCorrMap, TCorrelate,
+                         TShift, TStat, To3D, Volreg, Warp, ZCutUp)
 from .svm import (SVMTest, SVMTrain)
