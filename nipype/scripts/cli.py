3d0c790740844a64c61217c38bbee56dd72fc935
Alexandre Manhaes Savio <alexsavio@gmail.com>
2016-10-06 17:53:47 +0200

    fix missing import in cli.py

--- nipype/scripts/cli.py
+++ nipype/scripts/cli.py
@@ -1,6 +1,8 @@
 #!python
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
+from io import open
+
 import click
 
 from .instance import list_interfaces
