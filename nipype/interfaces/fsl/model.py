12ef6c18be0ff27c4269dc3e0caa4eafb12ede42
Horea Christian <horea.christ@yandex.com>
2016-10-30 19:23:31 +0100

    fixed syntax

--- nipype/interfaces/fsl/model.py
+++ nipype/interfaces/fsl/model.py
@@ -47,7 +47,7 @@ class Level1DesignInputSpec(BaseInterfaceInputSpec):
         desc=("name of basis function and options e.g., "
               "{'dgamma': {'derivs': True}}"))
     orthogonalization = traits.Dict(traits.Int, traits.Dict(traits.Int,
-        traits.Either(traits.Bool,traits.Int))
+        traits.Either(traits.Bool,traits.Int)),
         mandatory=False,
         desc=("which regressors to make orthogonal e.g., "
               "{1: {0:0,1:0,2:0}, 2: {0:1,1:1,2:0}} to make the second "
