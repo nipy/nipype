1cdedfb27caaed83e465c9e0a38f0ed9ccb1618c
Dorota Jarecka <djarecka@gmail.com>
2016-11-17 22:55:24 -0500

    changing tests from freesurface to pytest; did NOT run them (test containers do not have freesurface)

--- nipype/interfaces/freesurfer/tests/test_model.py
+++ nipype/interfaces/freesurfer/tests/test_model.py
@@ -8,12 +8,12 @@ import shutil
 import numpy as np
 import nibabel as nib
 
-from nipype.testing import assert_equal, skipif
+import pytest
 from nipype.interfaces.freesurfer import model, no_freesurfer
 import nipype.pipeline.engine as pe
 
 
-@skipif(no_freesurfer)
+@pytest.mark.skipif(no_freesurfer(), reason="freesurfer is not installed")
 def test_concatenate():
     tmp_dir = os.path.realpath(tempfile.mkdtemp())
     cwd = os.getcwd()
@@ -32,15 +32,13 @@ def test_concatenate():
 
     # Test default behavior
     res = model.Concatenate(in_files=[in1, in2]).run()
-    yield (assert_equal, res.outputs.concatenated_file,
-           os.path.join(tmp_dir, 'concat_output.nii.gz'))
-    yield (assert_equal, nib.load('concat_output.nii.gz').get_data(), out_data)
+    assert res.outputs.concatenated_file == os.path.join(tmp_dir, 'concat_output.nii.gz')
+    assert nib.load('concat_output.nii.gz').get_data() == out_data
 
     # Test specified concatenated_file
     res = model.Concatenate(in_files=[in1, in2], concatenated_file=out).run()
-    yield (assert_equal, res.outputs.concatenated_file,
-           os.path.join(tmp_dir, out))
-    yield (assert_equal, nib.load(out).get_data(), out_data)
+    assert res.outputs.concatenated_file == os.path.join(tmp_dir, out)
+    assert nib.load(out).get_data() == out_data
 
     # Test in workflow
     wf = pe.Workflow('test_concatenate', base_dir=tmp_dir)
@@ -49,14 +47,12 @@ def test_concatenate():
                      name='concat')
     wf.add_nodes([concat])
     wf.run()
-    yield (assert_equal, nib.load(os.path.join(tmp_dir, 'test_concatenate',
-                                               'concat', out)).get_data(),
-           out_data)
+    assert nib.load(os.path.join(tmp_dir, 'test_concatenate','concat', out)).get_data()== out_data
 
     # Test a simple statistic
     res = model.Concatenate(in_files=[in1, in2], concatenated_file=out,
                             stats='mean').run()
-    yield (assert_equal, nib.load(out).get_data(), mean_data)
+    assert nib.load(out).get_data() == mean_data
 
     os.chdir(cwd)
     shutil.rmtree(tmp_dir)
