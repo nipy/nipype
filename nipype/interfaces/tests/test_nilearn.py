3f7bd39c4f01185e67b18333fcc22919a35c3e1a
Dorota Jarecka <djarecka@gmail.com>
2016-11-24 23:17:55 -0500

    removing skipif from a new test (skipif is already applied to whole class)

--- nipype/interfaces/tests/test_nilearn.py
+++ nipype/interfaces/tests/test_nilearn.py
@@ -108,7 +108,7 @@ class TestSignalExtraction():
         # run & assert
         self._test_4d_label(wanted, self.fake_4d_label_data)
 
-    @skipif(no_nilearn)
+
     def test_signal_extr_traits_valid(self):
         ''' Test a node using the SignalExtraction interface.
         Unlike interface.run(), node.run() checks the traits
