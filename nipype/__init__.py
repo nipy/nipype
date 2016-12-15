f353fbbe2a21b05a7f314348014150ae93b7636c
Dorota Jarecka <djarecka@gmail.com>
2016-12-15 18:47:24 -0500

    Removing nosetest from nipype/__init__.py; pytest version is missing for now

--- nipype/__init__.py
+++ nipype/__init__.py
@@ -11,7 +11,6 @@ from .info import (LONG_DESCRIPTION as __doc__,
                    STATUS as __status__,
                    __version__)
 from .utils.config import NipypeConfig
-from .fixes.numpy.testing import nosetester
 from .utils.logger import Logging
 from .refs import due
 
@@ -24,48 +23,48 @@ except (ImportError, IOError) as e:
 config = NipypeConfig()
 logging = Logging(config)
 
+#NOTE_dj: it has to be changed to python
+#class _NoseTester(nosetester.NoseTester):
+#    """ Subclass numpy's NoseTester to add doctests by default
+#    """
 
-class _NoseTester(nosetester.NoseTester):
-    """ Subclass numpy's NoseTester to add doctests by default
-    """
+#    def _get_custom_doctester(self):
+#        return None
 
-    def _get_custom_doctester(self):
-        return None
+#    def test(self, label='fast', verbose=1, extra_argv=['--exe'],
+#             doctests=True, coverage=False):
+#        """Run the full test suite
+#
+#        Examples
+#        --------
+#        This will run the test suite and stop at the first failing
+#        example
+#        >>> from nipype import test
+#        >>> test(extra_argv=['--exe', '-sx'])  # doctest: +SKIP
+#        """
+#        return super(_NoseTester, self).test(label=label,
+#                                             verbose=verbose,
+#                                             extra_argv=extra_argv,
+#                                             doctests=doctests,
+#                                             coverage=coverage)
 
-    def test(self, label='fast', verbose=1, extra_argv=['--exe'],
-             doctests=True, coverage=False):
-        """Run the full test suite
-
-        Examples
-        --------
-        This will run the test suite and stop at the first failing
-        example
-        >>> from nipype import test
-        >>> test(extra_argv=['--exe', '-sx'])  # doctest: +SKIP
-        """
-        return super(_NoseTester, self).test(label=label,
-                                             verbose=verbose,
-                                             extra_argv=extra_argv,
-                                             doctests=doctests,
-                                             coverage=coverage)
-
-try:
-    test = _NoseTester(raise_warnings="release").test
-except TypeError:
+#try:
+#    test = _NoseTester(raise_warnings="release").test
+#except TypeError:
     # Older versions of numpy do not have a raise_warnings argument
-    test = _NoseTester().test
-del nosetester
+#    test = _NoseTester().test
+#del nosetester
 
 # Set up package information function
-from .pkg_info import get_pkg_info as _get_pkg_info
-get_info = lambda: _get_pkg_info(os.path.dirname(__file__))
+#from .pkg_info import get_pkg_info as _get_pkg_info
+#get_info = lambda: _get_pkg_info(os.path.dirname(__file__))
 
 # If this file is exec after being imported, the following lines will
 # fail
-try:
-    del Tester
-except:
-    pass
+#try:
+#    del Tester
+#except:
+#    pass
 
 
 from .pipeline import Node, MapNode, JoinNode, Workflow
