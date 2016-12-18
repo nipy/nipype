daba01cc3f46452c5c74e6393cc5ab411ed58d11
Satrajit Ghosh <satra@mit.edu>
2016-12-18 15:13:33 -0500

    enh: updated dependencies and tests for nipype

--- nipype/__init__.py
+++ nipype/__init__.py
@@ -13,6 +13,7 @@ from .info import (LONG_DESCRIPTION as __doc__,
 from .utils.config import NipypeConfig
 from .utils.logger import Logging
 from .refs import due
+from .pkg_info import get_pkg_info as _get_pkg_info
 
 try:
     import faulthandler
@@ -23,49 +24,27 @@ except (ImportError, IOError) as e:
 config = NipypeConfig()
 logging = Logging(config)
 
-#NOTE_dj: it has to be changed to python
-#class _NoseTester(nosetester.NoseTester):
-#    """ Subclass numpy's NoseTester to add doctests by default
-#    """
 
-#    def _get_custom_doctester(self):
-#        return None
+class NipypeTester(object):
+    def __call__(self, doctests=True):
+        try:
+            import pytest
+        except:
+            raise RuntimeError('py.test not installed, run: pip install pytest')
+        params = {'args': []}
+        if doctests:
+            params['args'].append('--doctest-modules')
+        nipype_path = os.path.dirname(__file__)
+        params['args'].extend(['-x', '--ignore={}/external'.format(nipype_path),
+                               nipype_path])
+        pytest.main(**params)
 
-#    def test(self, label='fast', verbose=1, extra_argv=['--exe'],
-#             doctests=True, coverage=False):
-#        """Run the full test suite
-#
-#        Examples
-#        --------
-#        This will run the test suite and stop at the first failing
-#        example
-#        >>> from nipype import test
-#        >>> test(extra_argv=['--exe', '-sx'])  # doctest: +SKIP
-#        """
-#        return super(_NoseTester, self).test(label=label,
-#                                             verbose=verbose,
-#                                             extra_argv=extra_argv,
-#                                             doctests=doctests,
-#                                             coverage=coverage)
+test = NipypeTester()
 
-#try:
-#    test = _NoseTester(raise_warnings="release").test
-#except TypeError:
-    # Older versions of numpy do not have a raise_warnings argument
-#    test = _NoseTester().test
-#del nosetester
 
-# Set up package information function
-from .pkg_info import get_pkg_info as _get_pkg_info
-get_info = lambda: _get_pkg_info(os.path.dirname(__file__))
-
-# If this file is exec after being imported, the following lines will
-# fail
-#try:
-#    del Tester
-#except:
-#    pass
-
+def get_info():
+    """Returns package information"""
+    return _get_pkg_info(os.path.dirname(__file__))
 
 from .pipeline import Node, MapNode, JoinNode, Workflow
 from .interfaces import (DataGrabber, DataSink, SelectFiles,
