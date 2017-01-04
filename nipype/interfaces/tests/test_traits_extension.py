64baff531cd4e27748270e2fccf0d6a239e0a5d2
Shoshana Berleant <berleant@stanford.edu>
2017-01-04 01:04:15 +0000

    start writing/planning tests

---
+++ nipype/interfaces/tests/test_traits_extension.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+""" tests for the custom traits defined in ./traits_extension.py """
+
+class TestNiftiFile(unittest.TestCase):
+    """ tests the custom Trait class NiftiFile """
+
+    def test_NiftiFile_no_dimension(self):
+        """ Initialize a NiftiFile trait without specifying dimensionality
+        or setting dimensionality = None """
+
+    def test_NiftiFile_dimensionality_check(self):
+        """ Initialize a NiftiFile trait, specifying dimensionality """
+
+    def test_NiftiFile_exists_false(self):
+        """ Fail to initialize a NiftiFile trait if exists=False """
+        with self.assertRaisesRegexp(TraitError, 'an existing nifti file'):
+            NiftiFile(exists=False)
+
+    def test_NiftiFile_exists_true(self):
+        """ Initialize a NiftiFile trait without specifying exists or setting exists=True """
+        nifti_files = [NiftiFile(), NiftiFile(exists=True)]
+
+        for nifti_file in nifti_files:
+            self.assertTrue(nifti_file.exists)
+
+    def test_integration(self):
+        """ test behavior while actually in a workflow """
