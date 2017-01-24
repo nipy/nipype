15e46ed3a55cf416978c321ef1436ea69fa5681b
byvernault <byvernault@gmail.com>
2017-01-24 14:57:40 +0000

    Editing the tests with the changes made to niftyfit

--- nipype/interfaces/niftyfit/tests/test_fit_asl.py
+++ nipype/interfaces/niftyfit/tests/test_fit_asl.py
@@ -1,13 +1,13 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 
+import os
 from nipype.interfaces.niftyfit import no_niftyfit, get_custom_path, FitAsl
 from nipype.testing import assert_equal, skipif, example_data
 
 
 @skipif(no_niftyfit(cmd='fit_asl'))
 def test_seg_em():
-
     # Create a reg_aladin object
     test_node = FitAsl()
 
@@ -18,10 +18,12 @@ def test_seg_em():
     in_file = example_data('im1.nii')
     test_node.inputs.source_file = in_file
 
-    cmd_tmp = '{cmd} -cbf im1_cbf -error im1_error -source {in_file} \
--syn im1_syn'
+    cmd_tmp = '{cmd} -cbf {cbf} -error {error} -source {in_file} -syn {syn}'
     expected_cmd = cmd_tmp.format(
         cmd=get_custom_path('fit_asl'),
-        in_file=in_file)
+        in_file=in_file,
+        cbf=os.path.join(os.getcwd(), 'im1_cbf.nii.gz'),
+        error=os.path.join(os.getcwd(), 'im1_error.nii.gz'),
+        syn=os.path.join(os.getcwd(), 'im1_syn.nii.gz'))
 
     yield assert_equal, test_node.cmdline, expected_cmd
--- nipype/interfaces/niftyfit/tests/test_fit_qt1.py
+++ nipype/interfaces/niftyfit/tests/test_fit_qt1.py
@@ -1,13 +1,13 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 
+import os
 from nipype.interfaces.niftyfit import no_niftyfit, get_custom_path, FitQt1
 from nipype.testing import assert_equal, skipif, example_data
 
 
 @skipif(no_niftyfit(cmd='fit_qt1'))
 def test_seg_em():
-
     # Create a reg_aladin object
     test_node = FitQt1()
 
@@ -18,10 +18,16 @@ def test_seg_em():
     in_file = example_data('im1.nii')
     test_node.inputs.source_file = in_file
 
-    cmd_tmp = '{cmd} -source {in_file} -error im1_error -m0map im1_m0map \
--mcmap im1_mcmap -res im1_res -syn im1_syn -t1map im1_t1map'
+    cmd_tmp = '{cmd} -source {in_file} -error {error} -m0map {map0} \
+-mcmap {cmap} -res {res} -syn {syn} -t1map {t1map}'
     expected_cmd = cmd_tmp.format(
         cmd=get_custom_path('fit_qt1'),
-        in_file=in_file)
+        in_file=in_file,
+        map0=os.path.join(os.getcwd(), 'im1_m0map.nii.gz'),
+        error=os.path.join(os.getcwd(), 'im1_error.nii.gz'),
+        cmap=os.path.join(os.getcwd(), 'im1_mcmap.nii.gz'),
+        res=os.path.join(os.getcwd(), 'im1_res.nii.gz'),
+        t1map=os.path.join(os.getcwd(), 'im1_t1map.nii.gz'),
+        syn=os.path.join(os.getcwd(), 'im1_syn.nii.gz'))
 
     yield assert_equal, test_node.cmdline, expected_cmd
