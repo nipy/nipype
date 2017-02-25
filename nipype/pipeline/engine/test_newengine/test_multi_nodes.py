6736f901e37f0192cfff9a1b9a17292ab2796ee5
Dorota Jarecka <djarecka@gmail.com>
2017-02-24 19:16:55 -0500

    include Satra suggestions

--- nipype/pipeline/engine/test_newengine/test_multi_nodes.py
+++ nipype/pipeline/engine/test_newengine/test_multi_nodes.py
@@ -10,7 +10,10 @@ def fun4(A, b, **dict):
 
 
 @pytest.mark.parametrize("inputs_dict1, inputs_dict2, expected_output", [
-        ({"a": [3, 4, 5]}, {"b": [10, 20, 30]}, [[19, 29, 39], [26, 36, 46], [35, 45, 55]]),
+        ({"a": [3, 4, 5]}, {"b": [10, 20, 30]}, 
+         [[(["a=3","b=10"], 19), (["a=3","b=20"], 29), (["a=3","b=30"], 39)], 
+          [(["a=4","b=10"], 26), (["a=4","b=20"], 36), (["a=4","b=30"], 46)], 
+          [(["a=5","b=10"], 35), (["a=5","b=20"], 45), (["a=5","b=30"], 55)]]),
           ])
 def test_2nodes_1(inputs_dict1, inputs_dict2, expected_output):
     # not sure how do you want to initiate (with Node or empty), assuming empty and adding node
@@ -28,28 +31,38 @@ def test_2nodes_1(inputs_dict1, inputs_dict2, expected_output):
 
     wf.run()
    
-    assert (wf.outputs["out"] == expected_output).all()
+    for (i, out) in enumerate(wf.output["out"]):
+        assert out[0] == expected_output[i][0]
+        assert (out[1] == expected_output[i][1]).all()
+
 
 
 @pytest.mark.parametrize("inputs_dict1, inputs_dict2, expected_output", [
-        ({"a": [3, 4, 5]}, {"b": [10, 20, 30]}, [19, 36, 55]),
+        ({"a": [3, 4, 5]}, {"b": [10, 20, 30]}, 
+         [(["a=3","b=10"], 19), (["a=4","b=20"], 36), (["a=5","b=30"], 55)]),
           ])
 def test_2nodes_2(inputs_dict1, inputs_dict2, expected_output):
     wf = Workflow()
     N1  = Node(inputs=inputs_dict1, mapper="a", interface=fun1)
     wf.add_nodes(N1)
 
-    N2 =  Node(inputs=inputs_dict2, mapper="(N1.a,b)", interface=fun4)
+    N2 =  Node(inputs=inputs_dict2, mapper=("N1.a","b"), interface=fun4)
     wf.connect(N1, "a", N2, "A")
 
     wf.run()
 
-    assert (wf.outputs["out"] == expected_output).all()
+    for (i, out) in enumerate(wf.output["out"]):
+        assert out[0] == expected_output[i][0]
+        assert (out[1] == expected_output[i][1]).all()
+
 
 
 @pytest.mark.parametrize("inputs_dict1, inputs_dict2, expected_output", [
         ({"a": [3, 4, 5]}, {"b": [10, 20, 30, 40]}, 
-         [[11, 23, 39, 67], [11, 24, 46, 104], [11, 25, 55, 165]]),
+         # should I also include N1.a in the state?
+         [[(["N1.a=3","A=1","b=10"], 11), (["N1.a=3","A=3","b=20"], 23), (["N1.a=3","A=9","b=30"], 39), (["N1.a=3","A=27","b=40"], 67)], 
+          [(["N1.a=4","A=1","b=10"], 11), (["N1.a=4","A=4","b=20"], 24), (["N1.a=4","A=16","b=30"], 46), (["N1.a=4","A=64","b=40"], 104)], 
+          [(["N1.a=5","A=1","b=10"], 11), (["N1.a=5","A=5","b=20"], 25), (["N1.a=5","A=25","b=30"], 55), (["N1.a=5","A=125","b=40"], 165)]]),
           ])
 def test_2nodes_3(inputs_dict1, inputs_dict2, expected_output):
     wf = Workflow()
@@ -58,18 +71,30 @@ def test_2nodes_3(inputs_dict1, inputs_dict2, expected_output):
 
     # IMPORTANT: I understand that if b is used in a mapper with A,
     # I do not anymore assume a x b, i.e. no "state input mapping"
-    N2 =  Node(inputs=inputs_dict2, mapper="(A,b)", interface=fun4)
+    N2 =  Node(inputs=inputs_dict2, mapper=("A","b"), interface=fun4)
     wf.connect(N1, "a", N2, "A")
 
     wf.run()
 
-    assert (wf.outputs["out"] == expected_output).all()
+    for (i, out) in enumerate(wf.output["out"]):
+        assert out[0] == expected_output[i][0]
+        assert (out[1] == expected_output[i][1]).all()
+
 
 @pytest.mark.parametrize("inputs_dict1, inputs_dict2, expected_output", [
         ({"a": [3, 4, 5]}, {"b": [10, 20, 30, 40]},
-         [[[11, 21, 31, 41], [13, 23, 33, 43], [19, 29, 39, 49], [37, 47, 57, 67]], 
-          [[11, 21, 31, 41], [14, 24, 34, 44], [26, 36, 46, 56], [74, 84, 94, 104]], 
-          [[11, 21, 31, 41], [15, 25, 35, 45], [35, 45, 55, 65], [135, 145, 155, 165]]]),
+         [[[(["N1.a=3","A=1","b=10"], 11), (["N1.a=3","A=1","b=20"], 21), (["N1.a=3","A=1","b=30"], 31), (["N1.a=3","A=1","b=40"], 41)], 
+           [(["N1.a=3","A=3","b=10"], 13), (["N1.a=3","A=3","b=20"], 23), (["N1.a=3","A=3","b=30"], 33), (["N1.a=3","A=3","b=40"], 43)], 
+           [(["N1.a=3","A=9","b=10"], 19), (["N1.a=3","A=9","b=20"], 29), (["N1.a=3","A=9","b=30"], 39), (["N1.a=3","A=9","b=40"], 49)], 
+           [(["N1.a=3","A=27","b=10"], 37), (["N1.a=3","A=27","b=20"], 47), (["N1.a=3","A=27","b=30"], 57), (["N1.a=3","A=1","b=10"], 67)]], 
+          [[(["N1.a=4","A=1","b=10"], 11), (["N1.a=4","A=1","b=20"], 21), (["N1.a=4","A=1","b=30"], 31), (["N1.a=4","A=1","b=40"], 41)], 
+           [(["N1.a=4","A=4","b=10"], 14), (["N1.a=4","A=4","b=20"], 24),(["N1.a=4","A=4","b=30"], 34), (["N1.a=4","A=4","b=40"], 44)], 
+           [(["N1.a=4","A=16","b=10"], 26), (["N1.a=4","A=16","b=20"], 36), (["N1.a=4","A=16","b=30"], 46), (["N1.a=4","A=16","b=40"], 56)], 
+           [(["N1.a=4","A=64","b=10"], 74), (["N1.a=4","A=64","b=20"], 84), (["N1.a=4","A=64","b=30"], 94), (["N1.a=4","A=64","b=40"], 104)]], 
+          [[(["N1.a=5","A=1","b=10"], 11), (["N1.a=5","A=1","b=20"], 21), (["N1.a=5","A=1","b=30"], 31), (["N1.a=5","A=1","b=40"], 41)], 
+           [(["N1.a=5","A=5","b=10"], 15), (["N1.a=5","A=5","b=20"], 25), (["N1.a=5","A=5","b=30"], 35), (["N1.a=5","A=5","b=40"], 45)], 
+           [(["N1.a=5","A=25","b=10"], 35), (["N1.a=5","A=25","b=20"], 45), (["N1.a=5","A=25","b=30"], 55), (["N1.a=5","A=25","b=40"], 65)], 
+           [(["N1.a=5","A=125","b=10"], 135), (["N1.a=5","A=125","b=20"], 145), (["N1.a=5","A=125","b=30"], 155), (["N1.a=5","A=125","b=40"], 165)]]]),
           ])
 def test_2nodes_3(inputs_dict1, inputs_dict2, expected_output):
     wf = Workflow()
@@ -78,12 +103,15 @@ def test_2nodes_3(inputs_dict1, inputs_dict2, expected_output):
 
     # IMPORTANT: I understand that if b is used in a mapper with A, 
     # I do not anymore assume a x b, i.e. no "state input mapping"       
-    N2 =  Node(inputs=inputs_dict2, mapper="[A,b]", interface=fun4)
+    N2 =  Node(inputs=inputs_dict2, mapper=["A","b"], interface=fun4)
     wf.connect(N1, "a", N2, "A")
 
     wf.run()
 
-    assert (wf.outputs["out"] == expected_output).all()
+    for (i, out) in enumerate(wf.output["out"]):
+        assert out[0] == expected_output[i][0]
+        assert (out[1] == expected_output[i][1]).all()
+
 
 
 
--- nipype/pipeline/engine/test_newengine/test_single_node.py
+++ nipype/pipeline/engine/test_newengine/test_single_node.py
@@ -17,73 +17,93 @@ def fun3(a, b, **dict):
 
 
 @pytest.mark.parametrize("inputs_dict, expected_output", [ 
-        ({"a": [3, 4, 5]}, [9, 16, 25]),
-        ({"a": [[3, 4, 5], [6, 7, 8]]}, [[9, 16, 25], [36, 49, 64]]),
-        ({"a": np.array([3, 4, 5])}, [9, 16, 25]),
+        ({"a": [3, 4, 5]}, [(["a=3"], 9), (["a=4"], 16), (["a=5"], 25)]), 
+        # do we want to allow 2D inputs a, when mapper="a"?
+        #({"a": [[3, 4, 5], [6, 7, 8]]}, [([3, 4, 5], [9, 16, 25]), ([6, 7, 8], [36, 49, 64])]),
+        ({"a": np.array([3, 4, 5])}, [(["a=3"], 9), (["a=4"], 16), (["a=5"], 25)]),
         ])
 def test_singlenode_1(inputs_dict, expected_output):
     N  = Node(inputs=inuts_dict, mapper="a", interface=fun1)
     N.run()
-    assert (N.outputs["out"] == expected_output).all()
+    
+    for (i, out) in enumerate(N.output["out"]):
+        assert out[0] == expected_output[i][0] # checking state values
+        assert (out[1] == expected_output[i][1]).all() # assuming that output value is an array (all() is used)
+
     
 
 @pytest.mark.parametrize("inputs_dict, expected_output", [
-        ({"a": [3, 4, 5]}, [9, 16, 25]),
-        ({"a": [[3, 4, 5], [6, 7, 8]]}, [[9, 16, 25], [36, 49, 64]]),
-        ({"a": np.array([3, 4, 5])}, [9, 16, 25]),
+        ({"a": [3, 4, 5]}, [(["a=3"], 9), (["a=4"], 16), (["a=5"], 25)]),
+        ({"a": np.array([3, 4, 5])}, [(["a=3"], 9), (["a=4"], 16), (["a=5"], 25)]),
         ])
 def test_singlenode_1a(inputs_dict, expected_output):
-    N  = Node(inputs=inputs_dict, mapper="a", interface=fun1)
+    N  = Node(mapper="a", interface=fun1)
     # testing if you can set inputs outside __init__
-    N.inputs = inputs_dic
+    N.inputs = inputs_dict
     N.run()
-    assert (N.outputs["out"] == expected_output).all()
+
+    for (i, out) in enumerate(N.output["out"]):
+        assert out[0] == expected_output[i][0] 
+        assert (out[1] == expected_output[i][1]).all()
 
 
+
+@pytest.mark.xfail
 def test_single_node_1b():
     # how to name or rename (from a default name, e.g. "out") outputs 
     sn = Node(interface=my_function_1, mapper='a', output_name=["out_1"])
     sn.inputs = {"a" : [3, 1, 8]}
     sn.run()
-    assert (sn.outputs["out_1"] == [9, 1, 64]).all()
+    assert (sn.outputs["out_1"] == [(["a=3"], 9), (["a=1"], 1), (["a=8"], 64)]).all()
 
 
 @pytest.mark.parametrize("inputs_dict, expected_output", [
-        ({"a": [3, 4, 5]}, [[1, 3, 9, 27], [1, 4, 16, 64], [1, 5, 25, 125]]),
-        ({"a": [[3, 4, 5], [6, 7, 8]]}, 
-         [[[1, 3, 9, 27], [1, 4, 16, 64], [1, 5, 25, 125]], 
-          [[1, 6, 36, 196], [1, 7, 49, 343], [1, 8, 64, 512]]]),
-        ({"a": np.array([3, 4, 5])}, [[1, 3, 9, 27], [1, 4, 16, 64], [1, 5, 25, 125]]),
+        ({"a": [3, 4, 5]}, [(["a=3"], [1, 3, 9, 27]), (["a=4"], [1, 4, 16, 64]), (["a=5"], [1, 5, 25, 125])]),
         ])
 def test_singlenode_2(inputs_dict, expected_output):
     N  = Node(inputs=inputs_dict, mapper="a", interface=fun2)
     N.run()
-    assert (N.outputs["out"] == expected_output).all()
+
+    for (i, out) in enumerate(N.output["out"]):
+        assert out[0] == expected_output[i][0]
+        assert (out[1] == expected_output[i][1]).all()
+
 
 
 @pytest.mark.parametrize("inputs_dict, expected_output", [
-        ({"a":[3, 1, 8], "b":[0, 1, 2]}, [0, 1, 16]),
-        ({"a":[3, 1, 8], "b":[2]}, [6, 2, 16]),
+        ({"a":[3, 1, 8], "b":[0, 1, 2]}, [(["a=3","b=0"], 0), (["a=1","b=1"], 1), (["a=8", "a=2"], 16)]),
+        ({"a":[3, 1, 8], "b":[2]}, [(["a=3","b=2"], 6), (["a=1","b=2"], 2), (["a=8","b=2"], 16)]),
         ])
 def test_single_node_3(inputs_dict, expected_output):
-    N = Node(interface=fun3, mapper='(a,b)')
+    N = Node(interface=fun3, mapper=('a','b'))
     N.inputs = inputs_dict
     N.run()
-    assert (N.outputs["out"] == expected_output).all()
+
+    for (i, out) in enumerate(N.output["out"]):
+        assert out[0] == expected_output[i][0]
+        assert (out[1] == expected_output[i][1]).all()
+
 
 
 @pytest.mark.parametrize("inputs_dict, expected_output", [
-        ({"a":[3, 1], "b":[1, 2, 4]}, [[3, 6, 12], [1, 2, 4]]),
-        ({"a":[[3, 1]], "b":[1, 2, 4]},[[[3, 6, 12], [1, 2, 4]]]),
+        ({"a":[3, 1], "b":[1, 2, 4]}, [[(["a=3","b=1"], 3), (["a=3","b=2"], 6), (["a=3","b=4"], 12)], 
+                                       [(["a=1","b=1"], 1), (["a=1","b=2"], 2), (["a=1","b=4"], 4)]]),
         ({"a":[[3, 1], [30, 10]], "b":[1, 2, 4]},
-         [[[3, 6, 12], [1, 2, 4]],[[30, 60, 120],[10, 20, 40]]]),
-        ({"a":[3, 1], "b":[2]}, np.array([[6], [2]])),
+         [[[(["a=3","b=1"], 3), (["a=3","b=2"], 6), (["a=3","b=4"], 12)], 
+           [(["a=1","b=1"], 1), (["a=1","b=2"], 2), (["a=1","b=4"], 4)]],
+          [[(["a=30","b=1"], 30), (["a=30","b=2"], 60), (["a=30","b=4"], 120)],
+           [(["a=10","b=1"], 10), (["a=10","b=2"], 20), (["a=10","b=4"], 40)]]]),
+        ({"a":[3, 1], "b":[2]}, np.array([[(["a=3","b=2"], 6)], [(["a=1","b=2"], 2)]])),
         ])
 def test_single_node_4(inputs_dict, expected_output):
-    sn = Node(interface=fun3, mapper='[a,b]')
+    sn = Node(interface=fun3, mapper=['a','b'])
     sn.inputs = inputs_dict
     sn.run()
-    assert (sn.outputs["out"] == expected_output).all()
+
+    for (i, out) in enumerate(sn.output["out"]):
+        assert out[0] == expected_output[i][0]
+        assert (out[1] == expected_output[i][1]).all()
+
 
 
 @pytest.mark.parametrize("inputs_dict", [
@@ -93,11 +113,11 @@ def test_single_node_4(inputs_dict, expected_output):
         ])
 def test_single_node_wrong_input(inputs_dict):
     with pytest.raises(Exception):
-        sn = Node(interface=fun3, mapper='(a,b)')
+        sn = Node(interface=fun3, mapper=('a','b'))
         sn.inputs = inputs_dict
         sn.run()
 
 def test_single_node_wrong_key():
     with pytest.raises(Exception):
-        sn = Node(interface=fun3, mapper='(a,b)')
+        sn = Node(interface=fun3, mapper=('a','b'))
         sn.inputs = {"a":[3], "c":[0]}
