nipy/nipype

commit d6bcef9ddceac5e1742c63000976eaa43eb49d4b

FIX: AntsJointFusion unicode command line formatting
When strings are formatted in unicode, then the format
of the AntsJointFusion -g, -t flags is not the same
as the python string representations of lists.  These
flags need to be unpacked one-by-one and a new string
representation needs to be provided on the command line.

nipype/interfaces/ants/segmentation.py
@@ -1129,11 +1129,28 @@ ants_joint_fusion_posterior_%d.nii.gz, ants_joint_fusion_voting_weight_%d.nii.gz
             retval = ''
             if not isdefined(self.inputs.out_label_fusion):
                 retval = '-o {0}'.format(self.inputs.out_intensity_fusion_name_format)
+        elif opt == 'atlas_image':
+            atlas_image_cmd=" ".join(
+                [ '-g [{0}]'.format(",".join(
+                    fn for fn in ai) ) for ai in self.inputs.atlas_image]
+            )
+            retval = atlas_image_cmd
+        elif opt == 'target_image':
+            target_image_cmd=" ".join(
+                [ '-t [{0}]'.format(",".join(
+                    fn for fn in ai) ) for ai in self.inputs.target_image]
+            )
+            retval = target_image_cmd
+        elif opt == 'atlas_segmentation_image':
+            assert len(val) == len(self.inputs.atlas_image), "Number of specified " \
+                "segmentations should be identical to the number of atlas image " \
+                "sets {0}!={1}".format(len(val), len(self.inputs.atlas_image))
+            atlas_segmentation_image_cmd=" ".join(
+                [ '-t [{0}]'.format( fn ) for fn in self.inputs.atlas_segmentation_image ]
+            )
+            retval = atlas_segmentation_image_cmd
         else:
-            if opt == 'atlas_segmentation_image':
-                assert len(val) == len(self.inputs.atlas_image), "Number of specified " \
-                    "segmentations should be identical to the number of atlas image " \
-                    "sets {0}!={1}".format(len(val), len(self.inputs.atlas_image))
+
             return super(ANTSCommand, self)._format_arg(opt, spec, val)
         return retval
 

commit 59a146d5e7eb226aff4183ee3a51c5b4d9d7fb4f

[FIX] Doctest of antsJointFusion failing
The doctest of antsJointFusion was failing since #1862, these changes
hopefully fix this problem without breaking the correct functionality
of the interface.

Also corrected what I suspect is an error: -l was replaced by -t for
the segmentations.

nipype/interfaces/ants/segmentation.py
@@ -1130,28 +1130,29 @@ ants_joint_fusion_posterior_%d.nii.gz, ants_joint_fusion_voting_weight_%d.nii.gz
             if not isdefined(self.inputs.out_label_fusion):
                 retval = '-o {0}'.format(self.inputs.out_intensity_fusion_name_format)
         elif opt == 'atlas_image':
-            atlas_image_cmd=" ".join(
-                [ '-g [{0}]'.format(",".join(
-                    fn for fn in ai) ) for ai in self.inputs.atlas_image]
+            atlas_image_cmd = " ".join(
+                ['-g [{0}]'.format(", ".join("'%s'" % fn for fn in ai))
+                                   for ai in self.inputs.atlas_image]
             )
             retval = atlas_image_cmd
         elif opt == 'target_image':
-            target_image_cmd=" ".join(
-                [ '-t [{0}]'.format(",".join(
-                    fn for fn in ai) ) for ai in self.inputs.target_image]
+            target_image_cmd = " ".join(
+                ['-t [{0}]'.format(", ".join("'%s'" % fn for fn in ai))
+                                   for ai in self.inputs.target_image]
             )
             retval = target_image_cmd
         elif opt == 'atlas_segmentation_image':
             assert len(val) == len(self.inputs.atlas_image), "Number of specified " \
                 "segmentations should be identical to the number of atlas image " \
                 "sets {0}!={1}".format(len(val), len(self.inputs.atlas_image))
-            atlas_segmentation_image_cmd=" ".join(
-                [ '-t [{0}]'.format( fn ) for fn in self.inputs.atlas_segmentation_image ]
+
+            atlas_segmentation_image_cmd = " ".join(
+                ['-l {0}'.format(fn) for fn in self.inputs.atlas_segmentation_image]
             )
             retval = atlas_segmentation_image_cmd
         else:
 
-            return super(ANTSCommand, self)._format_arg(opt, spec, val)
+            return super(AntsJointFusion, self)._format_arg(opt, spec, val)
         return retval
 
     def _list_outputs(self):