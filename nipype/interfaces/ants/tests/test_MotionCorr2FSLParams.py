7401ad0e59a45abbbdb65c5a8aeb6fc801fdf932
Ross Blair <rblair2@stanford.edu>
2017-03-16 13:35:51 -0700

    update motioncorr2fslparams test to check to see if the original and converted parameters are close in value

--- nipype/interfaces/ants/tests/test_MotionCorr2FSLParams.py
+++ nipype/interfaces/ants/tests/test_MotionCorr2FSLParams.py
@@ -1,34 +1,53 @@
-import csv
 import os
 
+import numpy
+
 from nipype.interfaces.ants.preprocess import MotionCorr2FSLParams
+from nipype.interfaces.fsl.utils import AvScale
 from nipype.utils.tmpdirs import InTemporaryDirectory
 from nipype.utils.filemanip import split_filename
 
 def test_MotionCorr2FSLParams():
     with InTemporaryDirectory():
         cwd = os.getcwd()
+        fsl_mat_fname = "fsl_style.mat"
+        fp = open(fsl_mat_fname, 'w+')
+        fp.write(
+            "1.000000 0.000000 -0.000935 0.062539\n"
+            "0.000001 0.999999 0.001470 -0.162467\n"
+            "0.000935 -0.001470 0.999999 -0.279038\n"
+            "0.000000 0.000000 0.000000 1.000000\n"
+        )
+        fp.close()
+
         in_filename = os.path.join(cwd, 'in_file.csv')
         fp = open(in_filename, 'w+')
         fp.write("this line is ignored\n")
         fp.write(
-            "0,-0.99918075422702,1.00028207678993,-7.41063731046199e-06,"
-            "3.93289649449106e-05,1.24969530535555e-05,1.00021114616063,"
-            "0.000233157514656132,-0.000195740079275366,-0.00033024846828514,"
-            "0.999495881936253,-0.0168301940330171,-0.0549722774931478,"
-            "0.202056708561567\n"
+            "0,-0.99918075422702,1.000000,0.000000,-0.000935,0.000001,"
+            "0.999999,0.001470,0.000935,-0.001470,0.999999,-0.279038,0.062539,"
+            "-0.162467,-0.279038\n"
         )
         fp.close()
+
         # m2p - matrix 2 parameters
         m2p = MotionCorr2FSLParams()
         m2p.inputs.ants_matrix = in_filename
         m2p.run()
 
         pth, fname, _ = split_filename(in_filename)
-        params_fname = '{}{}'.format(fname, '.par')
-        params_fname = os.path.join(pth, params_fname)
+        conv_params_fname = '{}{}'.format(fname, '.par')
+        conv_params_fname = os.path.join(pth, conv_params_fname)
 
-        params_fp = open(params_fname)
-        params_data = csv.reader(params_fp, delimiter=' ')
-        line = next(params_data)
-        assert len(line) == 6
+        avscale = AvScale()
+        avscale.inputs.all_param = True
+        avscale.inputs.mat_file = fsl_mat_fname
+        avscale.run()
+        avscale_out = avscale.aggregate_outputs()
+        orig_params = []
+        orig_params.extend(avscale_out.rot_angles)
+        orig_params.extend(avscale_out.translations)
+        conv_params = numpy.genfromtext(conv_params_fname, delimeter=' ')
+        comp = numpy.isclose(conv_params, orig_params)
+
+        assert(False not in comp)
