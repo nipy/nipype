d4c5ffbaee335a00bc77a9b36894786fe03b01e7
mathiasg <mathiasg@mit.edu>
2017-04-06 17:59:43 -0400

    fix: catch write_graph errors

--- nipype/pipeline/engine/workflows.py
+++ nipype/pipeline/engine/workflows.py
@@ -425,6 +425,9 @@ connected.
                 base_dir = os.getcwd()
         base_dir = make_output_dir(base_dir)
         if graph2use in ['hierarchical', 'colored']:
+            if self.name[:1].isdigit(): # these graphs break if int
+                raise ValueError('{} graph failed, workflow name cannot begin '
+                                 'with a number'.format(graph2use))
             dotfilename = op.join(base_dir, dotfilename)
             self.write_hierarchical_dotfile(dotfilename=dotfilename,
                                             colored=graph2use == "colored",
