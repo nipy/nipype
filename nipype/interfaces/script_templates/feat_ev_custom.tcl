66844b2e2bcf628a9e90df65f402412208ee87af
Horea Christian <h.chr@mail.ru>
2017-04-16 08:29:40 +0200

    fixed template

--- nipype/interfaces/script_templates/feat_ev_custom.tcl
+++ nipype/interfaces/script_templates/feat_ev_custom.tcl
@@ -37,4 +37,4 @@ set fmri(custom$ev_num) "$cond_file"
 set fmri(default_bfcustom) "${fsldir}/etc/default_flobs.flobs/hrfbasisfns.txt"
 set fmri(basisorth$ev_num) $basisorth
 set fmri(basisfnum$ev_num) $basisfnum
-set fmri(bfcustom) "$bfcustompath"
+set fmri(bfcustom$ev_num) "$bfcustompath"
