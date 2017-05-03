d4e43b32f2d521a72cbe6299521cafdc1b07f14e
jdkent <james-kent@uiowa.edu>
2017-05-03 17:23:58 -0500

    WIP: ICA_AROMA.py, adding effigies edits

--- nipype/interfaces/fsl/ICA_AROMA.py
+++ nipype/interfaces/fsl/ICA_AROMA.py
@@ -12,52 +12,59 @@ from nipype.interfaces.base import (
 import os
 
 class ICA_AROMAInputSpec(CommandLineInputSpec):
-    featDir = Directory(exists=True,
+    feat_dir = Directory(exists=True, mandatory=True,
+                        argstr='-feat %s', 
+                        xor=['in_file','mat_file','fnirt_warp_file','motion_parameters'],
                         desc='If a feat directory exists and temporal filtering '
                         'has not been run yet, ICA_AROMA can use the files in '
-                        'this directory.',mandatory=False,xor=['infile','mask','affmat','warp','mc'])
-    infile = File(exists=True, 
-                  desc='volume to be denoised', 
-                  argstr='-i %s',mandatory=False,xor=['featDir'])
-    outdir = Directory(desc='path to output directory', 
-                  argstr='-o %s',mandatory=True)
-    mask = File(exists=True, 
-                desc='path/name volume mask', 
-                argstr='-m %s',mandatory=False,xor=['featDir'])
-    dim = traits.Int(desc='Dimensionality reduction when running '
-                     'MELODIC (defualt is automatic estimation)', 
-                     argstr='-dim %d')
-    TR = traits.Float(desc='TR in seconds. If this is not specified '
-                      'the TR will be extracted from the header of the fMRI nifti file.',
-                      argstr='%.2f')
-    melodic_dir = Directory(exists=True,
-                            desc='path to MELODIC directory if MELODIC has already been ran',
-                            argstr='-meldir %s')
-    affmat = File(exists=True,
-                  desc='path/name of the mat-file describing the '
-                  'affine registration (e.g. FSL FLIRT) of the '
-                  'functional data to structural space (.mat file)',
-                  argstr='-affmat %s',mandatory=False,xor=['featDir'])
-    warp = File(exists=True,
-                desc='File name of the warp-file describing '
-                'the non-linear registration (e.g. FSL FNIRT) '
-                'of the structural data to MNI152 space (.nii.gz)',
-                argstr='-warp %s',mandatory=False,xor=['featDir'])
-    mc = File(exists=True,
-              desc='motion parameters file',
-              argstr='-mc %s',mandatory=False,xor=['featDir'])
-    denoise_type = traits.Str(argstr='-den %s',
+                        'this directory.')
+    in_file = File(exists=True, mandatory=True, 
+                  argstr='-i %s', xor=['feat_dir'], 
+                  desc='volume to be denoised')
+    out_dir = Directory('out', mandatory=True,
+                       argstr='-o %s',  
+                       desc='output directory')
+    mask = File(exists=True, argstr='-m %s', xor=['feat_dir'], 
+               desc='path/name volume mask')
+    dim = traits.Int(argstr='-dim %d',
+                    desc='Dimensionality reduction when running '
+                    'MELODIC (defualt is automatic estimation)')
+    TR = traits.Float(argstr='-tr %.3f',
+                     desc='TR in seconds. If this is not specified '
+                     'the TR will be extracted from the '
+                     'header of the fMRI nifti file.')
+    melodic_dir = Directory(exists=True, argstr='-meldir %s',
+                           desc='path to MELODIC directory if MELODIC has already been run')
+    mat_file = File(exists=True, mandatory=True,
+                   argstr='-affmat %s', xor=['feat_dir'],
+                   desc='path/name of the mat-file describing the '
+                   'affine registration (e.g. FSL FLIRT) of the '
+                   'functional data to structural space (.mat file)')
+    fnirt_warp_file = File(exists=True, mandatory=True,
+                          argstr='-warp %s', xor=['feat_dir'],
+                          desc='File name of the warp-file describing '
+                          'the non-linear registration (e.g. FSL FNIRT) '
+                          'of the structural data to MNI152 space (.nii.gz)')
+    motion_parameters = File(exists=True, mandatory=True,
+                            argstr='-mc %s', xor=['feat_dir'],
+                            desc='motion parameters file')
+    denoise_type = traits.Enum('nonaggr', 'aggr', 'both', 'no', usedefault=True,
+                              mandatory=True, argstr='-den %s',
                               desc='Type of denoising strategy: '
                               '-none: only classification, no denoising '
                               '-nonaggr (default): non-aggresssive denoising, i.e. partial component regression '
                               '-aggr: aggressive denoising, i.e. full component regression '
-                              '-both: both aggressive and non-aggressive denoising (two outputs)',
-                              mandatory=True)
+                              '-both: both aggressive and non-aggressive denoising (two outputs)')
 
 class ICA_AROMAOutputSpec(TraitedSpec):
-    out_file = OutputMultiPath(File(exists=True), 
-                               desc='if generated: 1-(non aggressive denoised volume),'
-                               '2-(aggressive denoised volume)')
+    aggr_denoised_file=File(exists=True, 
+                           desc='if generated: aggressively denoised volume')
+    nonaggr_denoised_file=File(exists=True,
+                              desc='if generated: non aggressively denoised volume' )
+    out_dir = Directory(exists=True, 
+                       desc='directory contains (in addition to the denoised files): '
+                       'melodic.ica + classified_motion_components + '
+                       'classification_overview + feature_scores + melodic_ic_mni)')
    
 class ICA_AROMA(CommandLine):
     """
@@ -66,29 +73,23 @@ class ICA_AROMA(CommandLine):
     -------
 
     >>> from nipype.interfaces.fsl import ICA_AROMA 
-    >>> AROMA_obj=ICA_AROMA.ICA_AROMA()
-    >>> outDir=os.path.join(os.getcwd(),'ICA_AROMA_testout')
-    >>> func='/path/to/mcImg_brain.nii.gz'
-    >>> affmat='/path/to/functoT1.mat'
-    >>> warp='/path/to/T1toMNI_warp.nii.gz'
-    >>> mc='/path/to/mcImg.par'
-    >>> mask='/path/to/mcImg_mask.nii.gz'
-    >>> denoise_type='both'
-    >>> AROMA_obj.inputs.infile=func
-    >>> AROMA_obj.inputs.affmat=affmat
-    >>> AROMA_obj.inputs.warp=warp
-    >>> AROMA_obj.inputs.mc=mc
-    >>> AROMA_obj.inputs.mask=mask
-    >>> AROMA_obj.inputs.denoise_type=denoise_type
-    >>> AROMA_obj.inputs.outdir=outDir
+    >>> from nipype.testing import example_data
+    >>> AROMA_obj = ICA_AROMA.ICA_AROMA()
+    >>> AROMA_obj.inputs.in_file=example_data('functional.nii')
+    >>> AROMA_obj.inputs.mat_file=example_data('func_to_struct.mat')
+    >>> AROMA_obj.inputs.fnirt_warp_file=example_data('warpfield.nii')
+    >>> AROMA_obj.inputs.motion_parameters=example_data('functional.par')
+    >>> AROMA_obj.inputs.mask=example_data('mask.nii.gz')
+    >>> AROMA_obj.inputs.denoise_type='both'
+    >>> AROMA_obj.inputs.out_dir='ICA_testout'
     >>> AROMA_obj.cmdline
-    'ICA_AROMA.py -affmat /path/to/functoT1.mat -den both 
-    -i /path/to/mcImg_brain.nii.gz 
-    -m /path/to/mcImg_mask.nii.gz 
-    -mc /path/to/mcImg.par 
-    -o /path/to/ICA_AROMA_testout 
-    -warp /path/to/T1toMNI_warp.nii.gz'
-    >>> AROMA_obj.run()
+    'ICA_AROMA.py -den both 
+    -warp /home/james/dev/nipype/nipype/testing/data/warpfield.nii 
+    -i /home/james/dev/nipype/nipype/testing/data/functional.nii 
+    -m /home/james/dev/nipype/nipype/testing/data/mask.nii.gz 
+    -affmat /home/james/dev/nipype/nipype/testing/data/func_to_struct.mat 
+    -mc /home/james/dev/nipype/nipype/testing/data/functional.par 
+    -o ICA_testout'
 
     """
     _cmd = 'ICA_AROMA.py'
@@ -96,20 +97,16 @@ class ICA_AROMA(CommandLine):
     output_spec = ICA_AROMAOutputSpec
 
     def _list_outputs(self):
-        outputs = self.output_spec.get()
-        outdir = self.input_spec.outdir
-        denoising_strategy = input_spec.denoise_type
 
-        if denoising_strategy is "noaggr":
-          outputs['out_file'] = os.path.join(outdir,'denoised_func_data_nonaggr.nii.gz')
-        elif denoising_strategy is "aggr":
-          outputs['out_file'] = os.path.join(outdir,'denoised_func_data_aggr.nii.gz')
-        elif denoising_strategy is "both":
-          outputs['out_file'] = (os.path.join(outdir,'denoised_func_data_nonaggr.nii.gz'), os.path.join(outdir,'denoised_func_data_aggr.nii.gz'))
-        elif denoising_strategy is "none":
-          print "No denoising selected"
-        else:
-          raise RuntimeError('denoise_type must be specified as one of'
-                             ' noaggr,aggr,both, or none')
+        out_dir = os.path.abspath(self.inputs.out_dir)
+        outputs['out_dir'] = out_dir
+        #outputs = self.output_spec.get()
+        #outdir = self.input_spec.outdir
+        #denoising_strategy = input_spec.denoise_type
 
+        if self.inputs.denoise_type in ('aggr', 'both'):
+            outputs['aggr_denoised_file'] = os.path.join(out_dir, 'denoised_func_data_aggr.nii.gz')
+        if self.inputs.denoise_type in ('nonaggr', 'both'):
+            outputs['nonaggr_denoised_file'] = os.path.join(out_dir, 'denoised_func_data_nonaggr.nii.gz')
+        
         return outputs
