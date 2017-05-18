5d80df397ab477b02dbddc020a9a2ba8f18b3292
oesteban <code@oscaresteban.es>
2017-05-18 13:50:32 -0700

    [ENH] Add AFNI's interface to GCOR computation
    This PR adds the wrapper for the ``@compute_gcor`` tool.

--- nipype/interfaces/afni/utils.py
+++ nipype/interfaces/afni/utils.py
@@ -1381,3 +1381,66 @@ class ZCutUp(AFNICommand):
     _cmd = '3dZcutup'
     input_spec = ZCutUpInputSpec
     output_spec = AFNICommandOutputSpec
+
+
+class GCORInputSpec(CommandLineInputSpec):
+    in_file = File(
+        desc='input dataset to compute the GCOR over',
+        argstr='-input %s',
+        position=-1,
+        mandatory=True,
+        exists=True,
+        copyfile=False)
+
+    in_mask = File(
+        desc='mask dataset, for restricting the computation',
+        argstr='-mask %s',
+        exists=True,
+        copyfile=False)
+
+    nfirst = traits.Int(0, argstr='-nfirst %d',
+                        desc='specify number of initial TRs to ignore')
+    no_demean = traits.Bool(False, argstr='-no_demean',
+                            desc='do not (need to) demean as first step')
+
+
+class GCOROutputSpec(TraitedSpec):
+    out = traits.Float(desc='global correlation value')
+
+
+class GCOR(CommandLine):
+    """
+    Computes the average correlation between every voxel
+    and ever other voxel, over any give mask.
+
+
+    For complete details, see the `@compute_gcor Documentation.
+    <https://afni.nimh.nih.gov/pub/dist/doc/program_help/@compute_gcor.html>`_
+
+    Examples
+    ========
+
+    >>> from nipype.interfaces import afni
+    >>> gcor = afni.GCOR()
+    >>> gcor.inputs.in_file = 'structural.nii'
+    >>> gcor.inputs.nfirst = 4
+    >>> gcor.cmdline  # doctest: +ALLOW_UNICODE
+    '@compute_gcor -nfirst 4 -input structural.nii'
+    >>> res = gcor.run()  # doctest: +SKIP
+
+    """
+
+    _cmd = '@compute_gcor'
+    input_spec = GCORInputSpec
+    output_spec = GCOROutputSpec
+
+    def _run_interface(self, runtime):
+        runtime = super(GCOR, self)._run_interface(runtime)
+
+        gcor_line = [line.strip() for line in runtime.stdout.split('\n')
+                     if line.strip().startswith('GCOR = ')][-1]
+        setattr(self, '_gcor', float(gcor_line[len('GCOR = '):]))
+        return runtime
+
+    def _list_outputs(self):
+        return {'out': getattr(self, '_gcor')}
