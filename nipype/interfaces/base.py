6c691265b4ba96254b7c49e803ac331abc3595f0
Dorota Jarecka <djarecka@gmail.com>
2017-06-04 23:01:51 -0400

    removing MpiCommandLine: didnt see any code that was using (besides the auto test - should remove it)

--- nipype/interfaces/base.py
+++ nipype/interfaces/base.py
@@ -2000,47 +2000,6 @@ class StdOutCommandLine(CommandLine):
     def _gen_outfilename(self):
         raise NotImplementedError
 
-# dj TODO: haven't tested yet, no tests in taste_base 
-class MpiCommandLineInputSpec(CommandLineInputSpec):
-    use_mpi = traits.Bool(False,
-                          desc="Whether or not to run the command with mpiexec",
-                          usedefault=True)
-    n_procs = traits.Int(desc="Num processors to specify to mpiexec. Do not "
-                         "specify if this is managed externally (e.g. through "
-                         "SGE)")
-
-
-# dj TODO: haven't tested yet, no tests in taste_base
-# dj TOASK: it's not used anywhere, can I remove?
-class MpiCommandLine(CommandLine):
-    """Implements functionality to interact with command line programs
-    that can be run with MPI (i.e. using 'mpiexec').
-
-    Examples
-    --------
-    >>> from nipype.interfaces.base import MpiCommandLine
-    >>> mpi_cli = MpiCommandLine(command='my_mpi_prog')
-    >>> mpi_cli.inputs.args = '-v'
-    >>> mpi_cli.cmdline # doctest: +ALLOW_UNICODE
-    'my_mpi_prog -v'
-
-    >>> mpi_cli.inputs.use_mpi = True
-    >>> mpi_cli.inputs.n_procs = 8
-    >>> mpi_cli.cmdline # doctest: +ALLOW_UNICODE
-    'mpiexec -n 8 my_mpi_prog -v'
-    """
-    input_spec = MpiCommandLineInputSpec
-
-    @property
-    def cmdline(self):
-        """Adds 'mpiexec' to begining of command"""
-        result = []
-        if self.inputs.use_mpi:
-            result.append('mpiexec')
-            if self.inputs.n_procs:
-                result.append('-n %d' % self.inputs.n_procs)
-        result.append(super(MpiCommandLine, self).cmdline)
-        return ' '.join(result)
 
 
 # dj TODO: haven't tested yet, no tests in taste_base 
