eb524e52acf92cdc34d7ccbdd9d7d3b9050d7fc7
Michael Waskom <mwaskom@nyu.edu>
2017-06-21 11:09:16 -0400

    Include realignment matrices in TOPUP outputs

--- nipype/interfaces/fsl/epi.py
+++ nipype/interfaces/fsl/epi.py
@@ -143,6 +143,9 @@ class TOPUPInputSpec(FSLCommandInputSpec):
     out_warp_prefix = traits.Str("warpfield", argstr='--dfout=%s', hash_files=False,
                                  desc='prefix for the warpfield images (in mm)',
                                  usedefault=True)
+    out_mat_prefix = traits.Str("xfm", argstr='--rbmout=%s', hash_files=False,
+                                desc='prefix for the realignment matrices',
+                                usedefault=True)
     out_jac_prefix = traits.Str("jac", argstr='--jacout=%s',
                                  hash_files=False,
                                  desc='prefix for the warpfield images',
@@ -247,7 +250,7 @@ class TOPUP(FSLCommand):
     'topup --config=b02b0.cnf --datain=topup_encoding.txt \
 --imain=b0_b0rev.nii --out=b0_b0rev_base --iout=b0_b0rev_corrected.nii.gz \
 --fout=b0_b0rev_field.nii.gz --jacout=jac --logout=b0_b0rev_topup.log \
---dfout=warpfield'
+--rbmout=xfm --dfout=warpfield'
     >>> res = topup.run() # doctest: +SKIP
 
     """
@@ -288,6 +291,9 @@ class TOPUP(FSLCommand):
             for i in range(1, n_vols + 1)]
         outputs['out_jacs'] = [
             fmt(prefix=self.inputs.out_jac_prefix, i=i, ext=ext)
+            for i in range(1, n_vols + 1)]
+        output['out_mats'] = [
+            fmt(prefix=self.inputs.out_mat_prefix, i=i, ext=".mat")
             for i in range(1, n_vols + 1)]
 
         if isdefined(self.inputs.encoding_direction):
