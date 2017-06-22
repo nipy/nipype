4f7c3530cf9c81cf56fb304f1f21718e6fff6b10
Taylor Salo <tsalo006@fiu.edu>
2017-06-22 13:59:22 -0700

    Add main Deconvolve command.

--- nipype/interfaces/afni/model.py
+++ nipype/interfaces/afni/model.py
@@ -158,6 +158,7 @@ class DeconvolveInputSpec(AFNICommandInputSpec):
         requires=['glt_sym'])
 
 
+
 class DeconvolveOutputSpec(TraitedSpec):
     pass
 
@@ -173,5 +174,38 @@ class Deconvolve(AFNICommand):
 
     >>> from nipype.interfaces import afni
     >>> deconvolve = afni.Deconvolve()
+    >>> deconvolve.inputs.in_file = 'functional.nii'
+    >>> deconvolve.inputs.bucket = 'output.nii'
+    >>> deconvolve.inputs.x1D = 'output.1D'
+    >>> stim_times = [(1, 'stims1.txt', 'SPMG1(4)'), (2, 'stims2.txt', 'SPMG2(4)')]
+    >>> deconvolve.inputs.stim_times = stim_times
+    >>> deconvolve.cmdline  # doctest: +ALLOW_UNICODE
+    '3dDeconvolve -input functional.nii -bucket output.nii -x1D output.1D -stim_times 1 stims1.txt SPMG1(4) 2 stims2.txt SPMG2(4)'
+    >>> res = deconvolve.run()  # doctest: +SKIP
     """
-    pass
+
+    _cmd = '3dDeconvolve'
+    input_spec = DeconvolveInputSpec
+    output_spec = DeconvolveOutputSpec
+
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        if isdefined(self.inputs.x1D):
+            if not self.inputs.x1D.endswith('.xmat.1D'):
+                outputs['x1D'] = outputs['x1D'] + '.xmat.1D'
+        return outputs
+
+    def _format_arg(self, name, trait_spec, value):
+        """
+        Argument num_glt is defined automatically from the number of contrasts
+        desired (defined by the length of glt_sym). No effort has been made to
+        make this compatible with glt.
+        """
+        if name in ['stim_times', 'stim_labels']:
+            arg = ''
+            for st in value:
+                arg += trait_spec.argstr % value
+            arg = arg.rstrip()
+            return arg
+        elif name == 'glt_sym':
+            self.inputs.num_glt = len(value)
