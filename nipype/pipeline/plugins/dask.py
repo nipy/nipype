0dcc418af0f25fe886617870a33ff830059715db
Ross Blair <rosswilsonblair@gmail.com>
2017-07-15 12:08:31 -0700

    added initial init function for dask plugin

--- nipype/pipeline/plugins/dask.py
+++ nipype/pipeline/plugins/dask.py
@@ -7,6 +7,8 @@ Parallel workflow execution via dask
 """
 from __future__ import print_function, division, unicode_literals, absolute_import
 
+from dask.distributed import Client
+
 from .base import PluginBase
 
 
@@ -14,6 +16,15 @@ class DaskPlugin(PluginBase):
     """
     Base class for plugins
     """
+
+    def __init__(self, plugin_args=None):
+        if IPython_not_loaded:
+            raise ImportError('Please install ipyparallel to use this plugin.')
+        super(DaskPlugin, self).__init__(plugin_args=plugin_args)
+        valid_args = ('scheduler_file', 'scheduler_ip')
+        self.client_args = {arg: plugin_args[arg]
+                            for arg in valid_args if arg in plugin_args}
+        self.daskclient = Client(**self.client_args)
 
     def run(self, graph, config, updatehash=False):
         pass
