9c2a8dad1c45e3758bdba7cac4ab571a2f57213b
oesteban <code@oscaresteban.es>
2017-09-28 02:59:44 -0700

    add test

--- nipype/pipeline/plugins/tests/test_multiproc.py
+++ nipype/pipeline/plugins/tests/test_multiproc.py
@@ -115,3 +115,23 @@ def test_no_more_threads_than_specified(tmpdir):
     with pytest.raises(RuntimeError):
         pipe.run(plugin='MultiProc',
                  plugin_args={'n_procs': max_threads})
+
+
+def test_hold_job_until_procs_available(tmpdir):
+    tmpdir.chdir()
+
+    pipe = pe.Workflow(name='pipe')
+    n1 = pe.Node(SingleNodeTestInterface(), name='n1', n_procs=2)
+    n2 = pe.Node(SingleNodeTestInterface(), name='n2', n_procs=2)
+    n3 = pe.Node(SingleNodeTestInterface(), name='n3', n_procs=2)
+    n4 = pe.Node(SingleNodeTestInterface(), name='n4', n_procs=2)
+
+    pipe.connect(n1, 'output1', n2, 'input1')
+    pipe.connect(n1, 'output1', n3, 'input1')
+    pipe.connect(n2, 'output1', n4, 'input1')
+    pipe.connect(n3, 'output1', n4, 'input2')
+    n1.inputs.input1 = 4
+
+    max_threads = 2
+    pipe.run(plugin='MultiProc',
+             plugin_args={'n_procs': max_threads})
