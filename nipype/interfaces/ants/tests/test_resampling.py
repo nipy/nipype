0b7742d224f06ae0f6d580d4f759cee780955fc3
Dorota Jarecka <djarecka@gmail.com>
2017-10-27 14:30:58 -0400

    adding fixtures to test_resamplig

--- nipype/interfaces/ants/tests/test_resampling.py
+++ nipype/interfaces/ants/tests/test_resampling.py
@@ -19,75 +19,67 @@ def change_dir(request):
    request.addfinalizer(move2orig)
 
 
-
-def test_WarpImageMultiTransform(change_dir):
+@pytest.fixture()
+def create_wimt():
    wimt = WarpImageMultiTransform()
    wimt.inputs.input_image = 'diffusion_weighted.nii'
    wimt.inputs.reference_image = 'functional.nii'
    wimt.inputs.transformation_series = ['func2anat_coreg_Affine.txt','func2anat_InverseWarp.nii.gz', \
                                            'dwi2anat_Warp.nii.gz','dwi2anat_coreg_Affine.txt']
+   return wimt
+
+def test_WarpImageMultiTransform(change_dir, create_wimt):
+   wimt = create_wimt
    assert wimt.cmdline == 'WarpImageMultiTransform 3 diffusion_weighted.nii diffusion_weighted_wimt.nii -R functional.nii \
 func2anat_coreg_Affine.txt func2anat_InverseWarp.nii.gz dwi2anat_Warp.nii.gz dwi2anat_coreg_Affine.txt'
 
 
-def test_WarpImageMultiTransform_invaffine_1(change_dir):
-   wimt = WarpImageMultiTransform() 
-   wimt.inputs.input_image = 'diffusion_weighted.nii'
-   wimt.inputs.reference_image = 'functional.nii'
-   wimt.inputs.transformation_series = ['func2anat_coreg_Affine.txt','func2anat_InverseWarp.nii.gz', \
-                                           'dwi2anat_Warp.nii.gz','dwi2anat_coreg_Affine.txt']  
+def test_WarpImageMultiTransform_invaffine_1(change_dir, create_wimt):
+   wimt = create_wimt
    wimt.inputs.invert_affine = [1]
    assert wimt.cmdline == 'WarpImageMultiTransform 3 diffusion_weighted.nii diffusion_weighted_wimt.nii -R functional.nii \
 -i func2anat_coreg_Affine.txt func2anat_InverseWarp.nii.gz dwi2anat_Warp.nii.gz dwi2anat_coreg_Affine.txt'
 
 
-def test_WarpImageMultiTransform_invaffine_2(change_dir):
-   wimt = WarpImageMultiTransform()
-   wimt.inputs.input_image = 'diffusion_weighted.nii'
-   wimt.inputs.reference_image = 'functional.nii'
-   wimt.inputs.transformation_series = ['func2anat_coreg_Affine.txt','func2anat_InverseWarp.nii.gz', \
-                                           'dwi2anat_Warp.nii.gz','dwi2anat_coreg_Affine.txt']
+def test_WarpImageMultiTransform_invaffine_2(change_dir, create_wimt):
+   wimt = create_wimt
    wimt.inputs.invert_affine = [2]
    assert wimt.cmdline == 'WarpImageMultiTransform 3 diffusion_weighted.nii diffusion_weighted_wimt.nii -R functional.nii func2anat_coreg_Affine.txt func2anat_InverseWarp.nii.gz dwi2anat_Warp.nii.gz -i dwi2anat_coreg_Affine.txt'
 
 
 @pytest.mark.xfail(reason="dj: should it fail?")
-def test_WarpImageMultiTransform_invaffine_wrong(change_dir):
-   wimt = WarpImageMultiTransform()
-   wimt.inputs.input_image = 'diffusion_weighted.nii'
-   wimt.inputs.reference_image = 'functional.nii'
-   wimt.inputs.transformation_series = ['func2anat_coreg_Affine.txt','func2anat_InverseWarp.nii.gz', \
-                                           'dwi2anat_Warp.nii.gz','dwi2anat_coreg_Affine.txt']
+def test_WarpImageMultiTransform_invaffine_wrong(change_dir, create_wimt):
+   wimt = create_wimt
    wimt.inputs.invert_affine = [3]
    with pytest.raises(Exception):
       assert wimt.cmdline
 
 
-def test_WarpTimeSeriesImageMultiTransform(change_dir):
-   wtsimt = WarpTimeSeriesImageMultiTransform()                                                        
-   wtsimt.inputs.input_image = 'resting.nii'                                                           
-   wtsimt.inputs.reference_image = 'ants_deformed.nii.gz'                                              
-   wtsimt.inputs.transformation_series = ['ants_Warp.nii.gz','ants_Affine.txt']
-   assert wtsimt.cmdline == 'WarpTimeSeriesImageMultiTransform 4 resting.nii resting_wtsimt.nii \
--R ants_deformed.nii.gz ants_Warp.nii.gz ants_Affine.txt'                                                                                            
-
-
-def test_WarpTimeSeriesImageMultiTransform_invaffine(change_dir):
+@pytest.fixture()
+def create_wtsimt():
    wtsimt = WarpTimeSeriesImageMultiTransform()
    wtsimt.inputs.input_image = 'resting.nii'
    wtsimt.inputs.reference_image = 'ants_deformed.nii.gz'
    wtsimt.inputs.transformation_series = ['ants_Warp.nii.gz','ants_Affine.txt']
+   return wtsimt
+
+
+def test_WarpTimeSeriesImageMultiTransform(change_dir, create_wtsimt):
+   wtsimt = create_wtsimt
+   assert wtsimt.cmdline == 'WarpTimeSeriesImageMultiTransform 4 resting.nii resting_wtsimt.nii \
+-R ants_deformed.nii.gz ants_Warp.nii.gz ants_Affine.txt'                                                                                            
+
+
+def test_WarpTimeSeriesImageMultiTransform_invaffine(change_dir, create_wtsimt):
+   wtsimt = create_wtsimt
    wtsimt.inputs.invert_affine = [1]
    assert wtsimt.cmdline == 'WarpTimeSeriesImageMultiTransform 4 resting.nii resting_wtsimt.nii \
 -R ants_deformed.nii.gz ants_Warp.nii.gz -i ants_Affine.txt'
 
 
 @pytest.mark.xfail(reason="dj: should it fail?")
-def test_WarpTimeSeriesImageMultiTransform_invaffine_wrong(change_dir):
-   wtsimt = WarpTimeSeriesImageMultiTransform()
-   wtsimt.inputs.input_image = 'resting.nii'
-   wtsimt.inputs.reference_image = 'ants_deformed.nii.gz'
-   wtsimt.inputs.transformation_series = ['ants_Warp.nii.gz','ants_Affine.txt']
+def test_WarpTimeSeriesImageMultiTransform_invaffine_wrong(change_dir, create_wtsimt):
+   wtsimt = create_wtsimt
    wtsimt.inputs.invert_affine = [0]
    with pytest.raises(Exception):
       wtsimt.cmdline
