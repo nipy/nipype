612ed03632153e80c2c9113200b4e894859251ea
oesteban <code@oscaresteban.es>
2017-11-15 12:40:05 -0800

    fix tests

--- nipype/pipeline/engine/workflows.py
+++ nipype/pipeline/engine/workflows.py
@@ -597,9 +597,10 @@ connected.
             write_workflow_prov(execgraph, prov_base, format='all')
 
         if config.resource_monitor:
+            base_dir = self.base_dir or os.getcwd()
             write_workflow_resources(
                 execgraph,
-                filename=op.join(self.base_dir, self.name, 'resource_monitor.json')
+                filename=op.join(base_dir, self.name, 'resource_monitor.json')
             )
         return execgraph
 
