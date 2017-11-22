d1b65be43a7b2bf44344b0863e6a85b7fb07e2e3
salma1601 <salmabougacha@hotmail.com>
2017-11-22 11:28:53 +0100

    add note option for experts

--- nipype/interfaces/afni/utils.py
+++ nipype/interfaces/afni/utils.py
@@ -2331,7 +2331,8 @@ class UnifizeInputSpec(AFNICommandInputSpec):
         xor=['gm'])
     rbt = traits.Tuple(
         traits.Float(), traits.Float(), traits.Float(),
-        desc='Specify the 3 parameters for the algorithm:\n'
+        desc='Option for AFNI experts only.'
+             'Specify the 3 parameters for the algorithm:\n'
              'R = radius; same as given by option \'-Urad\', [default=18.3]\n'
              'b = bottom percentile of normalizing data range, [default=70.0]\n'
              'r = top percentile of normalizing data range, [default=80.0]\n',
