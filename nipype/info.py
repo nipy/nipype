7dd8e79e9f56dcf5b14c12a5c3115b62d64e930d
mathiasg <mathiasg@mit.edu>
2018-01-25 17:35:38 -0500

    maint: move out of release mode

--- nipype/info.py
+++ nipype/info.py
@@ -11,7 +11,7 @@ import sys
 # full release.  '.dev' as a version_extra string means this is a development
 # version
 # Remove -dev for release
-__version__ = '1.0.0'
+__version__ = '1.0.1-dev'
 
 
 def get_nipype_gitversion():
