3cdb657fcc5025f0093e6d77836eb1c668e2aef6
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-02-01 11:37:41 -0500

    ENH: Reorder traits for better error message

--- nipype/interfaces/spm/preprocess.py
+++ nipype/interfaces/spm/preprocess.py
@@ -125,8 +125,8 @@ class SliceTiming(SPMCommand):
 
 class RealignInputSpec(SPMCommandInputSpec):
     in_files = InputMultiPath(
-        traits.Either(traits.List(ImageFileSPM(exists=True)),
-                      ImageFileSPM(exists=True)),
+        traits.Either(ImageFileSPM(exists=True),
+                      traits.List(ImageFileSPM(exists=True))),
         field='data',
         mandatory=True,
         copyfile=True,
