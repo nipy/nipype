8425ebd4437c118fc624209bec141234ea848c3a
Satrajit Ghosh <satra@mit.edu>
2018-02-02 21:33:45 -0500

    fix: updated custom search to current api

--- doc/conf.py
+++ doc/conf.py
@@ -73,7 +73,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'nipype'
-copyright = u'2009-17, Neuroimaging in Python team'
+copyright = u'2009-18, Neuroimaging in Python team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
