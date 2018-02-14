f9d05f5b16aa5cd022bf9d87d09e13fe0016c513
oesteban <code@oscaresteban.es>
2018-02-13 21:38:16 -0800

    remove -e which should not be there

--- tools/update_changes.sh
+++ tools/update_changes.sh
@@ -9,7 +9,6 @@
 #
 
 # Setting      # $ help set
-set -e         # Exit immediately if a command exits with a non-zero status.
 set -u         # Treat unset variables as an error when substituting.
 set -x         # Print command traces before executing command.
 
