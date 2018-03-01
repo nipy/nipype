ac6c2c9eac3a2c1545d485a767fa6a7f791eaefd
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-03-01 17:21:24 -0500

    FIX: Check for full node name match

--- nipype/pipeline/engine/utils.py
+++ nipype/pipeline/engine/utils.py
@@ -1050,7 +1050,7 @@ def generate_expanded_graph(graph_in):
             expansions = defaultdict(list)
             for node in graph_in.nodes():
                 for src_id in list(old_edge_dict.keys()):
-                    if node.itername.startswith(src_id):
+                    if node.itername.startswith(src_id + '.'):
                         expansions[src_id].append(node)
             for in_id, in_nodes in list(expansions.items()):
                 logger.debug("The join node %s input %s was expanded"
