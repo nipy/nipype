aa994d59a183093679a34cd16ace9072a4366b37
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-03-28 09:43:37 -0400

    MAINT: Add pure Bourne shell retry_cmd.sh

---
+++ tools/retry_cmd.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# retry_cmd.sh [-n NLOOPS] [-s SLEEP] CMD
+#
+# Retry command until success or pre-specified number of failures
+#
+# 2018 Chris Markiewicz
+# Released into public domain
+
+NLOOPS=3
+TOSLEEP=5
+
+while true; do
+    case "$1" in
+        -n ) NLOOPS="$2"; shift 2 ;;
+        -s ) TOSLEEP="$2"; shift 2 ;;
+        -- ) shift; break ;;
+        * ) break ;;
+    esac
+done
+
+RET=0
+for i in `seq $NLOOPS`; do
+    sh -c "$@"
+    RET="$?"
+    if [ "$RET" -eq 0 ]; then break; fi
+    if [ "$i" -ne "$NLOOPS" ]; then sleep $TOSLEEP; fi
+done
+
+exit $RET
