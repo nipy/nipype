6445acc649c6cf1c488ba35d81679877d038493a
Dorota Jarecka <djarecka@gmail.com>
2018-04-13 10:22:55 -0400

    using pytest skipif

--- nipype/tests/test_nipype.py
+++ nipype/tests/test_nipype.py
@@ -1,5 +1,6 @@
 from .. import get_info
 from ..info import get_nipype_gitversion
+import pytest
 
 
 def test_nipype_info():
@@ -11,7 +12,8 @@ def test_nipype_info():
     assert exception_not_raised
 
 
+@pytest.mark.skipif(not get_nipype_gitversion(), 
+                    reason="not able to get version from get_nipype_gitversion")
 def test_git_hash():
-    if get_nipype_gitversion():
-        # removing the first "g" from gitversion
-        get_nipype_gitversion()[1:] == get_info()['commit_hash']
+    # removing the first "g" from gitversion
+    get_nipype_gitversion()[1:] == get_info()['commit_hash']
