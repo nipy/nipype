6e53f016f819bd6330fa63f7d9002cd6c565432b
Dorota Jarecka <djarecka@gmail.com>
2018-04-17 15:18:56 -0400

    adding tests that checks pos/neg for normal distribution

--- nipype/algorithms/tests/test_stats.py
+++ nipype/algorithms/tests/test_stats.py
@@ -5,6 +5,7 @@
 import numpy as np
 import nibabel as nb
 from nipype.algorithms.stats import ActivationCount
+import pytest
 
 
 def test_ActivationCount(tmpdir):
@@ -20,3 +21,23 @@ def test_ActivationCount(tmpdir):
     pos = nb.load(res.outputs.acm_pos)
     neg = nb.load(res.outputs.acm_neg)
     assert np.allclose(diff.get_data(), pos.get_data() - neg.get_data())
+
+
+@pytest.mark.parametrize("threshold, above_thresh", [
+        (1, 15.865), # above one standard deviation (one side)
+        (2, 2.275),  # above two standard deviations (one side)
+        (3, 0.135)   # above three standard deviations (one side)
+        ])
+def test_ActivationCount_normaldistr(tmpdir, threshold, above_thresh):
+    tmpdir.chdir()
+    in_files = ['{:d}.nii'.format(i) for i in range(3)]
+    for fname in in_files:
+        nb.Nifti1Image(np.random.normal(size=(50, 50, 50)),
+                       np.eye(4)).to_filename(fname)
+
+    acm = ActivationCount(in_files=in_files, threshold=threshold)
+    res = acm.run()
+    pos = nb.load(res.outputs.acm_pos)
+    neg = nb.load(res.outputs.acm_neg)
+    assert np.isclose(pos.get_data().mean(), above_thresh*1.e-2, rtol=0.1, atol=1.e-6)
+    assert np.isclose(neg.get_data().mean(), above_thresh*1.e-2, rtol=0.1, atol=1.e-6)
