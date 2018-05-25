9e61ec7569d53cf45adc31fe21e4583d732fa44c
oesteban <code@oscaresteban.es>
2018-05-25 15:17:14 -0700

    add documentation. fix unused variable. fix PEP8 errors

--- nipype/interfaces/workbench/metric.py
+++ nipype/interfaces/workbench/metric.py
@@ -6,12 +6,12 @@ from __future__ import (print_function, division, unicode_literals,
                         absolute_import)
 import os
 
-from ..base import (TraitedSpec, File, traits, InputMultiObject,
-                    CommandLineInputSpec)
+from ..base import (TraitedSpec, File, traits, CommandLineInputSpec)
 from .base import WBCommand
 from ... import logging
 
 iflogger = logging.getLogger('interface')
+
 
 class MetricResampleInputSpec(CommandLineInputSpec):
     in_file = File(
@@ -96,6 +96,24 @@ class MetricResample(WBCommand):
     """
     Resample a metric file to a different mesh
 
+    Resamples a metric file, given two spherical surfaces that are in
+    register.  If ``ADAP_BARY_AREA`` is used, exactly one of -area-surfs or
+    ``-area-metrics`` must be specified.
+
+    The ``ADAP_BARY_AREA`` method is recommended for ordinary metric data,
+    because it should use all data while downsampling, unlike ``BARYCENTRIC``.
+    The recommended areas option for most data is individual midthicknesses
+    for individual data, and averaged vertex area metrics from individual
+    midthicknesses for group average data.
+
+    The ``-current-roi`` option only masks the input, the output may be slightly
+    dilated in comparison, consider using ``-metric-mask`` on the output when
+    using ``-current-roi``.
+
+    The ``-largest option`` results in nearest vertex behavior when used with
+    ``BARYCENTRIC``.  When resampling a binary metric, consider thresholding at
+    0.5 after resampling rather than using ``-largest``.
+
     >>> from nipype.interfaces.workbench import MetricResample
     >>> metres = MetricResample()
     >>> metres.inputs.in_file = 'sub-01_task-rest_bold_space-fsaverage5.L.func.gii'
@@ -139,5 +157,5 @@ class MetricResample(WBCommand):
         outputs = super(MetricResample, self)._list_outputs()
         if self.inputs.valid_roi_out:
             roi_file = self._gen_filename(self.inputs.in_file, suffix='_roi')
-            outputs['roi_file'] = os.path.abspath(roi)
+            outputs['roi_file'] = os.path.abspath(roi_file)
         return outputs
