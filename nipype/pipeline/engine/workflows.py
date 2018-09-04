nipy/nipype

commit f1d603ee377c05bafde09b6e6adc522b843345cd

adding index_generator to the state class

nipype/pipeline/engine/workflows.py
@@ -20,7 +20,7 @@ import shutil
 
 import numpy as np
 import networkx as nx
-import itertools, collections
+import collections
 
 from ... import config, logging
 from ...exceptions import NodeError, WorkflowError, MappingError, JoinError
@@ -1262,7 +1262,7 @@ class NewNode(NewBase):
     # dj: version without join
     def _check_all_results(self):
         # checking if all files that should be created are present
-        for ind in itertools.product(*self.state._all_elements):
+        for ind in self.state.index_generator:
             state_dict = self.state.state_values(ind)
             dir_nm_el = "_".join(["{}.{}".format(i, j) for i, j in list(state_dict.items())])
             for key_out in self._out_nm:

nipype/pipeline/engine/tests/test_newnode.py
@@ -8,7 +8,7 @@ import pytest, pdb
 python35_only = pytest.mark.skipif(sys.version_info < (3, 5),
                                    reason="requires Python>3.4")
 
-Plugins = ["mp", "serial", "cf", "dask"]
+Plugins = ["serial"]#, "mp", ""cf", "dask"]
 
 def fun_addtwo(a):
     time.sleep(3)

nipype/pipeline/engine/state.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+import itertools
 import pdb
 
 from . import auxiliary as aux
@@ -40,17 +41,14 @@ class State(object):
 
         # list of all possible indexes in each dim, will be use to iterate
         # e.g. [[0, 1], [0, 1, 2]]
-        self._all_elements = [range(i) for i in self._shape]
+        self.all_elements = [range(i) for i in self._shape]
+        self.index_generator = itertools.product(*self.all_elements)
 
 
     def __getitem__(self, key):
         if type(key) is int:
             key = (key,)
         return self.state_values(key)
-
-    @property
-    def all_elements(self):
-        return self._all_elements
 
     # not used?
     #@property

nipype/pipeline/engine/submitter.py
@@ -6,8 +6,6 @@ from future import standard_library
 standard_library.install_aliases()
 
 import os, pdb, time, glob
-import itertools, collections
-import queue
 
 from .workers import MpWorker, SerialWorker, DaskWorker, ConcurrentFuturesWorker
 
@@ -31,7 +29,7 @@ class Submitter(object):
 
 
     def submit_work(self, node):
-        for (i, ind) in enumerate(itertools.product(*node.state.all_elements)):
+        for (i, ind) in enumerate(node.state.index_generator):
             self._submit_work_el(node, i, ind)
 
     def _submit_work_el(self, node, i, ind):
@@ -82,7 +80,7 @@ class SubmitterWorkflow(Submitter):
         # iterating over all elements
         # (i think ordered list work well here, since it's more efficient to check within a specific order)
         for nn in list(self.graph)[i_n:]:
-            for (i, ind) in enumerate(itertools.product(*nn.state.all_elements)):
+            for (i, ind) in enumerate(nn.state.index_generator):
                 self.node_line.append((nn, i, ind))
 
         # this parts submits nodes that are waiting to be run

commit dfe7bd5396cc051d388454a17cd8838fabee2ed5

[skip ci] fixing _check_all_results (bug introduced in f1d603e, but worked for serial plugin anyway)

nipype/pipeline/engine/workflows.py
@@ -20,7 +20,7 @@ import shutil
 
 import numpy as np
 import networkx as nx
-import collections
+import collections, itertools
 
 from ... import config, logging
 from ...exceptions import NodeError, WorkflowError, MappingError, JoinError
@@ -1282,9 +1282,9 @@ class NewNode(NewBase):
     # dj: version without join
     def _check_all_results(self):
         # checking if all files that should be created are present
-        for ind in self.state.index_generator:
+        for ind in itertools.product(*self.state.all_elements):
             state_dict = self.state.state_values(ind)
-            dir_nm_el = "_".join(["{}.{}".format(i, j) for i, j in list(state_dict.items())])
+            dir_nm_el = "_".join(["{}:{}".format(i, j) for i, j in list(state_dict.items())])
             for key_out in self._out_nm:
                 if not os.path.isfile(os.path.join(self.nodedir, dir_nm_el, key_out+".txt")):
                     return False

nipype/pipeline/engine/tests/test_newnode.py
@@ -8,7 +8,7 @@ import pytest, pdb
 python35_only = pytest.mark.skipif(sys.version_info < (3, 5),
                                    reason="requires Python>3.4")
 
-Plugins = ["serial"]#, "mp", ""cf", "dask"]
+Plugins = ["serial", "mp", "cf", "dask"]
 
 def fun_addtwo(a):
     time.sleep(3)