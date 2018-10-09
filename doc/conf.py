d584b3fafe97b4b1a52216d0d04872a65746027a
miykael <michaelnotter@hotmail.com>
2018-10-09 18:51:34 -0400

    FIX: corrects readthedocs build error

--- doc/conf.py
+++ doc/conf.py
@@ -50,7 +50,7 @@ extensions = ['sphinx.ext.todo',
               'sphinx.ext.autosummary',
               'numpydoc',
               'matplotlib.sphinxext.plot_directive',
-              'matplotlib.sphinxext.only_directives',
+              #'matplotlib.sphinxext.only_directives',
               'nipype.sphinxext.plot_workflow',
               #'IPython.sphinxext.ipython_directive',
               #'IPython.sphinxext.ipython_console_highlighting'
