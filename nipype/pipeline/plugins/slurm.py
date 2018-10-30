2bd8f926bad39beab14c30ab3dfc4fc040cb836c
mathiasg <mathiasg@mit.edu>
2018-10-30 17:06:37 -0400

    fix: keep old logger

--- nipype/pipeline/plugins/slurm.py
+++ nipype/pipeline/plugins/slurm.py
@@ -17,7 +17,7 @@ from ... import logging
 from ...interfaces.base import CommandLine
 from .base import SGELikeBatchManagerBase, logger
 
-iflogger = logging.getLogger('nipype.workflow')
+iflogger = logging.getLogger('nipype.interface')
 
 
 class SLURMPlugin(SGELikeBatchManagerBase):
