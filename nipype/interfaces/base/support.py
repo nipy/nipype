7b085074a7c7bc47b52303981822a6054975205f
oesteban <code@oscaresteban.es>
2018-11-26 18:23:27 -0800

    fix tests

--- nipype/interfaces/base/support.py
+++ nipype/interfaces/base/support.py
@@ -9,32 +9,63 @@ Miscellaneous tools to support Interface functionality
 """
 from __future__ import (print_function, division, unicode_literals,
                         absolute_import)
-from builtins import object, str
+from builtins import object
 
-import os
-from copy import deepcopy
-
-from ... import logging, __version__
-from ...utils.misc import is_container
-from ...utils.filemanip import md5, to_str, hash_infile
-iflogger = logging.getLogger('nipype.interface')
+from ... import __version__
 
 
-class NipypeInterfaceError(Exception):
+class NipypeInterfaceError(RuntimeError):
     """Custom error for interfaces"""
-
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return '{}'.format(self.value)
+    pass
 
 
 class InterfaceRuntime(object):
-    """
+    """A class to store runtime details of interfaces
+
+    Elements can be set at initialization:
+
+    >>> rt = InterfaceRuntime(cmdline='/bin/echo', returncode=0)
+    >>> rt.returncode == 0
+    True
+
+    Unset elements are initialized to ``None``:
+
+    >>> rt.cwd is None
+    True
+
+    And any element can be set by attribute:
+
+    >>> rt.cwd = '/scratch/workflow'
+    >>> rt.cwd
+    '/scratch/workflow'
+
+    The structure raises ``AttributeError`` if trying to accessing
+    an invalid attribute:
+
+    >>> rt.nonruntime
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'InterfaceRuntime' object has no attribute 'nonruntime'
+
+
+    For Nipype to work, it is fundamental that runtime objects
+    can be pickled/unpickled:
+
+    >>> import pickle
+    >>> pickleds = pickle.dumps(rt)
+    >>> newrt = pickle.loads(pickleds)
+    >>> newrt == rt
+    True
+
+    >>> newrt != rt
+    False
+
+    >>> newrt
+    Bunch(cmdline='/bin/echo', cwd='/scratch/workflow', returncode=0)
 
     """
-    __slots__ = [
+
+    __slots__ = sorted([
         'cwd',
         'prevcwd',
         'returncode',
@@ -56,13 +87,18 @@ class InterfaceRuntime(object):
         'merged',
         'command_path',
         'dependencies',
-    ]
+    ])
 
     def __init__(self, **inputs):
-        for key in self.__class__.__slots__:
-            setattr(self, key, inputs.get(key, None))
+        self.__setstate__(inputs)
 
-    def dictcopy(self):
+    def __setstate__(self, state):
+        """Necessary for un-pickling"""
+        for key in self.__class__.__slots__:
+            setattr(self, key, state.get(key, None))
+
+    def __getstate__(self):
+        """Necessary for pickling"""
         outdict = {}
         for key in self.__class__.__slots__:
             value = getattr(self, key, None)
@@ -70,11 +106,73 @@ class InterfaceRuntime(object):
                 outdict[key] = value
         return outdict
 
+    def dictcopy(self):
+        """
+        Returns a dictionary of set attributes (backward compatibility)
+
+        >>> rt = InterfaceRuntime()
+        >>> rt.dictcopy()
+        {}
+
+        >>> rt = InterfaceRuntime(cmdline='/bin/echo', returncode=0)
+        >>> rt.dictcopy()
+        {'cmdline': '/bin/echo', 'returncode': 0}
+
+        """
+        return self.__getstate__()
+
     def items(self):
+        """Provide an interface for items
+
+        >>> rt = InterfaceRuntime()
+        >>> list(rt.items())
+        []
+
+        >>> rt = InterfaceRuntime(cmdline='/bin/echo', returncode=0)
+        >>> list(rt.items())
+        [('cmdline', '/bin/echo'), ('returncode', 0)]
+
+
+        """
         for key in self.__class__.__slots__:
             value = getattr(self, key, None)
             if value is not None:
                 yield (key, value)
+
+    def __repr__(self):
+        """representation of the runtime object"""
+        outstr = ['Bunch(']
+        first = True
+        for k, v in sorted(self.items()):
+            if not first:
+                outstr.append(', ')
+            if isinstance(v, dict):
+                pairs = []
+                for key, value in sorted(v.items()):
+                    pairs.append("'%s': %s" % (key, value))
+                v = '{' + ', '.join(pairs) + '}'
+                outstr.append('%s=%s' % (k, v))
+            else:
+                outstr.append('%s=%r' % (k, v))
+            first = False
+        outstr.append(')')
+        return ''.join(outstr)
+
+    def __str__(self):
+        return '%s' % self.__getstate__()
+
+    def __eq__(self, other):
+        """Overrides the default implementation"""
+        if isinstance(other, InterfaceRuntime):
+            return self.__getstate__() == other.__getstate__()
+        return NotImplemented
+
+    def __ne__(self, other):
+        """Overrides the default implementation (Python 2)"""
+        x = self.__eq__(other)
+        if x is not NotImplemented:
+            return not x
+        return NotImplemented
 
 
 class InterfaceResult(object):
