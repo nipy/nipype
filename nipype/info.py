b75bf7cc13301b7732565cc3a430e28098b69170
Chris Markiewicz <effigies@gmail.com>
2019-01-22 10:44:53 -0600

    Update nipype/info.py
    Co-Authored-By: adelavega <aleph4@gmail.com>

--- nipype/info.py
+++ nipype/info.py
@@ -176,7 +176,7 @@ EXTRA_REQUIRES = {
     'profiler': ['psutil>=5.0'],
     'duecredit': ['duecredit'],
     'xvfbwrapper': ['xvfbwrapper'],
-    'pybids': ['pybids==0.7.0'],
+    'pybids': ['pybids>=0.7.0'],
     'ssh': ['paramiko'],
     # 'mesh': ['mayavi']  # Enable when it works
 }
