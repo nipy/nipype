d3a15e94592d2e157bd9fefd209d889632203b58
Steven Tilley <stilley@hollandbloorview.ca>
2019-09-05 17:17:01 -0400

    test versioning in loadpkl

--- nipype/tests/test_utils.py
+++ nipype/tests/test_utils.py
@@ -2,10 +2,11 @@ from nipype import utils
 import pytest
 
 
-@pytest.mark.parametrize("versioning", [True, False])
-def test_pickle(tmp_path, versioning):
+@pytest.mark.parametrize("load_versioning", [True, False])
+@pytest.mark.parametrize("save_versioning", [True, False])
+def test_pickle(tmp_path, save_versioning, load_versioning):
     testobj = 'iamateststr'
     pickle_fname = str(tmp_path / 'testpickle.pklz')
-    utils.filemanip.savepkl(pickle_fname, testobj, versioning=versioning)
-    outobj = utils.filemanip.loadpkl(pickle_fname)
+    utils.filemanip.savepkl(pickle_fname, testobj, versioning=save_versioning)
+    outobj = utils.filemanip.loadpkl(pickle_fname, versioning=load_versioning)
     assert outobj == testobj
