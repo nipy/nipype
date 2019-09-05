c6047defb99987ad0f1fe1a32f34e7d281c4a767
Steven Tilley <stilley@hollandbloorview.ca>
2019-09-05 16:31:58 -0400

    parametrize test (suggested by @effigies)

--- nipype/tests/test_utils.py
+++ nipype/tests/test_utils.py
@@ -1,17 +1,11 @@
 from nipype import utils
+import pytest
 
 
-def test_pickle(tmp_path):
+@pytest.mark.parametrize("versioning", [True, False])
+def test_pickle(tmp_path, versioning):
     testobj = 'iamateststr'
     pickle_fname = str(tmp_path / 'testpickle.pklz')
-    utils.filemanip.savepkl(pickle_fname, testobj)
-    outobj = utils.filemanip.loadpkl(pickle_fname)
-    assert outobj == testobj
-
-
-def test_pickle_versioning(tmp_path):
-    testobj = 'iamateststr'
-    pickle_fname = str(tmp_path / 'testpickle.pklz')
-    utils.filemanip.savepkl(pickle_fname, testobj, versioning=True)
+    utils.filemanip.savepkl(pickle_fname, testobj, versioning=versioning)
     outobj = utils.filemanip.loadpkl(pickle_fname)
     assert outobj == testobj
