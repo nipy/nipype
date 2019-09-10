49ada18b729690a19f852cb9d65fd7a086c1a229
oesteban <code@oscaresteban.es>
2019-09-10 09:38:42 -0700

    fix: amend tests to work on py35 and add mobility test

--- nipype/pipeline/engine/tests/test_utils.py
+++ nipype/pipeline/engine/tests/test_utils.py
@@ -16,7 +16,8 @@ from ... import engine as pe
 from ....interfaces import base as nib
 from ....interfaces import utility as niu
 from .... import config
-from ..utils import clean_working_directory, write_workflow_prov, load_resultfile
+from ..utils import (clean_working_directory, write_workflow_prov,
+                     save_resultfile, load_resultfile)
 
 
 class InputSpec(nib.TraitedSpec):
@@ -286,14 +287,31 @@ def test_modify_paths_bug(tmpdir):
 
 
 def test_save_load_resultfile(tmpdir):
+    """Test minimally the save/load functions for result files."""
+    from shutil import copytree
     tmpdir.chdir()
 
     spc = pe.Node(StrPathConfuser(in_str='2'), name='spc')
     spc.base_dir = tmpdir.mkdir('node').strpath
     result = spc.run()
 
-    loaded_result = load_resultfile(tmpdir.join('node').join('spc').join('result_spc.pklz'))
+    loaded_result = load_resultfile(
+        tmpdir.join('node').join('spc').join('result_spc.pklz').strpath)
 
     assert result.runtime.dictcopy() == loaded_result.runtime.dictcopy()
     assert result.inputs == loaded_result.inputs
     assert result.outputs.get() == loaded_result.outputs.get()
+
+    # Test the mobility of the result file.
+    copytree(tmpdir.join('node').strpath, tmpdir.join('node2').strpath)
+    save_resultfile(result, tmpdir.join('node2').strpath, 'spc', rebase=True)
+    loaded_result2 = load_resultfile(
+        tmpdir.join('node2').join('spc').join('result_spc.pklz').strpath)
+
+    assert result.runtime.dictcopy() == loaded_result2.runtime.dictcopy()
+    assert result.inputs == loaded_result2.inputs
+    assert loaded_result2.outputs.get() != result.outputs.get()
+    newpath = result.outputs.out_path.replace('/node/', '/node2/')
+    assert loaded_result2.outputs.out_path == newpath
+    assert loaded_result2.outputs.out_tuple[0] == newpath
+    assert loaded_result2.outputs.out_dict_path['2'] == newpath
