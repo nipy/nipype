75205449908a19d45c99bd2dabb915c3596ecb21
mjoseph <josephmje.22@gmail.com>
2019-10-25 15:06:21 -0400

    revert enum to bool

--- nipype/interfaces/fsl/epi.py
+++ nipype/interfaces/fsl/epi.py
@@ -677,8 +677,8 @@ class EddyInputSpec(FSLCommandInputSpec):
         desc=('Final resampling method (jacobian/least '
               'squares)'))
 
-    repol = traits.Enum(
-        True, argstr='--repol', desc='Detect and replace outlier slices')
+    repol = traits.Bool(
+        False, argstr='--repol', desc='Detect and replace outlier slices')
     outlier_nstd = traits.Int(
         argstr='--ol_nstd',
         desc='Number of std off to qualify as outlier',
@@ -760,8 +760,8 @@ class EddyInputSpec(FSLCommandInputSpec):
         xor=['slice_order'],
         min_ver='6.0.1')
 
-    estimate_move_by_susceptibility = traits.Enum(
-        True,
+    estimate_move_by_susceptibility = traits.Bool(
+        False,
         argstr='--estimate_move_by_susceptibility',
         desc='Estimate how susceptibility field changes with subject movement',
         min_ver='6.0.1')
@@ -793,7 +793,7 @@ class EddyInputSpec(FSLCommandInputSpec):
               'date are acquired on a set of b-value '
               'shells'))
 
-    use_cuda = traits.Enum(True, desc='Run eddy using cuda gpu')
+    use_cuda = traits.Bool(False, desc='Run eddy using cuda gpu')
     cnr_maps = traits.Bool(
         False, desc="Output CNR-Maps", argstr="--cnr_maps", min_ver="5.0.10"
     )
