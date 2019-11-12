39fbd5411a844ce7c023964d3295eb7643b95af5
Christopher J. Markiewicz <markiewicz@stanford.edu>
2019-11-12 10:38:32 -0500

    FIX: Missed merge issue

--- nipype/interfaces/tests/test_io.py
+++ nipype/interfaces/tests/test_io.py
@@ -15,7 +15,7 @@ import nipype
 import nipype.interfaces.io as nio
 from nipype.interfaces.base.traits_extension import isdefined
 from nipype.interfaces.base import Undefined, TraitError
-from nipype.utils.filemanip import dist_is_editable, FileExistsError
+from nipype.utils.filemanip import dist_is_editable
 
 # Check for boto
 noboto = False
