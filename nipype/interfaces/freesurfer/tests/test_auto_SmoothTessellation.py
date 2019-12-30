6de90c57af3a98c35007ac875b236b03666f5c3d
oesteban <code@oscaresteban.es>
2019-12-29 16:58:01 -0800

    fix: add modified specs

--- nipype/interfaces/freesurfer/tests/test_auto_SmoothTessellation.py
+++ nipype/interfaces/freesurfer/tests/test_auto_SmoothTessellation.py
@@ -4,25 +4,25 @@ from ..utils import SmoothTessellation
 
 def test_SmoothTessellation_inputs():
     input_map = dict(
-        args=dict(argstr="%s",),
-        curvature_averaging_iterations=dict(argstr="-a %d",),
-        disable_estimates=dict(argstr="-nw",),
-        environ=dict(nohash=True, usedefault=True,),
-        gaussian_curvature_norm_steps=dict(argstr="%d ",),
-        gaussian_curvature_smoothing_steps=dict(argstr="%d",),
+        args=dict(argstr="%s"),
+        curvature_averaging_iterations=dict(argstr="-a %d"),
+        disable_estimates=dict(argstr="-nw"),
+        environ=dict(nohash=True, usedefault=True),
+        gaussian_curvature_norm_steps=dict(argstr="%d"),
+        gaussian_curvature_smoothing_steps=dict(argstr=" %d"),
         in_file=dict(
-            argstr="%s", copyfile=True, extensions=None, mandatory=True, position=-2,
+            argstr="%s", copyfile=True, extensions=None, mandatory=True, position=-2
         ),
-        normalize_area=dict(argstr="-area",),
-        out_area_file=dict(argstr="-b %s", extensions=None,),
-        out_curvature_file=dict(argstr="-c %s", extensions=None,),
-        out_file=dict(argstr="%s", extensions=None, genfile=True, position=-1,),
-        seed=dict(argstr="-seed %d",),
-        smoothing_iterations=dict(argstr="-n %d",),
-        snapshot_writing_iterations=dict(argstr="-w %d",),
+        normalize_area=dict(argstr="-area"),
+        out_area_file=dict(argstr="-b %s", extensions=None),
+        out_curvature_file=dict(argstr="-c %s", extensions=None),
+        out_file=dict(argstr="%s", extensions=None, genfile=True, position=-1),
+        seed=dict(argstr="-seed %d"),
+        smoothing_iterations=dict(argstr="-n %d"),
+        snapshot_writing_iterations=dict(argstr="-w %d"),
         subjects_dir=dict(),
-        use_gaussian_curvature_smoothing=dict(argstr="-g",),
-        use_momentum=dict(argstr="-m",),
+        use_gaussian_curvature_smoothing=dict(argstr="-g"),
+        use_momentum=dict(argstr="-m"),
     )
     inputs = SmoothTessellation.input_spec()
 
@@ -32,7 +32,7 @@ def test_SmoothTessellation_inputs():
 
 
 def test_SmoothTessellation_outputs():
-    output_map = dict(surface=dict(extensions=None,),)
+    output_map = dict(surface=dict(extensions=None))
     outputs = SmoothTessellation.output_spec()
 
     for key, metadata in list(output_map.items()):
