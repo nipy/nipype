312ae3d5395ee9f550a70076a9979bb65e4e154a
mathiasg <mathiasg@stanford.edu>
2020-02-23 22:49:49 -0500

    ENH: add testcase

--- nipype/interfaces/freesurfer/tests/test_utils.py
+++ nipype/interfaces/freesurfer/tests/test_utils.py
@@ -230,3 +230,19 @@ def test_mrisexpand(tmpdir):
     assert op.dirname(if_out_file) == op.dirname(fsavginfo["smoothwm"])
     # Node places output in working directory
     assert op.dirname(nd_out_file) == nd_res.runtime.cwd
+
+
+@pytest.mark.skipif(fs.no_freesurfer(), reason="freesurfer is not installed")
+def test_eulernumber(tmpdir):
+    # grab a surface from fsaverage
+    fssrc = FreeSurferSource(
+        subjects_dir=fs.Info.subjectsdir(), subject_id="fsaverage", hemi="lh"
+    )
+    pial = fssrc.run().outputs.pial
+    assert isinstance(pial, str), "Problem when fetching surface file"
+
+    eu = fs.EulerNumber()
+    eu.inputs.in_file = pial
+    res = eu.run()
+    assert res.outputs.defects == 0
+    assert res.outputs.euler == 2
