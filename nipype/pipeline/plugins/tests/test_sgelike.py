ce22e364266d814fdeba5110860248f6d96fa534
Daniel Ge <3453485+daniel-ge@users.noreply.github.com>
2020-03-08 14:26:14 +0100

    Update nipype/pipeline/plugins/tests/test_sgelike.py
    Co-Authored-By: Chris Markiewicz <effigies@gmail.com>

--- nipype/pipeline/plugins/tests/test_sgelike.py
+++ nipype/pipeline/plugins/tests/test_sgelike.py
@@ -27,7 +27,7 @@ def is_pending(self, taskid):
 @patch.object(SGELikeBatchManagerBase, '_submit_batchtask', new=submit_batchtask)
 @patch.object(SGELikeBatchManagerBase, '_is_pending', new=is_pending)
 def test_crashfile_creation(tmp_path):
-        pipe = pe.Workflow(name="pipe", base_dir=tmpdirname)
+        pipe = pe.Workflow(name="pipe", base_dir=str(tmp_path))
         pipe.config["execution"]["crashdump_dir"] = tmpdirname
         pipe.add_nodes([pe.Node(interface=Function(function=crasher), 
                             name="crasher")])    
