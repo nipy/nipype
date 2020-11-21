eef368740a61f26545c868438f94ed25a1e1b171
Chris Markiewicz <markiewicz@stanford.edu>
2020-11-21 12:13:30 -0500

    TEST: Verify behavior when an input has a default and max_ver

--- nipype/interfaces/base/tests/test_core.py
+++ nipype/interfaces/base/tests/test_core.py
@@ -272,6 +272,46 @@ def test_input_version_missing_error():
             obj._check_version_requirements(obj.inputs)
 
 
+def test_unavailable_input():
+    class WithInput(nib.BaseInterface):
+        class input_spec(nib.TraitedSpec):
+            foo = nib.traits.Int(3, usedefault=True, max_ver="0.5")
+
+        _version = "0.4"
+
+        def _run_interface(self, runtime):
+            return runtime
+
+    class WithoutInput(WithInput):
+        _version = "0.6"
+
+    has = WithInput()
+    hasnt = WithoutInput()
+    trying_anyway = WithoutInput(foo=3)
+    assert has.inputs.foo == 3
+    assert not nib.isdefined(hasnt.inputs.foo)
+    assert trying_anyway.inputs.foo == 3
+
+    has.run()
+    hasnt.run()
+    with pytest.raises(Exception):
+        trying_anyway.run()
+
+    # Still settable
+    has.inputs.foo = 4
+    hasnt.inputs.foo = 4
+    trying_anyway.inputs.foo = 4
+    assert has.inputs.foo == 4
+    assert hasnt.inputs.foo == 4
+    assert trying_anyway.inputs.foo == 4
+
+    has.run()
+    with pytest.raises(Exception):
+        hasnt.run()
+    with pytest.raises(Exception):
+        trying_anyway.run()
+
+
 def test_output_version():
     class InputSpec(nib.TraitedSpec):
         foo = nib.traits.Int(desc="a random int")
