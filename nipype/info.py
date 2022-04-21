5dbbd656cd8734c584770e5d488007d75696eb3e
Christopher J. Markiewicz <markiewicz@stanford.edu>
2022-04-21 11:26:03 -0400

    MNT: Set minimum Python to 3.7

--- nipype/info.py
+++ nipype/info.py
@@ -54,14 +54,13 @@ CLASSIFIERS = [
     "License :: OSI Approved :: Apache Software License",
     "Operating System :: MacOS :: MacOS X",
     "Operating System :: POSIX :: Linux",
-    "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
     "Topic :: Scientific/Engineering",
 ]
-PYTHON_REQUIRES = ">= 3.6"
+PYTHON_REQUIRES = ">= 3.7"
 
 description = "Neuroimaging in Python: Pipelines and Interfaces"
 
