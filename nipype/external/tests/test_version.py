f0fb21d6a871d44236ba4cd7e1dbedb28569e4b7
Christopher J. Markiewicz <markiewicz@stanford.edu>
2022-05-11 12:57:38 -0400

    TEST: Catch warnings... apparently they double dip

--- nipype/external/tests/test_version.py
+++ nipype/external/tests/test_version.py
@@ -15,7 +15,9 @@ with warnings.catch_warnings():
 @pytest.mark.parametrize("v1, v2", [("0.0.0", "0.0.0"), ("0.0.0", "1.0.0")])
 def test_LooseVersion_compat(v1, v2):
     vend1, vend2 = Vendored(v1), Vendored(v2)
-    orig1, orig2 = Original(v1), Original(v2)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        orig1, orig2 = Original(v1), Original(v2)
 
     assert vend1 == orig1
     assert orig1 == vend1
