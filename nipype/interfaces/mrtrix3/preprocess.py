e726ac4441e6e88586e5e1711d0c4c72b766279a
GalBenZvi <hershkovitz1@mail.tau.ac.il>
2022-05-13 13:35:56 +0300

    updated docstrings to match the new configurations

--- nipype/interfaces/mrtrix3/preprocess.py
+++ nipype/interfaces/mrtrix3/preprocess.py
@@ -192,9 +192,7 @@ class DWIBiasCorrectInputSpec(MRTrix3BaseInputSpec):
         mandatory=True,
         desc="input DWI image",
     )
-    in_mask = File(
-        argstr="-mask %s", desc="input mask image for bias field estimation"
-    )
+    in_mask = File(argstr="-mask %s", desc="input mask image for bias field estimation")
     use_ants = traits.Bool(
         argstr="ants",
         mandatory=True,
@@ -386,7 +384,7 @@ class DWIPreproc(MRTrix3Base):
     >>> preproc.inputs.rpe_options = 'none'
     >>> preproc.inputs.out_file = "preproc.mif"
     >>> preproc.inputs.eddy_options = '--slm=linear --repol'     # linear second level model and replace outliers
-    >>> preproc.inputs.export_grad_mrtrix = True    # export final gradient table in MRtrix format
+    >>> preproc.inputs.out_grad_mrtrix = "grad.b"    # export final gradient table in MRtrix format
     >>> preproc.inputs.ro_time = 0.165240   # 'TotalReadoutTime' in BIDS JSON metadata files
     >>> preproc.inputs.pe_dir = 'j'     # 'PhaseEncodingDirection' in BIDS JSON metadata files
     >>> preproc.cmdline
@@ -402,9 +400,7 @@ class DWIPreproc(MRTrix3Base):
         outputs = self.output_spec().get()
         outputs["out_file"] = op.abspath(self.inputs.out_file)
         if self.inputs.export_grad_mrtrix:
-            outputs["out_grad_mrtrix"] = op.abspath(
-                self.inputs.out_grad_mrtrix
-            )
+            outputs["out_grad_mrtrix"] = op.abspath(self.inputs.out_grad_mrtrix)
         if self.inputs.export_grad_fsl:
             outputs["out_fsl_bvec"] = op.abspath(self.inputs.out_grad_fsl[0])
             outputs["out_fsl_bval"] = op.abspath(self.inputs.out_grad_fsl[1])
@@ -438,15 +434,9 @@ class ResponseSDInputSpec(MRTrix3BaseInputSpec):
         usedefault=True,
         desc="output WM response text file",
     )
-    gm_file = File(
-        argstr="%s", position=-2, desc="output GM response text file"
-    )
-    csf_file = File(
-        argstr="%s", position=-1, desc="output CSF response text file"
-    )
-    in_mask = File(
-        exists=True, argstr="-mask %s", desc="provide initial mask image"
-    )
+    gm_file = File(argstr="%s", position=-2, desc="output GM response text file")
+    csf_file = File(argstr="%s", position=-1, desc="output CSF response text file")
+    in_mask = File(exists=True, argstr="-mask %s", desc="provide initial mask image")
     max_sh = InputMultiObject(
         traits.Int,
         argstr="-lmax %s",
