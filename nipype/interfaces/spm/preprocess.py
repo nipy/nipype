0a8316de4174641219518b4f4f52ecd9f814cbd0
fabioboh <fb.brnrdn@gmail.com>
2022-09-05 15:06:15 +0200

    FIX: Update ApplyVDM to handle 4D inputs (#3500)
    * extend apply VDM functionality to imaging data containing multiple frames (e.g. fMRI)
    
    Authored-by: Fabio Bernardoni <fabio.bernardoni@uniklinikum-dresden.de>

--- nipype/interfaces/spm/preprocess.py
+++ nipype/interfaces/spm/preprocess.py
@@ -337,7 +337,11 @@ class ApplyVDM(SPMCommand):
     def _format_arg(self, opt, spec, val):
         """Convert input to appropriate format for spm"""
 
-        if opt in ["in_files", "vdmfile"]:
+        if opt == 'in_files':
+            return scans_for_fnames(
+                ensure_list(val), keep4d=False, separate_sessions=False
+            )
+        if opt == 'vdmfile':
             return scans_for_fname(ensure_list(val))
         return super(ApplyVDM, self)._format_arg(opt, spec, val)
 
