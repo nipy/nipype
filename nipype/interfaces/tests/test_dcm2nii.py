73019f23c84d16b388b5f344e3cc5acb58d64a87
mauriliogenovese <mauriliogenovese@gmail.com>
2024-03-21 17:11:05 +0100

    Update test_dcm2nii.py

--- nipype/interfaces/tests/test_dcm2nii.py
+++ nipype/interfaces/tests/test_dcm2nii.py
@@ -5,17 +5,26 @@ from nipype.interfaces import dcm2nii
 
 
 @pytest.mark.parametrize(
-    "fname, extension",
+    "fname, extension, search_crop",
     [
-        ("output_1", ".txt"),
-        ("output_w_[]_meta_1", ".json"),
-        ("output_w_**^$?_meta_2", ".txt"),
+        ("output_1", ".txt", False),
+        ("output_w_[]_meta_1", ".json", False),
+        ("output_w_**^$?_meta_2", ".txt", False),
+        ("output_cropped", ".txt", True),
     ],
 )
-def test_search_files(tmp_path, fname, extension):
+def test_search_files(tmp_path, fname, extension, search_crop):
     tmp_fname = fname + extension
     test_file = tmp_path / tmp_fname
     test_file.touch()
-    actual_files_list = dcm2nii.search_files(str(tmp_path / fname), [extension])
+    if search_crop:
+        tmp_cropped_fname = fname + "_Crop_1" + extension
+        test_cropped_file = tmp_path / tmp_cropped_fname
+        test_cropped_file.touch()
+
+    actual_files_list = dcm2nii.search_files(str(tmp_path / fname), [extension], search_crop)
     for f in actual_files_list:
-        assert str(test_file) == f
+        if search_crop:
+            assert (f in (str(test_cropped_file), str(test_file)))
+        else:
+            assert str(test_file) == f
