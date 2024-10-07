635ac029bd7c1abd9d7d5815e4a31db32e7d644c
Chris Markiewicz <effigies@gmail.com>
2024-10-07 14:04:58 -0400

    DOC: Patch doc/conf.py to work with latest ReadTheDocs

--- doc/conf.py
+++ doc/conf.py
@@ -19,6 +19,14 @@ from packaging.version import Version
 import nipype
 import subprocess as sp
 
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
+
 # Disable etelemetry during doc builds
 os.environ["NIPYPE_NO_ET"] = "1"
 
